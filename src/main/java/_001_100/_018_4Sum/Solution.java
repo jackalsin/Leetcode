14a825b931af566c3a545d7f8f24ec7056c8c0b9
jackalsin <jackal.sin@hotmail.com>
2017-01-23 22:22:02 +0800

    update 24

--- src/main/java/_001_100/_018_4Sum/Solution.java
+++ src/main/java/_001_100/_018_4Sum/Solution.java
@@ -18,7 +18,7 @@ public class Solution {
     for (int i = 0; i < nums.length - 3; i++) {
       if (i == 0 || nums[i] != nums[i - 1]) {
         for (int j = i + 1; j < nums.length - 2; j++) {
-          if (j == i + 1 || nums[j] != nums[j - 1]) {
+          if (j == 0 || nums[j] != nums[j - 1]) {
             int left = j + 1, right = nums.length - 1;
             while (left < right) {
               int sum = nums[i] + nums[j] + nums[left] + nums[right];
