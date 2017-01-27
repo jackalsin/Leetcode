67d4dca4e684a7802a13e5104326a47ff0a3459f
jackalsin <jackal.sin@hotmail.com>
2017-01-27 20:29:40 +0800

    update 29

---
+++ src/main/java/_001_100/_029_Divide_Two_Integers/Solution.java
@@ -0,0 +1,39 @@
+package _001_100._029_Divide_Two_Integers;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/24/2017.
+ */
+public class Solution {
+  /**
+   * Divide two integers without using multiplication, division and mod operator. If it is
+   * overflow, return MAX_INT.
+   * @param dividend
+   * @param divisor
+   * @return
+   */
+  public int divide(int dividend, int divisor) {
+    if (divisor == 0) {
+      throw new IllegalArgumentException("The divisor cannot be zero");
+    } else if (Integer.MIN_VALUE == dividend && Math.abs(divisor) == 1){
+      return divisor == 1 ? Integer.MIN_VALUE: Integer.MAX_VALUE;
+    } else if (divisor == Integer.MIN_VALUE) {
+      return 0;
+    }else {
+      int result = 0;
+      int sign = ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) ? 1: -1;
+      long dividendLong = Math.abs((long) dividend);
+      long divisorLong = Math.abs(divisor);
+      while (dividendLong >= divisorLong) {
+        long cur = divisorLong, multiple = 1;
+        while (dividendLong >= (cur << 1)) {
+          multiple <<= 1;
+          cur <<= 1;
+        }
+        dividendLong -= cur;
+        result += multiple;
+      }
+      return sign * result;
+    }
+  }
+}
---
+++ src/test/java/_001_100/_028_Implement_strStr/SolutionTest.java
@@ -0,0 +1,34 @@
+package _001_100._028_Implement_strStr;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/24/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testWithNotExist() throws Exception {
+    assertEquals(-1 , solution.strStr("abcde", "ee"));
+  }
+
+  @Test
+  public void testWithOccurOnce() throws Exception {
+    assertEquals(3, solution.strStr("abcdf", "df"));
+  }
+
+  @Test
+  public void testWithMultipleOccurrance() throws Exception {
+    assertEquals( 1,solution.strStr("abcbcbc", "bc"));
+  }
+
+}
