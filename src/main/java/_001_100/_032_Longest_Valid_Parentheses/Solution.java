abedf1ae8e5222369dd553b0695265169d0f325e
jackalsin <jackal.sin@hotmail.com>
2017-02-07 14:32:15 +0800

    update 33

---
+++ src/main/java/_001_100/_032_Longest_Valid_Parentheses/Solution.java
@@ -0,0 +1,11 @@
+package _001_100._032_Longest_Valid_Parentheses;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2017/2/7.
+ */
+public class Solution {
+  public int longestValidParentheses(String s) {
+    return Integer.MIN_VALUE;
+  }
+}
