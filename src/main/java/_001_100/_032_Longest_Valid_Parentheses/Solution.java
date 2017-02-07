5501cdea3d2c1bdd1c82ddc8e295222a2f6b9750
jackalsin <jackal.sin@hotmail.com>
2017-02-07 15:49:07 +0800

    update 32

--- src/main/java/_001_100/_032_Longest_Valid_Parentheses/Solution.java
+++ src/main/java/_001_100/_032_Longest_Valid_Parentheses/Solution.java
@@ -1,11 +1,30 @@
 package _001_100._032_Longest_Valid_Parentheses;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @author jacka
  * @version 1.0 on 2017/2/7.
  */
 public class Solution {
   public int longestValidParentheses(String s) {
-    return Integer.MIN_VALUE;
+    if (s == null || s.isEmpty()) return 0;
+    int maxLen = 0, curLen = 0;
+    Deque<Character> stack = new ArrayDeque<>();
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '(') {
+        stack.addLast('(');
+      } else {
+        if (!s.isEmpty() && stack.peekLast() == '(') {
+          curLen++;
+          stack.removeLast();
+        } else {
+          maxLen = Math.max(maxLen, curLen);
+          curLen = 0;
+        }
+      }
+    }
+    return maxLen;
   }
 }
