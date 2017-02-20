a0b209abcb444798f96b92b99fd5ab41232d5bc7
jackalsin <jackal.sin@hotmail.com>
2017-02-20 15:48:29 -0800

    update 42

---
+++ src/test/java/_001_100/_041_First_Missing_Positive/SolutionTest.java
@@ -0,0 +1,30 @@
+package _001_100._041_First_Missing_Positive;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/20/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testMissing1() throws Exception {
+    final int[] input = new int[] { 0, -1, 3, 5, 2};
+    assertEquals(1, solution.firstMissingPositive(input));
+  }
+
+  @Test
+  public void testWith1ToN() throws Exception {
+    final int[] input = new int[]{1,2,3,4};
+    assertEquals(5, solution.firstMissingPositive(input));
+  }
+}
