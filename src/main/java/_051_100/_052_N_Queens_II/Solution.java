774fb9bed9b490532cd22c337ad82233bdd51e76
jackalsin <jackal.sin@hotmail.com>
2017-03-18 22:42:08 -0700

    update 52 uncompleted

---
+++ src/main/java/_051_100/_052_N_Queens_II/Solution.java
@@ -0,0 +1,15 @@
+package _051_100._052_N_Queens_II;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/18/2017.
+ */
+public class Solution {
+  public int totalNQueens(int n) {
+    if (n <= 0) {
+      throw new IllegalArgumentException("n must be positive, which now is " + n);
+    }
+
+    return -1;
+  }
+}
