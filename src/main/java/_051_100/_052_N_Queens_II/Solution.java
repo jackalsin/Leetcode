877dd69a99da26d8ad088dd2a62f9cd2213f0468
jackalsin <jackal.sin@hotmail.com>
2017-03-26 21:19:39 -0700

    update 52 completed

--- src/main/java/_051_100/_052_N_Queens_II/Solution.java
+++ src/main/java/_051_100/_052_N_Queens_II/Solution.java
@@ -1,5 +1,10 @@
 package _051_100._052_N_Queens_II;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author jacka
  * @version 1.0 on 3/18/2017.
@@ -9,7 +14,43 @@ public class Solution {
     if (n <= 0) {
       throw new IllegalArgumentException("n must be positive, which now is " + n);
     }
+    Set<Integer> candidates = new HashSet<>();
+    for (int i = 0; i < n; i++) {
+      candidates.add(i);
+    }
+    List<List<Integer>> results = new ArrayList<>();
+    solveRow(results, new ArrayList<>(), candidates, n);
+    return results.size();
+  }
+  public void solveRow(List<List<Integer>> results , List<Integer> curPath, Set<Integer>
+      candidates, int n) {
+    if (candidates.isEmpty()) {
+      results.add(new ArrayList<>(curPath));
+    } else {
+      for (int candidate: candidates) {
+        curPath.add(candidate);
+        if (isValid(curPath, n)) {
+          Set<Integer> candidatesCopy = new HashSet<>(candidates);
+          candidatesCopy.remove(candidate);
+          solveRow(results, curPath, candidatesCopy, n);
+        }
+        curPath.remove(curPath.size() - 1);
+      }
+    }
+  }
 
-    return -1;
+  private boolean isValid(List<Integer> curPath, int n) {
+    Set<Integer> curSet = new HashSet<>(curPath);
+    if (curPath.size() != curSet.size()) {
+      return false;
+    }
+    int curRow = curPath.size() - 1;
+    int curCol = curPath.get(curRow);
+    for (int row = 0; row < curPath.size() - 1; row++) {
+      if ((row - curPath.get(row) == curRow - curCol) || (row + curPath.get(row) == curRow + curCol)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
---
+++ src/test/java/_051_100/_052_N_Queens_II/SolutionTest.java
@@ -0,0 +1,41 @@
+package _051_100._052_N_Queens_II;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/26/2017.
+ */
+public class SolutionTest {
+
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testWith1() throws Exception {
+    assertEquals(1, solution.totalNQueens(1));
+  }
+
+  @Test
+  public void testWith2() throws Exception {
+    assertEquals(0, solution.totalNQueens(2));
+  }
+
+  @Test
+  public void testWith3() throws Exception {
+    assertEquals(0 , solution.totalNQueens(3));
+  }
+
+  @Test
+  public void testWith4() throws Exception {
+    assertEquals(2, solution.totalNQueens(4));
+  }
+
+}
