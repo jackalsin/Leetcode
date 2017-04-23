a2b0fbaf83e402e6b65d8a2d60860f94a1c76e04
jackalsin <jackal.sin@hotmail.com>
2017-04-22 17:42:08 -0700

    update 59

--- src/main/java/_051_100/_059_Spiral_Matrix_II/Solution.java
+++ src/main/java/_051_100/_059_Spiral_Matrix_II/Solution.java
@@ -11,12 +11,28 @@ public class Solution {
       return new int[][]{};
     }
     int[][] result = new int[n][n];
-    int count = 1, dirIndex = 0, row = 0, col = 0;
-    while (count != n *n + 1) {
-      result[row][col] = count++;
-      if (result[row + DIR[dirIndex%4][0]][col + DIR[dirIndex%4][1]] != 0) {
-        dirIndex++;
+    int count = 1, startRow = 0, endRow = n - 1, startCol = 0, endCol = n - 1;
+    while (startCol <= endCol && startRow <= endRow) {
+      for (int col = startCol; col <= endCol; col++) {
+        result[startRow][col] = count++;
       }
+      startRow++;
+      for (int row = startRow; row <= endRow; row++) {
+        result[row][endCol] = count++;
+      }
+      endCol--;
+      if (startRow <= endRow) {
+        for (int col = endCol; col >= startCol; col--) {
+          result[endRow][col] = count++;
+        }
+      }
+      endRow--;
+      if (startCol <= endCol) {
+        for (int row = endRow; row >= startRow; row--) {
+          result[row][startCol] = count++;
+        }
+      }
+      startCol++;
     }
     return result;
   }
