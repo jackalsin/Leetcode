f70f457893cddcd8297cffb37f8a6f5133fc6033
jackalsin <jackal.sin@hotmail.com>
2017-04-27 08:52:18 -0700

    update 68

---
+++ src/main/java/_051_100/_068_Text_Justification/Solution.java
@@ -0,0 +1,13 @@
+package _051_100._068_Text_Justification;
+
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/27/2017.
+ */
+public class Solution {
+  public List<String> fullJustify(String[] words, int maxWidth) {
+    return null;
+  }
+}
