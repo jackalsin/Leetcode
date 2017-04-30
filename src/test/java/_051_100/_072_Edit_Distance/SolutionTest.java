75d635d2a7dd53609dfcd5bda674e06d8e9296fe
jackalsin <jackal.sin@hotmail.com>
2017-04-29 22:55:58 -0700

    update 72

---
+++ src/test/java/_051_100/_072_Edit_Distance/SolutionTest.java
@@ -0,0 +1,33 @@
+package _051_100._072_Edit_Distance;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/29/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testWithEmptyCouples() throws Exception {
+    assertEquals(0, solution.minDistance("", ""));
+  }
+
+  @Test
+  public void testWithEmpty() throws Exception {
+    assertEquals(5, solution.minDistance("","word1"));
+  }
+
+  @Test
+  public void testWithCommonCase1() throws Exception {
+    assertEquals(2, solution.minDistance("word", "wcd"));
+  }
+}
