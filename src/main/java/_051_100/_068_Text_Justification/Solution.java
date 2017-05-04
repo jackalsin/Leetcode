cbdd009f97d0ffcb3d6ef22b0194162a07a5b8b8
jackalsin <jackal.sin@hotmail.com>
2017-05-03 23:45:13 -0700

    update 68

--- src/main/java/_051_100/_068_Text_Justification/Solution.java
+++ src/main/java/_051_100/_068_Text_Justification/Solution.java
@@ -8,38 +8,72 @@ import java.util.List;
  * @version 1.0 on 4/27/2017.
  */
 public class Solution {
+
   public List<String> fullJustify(String[] words, int maxWidth) {
-    if (words == null || words.length == 0 || maxWidth <= 0) {
+    if (words == null || words.length == 0 ) {
       return new ArrayList<>();
     }
     int curLen = 0;
     List<String> result = new ArrayList<>();
     List<String> curLine = new ArrayList<>();
-    for (String word : words) {
+    for (int wordIndex = 0; wordIndex < words.length; wordIndex++) {
+      String word = words[wordIndex];
       curLen += word.length();
       if (curLen >= maxWidth) {
-        int actualLen = curLen - word.length();
-        StringBuilder curSb = new StringBuilder();
-        int lessSpaces = (maxWidth - actualLen) / (curLine.size() - 1);
-        int moreSpacesIndex = maxWidth - actualLen - lessSpaces * (curLine.size() - 1);
-        for (int i = 0; i < curLen; i++) {
-          curSb.append(curLine.get(i));
-          if (i != curLen - 1) {
-            for(int j = 0; j < ((i < moreSpacesIndex) ? lessSpaces + 1: lessSpaces); j++) {
-              curSb.append(" ");
-            }
-          }
+        if (curLen == maxWidth) {
+          curLine.add(word);
+        } else {
+          curLen = curLen - word.length();
         }
-        result.add(curSb.toString());
+        result.add(paddingToWidth(maxWidth, curLine));
         //reset
-        curLen = word.length() + 1;
         curLine.clear();
-        curLine.add(word);
+        if (curLen != maxWidth) {
+          curLen = word.length();
+          curLine.add(word);
+        } else {
+          curLen = 0;
+        }
       } else {
-        curLen += 1;
+        if (!curLine.isEmpty()) {
+          curLen += 1;
+        }
         curLine.add(word);
+      }
+      if (wordIndex == words.length - 1 && !curLine.isEmpty()) {
+        result.add(paddingToWidth(maxWidth, curLine));
       }
     }
     return result;
+  }
+
+  String paddingToWidth(final int maxWidth, final List<String> curLine) {
+    StringBuilder curSb = new StringBuilder();
+    int actualLen = 0;
+    for (int i = 0; i < curLine.size() - 1; i++) {
+      actualLen += (curLine.get(i).length() + 1);
+    }
+    actualLen += curLine.get(curLine.size() - 1).length();
+    if (curLine.size() == 1) {
+      curSb.append(curLine.get(0));
+      addSpace(curSb, maxWidth - actualLen);
+    } else {
+      int lessSpaces = (maxWidth - actualLen) / (curLine.size() - 1);
+      int moreSpacesIndex = maxWidth - actualLen - lessSpaces * (curLine.size() - 1);
+      for (int i = 0; i < curLine.size(); i++) {
+        curSb.append(curLine.get(i));
+        if (i != curLine.size() - 1) {
+          curSb.append(" ");
+          addSpace(curSb, (i < moreSpacesIndex) ? lessSpaces + 1 : lessSpaces);
+        }
+      }
+    }
+    return  curSb.toString();
+  }
+
+  private void addSpace(StringBuilder sb, int spaceNum) {
+    for (int i = 0; i < spaceNum; i++) {
+      sb.append(" ");
+    }
   }
 }
--- src/test/java/_051_100/_068_Text_Justification/SolutionTest.java
+++ src/test/java/_051_100/_068_Text_Justification/SolutionTest.java
@@ -3,10 +3,11 @@ package _051_100._068_Text_Justification;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Zhiwei.Xin
@@ -20,11 +21,84 @@ public class SolutionTest {
   public void setUp() throws Exception {
     solution = new Solution();
   }
-
+//
 //  @Test
-//  public void testWithOnlineCase() throws Exception {
-//    String[] words = new String[]{"This", "is", "an", "example", "of", "text", "justification."};
-//    List<String> output = Arrays.asList("This    is    an", "example  of text", "justification.  ");
-//    assertEquals(output, solution.fullJustify(words, 16));
+//  public void testWithMissed() throws Exception {
+//    String[] words = new String[] {"a","b","c","d","e"};
+//    List<String> output = new ArrayList<>();
+//    output.add("a b");
+//    output.add("c d");
+//    output.add("e  ");
+//    assertEquals(output, solution.fullJustify(words, 3));
 //  }
+
+  @Test
+  public void testWithMissed2() throws Exception {
+    String[] words = new String[] {"a","b","c","d","e"};
+    List<String> output = new ArrayList<>();
+    output.add("a");
+    output.add("b");
+    output.add("c");
+    output.add("d");
+    output.add("e");
+    assertEquals(output, solution.fullJustify(words, 1));
+  }
+
+  @Test
+  public void testWithEmptyStringAndZeroLength() throws Exception {
+    String[] words = new String[] {""};
+    List<String> output = new ArrayList<>();
+    output.add("");
+    assertEquals(output, solution.fullJustify(words, 0));
+  }
+
+  @Test
+  public void testWithWordEqualMax() throws Exception {
+    String[] words = new String[] {"a"};
+    List<String> expected = new ArrayList<>();
+    expected.add("a");
+    assertEquals(expected, solution.fullJustify(words, 1));
+  }
+
+  @Test
+  public void testWithOnlineCase() throws Exception {
+    String[] words = new String[]{"This", "is", "an", "example", "of", "text", "justification."};
+    List<String> output = Arrays.asList("This    is    an", "example  of text", "justification.  ");
+    assertEquals(output, solution.fullJustify(words, 16));
+  }
+
+  @Test
+  public void testWithPaddingOneWord() throws Exception {
+    final String input = "justification.";
+    final List<String> curLine = new ArrayList<>();
+    curLine.add("justification.");
+    assertEquals("justification.  ",
+        solution.paddingToWidth(16, curLine));
+  }
+
+  @Test
+  public void testWithActualLenLine2() throws Exception {
+    final String expected = "example  of text";
+    final String input = "example of text";
+    final List<String> curLine = new ArrayList<>();
+    curLine.add("example");
+    curLine.add("of");
+    curLine.add("text");
+    assertEquals(expected,
+        solution.paddingToWidth(16, curLine));
+  }
+
+  @Test
+  public void testWithActualLenLine1() throws Exception {
+    final String expected = "This    is    an";
+    final String input = "This is an";
+    final List<String> curLine = new ArrayList<>();
+    curLine.add("This");
+    curLine.add("is");
+    curLine.add("an");
+    assertEquals(expected,
+        solution.paddingToWidth(16, curLine));
+  }
+
+
 }
