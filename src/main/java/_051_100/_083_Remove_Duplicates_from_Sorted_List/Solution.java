a08e034a23037ac2fea64b20fb029d81f453b7e6
Zhiwei.Xin <jackal.sin@hotmail.com>
2017-05-19 14:45:21 -0700

    update 84

--- src/main/java/_051_100/_083_Remove_Duplicates_from_Sorted_List/Solution.java
+++ src/main/java/_051_100/_083_Remove_Duplicates_from_Sorted_List/Solution.java
@@ -1,4 +1,4 @@
-package _051_100._082_Remove_Duplicates_from_Sorted_List_II;
+package _051_100._083_Remove_Duplicates_from_Sorted_List;
 
 import utils.ListNode;
 
@@ -8,7 +8,7 @@ import utils.ListNode;
  */
 public class Solution {
   public ListNode deleteDuplicates(ListNode head) {
-    ListNode dummyHead = new ListNode(Integer.MIN_VALUE);
+    final ListNode dummyHead = new ListNode(Integer.MIN_VALUE);
     dummyHead.next = head;
     ListNode prev = dummyHead;
     ListNode cur = prev.next;
@@ -16,11 +16,8 @@ public class Solution {
       while (cur.next != null && cur.next.val == cur.val) {
         cur = cur.next;
       }
-      if (prev.next == cur) { // no dup, prev move
-        prev = prev.next;
-      } else { //has dup
-        prev.next = cur.next;
-      }
+      prev.next = cur;
+      prev = prev.next;
       cur = cur.next;
     }
     return dummyHead.next;
