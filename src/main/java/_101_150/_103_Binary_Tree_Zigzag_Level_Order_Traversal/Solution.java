131123dcce2bbf9d37efc4de855061e58e9f9c7f
zhiwei.xin <zhiwei.xin@veritas.com>
2017-06-16 16:12:11 -0700

    103

---
+++ src/main/java/_101_150/_103_Binary_Tree_Zigzag_Level_Order_Traversal/Solution.java
@@ -0,0 +1,36 @@
+package _101_150._103_Binary_Tree_Zigzag_Level_Order_Traversal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import utils.TreeNode;
+
+/**
+ * @author Zhiwei.Xin
+ * @version 1.0 on 6/16/2017.
+ */
+public class Solution {
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<>();
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    List<Integer> curLevelNodes = new ArrayList<>();
+    boolean isInorder = false;
+    while (!queue.isEmpty()) {
+      isInorder = !isInorder;
+      int curLevelSize = queue.size();
+      for (int i = 0; i < curLevelSize; i++) {
+        curLevelNodes.add(queue.poll().val);
+      }
+      if (isInorder) {
+        Collections.reverse(curLevelNodes);
+      }
+      result.add(new ArrayList<>(curLevelNodes));
+      curLevelNodes.clear();
+    }
+    return result;
+  }
+}
---
+++ src/main/java/_101_150/_104_Maximum_Depth_of_Binary_Tree/Solution.java
@@ -0,0 +1,21 @@
+package _101_150._104_Maximum_Depth_of_Binary_Tree;
+
+import utils.TreeNode;
+
+/**
+ * @author Zhiwei.Xin
+ * @version 1.0 on 6/16/2017.
+ */
+public class Solution {
+  public int maxDepth(TreeNode root) {
+    return maxTreeHight(root);
+  }
+
+  private static int maxTreeHight(TreeNode root) {
+    if (root == null) {
+      return 0;
+    } else {
+      return Math.max(maxTreeHight(root.left), maxTreeHight(root.right)) + 1;
+    }
+  }
+}
