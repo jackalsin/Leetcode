9d8cc12bc867579dc1a9aac2ad8182139bcace6f
zhiwei.xin <zhiwei.xin@veritas.com>
2017-06-19 15:26:19 -0700

    add 108

--- src/main/java/_101_150/_108_Convert_Sorted_Array_to_Binary_Search_Tree/Solution.java
+++ src/main/java/_101_150/_108_Convert_Sorted_Array_to_Binary_Search_Tree/Solution.java
@@ -8,6 +8,15 @@ import utils.TreeNode;
  */
 public class Solution {
   public TreeNode sortedArrayToBST(int[] nums) {
-    return null;
+    return sortedArrayToBST(nums, 0, nums.length - 1);
+  }
+
+  private TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+    if (start > end) return null;
+    int mid = (end - start + 1) / 2 + start;
+    TreeNode root = new TreeNode(nums[mid]);
+    root.left = sortedArrayToBST(nums, start, mid - 1);
+    root.right = sortedArrayToBST(nums, mid + 1, end);
+    return root;
   }
 }
