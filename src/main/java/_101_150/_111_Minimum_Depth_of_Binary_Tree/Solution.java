b5b5ccbd4c4c6e54e240dae1dfdddb0b6116f5c7
jackalsin <jackal.sin@hotmail.com>
2017-06-21 08:33:48 -0700

    update 111

---
+++ src/main/java/_101_150/_111_Minimum_Depth_of_Binary_Tree/Solution.java
@@ -0,0 +1,41 @@
+package _101_150._111_Minimum_Depth_of_Binary_Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 6/21/2017.
+ */
+public class Solution {
+
+  /**
+   * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+   * @param root
+   * @return
+   */
+  public int minDepth(TreeNode root) {
+    Queue<TreeNode> queue = new LinkedList<>();
+    if (root == null) return 0;
+    queue.offer(root);
+    int level = 1;
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode curNode = queue.poll();
+        if (curNode.left == null && curNode.right == null) {
+          return level;
+        } else {
+          if (curNode.left != null)
+            queue.offer(curNode.left);
+          if (curNode.right != null)
+            queue.offer(curNode.right);
+        }
+      }
+      level++;
+    }
+    throw new IllegalStateException("No leaf node found in " + root);
+  }
+}
---
+++ src/test/java/_101_150/_111_Minimum_Depth_of_Binary_Tree/SolutionTest.java
@@ -0,0 +1,43 @@
+package _101_150._111_Minimum_Depth_of_Binary_Tree;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import utils.TreeNode;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 6/21/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testMinLevel2() throws Exception {
+    TreeNode root = new TreeNode(3);
+      root.left = new TreeNode(4);
+    assertEquals(2, solution.minDepth(root));
+  }
+  @Test
+  public void testMinLevel2Balanced() throws Exception {
+    TreeNode root = new TreeNode(3);
+      root.left = new TreeNode(4);
+      root.right= new TreeNode(4);
+    assertEquals(2, solution.minDepth(root));
+  }
+
+  @Test
+  public void testMinLevel2Inbalanced() throws Exception {
+    TreeNode root = new TreeNode(3);
+    root.left = new TreeNode(4);
+    root.right = new TreeNode(4);
+    root.left.left = new TreeNode(4);
+    assertEquals(2, solution.minDepth(root));
+  }
+}
