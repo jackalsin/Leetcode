3f0278c8302614018daa9d2ef71ecdee20267de5
jackalsin <jackal.sin@hotmail.com>
2017-06-23 07:47:51 -0700

    update 113

--- src/main/java/_101_150/_112_Path_Sum/Solution.java
+++ src/main/java/_101_150/_112_Path_Sum/Solution.java
@@ -9,17 +9,8 @@ import utils.TreeNode;
 public class Solution {
   public boolean hasPathSum(TreeNode root, int sum) {
     if (root == null) return false;
-    return hasPathSum(root, sum, 0);
+    if (root.left == null && root.right == null && root.val == sum) return true;
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
   }
 
-  private boolean hasPathSum(TreeNode root, final int targetSum, int preSum) {
-    if (root.left == null && root.right == null) {
-      return targetSum == preSum + root.val;
-    } else{
-      boolean isPathSumLeft = root.left != null && hasPathSum(root.left, targetSum, preSum + root.val);
-      boolean isPathSumRight = root.right != null && hasPathSum(root.right, targetSum, preSum
-                                                                                           + root.val);
-      return isPathSumLeft || isPathSumRight;
-    }
-  }
 }
