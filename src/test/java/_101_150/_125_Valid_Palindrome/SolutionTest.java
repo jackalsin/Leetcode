185644f7e91c57a9a805ff27b1c98e20e67a18e5
zhiwei.xin <zhiwei.xin@veritas.com>
2017-06-26 21:33:26 -0700

    update 125

---
+++ src/test/java/_101_150/_125_Valid_Palindrome/SolutionTest.java
@@ -0,0 +1,56 @@
+package _101_150._125_Valid_Palindrome;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Zhiwei.Xin
+ * @version 1.0 on 6/26/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testEmpty() throws Exception {
+    assertTrue(solution.isPalindrome(""));
+  }
+
+  @Test
+  public void test1() throws Exception {
+    assertTrue(solution.isPalindrome("1"));
+  }
+
+  @Test
+  public void test2True() throws Exception {
+    assertTrue(solution.isPalindrome("11"));
+  }
+
+  @Test
+  public void test2False() throws Exception {
+    assertFalse(solution.isPalindrome("12"));
+  }
+
+  @Test
+  public void test3True() throws Exception {
+    assertTrue(solution.isPalindrome("121"));
+  }
+
+  @Test
+  public void test3False() throws Exception {
+    assertFalse(solution.isPalindrome("122"));
+  }
+
+  @Test
+  public void testADot() throws Exception {
+    assertTrue(solution.isPalindrome("a."));
+  }
+
+}
