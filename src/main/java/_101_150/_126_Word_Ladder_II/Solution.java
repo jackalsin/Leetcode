1bb07478d08f4f4ee0e7d2fb73fd59781d2119b2
jackalsin <jackal.sin@hotmail.com>
2017-06-27 23:27:01 -0700

    126 Pass TLE1

--- src/main/java/_101_150/_126_Word_Ladder_II/Solution.java
+++ src/main/java/_101_150/_126_Word_Ladder_II/Solution.java
@@ -1,9 +1,12 @@
 package _101_150._126_Word_Ladder_II;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -11,6 +14,8 @@ import java.util.Set;
  * @version 1.0 on 6/27/2017.
  */
 public class Solution {
+
+  private Map<String, Set<String>> validPairs;
   /**
    *
    * @param beginWord
@@ -19,6 +24,7 @@ public class Solution {
    * @return
    */
   public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+    generateValidPairs(beginWord, wordList);
     List<List<String>> result = new ArrayList<>();
     List<String> curPath = new ArrayList<>();
     curPath.add(beginWord);
@@ -26,8 +32,32 @@ public class Solution {
     return result;
   }
 
+  private void generateValidPairs(String beginWord, List<String> wordList) {
+    validPairs = new HashMap<>();
+    List<String> words = new ArrayList<>(wordList);
+    words.add(beginWord);
+    for (int i = 0; i < words.size(); i++) {
+      String word = words.get(i);
+      Set<String> curWordSet = validPairs.getOrDefault(word, new HashSet<>());
+      for (int j = i + 1; j < words.size(); j++) {
+        String tgtWord = words.get(j);
+        Set<String> tgtWordSet = validPairs.getOrDefault(tgtWord, new HashSet<>());
+        if (isValid(word, tgtWord)) {
+          tgtWordSet.add(word);
+          curWordSet.add(tgtWord);
+        }
+        validPairs.put(tgtWord, tgtWordSet);
+      }
+      validPairs.put(word, curWordSet);
+    }
+  }
+
   private void findLadders(List<List<String>> result, List<String> curPath, Collection<String> candidates,
                            String beginWord, String endWord) {
+//    System.out.println("Result = " + result);
+//    System.out.println("curPath = " + curPath);
+//    System.out.println("candidates = " + candidates);
+//    System.out.println("beginWord = " + beginWord);
     if (!curPath.isEmpty() && curPath.get(curPath.size() - 1).equals(endWord)) {
       if (!result.isEmpty()) {
         int minSizeForCurPath = result.get(0).size();
@@ -42,13 +72,14 @@ public class Solution {
       }
     } else {
       if (!result.isEmpty() && result.get(0).size() <= curPath.size()) return;
+      Set<String> candidatesForChild = new HashSet<>(candidates);
       for (String child : candidates) {
-        Set<String> candidatesForChild = new HashSet<>(candidates);
-        if (isValid(beginWord, child)) {
+        if (isValidCached(beginWord, child)) {
           candidatesForChild.remove(child);
           curPath.add(child);
           findLadders(result, curPath, candidatesForChild, child, endWord);
           curPath.remove(curPath.size() - 1);
+          candidatesForChild.add(child);
         }
       }
     }
@@ -56,6 +87,10 @@ public class Solution {
 
   private void updateResult(List<List<String>> result, List<String> curPath) {
     result.add(new ArrayList<>(curPath));
+  }
+
+  private boolean isValidCached(String beginWord, String child) {
+    return validPairs.get(beginWord).contains(child);
   }
 
   private boolean isValid(String beginWord, String child) {
@@ -70,5 +105,18 @@ public class Solution {
     }
     return true;
   }
+
+
+  public static void main(String[] args) {
+    Map<String, Set<Integer>> map = new HashMap<>();
+    Set<Integer> aSet = new HashSet<>();
+      aSet.addAll(Arrays.asList(1, 2, 3, 4));
+    final String key = "as";
+    map.put(key, aSet);
+    aSet.remove(2);
+    System.out.println("Return a dup copy of result " + map.get(key).contains(2));
+    System.out.println("Result = " + map.get(key));
+  }
+
 
 }
