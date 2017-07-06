9594ba4217622c4d555abf35dfc12a7f5f40c551
zhiwei.xin <zhiwei.xin@veritas.com>
2017-07-05 19:19:58 -0700

    update 136

--- src/main/java/_101_150/_136_Single_Number/Solution.java
+++ src/main/java/_101_150/_136_Single_Number/Solution.java
@@ -1,8 +1,5 @@
 package _101_150._136_Single_Number;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @author jacka
  * @version 1.0 on 7/1/2017.
@@ -12,18 +9,11 @@ public class Solution {
     if (nums == null || nums.length == 0) {
       return 0;
     } else {
-      Set<Integer> visited = new HashSet<>();
-      for (int child: nums) {
-        if (visited.contains(child)) {
-          visited.remove(child);
-        } else {
-          visited.add(child);
-        }
+      int result = 0;
+      for (int i : nums) {
+        result ^= i;
       }
-      for (int i : visited) {
-        return i;
-      }
-      throw new IllegalStateException();
+      return result;
     }
   }
 }
