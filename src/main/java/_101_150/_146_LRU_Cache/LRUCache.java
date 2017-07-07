77ab4e0e528b2495b77b0ab744475e3fd725f751
jackalsin <jackal.sin@hotmail.com>
2017-07-07 09:16:48 -0700

    Flash LinkedHashMap

--- src/main/java/_101_150/_146_LRU_Cache/LRUCache.java
+++ src/main/java/_101_150/_146_LRU_Cache/LRUCache.java
@@ -1,7 +1,10 @@
 package _101_150._146_LRU_Cache;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
@@ -13,30 +16,76 @@ public final class LRUCache {
 
   private final int capacity;
 
-  private final int[] keyToValue;
+  private final Map<Integer, Integer> keyToValue;
 
-  private final Queue<Integer> queue;
+  private final Queue<KeyCount> pq;
+  private static int count;
 
   public LRUCache(int capacity) {
     if (capacity <= 0) {
       throw new IllegalArgumentException("Capacity must be greater than 0, but now is " + capacity);
     }
+    count = 0;
     this.capacity = capacity;
-    keyToValue = new int[capacity + 1];
-    Arrays.fill(keyToValue, INVALID);
-    queue = new ArrayDeque<>();
+    keyToValue = new HashMap<>();
+    pq = new PriorityQueue<>(new Comparator<KeyCount>() {
+      @Override
+      public int compare(KeyCount o1, KeyCount o2) {
+        return Integer.compare(o1.count, o2.count);
+      }
+    });
   }
 
   public int get(int key) {
-    return keyToValue[key];
+    if (keyToValue.containsKey(key)) {
+      int result = keyToValue.get(key);
+
+
+
+
+      return result;
+    } else {
+      return INVALID;
+    }
   }
 
   public void put(int key, int value) {
-    if (queue.size() == capacity) {
-      Integer evictKey = queue.poll();
-      keyToValue[evictKey] = INVALID;
-      queue.offer(key);
+//    if (keyUsed.size() == capacity) {
+//      Integer evictKey = keyUsed.;
+//      keyToValue.remove(evictKey);
+//      queue.offer(key);
+//    }
+    keyToValue.put(key, value);
+  }
+
+  private static final class KeyCount {
+    final int key;
+    int count;
+
+    KeyCount(int k, int count) {
+      key = k;
+      this.count = count;
     }
-    keyToValue[key] = value;
+
+    @Override
+    public String toString() {
+      return "(" + key + ", " + count + ")";
+    }
+  }
+
+
+  public static void main(String[] args) {
+    Map<Integer, Integer> map = new LinkedHashMap<>(5, 0.75f, true);
+    for (int i = 0; i < 5; i++) {
+      map.put(i, i);
+    }
+
+    map.get(3);
+    map.put(2, 2);
+    map.getOrDefault(1, 3);
+    map.containsKey(0);
+    for (Map.Entry<Integer, Integer> integerIntegerEntry : map.entrySet()) {
+      System.out.println(integerIntegerEntry);
+    }
   }
 }
