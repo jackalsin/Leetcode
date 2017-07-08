98f40f2ed6f5f592077868e90912ca476dd3312d
jackalsin <jackal.sin@hotmail.com>
2017-07-07 22:49:43 -0700

    Finish 146

--- src/main/java/_101_150/_146_LRU_Cache/LRUCache.java
+++ src/main/java/_101_150/_146_LRU_Cache/LRUCache.java
@@ -1,11 +1,7 @@
 package _101_150._146_LRU_Cache;
 
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
 /**
  * @author jacka
@@ -16,76 +12,98 @@ public final class LRUCache {
 
   private final int capacity;
 
-  private final Map<Integer, Integer> keyToValue;
+  private final Map<Integer, Node> keyToValue;
 
-  private final Queue<KeyCount> pq;
-  private static int count;
+  private static int size;
+
+  private Node head, tail;
 
   public LRUCache(int capacity) {
     if (capacity <= 0) {
       throw new IllegalArgumentException("Capacity must be greater than 0, but now is " + capacity);
     }
-    count = 0;
+    size = 0;
     this.capacity = capacity;
     keyToValue = new HashMap<>();
-    pq = new PriorityQueue<>(new Comparator<KeyCount>() {
-      @Override
-      public int compare(KeyCount o1, KeyCount o2) {
-        return Integer.compare(o1.count, o2.count);
-      }
-    });
+    head = new Node();
+    tail = new Node();
+    head.next = tail;
+    tail.prev = head;
   }
 
   public int get(int key) {
-    if (keyToValue.containsKey(key)) {
-      int result = keyToValue.get(key);
-
-
-
-
-      return result;
-    } else {
+    Node res = keyToValue.get(key);
+    if (res == null) {
       return INVALID;
+    } else {
+      update(res);
+      return res.value;
     }
   }
+
 
   public void put(int key, int value) {
-//    if (keyUsed.size() == capacity) {
-//      Integer evictKey = keyUsed.;
-//      keyToValue.remove(evictKey);
-//      queue.offer(key);
-//    }
-    keyToValue.put(key, value);
-  }
-
-  private static final class KeyCount {
-    final int key;
-    int count;
-
-    KeyCount(int k, int count) {
-      key = k;
-      this.count = count;
+    Node node = keyToValue.get(key);
+    if (node == null) {
+      node = new Node(key, value);
+      keyToValue.put(key, node);
+      add(node);
+      size++;
+    } else {
+      node.value = value;
+      update(node);
     }
 
+    if (size > capacity) {
+      Node toRemove = tail.prev;
+      remove(toRemove);
+      keyToValue.remove(toRemove.key);
+      size--;
+    }
+  }
+
+  private void update(Node res) {
+    remove(res);
+    add(res);
+  }
+
+  /**
+   * Add the a node right after the head.
+   *
+   * @param toAdd
+   */
+  private void add(Node toAdd) {
+    Node sec = head.next;
+    head.next = toAdd;
+    toAdd.prev = head;
+    toAdd.next = sec;
+    sec.prev = toAdd;
+  }
+
+  private void remove(Node toRemove) {
+    Node prev = toRemove.prev, next = toRemove.next;
+    prev.next = next;
+    next.prev = prev;
+    toRemove.prev = null;
+    toRemove.next = null;
+  }
+
+  private static final class Node {
+    int key, value;
+    Node prev, next;
+
+    Node(int key, int value) {
+      this.key = key;
+      this.value= value;
+    }
+
+    Node() {
+      this(0, 0);
+    }
     @Override
     public String toString() {
-      return "(" + key + ", " + count + ")";
+      return "(" + key + ", " + size + ")";
     }
   }
 
-
-  public static void main(String[] args) {
-    Map<Integer, Integer> map = new LinkedHashMap<>(5, 0.75f, true);
-    for (int i = 0; i < 5; i++) {
-      map.put(i, i);
-    }
-
-    map.get(3);
-    map.put(2, 2);
-    map.getOrDefault(1, 3);
-    map.containsKey(0);
-    for (Map.Entry<Integer, Integer> integerIntegerEntry : map.entrySet()) {
-      System.out.println(integerIntegerEntry);
-    }
-  }
 }
