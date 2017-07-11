2005a774f309b930843f240d745067323acf1cf4
zhiwei.xin <zhiwei.xin@veritas.com>
2017-07-10 20:09:58 -0700

    finish 150

---
+++ src/test/java/_101_150/_150_Evaluate_Reverse_Polish_Notation/SolutionTest.java
@@ -0,0 +1,36 @@
+package _101_150._150_Evaluate_Reverse_Polish_Notation;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Zhiwei.Xin
+ * @version 1.0 on 7/10/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testEmpty() throws Exception {
+    final String[] input = new String[]{};
+    assertEquals(0, solution.evalRPN(input));
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+    final String[] input = new String[]{"2", "1", "+", "3", "*"};
+    assertEquals(9, solution.evalRPN(input));
+  }
+
+  @Test
+  public void testOnlineCase2() throws Exception {
+    final String[] input = new String[]{"4", "13", "5", "/", "+"};
+    assertEquals(6, solution.evalRPN(input));
+  }
+}
