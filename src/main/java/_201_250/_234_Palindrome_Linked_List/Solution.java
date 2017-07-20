2487f4ef7ebd1131edbe6b7cbb98557ee26b9326
jackalsin <jackal.sin@hotmail.com>
2017-07-19 22:04:45 -0700

    finish 234

--- src/main/java/_201_250/_234_Palindrome_Linked_List/Solution.java
+++ src/main/java/_201_250/_234_Palindrome_Linked_List/Solution.java
@@ -4,6 +4,38 @@ import utils.ListNode;
 
 public class Solution {
     public boolean isPalindrome(ListNode head) {
-        return false;
+        if (head == null) return true;
+        ListNode prevMid = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            prevMid = prevMid.next;
+            fast = fast.next.next;
+        }
+        ListNode another = reverse(prevMid.next);
+        ListNode anotherPointer = another;
+        ListNode pointer = head;
+        while (anotherPointer != null) {
+            if (anotherPointer.val == pointer.val) {
+                anotherPointer = anotherPointer.next;
+                pointer = pointer.next;
+            } else {
+                prevMid.next = reverse(another);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
     }
 }
