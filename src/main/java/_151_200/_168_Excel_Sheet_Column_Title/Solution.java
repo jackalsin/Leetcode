782cf9a187fb37b6255f37f89ec02e3aa1b9c6de
jackalsin <jackal.sin@hotmail.com>
2017-07-21 23:30:34 -0700

    finish 168

---
+++ src/main/java/_151_200/_168_Excel_Sheet_Column_Title/Solution.java
@@ -0,0 +1,14 @@
+package _151_200._168_Excel_Sheet_Column_Title;
+
+public class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n != 0) {
+            int order = n % 26;
+            int diff = order == 0 ? 25 : (order - 1);
+            sb.append((char) ('A' + diff));
+            n = (n - diff - 1) / 26;
+        }
+        return sb.reverse().toString();
+    }
+}
