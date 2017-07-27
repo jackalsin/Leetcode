8239219ec6244f45d0eaa8f5e6c428b2dd22e329
zhiwei.xin <zhiwei.xin@veritas.com>
2017-07-26 22:52:52 -0700

    Finish 191

---
+++ src/main/java/_151_200/_191_Number_of_1_Bits/Solution.java
@@ -0,0 +1,16 @@
+package _151_200._191_Number_of_1_Bits;
+
+/**
+ * @author Zhiwei.Xin
+ * @version 1.0 on 7/26/2017.
+ */
+public class Solution {
+  public int hammingWeight(int n) {
+    int count = 0;
+    for (int i = 0; i < 32; i++) {
+      count += (n & 1);
+      n >>= 1;
+    }
+    return count;
+  }
+}
