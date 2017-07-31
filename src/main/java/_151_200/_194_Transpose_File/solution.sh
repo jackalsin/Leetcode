971bc6a6fc8e129fe6f6ce01f5a3664de4c08719
jackalsin <jackal.sin@hotmail.com>
2017-07-31 08:51:30 -0700

    udpate 194

--- src/main/java/_151_200/_194_Transpose_File/solution.sh
+++ src/main/java/_151_200/_194_Transpose_File/solution.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-git
+awk '{
+    for (i = 1; i <= NF; ++i) {
+        if(s[i] == "") {
+            s[i] = $i;
+        } else {
+            s[i] = s[i] " " $i;
+        }
+    }
+} END {
+    for (i = 1; i<= NF;++i) {
+        print s[i];
+    }
+}' failedCase1.txt
