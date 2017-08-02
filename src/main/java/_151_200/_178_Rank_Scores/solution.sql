7d79d818bb4d01da5688d1dfaa8fc8e01da3860c
jackalsin <jackal.sin@hotmail.com>
2017-08-02 08:57:10 -0700

    finish 178

---
+++ src/main/java/_151_200/_178_Rank_Scores/solution.sql
@@ -0,0 +1,10 @@
+Create table If Not Exists Scores (Id int, Score DECIMAL(3,2));
+Truncate table Scores;
+insert into Scores (Id, Score) values ('1', '3.5');
+insert into Scores (Id, Score) values ('2', '3.65');
+insert into Scores (Id, Score) values ('3', '4.0');
+insert into Scores (Id, Score) values ('4', '3.85');
+insert into Scores (Id, Score) values ('5', '4.0');
+insert into Scores (Id, Score) values ('6', '3.65');
+-- RANK works as "AS"
+SELECT Score, (SELECT COUNT(DISTINCT Score) FROM Scores WHERE Score >= s.Score) RANK FROM Scores s ORDER BY Score DESC;
