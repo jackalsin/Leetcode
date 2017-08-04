54f4e0a1982b2fb0f010135432488ba2c11910b1
jackalsin <jackal.sin@hotmail.com>
2017-08-04 08:46:46 -0700

    finish 187

--- src/main/java/_151_200/_187_Repeated_DNA_Sequences/Solution.java
+++ src/main/java/_151_200/_187_Repeated_DNA_Sequences/Solution.java
@@ -1,8 +1,10 @@
 package _151_200._187_Repeated_DNA_Sequences;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -11,6 +13,34 @@ import java.util.Set;
  */
 public class Solution {
   public List<String> findRepeatedDnaSequences(String s) {
+//    return characterManipulation(s);
+    return bitManipulation(s);
+  }
+
+  private static List<String> bitManipulation(String s) {
+    final int[] map = new int['T' - 'A' + 1];
+    map['C' - 'A'] = 1;
+    map['G' - 'A'] = 2;
+    map['T' - 'A'] = 3;
+    List<String> result = new ArrayList<>();
+    Map<Integer, Integer> wordCounts = new HashMap<>();
+    for(int start = 0; start + 9 < s.length(); ++start) {
+      int curNum = 0;
+      for (int i = 0; i < 10; i++) {
+        curNum <<= 2;
+        curNum = curNum | map[s.charAt(start + i) - 'A'];
+      }
+      int counts = wordCounts.getOrDefault(curNum, 0);
+      if (counts == 1) {
+        result.add(s.substring(start, start + 10));
+      }
+      wordCounts.put(curNum, counts + 1);
+    }
+
+    return result;
+  }
+
+  private List<String> characterManipulation(String s) {
     Set<String> preVisited = new HashSet<>();
     Set<String> result = new HashSet<>();
     for (int i = 0; i <= s.length() - 10; ++i) {
@@ -23,5 +53,4 @@ public class Solution {
     }
     return new ArrayList<>(result);
   }
-
 }
