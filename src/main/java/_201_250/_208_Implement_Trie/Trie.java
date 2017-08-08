794114f20678b99829d0344c4e5b5a66b505cfec
jackalsin <jackal.sin@hotmail.com>
2017-08-07 21:56:43 -0700

    finish 208

--- src/main/java/_201_250/_208_Implement_Trie/Trie.java
+++ src/main/java/_201_250/_208_Implement_Trie/Trie.java
@@ -9,48 +9,94 @@ public final class Trie {
   private final Node[] vals = new Node[N];
   /** Initialize your data structure here. */
   public Trie() {
-    for (int i = 0; i < N; i++) {
-      vals[i] = new Node((char) ('a' + i));
-    }
   }
 
   /** Inserts a word into the trie. */
   public void insert(String word) {
-    Node root = vals[word.charAt(0)];
+    if (word == null || word.length() == 0) return;
+    Node root = vals[word.charAt(0) - 'a'];
+    if (root == null) {
+      vals[word.charAt(0) - 'a'] = new Node(word.charAt(0));
+      root = vals[word.charAt(0) - 'a'];
+    }
     insert(word, 0, root);
   }
 
   private void insert(String word, int start, Node root) {
-    if(start ==  word.length()) {
-      root.isEnd = true;
-    } else {
-      char curChar = word.charAt(start);
-      if (root.val == curChar) {
-        if (root.next == null) {
-          root.next = new Node(curChar);
-        }
-        insert(word, start + 1, root.next);
-      } else if (curChar < root.val) {
-        if (root.left == null) {
-          root.left = new Node(curChar);
-        }
-        insert(word, start, root.left);
-      } else {
-        if (root.right == null) {
-          root.right = new Node(curChar);
-        }
-        insert(word, start, root.right);
+    char curChar = word.charAt(start);
+    if (root.val == curChar) {
+      if (start == word.length() - 1) {
+        root.isEnd = true;
+        return;
       }
+      if (root.next == null) {
+        root.next = new Node(word.charAt(start + 1));
+      }
+      insert(word, start + 1, root.next);
+    } else if (curChar < root.val) {
+      if (root.left == null) {
+        root.left = new Node(curChar);
+      }
+      insert(word, start, root.left);
+    } else {
+      if (root.right == null) {
+        root.right = new Node(curChar);
+      }
+      insert(word, start, root.right);
     }
+
   }
 
   /** Returns if the word is in the trie. */
   public boolean search(String word) {
+    if (word == null || word.length() == 0) return false;
+    Node root = vals[word.charAt(0) - 'a'];
+    for (int i = 0; i < word.length(); ++i) {
+      char chr = word.charAt(i);
+      if (root == null) {
+        return false;
+      } else if (chr == root.val) {
+        if (i == word.length() - 1) {
+          return root.isEnd;
+        } else {
+          root = root.next;
+        }
+      } else if (chr < root.val) {
+        root = root.left;
+        i--;
+      } else if (chr > root.val) {
+        root = root.right;
+        i--;
+      } else {
+        throw new IllegalStateException("You should not come here");
+      }
+    }
     return false;
   }
 
   /** Returns if there is any word in the trie that starts with the given prefix. */
   public boolean startsWith(String prefix) {
+    Node root = vals[prefix.charAt(0) - 'a'];
+    for (int i = 0; i < prefix.length(); ++i) {
+      char chr = prefix.charAt(i);
+      if (root == null){
+        return false;
+      } else if (chr == root.val) {
+        if (i == prefix.length() - 1) {
+          return true;
+        } else {
+          root = root.next;
+        }
+      } else if (chr < root.val) {
+        root = root.left;
+        i--;
+      } else if (chr > root.val) {
+        root = root.right;
+        i--;
+      } else {
+        throw new IllegalStateException("You should not come here");
+      }
+    }
     return false;
   }
 
---
+++ src/test/java/_201_250/_208_Implement_Trie/TrieTest.java
@@ -0,0 +1,61 @@
+package _201_250._208_Implement_Trie;
+
+import com.sun.xml.internal.ws.encoding.soap.SOAP12Constants;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/7/2017.
+ */
+public class TrieTest {
+  private Trie trie;
+
+  @Before
+  public void setUp() throws Exception {
+    trie = new Trie();
+  }
+
+  @Test
+  public void testInsert() throws Exception {
+    trie.insert("a");
+    trie.insert("abc");
+    trie.insert("abcd");
+    trie.insert("abdd");
+    trie.insert("dbcd");
+
+  }
+
+  @Test
+  public void testSearch() throws Exception {
+    testInsert();
+    assertTrue(trie.search("a"));
+    assertTrue(trie.search("abc"));
+    assertTrue(trie.search("abcd"));
+    assertTrue(trie.search("dbcd"));
+    assertFalse(trie.search("aw"));
+    assertFalse(trie.search("abce"));
+    assertTrue(trie.search("abdd"));
+  }
+
+  @Test
+  public void testStartWith() throws Exception {
+    testInsert();
+    assertTrue(trie.startsWith("a"));
+    assertTrue(trie.startsWith("ab"));
+    assertTrue(trie.startsWith("abc"));
+    assertTrue(trie.startsWith("abcd"));
+    assertFalse(trie.startsWith("abcde"));
+    assertTrue(trie.startsWith("db"));
+    assertFalse(trie.startsWith("dc"));
+  }
+
+  @Test
+  public void testEmpty() throws Exception {
+    assertFalse(trie.startsWith("a"));
+    assertFalse(trie.search("a"));
+  }
+}
