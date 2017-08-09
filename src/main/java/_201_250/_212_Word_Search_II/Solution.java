bbbd5c66a3e6db0ed5fdfcaca7e3e184cad6d1ff
jackalsin <jackal.sin@hotmail.com>
2017-08-09 06:58:39 -0700

    finish 212

--- src/main/java/_201_250/_212_Word_Search_II/Solution.java
+++ src/main/java/_201_250/_212_Word_Search_II/Solution.java
@@ -5,91 +5,74 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import _201_250._208_Implement_Trie.Trie;
+
 /**
  * @author jacka
  * @version 1.0 on 8/8/2017.
  */
 public class Solution {
+  private static final int N = 26;
+  private static final char VISITED = '#';
+  private int rows;
+  private int cols;
 
   public List<String> findWords(char[][] board, String[] words) {
-    Set<String> result = new HashSet<>();
-    if (board != null && board.length != 0 && board[0].length != 0) {
-      for (String word : words) {
-        if (findWord(board, word)) {
-          result.add(word);
-        }
-      }
+    List<String> result = new ArrayList<>();
+    TrieNode root = buildTrie(words);
+    rows = board.length;
+    if (rows == 0) {
+      return result;
     }
-    return new ArrayList<>(result);
-  }
-
-  private boolean findWord(char[][] board, String word) {
-    int rows = board.length;
-    int cols = board[0].length;
-    Set<Coordinate> visited = new HashSet<>();
+    cols = board[0].length;
     for (int row = 0; row < rows; ++row) {
       for (int col = 0; col < cols; ++col) {
-        if (isValid(board, word, row, col, 0, visited)) {
-          return true;
+        search(result, board, row, col, root);
+      }
+    }
+    return result;
+  }
+
+  private void search(final List<String> result,
+                      final char[][] board, int startRow, int startCol, final TrieNode parentNode) {
+    if (startRow >= 0 && startCol >= 0 && startCol < cols && startRow < rows) {
+      char chr = board[startRow][startCol];
+      if (chr != VISITED && parentNode.next[chr - 'a'] != null) {
+        TrieNode curNode = parentNode.next[chr - 'a'];
+        if (curNode.word != null) {
+          result.add(curNode.word);
+          curNode.word = null;
         }
+        board[startRow][startCol] = VISITED;
+        search(result, board, startRow + 1, startCol, curNode);
+        search(result, board, startRow, startCol + 1, curNode);
+        search(result, board, startRow - 1, startCol, curNode);
+        search(result, board, startRow, startCol - 1, curNode);
+        board[startRow][startCol] = chr;
       }
-    }
-    return false;
-  }
-
-  private boolean isValid(char[][] board, String word, int startRow, int startCol, int index,
-                          Set<Coordinate> visited) {
-    int rows = board.length;
-    int cols = board[0].length;
-    if (index == word.length()) {
-      return true;
-    } else if (startCol < 0 || startRow < 0 || startCol >= cols || startRow >= rows
-        || visited.contains(new Coordinate(startRow, startCol))
-        || word.charAt(index) != board[startRow][startCol]) {
-      return false;
-    } else {
-      Coordinate curCoordinate = new Coordinate(startRow, startCol);
-      visited.add(curCoordinate);
-      boolean result = isValid(board, word, startRow + 1, startCol, index + 1, visited)
-          ||isValid(board, word, startRow - 1, startCol, index + 1, visited)
-          ||isValid(board, word, startRow, startCol + 1, index + 1, visited)
-          ||isValid(board, word, startRow, startCol - 1, index + 1, visited);
-      visited.remove(curCoordinate);
-      return result;
     }
   }
 
-  /**
-   * Class to represent coordinate
-   */
-  private static final class Coordinate {
-    private final int row;
-    private final int col;
-
-    Coordinate(int row, int col) {
-      this.row = row;
-      this.col = col;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
+  private TrieNode buildTrie(String[] words) {
+    TrieNode root = new TrieNode();
+    for (String word : words) {
+      TrieNode parent = root;
+      for (char chr : word.toCharArray()) {
+        int index = chr - 'a';
+        if (parent.next[index] == null) {
+          parent.next[index] = new TrieNode();
+        }
+        parent = parent.next[index];
       }
-      if (!(o instanceof Coordinate)) {
-        return false;
-      }
-
-      Coordinate coordinate = (Coordinate) o;
-
-      return row == coordinate.row && col == coordinate.col;
+      parent.word = word;
     }
 
-    @Override
-    public int hashCode() {
-      int result = row;
-      result = 31 * result + col;
-      return result;
-    }
+    return root;
+  }
+
+  private static final class TrieNode {
+    String word;
+    TrieNode[] next = new TrieNode[N];
+    TrieNode() {}
   }
 }
