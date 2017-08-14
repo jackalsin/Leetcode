9715ebd294b6fc94d8411eb24316b3df5f0492c1
jackalsin <jackal.sin@hotmail.com>
2017-08-13 23:59:12 -0700

    finish 219

---
+++ src/main/java/_201_250/_219_Contains_Duplicate_II/Solution.java
@@ -0,0 +1,45 @@
+package _201_250._219_Contains_Duplicate_II;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/13/2017.
+ */
+public class Solution {
+  public boolean containsNearbyDuplicate(int[] nums, int k) {
+//    return hashMapSolution(nums, k);
+    return hashSetSolution(nums, k);
+  }
+
+  private boolean hashSetSolution(int[] nums, int k) {
+    Set<Integer> elementsInK = new HashSet<>();
+    for (int i = 0; i < nums.length; ++i) {
+      if (i > k) {
+        elementsInK.remove(nums[i - k - 1]);
+      }
+      if (!elementsInK.add(nums[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean hashMapSolution(int[] nums, int k) {
+    Map<Integer, Integer> lastSeen = new HashMap<>();
+    for (int i = 0; i < nums.length; ++i) {
+      int curDigit = nums[i];
+      if (lastSeen.containsKey(curDigit)) {
+        int lastSeenPos = lastSeen.get(curDigit);
+        if (i - lastSeenPos <= k) {
+          return true;
+        }
+      }
+      lastSeen.put(curDigit, i);
+    }
+    return false;
+  }
+}
