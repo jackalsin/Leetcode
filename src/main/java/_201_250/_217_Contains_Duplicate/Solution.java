73655f378c5b6d459819a50537f5b2e700eb19bd
jackalsin <jackal.sin@hotmail.com>
2017-08-13 23:15:39 -0700

    finish 217

---
+++ src/main/java/_201_250/_217_Contains_Duplicate/Solution.java
@@ -0,0 +1,22 @@
+package _201_250._217_Contains_Duplicate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/13/2017.
+ */
+public class Solution {
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> visited = new HashSet<>();
+    for (int i : nums) {
+      if (visited.contains(i)) {
+        return true;
+      } else {
+        visited.add(i);
+      }
+    }
+    return false;
+  }
+}
