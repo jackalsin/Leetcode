75b815ccd5cefd59d23eba50ff850d44ee6b8a10
zhiwei.xin <zhiwei.xin@veritas.com>
2017-08-19 14:02:02 -0700

    finish 232

---
+++ src/test/java/_201_250/_232_Implement_Queue_using_Stacks/MyQueueTest.java
@@ -0,0 +1,21 @@
+package _201_250._232_Implement_Queue_using_Stacks;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import _101_150._140_Word_Break_II.Solution;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Zhiwei.Xin
+ * @version 1.0 on 8/19/2017.
+ */
+public class MyQueueTest {
+  private MyQueue queue;
+  @Before
+  public void setUp() throws Exception {
+    queue = new MyQueue();
+  }
+
+}
