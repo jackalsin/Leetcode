63d24e78eea44792e0deb40a2b65a3772eb3a426
jackalsin <jackal.sin@hotmail.com>
2017-08-21 00:28:35 -0700

    update 242

---
+++ src/main/java/_201_250/_242_Valid_Anagram/Solution.java
@@ -0,0 +1,29 @@
+package _201_250._242_Valid_Anagram;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/20/2017.
+ */
+public class Solution {
+  public boolean isAnagram(String s, String t) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (char child : s.toCharArray()) {
+      map.put(child, map.getOrDefault(child, 0) + 1);
+    }
+
+    for(char child: t.toCharArray()) {
+      int beforeRemove = map.getOrDefault(child, 0);
+      if(beforeRemove == 0) {
+        return false;
+      } else if (beforeRemove == 1) {
+        map.remove(child);
+      } else {
+        map.put(child, beforeRemove - 1);
+      }
+    }
+    return map.size() == 0;
+  }
+}
