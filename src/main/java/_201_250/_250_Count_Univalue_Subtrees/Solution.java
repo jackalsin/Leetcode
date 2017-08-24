2b17bb593bba4bbdedbd77018a4baac4468e3c72
jackalsin <jackal.sin@hotmail.com>
2017-08-23 23:31:32 -0700

    finish 250

---
+++ src/main/java/_201_250/_250_Count_Univalue_Subtrees/Solution.java
@@ -0,0 +1,32 @@
+package _201_250._250_Count_Univalue_Subtrees;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/23/2017.
+ */
+public class Solution {
+  private int count = 0;
+  public int countUnivalSubtrees(TreeNode root) {
+    isUnivalSubtreesAndCount(root);
+    return count;
+  }
+
+  private boolean isUnivalSubtreesAndCount(TreeNode root) {
+    if (root == null) {
+      return true;
+    } else {
+      boolean left = isUnivalSubtreesAndCount(root.left);
+      boolean right = isUnivalSubtreesAndCount(root.right);
+      if (left && right) {
+        if (root.left != null && root.val != root.left.val) return false;
+        if (root.right != null && root.val != root.right.val) return false;
+        count++;
+        return true;
+      }
+      return false;
+    }
+  }
+
+}
