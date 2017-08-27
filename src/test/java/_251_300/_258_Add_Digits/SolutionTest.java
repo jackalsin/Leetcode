b322b61d781a531227f5d73a15f3d110db0f8017
jackalsin <jackal.sin@hotmail.com>
2017-08-27 16:14:11 -0700

    finish 259

---
+++ src/test/java/_251_300/_258_Add_Digits/SolutionTest.java
@@ -0,0 +1,33 @@
+package _251_300._258_Add_Digits;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/27/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void test38() throws Exception {
+    assertEquals(2, solution.addDigits(38));
+  }
+
+  @Test
+  public void test1() throws Exception {
+    assertEquals(1, solution.addDigits(1));
+  }
+
+  @Test
+  public void test0() throws Exception {
+    assertEquals(0, solution.addDigits(0));
+  }
+}
