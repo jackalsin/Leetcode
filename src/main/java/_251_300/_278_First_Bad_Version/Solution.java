cf442416bba7299fc4c2f95d75b73007e867e83f
jackalsin <jackal.sin@hotmail.com>
2017-09-02 23:13:34 -0700

    finish 278

---
+++ src/main/java/_251_300/_278_First_Bad_Version/Solution.java
@@ -0,0 +1,17 @@
+package _251_300._278_First_Bad_Version;
+
+/**
+ * @author jacka
+ * @version 1.0 on 9/2/2017.
+ */
+public class Solution extends VersionControl {
+  public int firstBadVersion(int n) {
+    int left = 1, right = n;
+    while(left <= right) {
+      int mid = (right - left) / 2 + left;
+      if (isBadVersion(mid)) right = mid - 1;
+      else left = mid + 1;
+    }
+    return right + 1;
+  }
+}
