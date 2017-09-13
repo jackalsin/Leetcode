0b7698ca476210143b4c7d28567ad3a814535a0e
jackalsin <jackal.sin@hotmail.com>
2017-09-12 23:58:34 -0700

    add 303

---
+++ src/test/java/_301_350/_303_Range_Sum_Query_Immutable/NumArrayTest.java
@@ -0,0 +1,32 @@
+package _301_350._303_Range_Sum_Query_Immutable;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jacka
+ * @version 1.0 on 9/12/2017.
+ */
+public class NumArrayTest {
+  private NumArray numArray;
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @Test
+  public void testOnline() throws Exception {
+    final int[] input = {-2, 0, 3, -5, 2, -1};
+    numArray = new NumArray(input);
+    assertEquals(1, numArray.sumRange(0, 2));
+    assertEquals(-1, numArray.sumRange(2, 5));
+    assertEquals(-3, numArray.sumRange(0, 5));
+  }
+
+  @Test
+  public void testEmpty() throws Exception {
+    numArray = new NumArray(new int[]{});
+  }
+}
