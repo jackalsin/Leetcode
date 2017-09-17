05f3287636a46422ef19080b1ab9ca8923caa2d0
jackalsin <jackal.sin@hotmail.com>
2017-09-16 22:48:05 -0700

    finish 308

---
+++ src/test/java/_301_350/_308_Range_Sum_Query_2D_Mutable/NumMatrixTest.java
@@ -0,0 +1,39 @@
+package _301_350._308_Range_Sum_Query_2D_Mutable;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.jws.soap.SOAPBinding;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jacka
+ * @version 1.0 on 9/16/2017.
+ */
+public class NumMatrixTest {
+  private static final int[][] INPUT_1 = {
+      {3, 0, 1, 4, 2},
+      {5, 6, 3, 2, 1},
+      {1, 2, 0, 1, 5},
+      {4, 1, 0, 1, 7},
+      {1, 0, 3, 0, 5}
+  };
+  private NumMatrix numMatrix;
+  @Before
+  public void setUp() throws Exception {
+    numMatrix = new NumMatrix(INPUT_1);
+  }
+
+  @Test
+  public void testOnline() throws Exception {
+    assertEquals(8, numMatrix.sumRegion(2, 1, 4, 3));
+    numMatrix.update(3, 2, 2);
+    assertEquals(10, numMatrix.sumRegion(2, 1, 4, 3));
+  }
+
+  @Test
+  public void testEmpty() throws Exception {
+    numMatrix = new NumMatrix(new int[][] {});
+  }
+}
