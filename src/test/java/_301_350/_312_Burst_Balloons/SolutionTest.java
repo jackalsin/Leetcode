d19db54ca776a2cfd0a6cbcf09e0be48ac79e993
jackalsin <jackal.sin@hotmail.com>
2017-09-18 09:39:22 -0700

    finish 312

---
+++ src/test/java/_301_350/_312_Burst_Balloons/SolutionTest.java
@@ -0,0 +1,26 @@
+package _301_350._312_Burst_Balloons;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 9/18/2017.
+ */
+public class SolutionTest {
+
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void onlineCase() throws Exception {
+    final int[] input = {3, 1, 5, 8};
+    assertEquals(167, solution.maxCoins(input));
+  }
+}
