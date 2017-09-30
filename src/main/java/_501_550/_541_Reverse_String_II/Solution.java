8951fda2d6bf8e523da240d80fd14f2ee856c8f9
jackalsin <jackal.sin@hotmail.com>
2017-09-30 11:55:23 -0700

    Finish 344 and 541

---
+++ src/main/java/_501_550/_541_Reverse_String_II/Solution.java
@@ -0,0 +1,27 @@
+package _501_550._541_Reverse_String_II;
+
+/**
+ * @author jacka
+ * @version 1.0 on 9/30/2017.
+ */
+public class Solution {
+  public String reverseStr(String s, int k) {
+    if (s == null) return null;
+    else {
+      char[] chars = s.toCharArray();
+      for (int start = 0; start < s.length(); start += 2 * k) {
+        int left = start, right = Math.min(start + k - 1, s.length() - 1);
+        while (left < right) {
+          swap(chars, left++, right--);
+        }
+      }
+      return new String(chars);
+    }
+  }
+
+  private static void swap(char[] chars, int left, int right) {
+    char tmp = chars[left];
+    chars[left] = chars[right];
+    chars[right] = tmp;
+  }
+}
