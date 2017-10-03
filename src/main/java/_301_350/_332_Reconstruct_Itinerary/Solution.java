5340764eda3509c763256498435f4c4018300acf
jackalsin <jackal.sin@hotmail.com>
2017-10-02 23:56:43 -0700

    Finish 334

--- src/main/java/_301_350/_332_Reconstruct_Itinerary/Solution.java
+++ src/main/java/_301_350/_332_Reconstruct_Itinerary/Solution.java
@@ -1,6 +1,10 @@
 package _301_350._332_Reconstruct_Itinerary;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author jacka
@@ -8,6 +12,11 @@ import java.util.List;
  */
 public class Solution {
   public List<String> findItinerary(String[][] tickets) {
+    Map<String, Set<String>> fromToMapping = new HashMap<>();
+    for (String[] it : tickets) {
+      fromToMapping.putIfAbsent(it[0], new HashSet<>());
+      fromToMapping.get(it[0]).add(it[1]);
+    }
     return null;
   }
 }
