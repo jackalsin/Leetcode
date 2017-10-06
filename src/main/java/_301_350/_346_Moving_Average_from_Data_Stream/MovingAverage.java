3da8e55cb954048d26a475dc2a980db5b13e8378
jackalsin <jackal.sin@hotmail.com>
2017-10-05 23:31:34 -0700

    Finish 346

---
+++ src/main/java/_301_350/_346_Moving_Average_from_Data_Stream/MovingAverage.java
@@ -0,0 +1,31 @@
+package _301_350._346_Moving_Average_from_Data_Stream;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/5/2017.
+ */
+public class MovingAverage {
+  private final Queue<Integer> queue;
+  private double sum = 0;
+  private final int size;
+
+  public MovingAverage(int size) {
+    queue = new ArrayDeque<>();
+    this.size = size;
+  }
+
+  public double next(int val) {
+    if (size == 0) {
+      return 0d;
+    }
+    if (queue.size() == size) {
+      sum -= queue.poll();
+    }
+    sum += val;
+    queue.add(val);
+    return sum / queue.size();
+  }
+}
