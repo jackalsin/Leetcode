09219e84bb14141cc29da19f5bac01c1be24d655
jackalsin <jackal.sin@hotmail.com>
2017-10-05 19:53:27 -0700

    Finish 337

--- src/test/java/_301_350/_337_House_Robber_III/SolutionTest.java
+++ src/test/java/_301_350/_337_House_Robber_III/SolutionTest.java
@@ -1,4 +1,12 @@
-import static org.junit.Assert.*;
+package _301_350._337_House_Robber_III;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author jacka
@@ -6,4 +14,22 @@ import static org.junit.Assert.*;
  */
 public class SolutionTest {
 
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testOnlineCase() throws Exception {
+    TreeNode root = TreeNodes.getTreeLevelOrder(3, 2, 3, null, 3, null, 1);
+    assertEquals(7, solution.rob(root));
+  }
+
+  @Test
+  public void testOnlineCase2() throws Exception {
+    TreeNode root = TreeNodes.getTreeLevelOrder(3, 4, 5, 1, 3, null, 1);
+    assertEquals(9, solution.rob(root));
+  }
 }
