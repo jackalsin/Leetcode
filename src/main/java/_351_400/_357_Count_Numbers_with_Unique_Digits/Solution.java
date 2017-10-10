3c720c3b59d7274c53936a2126b70aa65048b56d
jackalsin <jackal.sin@hotmail.com>
2017-10-10 08:41:41 -0700

    Finish 357

---
+++ src/main/java/_351_400/_357_Count_Numbers_with_Unique_Digits/Solution.java
@@ -0,0 +1,31 @@
+package _351_400._357_Count_Numbers_with_Unique_Digits;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/10/2017.
+ */
+public class Solution {
+  /**
+   * Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+   */
+  public int countNumbersWithUniqueDigits(int n) {
+    if (n == 0) {
+      return 1;
+    }
+    n = Math.min(n, 10);
+    final int[] counts = new int[n + 1];
+    final int[] permutation = new int[n + 1];
+    permutation[1] = 9;
+    for (int i = 2; i < permutation.length; ++i) {
+      int k = 10 - i;
+      permutation[i] = permutation[i - 1] * k;
+    }
+
+    counts[1] = 10;
+    for (int i = 2; i < counts.length; ++i) {
+      counts[i] = counts[i - 1] + 9 * permutation[i - 1];
+    }
+    return counts[n];
+  }
+
+}
