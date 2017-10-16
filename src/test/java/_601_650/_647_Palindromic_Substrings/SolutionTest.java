ed13f3385c56656f0f0320b6aed66066f0a58e52
jackalsin <jackal.sin@hotmail.com>
2017-10-15 21:58:18 -0700

    Finish 647

---
+++ src/test/java/_601_650/_647_Palindromic_Substrings/SolutionTest.java
@@ -0,0 +1,36 @@
+package _601_650._647_Palindromic_Substrings;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/15/2017.
+ */
+public class SolutionTest {
+
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new ONSquareSolution();
+  }
+
+  @Test
+  public void testAaa() throws Exception {
+    assertEquals(6, solution.countSubstrings("aaa"));
+  }
+
+  @Test
+  public void testEmpty() throws Exception {
+    assertEquals(0, solution.countSubstrings(""));
+  }
+
+  @Test
+  public void testAbc() throws Exception {
+    assertEquals(3, solution.countSubstrings("abc"));
+  }
+
+}
