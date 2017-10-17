f081c3e72c9e14653b8b6ae84c5f9747743fddc0
jackalsin <jackal.sin@hotmail.com>
2017-10-16 23:29:00 -0700

    Finish 046

---
+++ src/main/java/interviews/linkedin/_046_Permutations/Solution.java
@@ -0,0 +1,11 @@
+package interviews.linkedin._046_Permutations;
+
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/16/2017.
+ */
+public interface Solution {
+  List<List<Integer>> permute(int[] nums);
+}
