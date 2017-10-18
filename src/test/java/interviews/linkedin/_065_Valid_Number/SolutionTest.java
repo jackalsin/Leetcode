7e7354a6f5fd6d17ba3726fc2a5c9d33d8afa5ba
jackalsin <jackal.sin@hotmail.com>
2017-10-18 09:06:16 -0700

    Finish 065

---
+++ src/test/java/interviews/linkedin/_065_Valid_Number/SolutionTest.java
@@ -0,0 +1,77 @@
+package interviews.linkedin._065_Valid_Number;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.plaf.PanelUI;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/17/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void test0() throws Exception {
+    assertTrue(solution.isNumber("0"));
+  }
+
+  @Test
+  public void test0_1() throws Exception {
+    assertTrue(solution.isNumber("0.1"));
+    assertTrue(solution.isNumber(" 0.1 "));
+  }
+
+  @Test
+  public void testAbc() throws Exception {
+    assertFalse(solution.isNumber("abc"));
+  }
+
+  @Test
+  public void test1a() throws Exception {
+    assertFalse(solution.isNumber("1a"));
+  }
+
+  @Test
+  public void test03() throws Exception {
+    assertTrue(solution.isNumber(".3"));
+  }
+
+  @Test
+  public void test3Point0() throws Exception {
+    assertTrue(solution.isNumber("3."));
+  }
+
+  @Test
+  public void test2e10() throws Exception {
+    assertTrue(solution.isNumber("2e10"));
+  }
+
+  @Test
+  public void test2E10() throws Exception {
+    assertTrue(solution.isNumber("2E10"));
+  }
+
+  @Test
+  public void test2E3Point5() throws Exception {
+    assertFalse(solution.isNumber("2E3.5"));
+  }
+
+  @Test
+  public void test2ENegative3Point5() throws Exception {
+    assertFalse(solution.isNumber("2E3.5"));
+  }
+
+  @Test
+  public void testEmpty() throws Exception {
+    assertFalse(solution.isNumber(""));
+  }
+}
