6afd510757d00d7d1c1a5f56e93c48cf04b309db
jackalsin <jackal.sin@hotmail.com>
2017-10-22 22:45:36 -0700

    Finsih 339

--- src/main/java/interviews/linkedin/_339_Nested_List_Weight_Sum/Solution.java
+++ src/main/java/interviews/linkedin/_339_Nested_List_Weight_Sum/Solution.java
@@ -1,8 +1,27 @@
 package interviews.linkedin._339_Nested_List_Weight_Sum;
 
+import java.util.List;
+
+import utils.nestedInteger.NestedInteger;
+
 /**
  * @author jacka
  * @version 1.0 on 10/22/2017.
  */
 public class Solution {
+  public int depthSum(List<NestedInteger> nestedList) {
+    return depthSum(nestedList, 1);
+  }
+
+  private int depthSum(List<NestedInteger> nestedList, int depth) {
+    int result = 0;
+    for (NestedInteger nestedInteger : nestedList) {
+      if (nestedInteger.isInteger()) {
+        result += nestedInteger.getInteger() * depth;
+      } else {
+        result += depthSum(nestedInteger.getList(), depth + 1);
+      }
+    }
+    return result;
+  }
 }
