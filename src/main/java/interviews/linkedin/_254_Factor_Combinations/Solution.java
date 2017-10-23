b2e5e524d330b445ca5e85c759048d62749a629a
jackalsin <jackal.sin@hotmail.com>
2017-10-22 20:15:07 -0700

    Finish 254

---
+++ src/main/java/interviews/linkedin/_254_Factor_Combinations/Solution.java
@@ -0,0 +1,34 @@
+package interviews.linkedin._254_Factor_Combinations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/22/2017.
+ */
+public class Solution {
+  public List<List<Integer>> getFactors(int n) {
+    final List<List<Integer>> result = new ArrayList<>();
+    getFactors(result, new ArrayList<>(), n, 2);
+    return result;
+  }
+
+  private void getFactors(List<List<Integer>> result, List<Integer> path, int n, int start) {
+    if (n != 1) {
+      path.add(n);
+      if (path.size() > 1) {
+        result.add(new ArrayList<>(path));
+      }
+      path.remove(path.size() - 1);
+    }
+    for (int i = start; i * i <= n; i++) {
+      if (n % i == 0) {
+        path.add(i);
+        getFactors(result, path, n / i, i);
+        path.remove(path.size() - 1);
+      }
+    }
+  }
+
+}
