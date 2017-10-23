cdb3003abb7c966934b0526655a5f78b537fb971
jackalsin <jackal.sin@hotmail.com>
2017-10-22 19:40:12 -0700

    Finish 244

---
+++ src/test/java/interviews/linkedin/_244_Shortest_Word_Distance_II/WordDistanceTest.java
@@ -0,0 +1,25 @@
+package interviews.linkedin._244_Shortest_Word_Distance_II;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/22/2017.
+ */
+public class WordDistanceTest {
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void testOnlineCase() throws Exception {
+    final String[] words = {"practice", "makes", "perfect", "coding", "makes"};
+    final WordDistance wordDistance = new WordDistance(words);
+    assertEquals(3, wordDistance.shortest("coding", "practice"));
+    assertEquals(1, wordDistance.shortest("coding", "makes"));
+  }
+
+}
