f8311847c78b2af0e7a383b54904d394f156a119
jackalsin <jackal.sin@hotmail.com>
2017-10-22 19:48:54 -0700

    Finish 245

---
+++ src/main/java/interviews/linkedin/_245_Shortest_Word_Distance_III/Solution.java
@@ -0,0 +1,24 @@
+package interviews.linkedin._245_Shortest_Word_Distance_III;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/22/2017.
+ */
+public class Solution {
+  public int shortestWordDistance(String[] words, String word1, String word2) {
+    long minDistance = Integer.MAX_VALUE, i1 = minDistance, i2 = -minDistance;
+    for (int i = 0; i < words.length; ++i) {
+      if (word1.equals(words[i])) {
+        i1 = i;
+      }
+      if (word2.equals(words[i])) {
+        if (i1 == i) {
+          i1 = i2;
+        }
+        i2 = i;
+      }
+      minDistance = Math.min(minDistance, Math.abs(i1 - i2));
+    }
+    return (int) minDistance;
+  }
+}
