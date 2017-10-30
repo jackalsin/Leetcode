0c8ec63079af4c47bb8185f866b04b17e41cfc9d
jackalsin <jackal.sin@hotmail.com>
2017-10-29 22:49:36 -0700

    Finish 513

---
+++ src/main/java/_501_550/_513_Find_Bottom_Left_Tree_Value/Solution.java
@@ -0,0 +1,36 @@
+package _501_550._513_Find_Bottom_Left_Tree_Value;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/29/2017.
+ */
+public class Solution {
+  public int findBottomLeftValue(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.add(root);
+    int result = 0;
+    while (!queue.isEmpty()) {
+      final int size = queue.size();
+      result = queue.peek().val;
+      for (int i = 0; i < size; i++) {
+        final TreeNode curNode = queue.remove();
+        if (curNode.left != null) {
+          queue.add(curNode.left);
+        }
+        if (curNode.right != null) {
+          queue.add(curNode.right);
+        }
+      }
+    }
+    return result;
+  }
+
+}
