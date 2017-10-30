d37480432de43324f78daca59b1ccef5a6866195
jackalsin <jackal.sin@hotmail.com>
2017-10-30 09:35:15 -0700

    Add 567 TLE

---
+++ src/main/java/_551_600/_567_Permutation_in_String/Solution.java
@@ -0,0 +1,11 @@
+package _551_600._567_Permutation_in_String;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/29/2017.
+ */
+public interface Solution {
+
+  boolean checkInclusion(String s1, String s2);
+
+}
---
+++ src/test/java/_551_600/_567_Permutation_in_String/SolutionTest.java
@@ -0,0 +1,56 @@
+package _551_600._567_Permutation_in_String;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/29/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new BruteForce();
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+    final String s1 = "ab", s2 = "eidbaooo";
+    assertTrue(solution.checkInclusion(s1, s2));
+  }
+
+  @Test
+  public void testOnlineCase2() throws Exception {
+    final String s1 = "ab", s2 = "eidboaoo";
+    assertFalse(solution.checkInclusion(s1, s2));
+  }
+
+  @Test
+  public void testTLE1() throws Exception {
+    final String s1 = "dinitrophenylhydrazine", s2 = "acetylphenylhydrazine";
+    assertFalse(solution.checkInclusion(s1, s2));
+  }
+
+  @Test
+  public void testTLE2() throws Exception {
+    final String s1 =
+        "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl" +
+            "mnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstu" +
+            "vwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd" +
+            "efghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefg" +
+            "hijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghij" +
+            "klmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdef",
+        s2 = "bcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcde" +
+            "fghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghij" +
+            "klmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmno" +
+            "pqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrst" +
+            "uvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy" +
+            "zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefg";
+    assertFalse(solution.checkInclusion(s1, s2));
+  }
+}
