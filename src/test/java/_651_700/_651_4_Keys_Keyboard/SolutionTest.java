7e1d3651cb4f61d77c6499b2fdda9102c30a1e4d
jackalsin <jackal.sin@hotmail.com>
2017-10-31 09:39:52 -0700

    Update 651

---
+++ src/test/java/_651_700/_651_4_Keys_Keyboard/SolutionTest.java
@@ -0,0 +1,23 @@
+package _651_700._651_4_Keys_Keyboard;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/31/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+
+  }
+
+}
