2be91b335204ba119a53fc23bb43056e2d5316f4
jackalsin <jackal.sin@hotmail.com>
2017-11-02 09:32:21 -0700

    update 631

---
+++ src/main/java/_601_650/_631_Design_Excel_Sum_Formula/Excel.java
@@ -0,0 +1,25 @@
+package _601_650._631_Design_Excel_Sum_Formula;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/2/2017.
+ */
+public final class Excel {
+  private static final int N = 27;
+  private final int[][] values;
+  public Excel(int H, char W) {
+    values = new int[N][N];
+  }
+
+  public void set(int r, char c, int v) {
+
+  }
+
+  public int get(int r, char c) {
+    return 0;
+  }
+
+  public int sum(int r, char c, String[] strs) {
+    return 0;
+  }
+}
