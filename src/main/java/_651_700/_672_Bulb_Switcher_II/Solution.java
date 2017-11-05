3cc76caee5b05ff10bea4d90f8392ebd924509d8
jackalsin <jackal.sin@hotmail.com>
2017-11-05 11:34:13 -0800

    Finish 672

--- src/main/java/_651_700/_672_Bulb_Switcher_II/Solution.java
+++ src/main/java/_651_700/_672_Bulb_Switcher_II/Solution.java
@@ -6,6 +6,25 @@ package _651_700._672_Bulb_Switcher_II;
  */
 public class Solution {
   public int flipLights(int n, int m) {
-    return 0;
+    if (n == 0 || m == 0) return 1;
+    if (m >= 3) {
+      if (n >= 3) return 8;
+      else if (n == 2) return 4;
+      else {  // if (n == 1)
+         return 2;
+      }
+    } else if (m == 2) {
+      if (n >= 3) return 7;
+      else if (n == 2) return 4;
+      else {  // if (n == 1) {
+        return 2;
+      }
+    } else { // m == 1
+      if (n >= 3) return 4;
+      else if (n == 2) return 3; // no 0, 0
+      else {  //(n == 1)
+        return 2;
+      }
+    }
   }
 }
