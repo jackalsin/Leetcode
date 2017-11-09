20fd209d11e3b53f53c532326d8b23a34b2dd44c
jackalsin <jackal.sin@hotmail.com>
2017-11-09 09:14:14 -0800

    Finish 380

---
+++ src/test/java/_351_400/_380_Insert_Delete_GetRandom_O_1/RandomizedSetTest.java
@@ -0,0 +1,20 @@
+package _351_400._380_Insert_Delete_GetRandom_O_1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/9/2017.
+ */
+public class RandomizedSetTest {
+
+  @Test
+  public void testFailedCase1() throws Exception {
+//  ["RandomizedSet","insert","remove","insert","getRandom","remove","insert","getRandom"]
+//  [[],[1],[2],[2],[],[1],[2],[]]
+  }
+
+}
