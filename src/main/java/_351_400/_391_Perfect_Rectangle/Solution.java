e54ea157dfdec531f4ba689ffac6d297f9e55d53
jackalsin <jackal.sin@hotmail.com>
2017-11-10 09:12:21 -0800

    Finish 391

---
+++ src/main/java/_351_400/_391_Perfect_Rectangle/Solution.java
@@ -0,0 +1,59 @@
+package _351_400._391_Perfect_Rectangle;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/9/2017.
+ */
+public class Solution {
+
+  /**
+   * Satisfy 2 conditions,
+   *  1) the area of the largest equals the sum
+   *  2) all inner corner points  are even
+   * @param rectangles
+   * @return
+   */
+  public boolean isRectangleCover(int[][] rectangles) {
+    if (rectangles == null || rectangles.length == 0 || rectangles[0].length == 0) return false;
+    int left = Integer.MAX_VALUE;
+    int bottom = Integer.MAX_VALUE;
+    int top = Integer.MIN_VALUE;
+    int right = Integer.MIN_VALUE;
+    Set<Long> usedPoints = new HashSet<>();
+    int sum = 0;
+    for (final int[] rectangle: rectangles) {
+      int curLeft = rectangle[0], curBottom = rectangle[1], curRight = rectangle[2],
+          curTop = rectangle[3];
+      left = Math.min(left, curLeft);
+      right = Math.max(right, curRight);
+      top = Math.max(top, curTop);
+      bottom = Math.min(bottom, curBottom);
+
+      sum += (curTop - curBottom) * (curRight - curLeft);
+      System.out.println(List.of(getPoint(curLeft, curTop), getPoint(curLeft, curBottom),
+          getPoint(curRight, curTop), getPoint(curRight, curBottom)));
+      if (!usedPoints.add(getPoint(curLeft, curTop)))
+        usedPoints.remove(getPoint(curLeft, curTop));
+      if (!usedPoints.add(getPoint(curLeft, curBottom)))
+        usedPoints.remove(getPoint(curLeft, curBottom));
+      if (!usedPoints.add(getPoint(curRight, curTop)))
+        usedPoints.remove(getPoint(curRight, curTop));
+      if (!usedPoints.add(getPoint(curRight, curBottom)))
+        usedPoints.remove(getPoint(curRight, curBottom));
+
+    }
+
+    return (sum == (right - left) * (top - bottom)) && usedPoints.size() == 4 &&
+        (usedPoints.contains(getPoint(left, top)) && usedPoints.contains(getPoint(left, bottom))
+            && usedPoints.contains(getPoint(right, top)) && usedPoints.contains(getPoint(right, bottom)));
+  }
+
+  static long getPoint(int left, int top) {
+    return (long) left << 32 | (top & 0xFFFFFFFFL);
+  }
+
+}
---
+++ src/test/java/_351_400/_391_Perfect_Rectangle/SolutionTest.java
@@ -0,0 +1,94 @@
+package _351_400._391_Perfect_Rectangle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/10/2017.
+ */
+public class SolutionTest {
+
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testExample1() throws Exception {
+    final int[][] rectangles = {
+        {1, 1, 3, 3},
+        {3, 1, 4, 2},
+        {3, 2, 4, 4},
+        {1, 3, 2, 4},
+        {2, 3, 3, 4}
+    };
+
+    assertTrue(solution.isRectangleCover(rectangles));
+  }
+  @Test
+  public void testExample2() throws Exception {
+    final int[][] rectangles = {
+        {1,1,2,3},
+    {1,3,2,4},
+      {3,1,4,2},
+        {3,2,4,4}
+    };
+    assertFalse(solution.isRectangleCover(rectangles));
+  }
+  @Test
+  public void testExample3() throws Exception {
+    final int[][] rectangles = {
+        {1,1,3,3},
+    {3,1,4,2},
+      {1,3,2,4},
+        {3,2,4,4}
+    };
+
+    assertFalse(solution.isRectangleCover(rectangles));
+  }
+  @Test
+  public void testExample4() throws Exception {
+    final int[][] rectangles = {
+        {1,1,3,3},
+    {3,1,4,2},
+      {1,3,2,4},
+        {2,2,4,4}
+    };
+
+    assertFalse(solution.isRectangleCover(rectangles));
+  }
+
+  /**
+   * Failed At validation of point set size should be only 4.
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testFailedCase1() throws Exception {
+    final int[][] rectangles = {{0,0,2,2},{1,1,3,3},{2,0,3,1},{0,3,3,4}};
+
+    assertFalse(solution.isRectangleCover(rectangles));
+  }
+
+  @Test
+  public void testFailedCase2() throws Exception {
+    final int N = 2;
+    final int[][] rectangles = new int[N][4];
+    for (int i = 0; i < N; i++) {
+      rectangles[i] = new int[] {0, i - 1, 1, i};
+    }
+    assertTrue(solution.isRectangleCover(rectangles));
+  }
+
+  @Test
+  public void testGetPoint() throws Exception {
+    System.out.println(1L << 32);
+    System.out.println(Solution.getPoint(1, 1));
+  }
+}
