9c417ff662954d4f237429c98987ce99922a7573
zhiwei.xin <zhiwei.xin@veritas.com>
2017-11-19 20:53:19 -0800

    Finish 419

---
+++ src/test/java/_401_450/_421_Maximum_XOR_of_Two_Numbers_in_an_Array/SolutionTest.java
@@ -0,0 +1,22 @@
+package _401_450._421_Maximum_XOR_of_Two_Numbers_in_an_Array;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+    final int[] nums = {3, 10, 5, 25, 2, 8};
+    assertEquals(28, solution.findMaximumXOR(nums));
+  }
+
+}
