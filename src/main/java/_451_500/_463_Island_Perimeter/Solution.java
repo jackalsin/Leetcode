eaa4a631e1731c85b1962386efd2c98eb33c90c1
zhiwei.xin <zhiwei.xin@veritas.com>
2017-11-21 19:07:28 -0800

    Finish 463

---
+++ src/main/java/_451_500/_463_Island_Perimeter/Solution.java
@@ -0,0 +1,25 @@
+package _451_500._463_Island_Perimeter;
+
+public class Solution {
+  public int islandPerimeter(int[][] grid) {
+    int count = 0;
+    if (grid == null || grid.length == 0 || grid[0].length == 0) {
+      return 0;
+    }
+    final int rows = grid.length, cols = grid[0].length;
+    for (int row = 0; row < rows; row++) {
+      for (int col = 0; col < cols; col++) {
+        if (grid[row][col] == 1) {
+          count += 4;
+          if (row != 0 && grid[row - 1][col] == 1) {
+            count -= 2;
+          }
+          if (col != 0 && grid[row][col - 1] == 1) {
+            count -= 2;
+          }
+        }
+      }
+    }
+    return count;
+  }
+}
