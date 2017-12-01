d2b789aab488d8c3ff32cb7d2ffe6853926dba96
zhiwei.xin <zhiwei.xin@veritas.com>
2017-11-30 22:39:05 -0800

    Finish 484

---
+++ src/main/java/_451_500/_484_Find_Permutation/Solution.java
@@ -0,0 +1,49 @@
+package _451_500._484_Find_Permutation;
+
+public class Solution {
+  private static final char D = 'D';
+  private static final char I = 'I';
+
+  public int[] findPermutation(String s) {
+    final char[] sArray = s.toCharArray();
+    final int[] res = new int[s.length() + 1];
+    int start = 1, i = 0, sPtr = 0; // max = s.length()
+    boolean isFirstAdd = true;
+    while (i < res.length) {
+      final int dCount = getCount(sArray, sPtr, D);
+      sPtr += dCount;
+
+      for (int j = 0; j <= dCount; j++) {
+        res[i] = start + dCount - j;
+        i++;
+      }
+      start += dCount + 1;
+
+
+      int iCount = getCount(sArray, sPtr, I);
+      sPtr += iCount;
+      iCount -= 1;
+      while (iCount-- > 0) {
+        res[i++] = start++;
+      }
+
+      if (i == res.length - 1) {
+        res[i++] = start++;
+      }
+    }
+
+    return res;
+  }
+
+  private int getCount(char[] sArray, int start, char d) {
+    int count = 0;
+    for (int i = start; i < sArray.length; i++) {
+      if (sArray[i] == d) {
+        count++;
+      } else {
+        break;
+      }
+    }
+    return count;
+  }
+}
---
+++ src/test/java/_451_500/_484_Find_Permutation/SolutionTest.java
@@ -0,0 +1,44 @@
+package _451_500._484_Find_Permutation;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+    final String s = "DDIIDI";
+    final int[] expected = {3, 2, 1, 4, 6, 5, 7};
+    assertArrayEquals(expected, solution.findPermutation(s));
+  }
+
+  @Test
+  public void testOnlineCase2() throws Exception {
+    final String s = "I";
+    final int[] expected = {1, 2};
+    assertArrayEquals(expected, solution.findPermutation(s));
+  }
+
+  @Test
+  public void testOnlineCase3() throws Exception {
+    final String s = "DI";
+    final int[] expected = {2, 1, 3};
+    assertArrayEquals(expected, solution.findPermutation(s));
+  }
+
+  @Test
+  public void testFailedCase1() throws Exception {
+    final String s = "D";
+    final int[] expected = {2, 1};
+    assertArrayEquals(expected, solution.findPermutation(s));
+  }
+
+}
