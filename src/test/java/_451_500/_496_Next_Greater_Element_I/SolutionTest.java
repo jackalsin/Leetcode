7a414d8f0cfb6e24bf1b6277f3d33ec108ea5c89
zhiwei.xin <zhiwei.xin@veritas.com>
2017-12-10 23:09:36 -0800

    Finish 496

---
+++ src/test/java/_451_500/_496_Next_Greater_Element_I/SolutionTest.java
@@ -0,0 +1,21 @@
+package _451_500._496_Next_Greater_Element_I;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+    final int[] nums1 = {4, 1, 2}, nums2 = {1, 3, 4, 2}, expected = {-1, 3, -1};
+    assertArrayEquals(expected, solution.nextGreaterElement(nums1, nums2));
+  }
+}
