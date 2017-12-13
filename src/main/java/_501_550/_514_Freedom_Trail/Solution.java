05411ae23d6276a2f80d53415ab68d13afae7911
zhiwei.xin <zhiwei.xin@veritas.com>
2017-12-12 20:29:32 -0800

    Finish Code 514

---
+++ src/main/java/_501_550/_514_Freedom_Trail/Solution.java
@@ -0,0 +1,57 @@
+package _501_550._514_Freedom_Trail;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+
+  public int findRotateSteps(String ring, String key) {
+    final Map<Character, List<Integer>> charToIndex = new HashMap<>();
+    final char[] rings = ring.toCharArray();
+    for (int i = 0; i < rings.length; i++) {
+      List<Integer> index = charToIndex.getOrDefault(rings[i], new ArrayList<>());
+      index.add(i);
+      charToIndex.put(rings[i], index);
+    }
+
+    return dfs(charToIndex, key, new int[ring.length()][key.length()], ring.length(), 0, 0);
+
+  }
+
+  private int dfs(final Map<Character, List<Integer>> charToIndex, final String key, final int[][] cache, int ringLen,
+                  int curKeyIndex, int ringIndex) {
+    if (curKeyIndex >= key.length()) {
+      return 0; // may be 1 ??
+    }
+    if (cache[ringIndex][curKeyIndex] > 0) {
+//      System.out.println("Hit " + ringIndex + " " + curKeyIndex);
+      return cache[ringIndex][curKeyIndex];
+    }
+    final char candidateChar = key.charAt(curKeyIndex);
+    final List<Integer> candidateIndex = charToIndex.get(candidateChar);
+    assert candidateIndex != null;
+    int minDistance = Integer.MAX_VALUE;
+    for (int curCharIndexInRing : candidateIndex) {
+      int curDistance = getDistance(curCharIndexInRing, ringIndex, ringLen);
+      curDistance++; // push the button
+      curDistance += dfs(charToIndex, key, cache, ringLen, curKeyIndex + 1, curCharIndexInRing);
+      minDistance = Math.min(minDistance, curDistance);
+    }
+    cache[ringIndex][curKeyIndex] = minDistance;
+    return minDistance;
+  }
+
+  private static int getDistance(int curCharIndexInRing, int ringIndex, int ringLen) {
+    int clockwise = ringIndex - curCharIndexInRing;
+    if (clockwise < 0) {
+      clockwise += ringLen;
+    }
+    int antiClockwise = curCharIndexInRing - ringIndex;
+    if (antiClockwise < 0) {
+      antiClockwise += ringLen;
+    }
+    return Math.min(clockwise, antiClockwise);
+  }
+}
---
+++ src/test/java/_501_550/_514_Freedom_Trail/SolutionTest.java
@@ -0,0 +1,27 @@
+package _501_550._514_Freedom_Trail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+    final String ring = "godding", key = "gd";
+    assertEquals(4, solution.findRotateSteps(ring, key));
+  }
+
+  @Test
+  public void testTLECase1() {
+    final String ring = "caotmcaataijjxi", key = "oatjiioicitatajtijciocjcaaxaaatmctxamacaamjjx";
+    solution.findRotateSteps(ring, key);
+  }
+}
