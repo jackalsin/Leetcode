91dc58237b1c8eead21a95f7b780979ba534338a
zhiwei.xin <zhiwei.xin@veritas.com>
2017-12-15 17:51:06 -0800

    Finish 524

---
+++ src/main/java/_501_550/_524_Longest_Word_in_Dictionary_through_Deleting/UnSortedSolution.java
@@ -0,0 +1,28 @@
+package _501_550._524_Longest_Word_in_Dictionary_through_Deleting;
+
+import java.util.List;
+
+public final class UnSortedSolution implements Solution {
+
+  @Override
+  public String findLongestWord(String s, List<String> d) {
+    String candidate = "";
+    for (String subStr : d) {
+      int subPtr = 0;
+      for (int sPtr = 0; sPtr < s.length(); sPtr++) {
+        if (subPtr < subStr.length() && subStr.charAt(subPtr) == s.charAt(sPtr)) {
+          subPtr++;
+        }
+
+      }
+      if (subPtr == subStr.length()) {
+        if (candidate.length() < subStr.length()) {
+          candidate = subStr;
+        } else if (candidate.length() == subStr.length() && candidate.compareTo(subStr) > 0) {
+          candidate = subStr;
+        }
+      }
+    }
+    return candidate;
+  }
+}
