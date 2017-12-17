74520bbbdf1052b2fccfe96780edf24b03784799
zhiwei.xin <zhiwei.xin@veritas.com>
2017-12-16 22:42:27 -0800

    Finish 530

---
+++ src/test/java/example/tree/InOrderTraversalTest.java
@@ -0,0 +1,38 @@
+package example.tree;
+
+import org.junit.Before;
+import org.junit.Test;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class InOrderTraversalTest {
+  private Traversal traversal;
+
+  @Before
+  public void setUp() throws Exception {
+    traversal = new InOrderTraversal();
+  }
+
+
+  @Test
+  public void testFullBalancedTree() {
+    final TreeNode root = TreeNodes.getTreeLevelOrder(
+        8,
+        4, 12,
+        2, 6, 10, 14,
+        1, 3, 5, 7, 9, 11, 13, 15
+    );
+    List<Integer> actual = traversal.traverse(root);
+    final List<Integer> expected = new ArrayList<>();
+    for (int i = 1; i < 16; i++) {
+      expected.add(i);
+    }
+    assertEquals(expected, actual);
+  }
+
+}
