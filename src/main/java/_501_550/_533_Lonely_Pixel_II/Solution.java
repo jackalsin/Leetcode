27d00c4f9e06f0b94e5c624e7fdd09807d7ee551
zhiwei.xin <zhiwei.xin@veritas.com>
2017-12-17 15:32:24 -0800

    Finish 533

---
+++ src/main/java/_501_550/_533_Lonely_Pixel_II/Solution.java
@@ -0,0 +1,46 @@
+package _501_550._533_Lonely_Pixel_II;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+  public int findBlackPixel(char[][] picture, int N) {
+    if (picture == null || picture.length == 0 || picture[0].length == 0) {
+      return 0;
+    }
+    final int rows = picture.length, cols = picture[0].length;
+    final int[] rowCounts = new int[rows], colCounts = new int[cols];
+    List<Set<Integer>> rowsHavingSameBCol = new ArrayList<>();
+    for (int i = 0; i < cols; i++) rowsHavingSameBCol.add(new HashSet<>());
+    for (int row = 0; row < rows; row++) {
+      for (int col = 0; col < cols; col++) {
+        if (picture[row][col] == 'B') {
+          rowsHavingSameBCol.get(col).add(row);
+          rowCounts[row]++;
+          colCounts[col]++;
+        }
+      }
+    }
+
+    int count = 0;
+    for (int row = 0; row < rows; row++) {
+      for (int col = 0; col < cols; col++) {
+        if (rowCounts[row] == N && colCounts[col] == N) {
+          Set<Integer> rowIndex = rowsHavingSameBCol.get(col);
+          boolean isValid = true;
+          for (int candidateRow : rowIndex) {
+            if (!Arrays.equals(picture[candidateRow], picture[row])) {
+              isValid = false;
+            }
+          }
+          if (isValid) count++;
+        }
+      }
+    }
+
+    return count;
+  }
+}
