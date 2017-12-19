4fe390b5a7ae527bfc94e6a6e6092a36c3471d2c
jackalsin <jackal.sin@hotmail.com>
2017-12-18 23:40:34 -0800

    Finish 551

---
+++ src/test/java/_551_600/_551_Student_Attendance_Record_I/SolutionTest.java
@@ -0,0 +1,32 @@
+package _551_600._551_Student_Attendance_Record_I;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 12/18/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+    final String input = "PPALLP";
+    assertTrue(solution.checkRecord(input));
+  }
+
+  @Test
+  public void testOnlineCase2() throws Exception {
+    final String input = "PPALLL";
+    assertFalse(solution.checkRecord(input));
+  }
+}
