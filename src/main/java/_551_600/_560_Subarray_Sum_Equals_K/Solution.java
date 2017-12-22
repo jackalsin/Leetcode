b7a9c1318617378d432ff88922d32d9cd07f10d4
jackalsin <jackal.sin@hotmail.com>
2017-12-21 18:26:56 -0800

    Finish 560

---
+++ src/main/java/_551_600/_560_Subarray_Sum_Equals_K/Solution.java
@@ -0,0 +1,22 @@
+package _551_600._560_Subarray_Sum_Equals_K;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jacka
+ * @version 1.0 on 12/20/2017.
+ */
+public class Solution {
+  public int subarraySum(int[] nums, int k) {
+    final Map<Integer, Integer> preSumCount = new HashMap<>();
+    int sum = 0, result = 0;
+    preSumCount.put(0, 1);
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      result += preSumCount.getOrDefault(sum - k, 0);
+      preSumCount.put(sum, preSumCount.getOrDefault(sum, 0) + 1);
+    }
+    return result;
+  }
+}
