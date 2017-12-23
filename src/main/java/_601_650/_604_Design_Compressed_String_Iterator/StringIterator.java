0eaff8faaf3397af88687277b8c5bed34ebf70e2
zhiwei.xin <zhiwei.xin@veritas.com>
2017-12-23 10:32:44 -0800

    Finish 604 with optimization

--- src/main/java/_601_650/_604_Design_Compressed_String_Iterator/StringIterator.java
+++ src/main/java/_601_650/_604_Design_Compressed_String_Iterator/StringIterator.java
@@ -3,10 +3,7 @@ package _601_650._604_Design_Compressed_String_Iterator;
 public class StringIterator {
   private final String string;
 
-  /* count limit now */
   private int count = 0;
-
-  private int curCount = 0;
   private char curChar = ' ';
 
   /* String pointer after move */
@@ -15,29 +12,25 @@ public class StringIterator {
   public StringIterator(String compressedString) {
     string = compressedString;
 
-    updateTheNextPart();
   }
 
   private void updateTheNextPart() {
     if (curPointer == string.length()) return;
     curChar = string.charAt(curPointer++);
-    final int start = curPointer;
     count = 0;
     while (curPointer < string.length() && Character.isDigit(string.charAt(curPointer))) {
       count = count * 10 + string.charAt(curPointer++) - '0';
     }
     // manually add to optimize
-//    count = Integer.parseInt(string.substring(start, curPointer));
-    curCount = 0;
   }
 
   public char next() {
-    if (curCount == count) {
+    if (count == 0) {
       updateTheNextPart();
     }
 
     if (hasNext()) {
-      curCount++;
+      count--;
       return curChar;
     } else {
       return ' ';
@@ -45,9 +38,6 @@ public class StringIterator {
   }
 
   public boolean hasNext() {
-    if (curCount == count) {
-      updateTheNextPart();
-    }
-    return curCount < count || curPointer != string.length();
+    return count > 0 || curPointer != string.length();
   }
 }
