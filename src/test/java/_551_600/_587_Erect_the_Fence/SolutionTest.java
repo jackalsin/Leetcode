b65986d676bb559ee21461b8eade11162f710c27
zhiwei.xin <zhiwei.xin@veritas.com>
2017-12-23 21:23:57 -0800

    Finish 587

--- src/test/java/_551_600/_587_Erect_the_Fence/SolutionTest.java
+++ src/test/java/_551_600/_587_Erect_the_Fence/SolutionTest.java
@@ -2,12 +2,11 @@ package _551_600._587_Erect_the_Fence;
 
 import org.junit.Before;
 import org.junit.Test;
+import utils.Point;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import utils.Point;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,7 +19,7 @@ public class SolutionTest {
 
   @Before
   public void setUp() throws Exception {
-    solution = new Solution();
+    solution = new JavisAlgorithm();
   }
 
   /**
@@ -30,12 +29,12 @@ public class SolutionTest {
   @Test
   public void testOnlineCase1() throws Exception {
     final Point[] input = {
-        new Point(1, 1),
-        new Point(2, 2),
-        new Point(2, 0),
-        new Point(2, 4),
-        new Point(3, 3),
-        new Point(4, 2)
+        new Point(1, 1),  // 0
+        new Point(2, 2),  // 1
+        new Point(2, 0),  // 2
+        new Point(2, 4),  // 3
+        new Point(3, 3),  // 4
+        new Point(4, 2)   // 5
     };
     final Set<Point> output = Set.of(
         new Point(1, 1),
@@ -44,8 +43,10 @@ public class SolutionTest {
         new Point(3, 3),
         new Point(2, 4)
     );
-    final List<Point> actual = solution.outerTrees(input);
 
+    // 0 3 4 5
+    final List<Point> actual = solution.outerTrees(input);
+    assertEquals(output.size(), actual.size());
     assertEquals(output, new HashSet<>(actual));
   }
 
@@ -66,7 +67,7 @@ public class SolutionTest {
         new Point(4, 2)
     );
     final List<Point> actual = solution.outerTrees(input);
-
+    assertEquals(output.size(), actual.size());
     assertEquals(output, new HashSet<>(actual));
   }
 }
