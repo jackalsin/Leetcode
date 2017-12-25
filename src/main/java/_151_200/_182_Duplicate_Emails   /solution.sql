b42961d8d28e0fa14f4aa8f4f6390c8abad41736
zhiwei.xin <zhiwei.xin@veritas.com>
2017-12-24 18:24:17 -0800

    Add ignored files 182/184/185/197

--- src/main/java/_151_200/_182_Duplicate_Emails   /solution.sql
+++
@@ -1,9 +0,0 @@
-use leetcode;
-Create table If Not Exists Person (Id int, Email varchar(255));
-Truncate table Person;
-insert into Person (Id, Email) values ('1', 'a@b.com');
-insert into Person (Id, Email) values ('2', 'c@d.com');
-insert into Person (Id, Email) values ('3', 'a@b.com');
-
--- solution
-SELECT DISTINCT p1.Email FROM Person p1, Person p2 WHERE p1.Id != p2.Id AND p1.Email = p2.Email;
