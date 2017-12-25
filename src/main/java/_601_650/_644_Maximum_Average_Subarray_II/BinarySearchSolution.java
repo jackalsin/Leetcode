dfe63cc3fa04b13172defb14bb691581f5f0c461
jackalsin <jackal.sin@hotmail.com>
2017-12-25 15:00:33 -0800

    Finish 644

--- src/main/java/_601_650/_644_Maximum_Average_Subarray_II/BinarySearchSolution.java
+++ src/main/java/_601_650/_644_Maximum_Average_Subarray_II/BinarySearchSolution.java
@@ -3,6 +3,7 @@ package _601_650._644_Maximum_Average_Subarray_II;
 /**
  * @author jacka
  * @version 1.0 on 12/25/2017.
+ * Time Complexity O(n * log(MaxValue - MinValue))
  */
 public class BinarySearchSolution implements Solution {
   private static final double STEP = 1e-5;
@@ -12,11 +13,7 @@ public class BinarySearchSolution implements Solution {
     if (nums == null || nums.length == 0 || k <= 0) {
       return Integer.MAX_VALUE;
     }
-    double min = nums[0], max = nums[0];
-    for (int i = 1; i < nums.length; i++) {
-      min = Math.min(min, nums[i]);
-      max = Math.max(max, nums[i]);
-    }
+    double min = Integer.MIN_VALUE, max = Integer.MAX_VALUE;
     while (min <= max) {
       double mid = (max - min) / 2.0 + min;
       if (hasAboveAverage(nums, k, mid)) {
@@ -25,7 +22,6 @@ public class BinarySearchSolution implements Solution {
         max = mid - STEP;
       }
     }
-//    System.out.println("min = " + min + "max = " + max);
     return max;
   }
 
@@ -37,10 +33,13 @@ public class BinarySearchSolution implements Solution {
     if (kSum >= 0) {
       return true;
     }
+    // must have at least k elements; the nums before the last k element should be kept if their
+    // sum > 0;
+    // else we should remove them from the current sum (equivalent to update the start position)
     int cur = k;
     while (cur < nums.length) {
       kSum += nums[cur] - average;
-      extraSum += nums[cur - k] - average;
+      extraSum += nums[cur - k] - average; // extraSum: the sum before
       if (extraSum < 0) {
         kSum -= extraSum;
         extraSum = 0;
