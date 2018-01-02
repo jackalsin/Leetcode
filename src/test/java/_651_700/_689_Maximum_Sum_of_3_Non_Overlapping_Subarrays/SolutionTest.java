07e182e8907630ff99f13a0905b0715c1fefbb03
zhiwei.xin <zhiwei.xin@veritas.com>
2018-01-01 16:34:15 -0800

    Remove unnecessary println statement

--- src/test/java/_651_700/_689_Maximum_Sum_of_3_Non_Overlapping_Subarrays/SolutionTest.java
+++ src/test/java/_651_700/_689_Maximum_Sum_of_3_Non_Overlapping_Subarrays/SolutionTest.java
@@ -3,8 +3,6 @@ package _651_700._689_Maximum_Sum_of_3_Non_Overlapping_Subarrays;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static org.junit.Assert.assertArrayEquals;
 
 public class SolutionTest {
@@ -21,7 +19,7 @@ public class SolutionTest {
     final int[] input = {1, 2, 1, 2, 6, 7, 5, 1};
     int k = 2;
     final int[] actual = solution.maxSumOfThreeSubarrays(input, k);
-    System.out.println(Arrays.toString(actual));
+//    System.out.println(Arrays.toString(actual));
     assertArrayEquals(new int[]{0, 3, 5}, actual);
   }
 
@@ -30,7 +28,7 @@ public class SolutionTest {
     final int[] input = {1, 2, 1, 2, 6, 7, 5, 1};
     int k = 1;
     final int[] actual = solution.maxSumOfThreeSubarrays(input, k);
-    System.out.println(Arrays.toString(actual));
+//    System.out.println(Arrays.toString(actual));
     assertArrayEquals(new int[]{4, 5, 6}, actual);
   }
 }
