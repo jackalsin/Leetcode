7c898c993b9ca3dfe98a54fb0158bb7a9cbac52b
jackalsin <jackal.sin@hotmail.com>
2018-01-04 19:12:53 -0800

    Finish 734

---
+++ src/test/java/_701_750/_734_Sentence_Similarity/SolutionTest.java
@@ -0,0 +1,28 @@
+package _701_750._734_Sentence_Similarity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/4/2018.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+    final String[] word1 = {"great", "acting", "skills"}, word2 = new String[] {"fine", "drama",
+        "talent"};
+    final String[][] pairs =
+        {{"great", "fine"}, {"drama", "acting"}, {"skills", "talent"}};
+    assertTrue(solution.areSentencesSimilar(word1, word2, pairs));
+  }
+}
