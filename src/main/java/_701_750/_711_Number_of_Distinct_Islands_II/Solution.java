87880c5d4fa5987d28c9f9e62d30d75bb1cd2a45
jackalsin <jackal.sin@hotmail.com>
2018-01-07 00:00:41 -0800

    Fix wrong order 719 * 711

--- src/main/java/_701_750/_711_Number_of_Distinct_Islands_II/Solution.java
+++ src/main/java/_701_750/_711_Number_of_Distinct_Islands_II/Solution.java
@@ -1,65 +1,145 @@
 package _701_750._711_Number_of_Distinct_Islands_II;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
 
 public class Solution {
+  private static final int[][] DIRS = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+  private static final int VISITED = -1;
 
-  /**
-   * 这道题的最大难点在于是要数unique pair
-   *
-   * @param nums
-   * @param k
-   * @return
-   */
-  public int smallestDistancePair(int[] nums, int k) {
-    Arrays.sort(nums);
-    int min = Integer.MAX_VALUE;
-    for (int i = 1; i < nums.length; i++) {
-      min = Math.min(min, nums[i] - nums[i - 1]);
+  public int numDistinctIslands2(int[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
     }
-    int max = nums[nums.length - 1] - nums[0];
-
-    System.out.println(Arrays.toString(nums));
-    while (min < max) {
-      int mid = min + (max - min) / 2;
-      int curCount = getCount(nums, mid);
-      System.out.println("mid = " + mid + " curCount = " + curCount);
-      if (curCount < k) {
-        min = mid + 1;
-      } else {
-        max = mid;
+    final int rows = grid.length;
+    if (grid[0].length == 0) {
+      return 0;
+    }
+    final int cols = grid[0].length;
+//    final boolean[][] visited = new boolean[rows][cols];
+    final Set<List<Point>> islands = new HashSet<>();
+    for (int row = 0; row < rows; ++row) {
+      for (int col = 0; col < cols; ++col) {
+        if (grid[row][col] == 1) {
+          final List<Point> island = new ArrayList<>();
+          island.add(new Point(row, col));
+          grid[row][col] = VISITED;
+          dfs(grid, row, col, island);
+          islands.add(norm(island));
+        }
       }
     }
-    return min;
+    return islands.size();
   }
 
-
-  private static int getCount(final int[] nums, int target) {
-    int count = 0;
-    for (int num : nums) {
-      count += getCountLessThanOrEquals(nums, num + target);
+  private List<Point> norm(List<Point> island) {
+    List<List<Point>> allShapes = new ArrayList<>();
+    for (int i = 0; i < 8; i++) {
+      allShapes.add(new ArrayList<>());
     }
-    return count;
+    for (Point p : island) {
+      allShapes.get(0).add(new Point(p.x, p.y));
+      allShapes.get(1).add(new Point(p.x, -p.y));
+      allShapes.get(2).add(new Point(-p.x, p.y));
+      allShapes.get(3).add(new Point(-p.x, -p.y));
+      allShapes.get(4).add(new Point(p.y, p.x));
+      allShapes.get(5).add(new Point(-p.y, p.x));
+      allShapes.get(6).add(new Point(p.y, -p.x));
+      allShapes.get(7).add(new Point(-p.y, -p.x));
+    }
+    for (List<Point> i : allShapes) {
+      Collections.sort(i);
+      Point base = i.get(0);
+      for (int k = 1; k < i.size(); k++) {
+        Point cur = i.get(k);
+        i.set(k, new Point(cur.x - base.x, cur.y - base.y));
+      }
+      i.set(0, new Point(0, 0));
+    }
+    allShapes.sort(new Comparator<List<Point>>() {
+      @Override
+      public int compare(List<Point> o1, List<Point> o2) {
+        if (o1.size() == o2.size()) {
+          ListIterator<Point> itr1 = o1.listIterator();
+          ListIterator<Point> itr2 = o2.listIterator();
+          while (itr1.hasNext()) {
+            final Point p1 = itr1.next();
+            final Point p2 = itr2.next();
+            if (!p1.equals(p2)) {
+              return p1.compareTo(p2);
+            }
+          }
+        }
+        return Integer.compare(o1.size(), o2.size());
+      }
+    });
+
+    return allShapes.get(0);
   }
 
-  /**
-   * Return the count smaller than or equal to <t>target</t>
-   *
-   * @param nums
-   * @param target
-   * @return
-   */
-  private static int getCountLessThanOrEquals(final int[] nums, final int target) {
-    int left = 0, right = nums.length - 1;
-    while (left < right) {
-      int mid = (right - left + 1) / 2 + left;
-      if (nums[mid] <= target) {
-        left = mid;
-      } else {
-        right = mid - 1;
+  private void dfs(final int[][] grid, final int row, final int col, final List<Point> island) {
+    final int rows = grid.length, cols = grid[0].length;
+    for (int[] dir : DIRS) {
+      final int nextRow = row + dir[0], nextCol = col + dir[1];
+      if (nextCol >= 0 && nextCol < cols && nextRow >= 0 && nextRow < rows &&
+          grid[nextRow][nextCol] == 1) {
+        island.add(new Point(nextRow, nextCol));
+        grid[nextRow][nextCol] = VISITED;
+        dfs(grid, nextRow, nextCol, island);
       }
     }
-    return left;
+  }
+
+  private static final class Point implements Comparable<Point> {
+    private final int x;
+    private final int y;
+
+    Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (!(o instanceof Point)) {
+        return false;
+      }
+
+      Point point = (Point) o;
+
+      return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+      int result = x;
+      result = 31 * result + y;
+      return result;
+    }
+
+    @Override
+    public String toString() {
+      return "(" +
+          "x=" + x +
+          ", y=" + y +
+          ')';
+    }
+
+    @Override
+    public int compareTo(Point o) {
+      if (x == o.x) {
+        return Integer.compare(y, o.y);
+      }
+      return Integer.compare(x, o.x);
+    }
   }
 
 }
--- src/test/java/_701_750/_719_Find_K_th_Smallest_Pair_Distance/SolutionTest.java
+++ src/test/java/_701_750/_719_Find_K_th_Smallest_Pair_Distance/SolutionTest.java
@@ -33,4 +33,21 @@ public class SolutionTest {
     final int k = 4;
     assertEquals(4, solution.smallestDistancePair(nums, k));
   }
+
+  @Test
+  public void testOnlineCase2() throws Exception {
+    final int[] input = {1, 1, 2, 4, 7, 11};
+    int actual2 = solution.smallestDistancePair(input, 2);
+    int actual7 = solution.smallestDistancePair(input, 7);
+    assertEquals(1, actual2);
+    assertEquals(3, actual7);
+  }
+
+  @Test
+  public void testFailedCase1() throws Exception {
+    final int[] nums = {9, 10, 7, 10, 6, 1, 5, 4, 9, 8};
+    final int k = 18;
+    Solution solution = new Solution();
+    assertEquals(2, solution.smallestDistancePair(nums, k));
+  }
 }
