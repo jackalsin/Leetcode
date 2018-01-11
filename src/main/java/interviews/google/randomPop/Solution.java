4a2a1f0fc689d291e1357e5b85bde6282fcb5613
jackalsin <jackal.sin@hotmail.com>
2018-01-11 00:36:38 -0800

    Finish 382

---
+++ src/main/java/interviews/google/randomPop/Solution.java
@@ -0,0 +1,13 @@
+package interviews.google.randomPop;
+
+/**
+ * 能不能先有一个n size array， 然后pop第一个，在pop第二个，如果第二个小于第一个，说明第一个是原来sorted
+ * list最大的那个就直接放在array最末尾，如果第二个大于第一个就把第一个放在第一位，如果相等就两个都先放着…继续pop直到不一样的东西出现，时间是O（n），space应该是O（1）吧
+ *
+ * @author jacka
+ * @version 1.0 on 1/10/2018.
+ */
+public class Solution {
+
+
+}
---
+++ src/main/java/interviews/google/reservoir_sampling/_382_Linked_List_Random_Node/Solution.java
@@ -0,0 +1,47 @@
+package interviews.google.reservoir_sampling._382_Linked_List_Random_Node;
+
+import java.util.Random;
+
+import utils.ListNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/10/2018.
+ */
+public class Solution {
+  private ListNode head;
+  private static final int K = 1;
+  private final Random random = new Random();
+
+  /**
+   * @param h The linked list's head.
+   *          Note that the head is guaranteed to be not null, so it contains at least one node.
+   */
+  public Solution(ListNode h) {
+    this.head = h;
+
+  }
+
+  /**
+   * Returns a random node's value.
+   */
+  public int getRandom() {
+    ListNode head = this.head;
+    int[] reserved = new int[K];
+    int i = 0;
+    for (; i < K; i++) {
+      reserved[i] = head.val;
+      head = head.next;
+    }
+    while (head != null) {
+      int n = random.nextInt(i + 1);
+      i++;
+      int candidate = head.val;
+      head = head.next;
+      if (n < K) {
+        reserved[n] = candidate;
+      }
+    }
+    return reserved[0];
+  }
+}
