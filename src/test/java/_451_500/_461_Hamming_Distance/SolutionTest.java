a038941ccf75587fb58ed9ec2a4a288d87f95cde
jackalsin <jackal.sin@hotmail.com>
2018-01-14 21:03:49 -0800

    Finish 461

---
+++ src/test/java/_451_500/_461_Hamming_Distance/SolutionTest.java
@@ -0,0 +1,24 @@
+package _451_500._461_Hamming_Distance;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/14/2018.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+    assertEquals(2, solution.hammingDistance(1, 4));
+  }
+}
