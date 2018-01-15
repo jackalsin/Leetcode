2bea1dcb5ba0c3ea24525d3549cd46f6c816b8e4
jackalsin <jackal.sin@hotmail.com>
2018-01-14 23:50:32 -0800

    Finish 296

---
+++ src/test/java/interviews/google/_296_Best_Meeting_Point/SolutionTest.java
@@ -0,0 +1,26 @@
+package interviews.google._296_Best_Meeting_Point;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/14/2018.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void test11() throws Exception {
+    final int[][] board = {{1, 1}};
+    assertEquals(1, solution.minTotalDistance(board));
+  }
+
+}
