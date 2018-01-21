e40ab53e587df38ac894485d8ec6fee93d586db1
jackalsin <jackal.sin@hotmail.com>
2018-01-20 20:51:01 -0800

    Add first non repeat char in string

---
+++ src/main/java/interviews/google/first_non_repeatable_char/OneMapSolution.java
@@ -0,0 +1,25 @@
+package interviews.google.first_non_repeatable_char;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/20/2018.
+ */
+public final class OneMapSolution implements Solution {
+  @Override
+  public char firstNonRepeatableChar(String s) {
+    final Map<Character, Integer> charCounts = new HashMap<>();
+    for (char chr : s.toCharArray()) {
+      charCounts.put(chr, charCounts.getOrDefault(chr, 0) + 1);
+    }
+
+    for (char chr : s.toCharArray()) {
+      if (charCounts.get(chr) == 1) {
+        return chr;
+      }
+    }
+    throw new IllegalStateException("No such good stuff");
+  }
+}
---
+++ src/main/java/interviews/google/first_non_repeatable_char/Solution.java
@@ -0,0 +1,10 @@
+package interviews.google.first_non_repeatable_char;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/20/2018.
+ */
+public interface Solution {
+
+  char firstNonRepeatableChar(final String s);
+}
