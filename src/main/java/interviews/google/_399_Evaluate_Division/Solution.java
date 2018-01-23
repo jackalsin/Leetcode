dac9a6b8e6a234c3f6c9c5d2d00c1887d38d0e7e
jackalsin <jackal.sin@hotmail.com>
2018-01-23 09:00:38 -0800

    Add 399

---
+++ src/main/java/interviews/google/_399_Evaluate_Division/Solution.java
@@ -0,0 +1,11 @@
+package interviews.google._399_Evaluate_Division;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/23/2018.
+ */
+public class Solution {
+  public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+    return null;
+  }
+}
