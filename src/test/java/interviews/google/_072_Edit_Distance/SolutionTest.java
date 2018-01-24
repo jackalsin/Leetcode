6ff78ec1bc5e4758cb549703ccf210878cb1188d
jackalsin <jackal.sin@hotmail.com>
2018-01-24 09:11:11 -0800

    Finish 072 without move

---
+++ src/test/java/interviews/google/_072_Edit_Distance/SolutionTest.java
@@ -0,0 +1,35 @@
+package interviews.google._072_Edit_Distance;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/24/2018.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+
+  @Test
+  public void testWithEmptyCouples() throws Exception {
+    assertEquals(0, solution.minDistance("", ""));
+  }
+
+  @Test
+  public void testWithEmpty() throws Exception {
+    assertEquals(5, solution.minDistance("", "word1"));
+  }
+
+  @Test
+  public void testWithCommonCase1() throws Exception {
+    assertEquals(2, solution.minDistance("word", "wcd"));
+  }
+}
