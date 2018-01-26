3b210b769113b2abc71ca8e701726172280396c7
jackalsin <jackal.sin@hotmail.com>
2018-01-25 22:57:13 -0800

    Finish 769

---
+++ src/test/java/interviews/google/_769_Max_Chunks_To_Make_Sorted/SolutionTest.java
@@ -0,0 +1,31 @@
+package interviews.google._769_Max_Chunks_To_Make_Sorted;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/25/2018.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testOnlineCase1() throws Exception {
+    final int[] nums = {4, 3, 2, 1, 0};
+    assertEquals(1, solution.maxChunksToSorted(nums));
+  }
+
+  @Test
+  public void testOnlineCase2() throws Exception {
+    final int[] nums = {1, 0, 2, 3, 4};
+    assertEquals(4, solution.maxChunksToSorted(nums));
+  }
+}
