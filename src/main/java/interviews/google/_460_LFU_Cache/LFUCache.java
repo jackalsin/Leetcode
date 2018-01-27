dd8e62c3b72b699e203fa858e118edfc255da234
jackalsin <jackal.sin@hotmail.com>
2018-01-27 11:32:50 -0800

    Finish 460

--- src/main/java/interviews/google/_460_LFU_Cache/LFUCache.java
+++ src/main/java/interviews/google/_460_LFU_Cache/LFUCache.java
@@ -1,7 +1,7 @@
 package interviews.google._460_LFU_Cache;
 
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -26,26 +26,67 @@ public final class LFUCache {
     if (keyToValue.containsKey(key)) {
       int res = keyToValue.get(key);
       final CountNode countNode = keyToCounts.get(key);
-      // remove a key
-      countNode.keySet.remove(key);
-
-      // next is the right place
-      if (countNode.next.count == countNode.count + 1) {
-        countNode.next.keySet.add(key);
-        keyToCounts.put(key, countNode.next);
-      } else { // not the right place
-        final CountNode toAdd = new CountNode(countNode.count + 1);
-        addToLink(countNode, toAdd, countNode.next);
-      }
-
-      if (countNode.keySet.isEmpty()) {
-        removeCountNode(countNode);
-      }
+      removeKeyFromCountNodeAndAddToNext(countNode, key);
       return res;
     } else {
       return INVALID;
     }
   }
+
+  /**
+   * put also counts as use
+   */
+  public void put(int key, int value) {
+    if (capacity == 0) {
+      return;
+    }
+    keyToValue.put(key, value);
+    if (keyToCounts.containsKey(key)) {
+      final CountNode countNode = keyToCounts.get(key);
+      removeKeyFromCountNodeAndAddToNext(countNode, key);
+    } else {
+      if (keyToCounts.size() == capacity) {
+        final int toRemoveKey = head.next.keySet.iterator().next();
+        keyToValue.remove(toRemoveKey);
+        removeKeyFromCountNode(head.next, toRemoveKey);
+      }
+      if (head.next.count != 1) {
+        final CountNode countNode = new CountNode(1);
+        addToLink(head, countNode, head.next);
+      }
+      final CountNode countNode = head.next;
+      countNode.keySet.add(key);
+      keyToCounts.put(key, countNode);
+    }
+  }
+
+  private void removeKeyFromCountNode(final CountNode countNode, int key) {
+    countNode.keySet.remove(key);
+    if (countNode.keySet.isEmpty()) {
+      removeCountNode(countNode);
+    }
+    keyToCounts.remove(key);
+  }
+
+  private void removeKeyFromCountNodeAndAddToNext(CountNode countNode, int key) {
+    // remove a key
+    countNode.keySet.remove(key);
+    // next is the right place
+    if (countNode.next.count == countNode.count + 1) {
+      countNode.next.keySet.add(key);
+      keyToCounts.put(key, countNode.next);
+    } else { // not the right place
+      final CountNode toAdd = new CountNode(countNode.count + 1);
+      toAdd.keySet.add(key);
+      addToLink(countNode, toAdd, countNode.next);
+    }
+
+    if (countNode.keySet.isEmpty()) {
+      removeCountNode(countNode);
+    }
+    keyToCounts.put(key, countNode.next);
+  }
+
 
   private void removeCountNode(CountNode countNode) {
     final CountNode prev = countNode.prev, next = countNode.next;
@@ -61,25 +102,34 @@ public final class LFUCache {
     next.prev = toAdd;
   }
 
-  /**
-   * put also counts as use
-   *
-   * @param key
-   * @param value
-   */
-  public void put(int key, int value) {
+  public void displayHead() {
+    System.out.println(keyToCounts);
+    System.out.println(keyToValue);
+    CountNode cur = head;
+    while (cur != null) {
+      System.out.println(cur);
+      cur = cur.next;
+    }
 
+    System.out.println();
   }
-
-
   private static final class CountNode {
     private final int count;
-    private final Set<Integer> keySet = new HashSet<>();
+    private final Set<Integer> keySet = new LinkedHashSet<>();
     private CountNode prev, next;
 
     public CountNode(int count) {
       this.count = count;
     }
-  }
 
+    @Override
+    public String toString() {
+      return "CountNode{" +
+          "count=" + count +
+          ", keySet=" + keySet +
+          ", prev=" + (prev == null ? null : prev.count) +
+          ", next=" + (next == null ? null : next.count) +
+          '}';
+    }
+  }
 }
