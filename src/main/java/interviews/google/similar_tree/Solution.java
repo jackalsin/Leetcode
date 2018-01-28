1a3d687a24e89332f8dacf1429a3cacba9d98493
jackalsin <jackal.sin@hotmail.com>
2018-01-27 22:17:26 -0800

    Add Similar Tree O(N^2) Solution

---
+++ src/main/java/interviews/google/similar_tree/Solution.java
@@ -0,0 +1,11 @@
+package interviews.google.similar_tree;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/27/2018.
+ */
+public interface Solution {
+  boolean isSimilarTree(final TreeNode root1, final TreeNode root2);
+}
