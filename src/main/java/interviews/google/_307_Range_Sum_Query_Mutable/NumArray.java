fd4fff24e00090c7377bdba3a2d1f95bf6df06d1
jackalsin <jackal.sin@hotmail.com>
2018-01-30 20:00:58 -0800

    Add 289 307 308 737

--- src/main/java/interviews/google/_307_Range_Sum_Query_Mutable/NumArray.java
+++ src/main/java/interviews/google/_307_Range_Sum_Query_Mutable/NumArray.java
@@ -4,42 +4,46 @@ package interviews.google._307_Range_Sum_Query_Mutable;
  * @author jacka
  * @version 1.0 on 1/30/2018.
  */
-public class NumArray {
+public final class NumArray {
   private final int n;
   private final int[] tree;
+  private final int[] num;
 
   public NumArray(int[] nums) {
     n = nums.length;
+    this.num = nums;
     tree = new int[nums.length + 1];
     for (int i = 0; i < n; i++) {
-      update(i, nums[i]);
+      updateByDiff(i, nums[i]);
     }
+
+//    System.out.println(Arrays.toString(tree));
   }
 
   public void update(int i, int val) {
+    updateByDiff(i, val - num[i]);
+    num[i] = val;
+  }
+
+  void updateByDiff(int i, int diff) {
     ++i;
     while (i <= n) {
-      tree[i] += val;
+      tree[i] += diff;
       i += (i & -i);
     }
   }
 
   public int sumRange(int i, int j) {
-    int sum = 0;
+    return getPreSum(j) - getPreSum(i - 1);
+  }
+
+  public int getPreSum(int i) {
     ++i;
-    while (i != 0) {
+    int sum = 0;
+    while (i > 0) {
       sum += tree[i];
       i -= (i & -i);
     }
     return sum;
-  }
-
-  public static void main(String[] args) {
-    int a = 13;
-    System.out.println(Integer.toBinaryString(a));
-    while (a != 0) {
-      System.out.print(Integer.toBinaryString(a & -a) + " ");
-      a -= (a & -a);
-    }
   }
 }
