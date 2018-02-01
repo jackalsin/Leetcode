2bad70492c897aff1bbfec578976188486fed320
jackalsin <jackal.sin@hotmail.com>
2018-01-31 20:45:05 -0800

    Finish treasure

---
+++ src/main/java/interviews/google/treasure/Edge.java
@@ -0,0 +1,38 @@
+package interviews.google.treasure;
+
+import java.util.Objects;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/31/2018.
+ */
+public final class Edge {
+
+  public final Room from, to;
+  public final boolean needsKey;
+
+  public Edge(Room from, Room to, boolean needsKey) {
+    this.from = from;
+    this.to = to;
+    this.needsKey = needsKey;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Edge)) {
+      return false;
+    }
+    Edge edge = (Edge) o;
+    return needsKey == edge.needsKey &&
+        Objects.equals(from, edge.from) &&
+        Objects.equals(to, edge.to);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(from, to, needsKey);
+  }
+}
--- src/main/java/interviews/google/videoPlayList/Solution.java
+++ src/main/java/interviews/google/videoPlayList/Solution.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
  * @version 1.0 on 1/30/2018.
  */
 public class Solution {
-  // TODO: www.1point3acres.com/bbs/thread-308110-1-1.html
   // 第一轮：华人小哥 很明显的一道union find题
   //给定一堆video playlist l1: [1, 2, 4, 5], l2: [2, 4], l3: [6], l4: [5].
   // 求里面有几个cluster。规则：1.拥有相同video 的两个list属于同一cluster； 2. 以上的property可以commute：如果l1,
