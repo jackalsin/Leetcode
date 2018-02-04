7b882d032558f70b98f594175c2a8ae8da123a89
jackalsin <jackal.sin@hotmail.com>
2018-02-04 11:17:13 -0800

    Add longest prefix and suffix

---
+++ src/main/java/_701_750/_735_Asteroid_Collision/Solution.java
@@ -0,0 +1,43 @@
+package _701_750._735_Asteroid_Collision;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Time Complexity O(N), space complexity O(N)
+ *
+ * @author jacka
+ * @version 1.0 on 2/3/2018.
+ */
+public class Solution {
+  public int[] asteroidCollision(int[] asteroids) {
+    final Deque<Integer> stack = new ArrayDeque<>();
+    for (int asteroid : asteroids) {
+      if (stack.isEmpty()) {
+        stack.addLast(asteroid);
+      } else {
+        if (asteroid > 0) {
+          stack.add(asteroid);
+        } else if (asteroid < 0) {
+          /**/
+          while ((!stack.isEmpty()) && stack.peekLast() > 0 &&
+              Math.abs(stack.peekLast()) < Math.abs(asteroid)) {
+            stack.removeLast();
+          }
+          if ((!stack.isEmpty()) && stack.peekLast() > 0 &&
+              Math.abs(stack.peekLast()) == Math.abs(asteroid)) {
+            stack.removeLast();
+          } else if (stack.isEmpty() || stack.peekLast() < 0) {
+            stack.addLast(asteroid);
+          }
+        }
+      }
+//      System.out.println(stack);
+    }
+    final int[] result = new int[stack.size()];
+    for (int i = stack.size() - 1; i >= 0; i--) {
+      result[i] = stack.removeLast();
+    }
+    return result;
+  }
+}
