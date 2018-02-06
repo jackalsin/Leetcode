ad3c40258c8c1014b469e3bc55c9831b8eb18c5d
jackalsin <jackal.sin@hotmail.com>
2018-02-05 23:04:22 -0800

    Finish 733

---
+++ src/main/java/_701_750/_733_Flood_Fill/Solution.java
@@ -0,0 +1,35 @@
+package _701_750._733_Flood_Fill;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/5/2018.
+ */
+public class Solution {
+  private static final int[][] DIRS = {
+      {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+  public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+    final int replacedColor = image[sr][sc];
+    final Queue<int[]> queue = new ArrayDeque<>();
+    final int rows = image.length, cols = image[0].length;
+    if (image[sr][sc] == newColor) {
+      return image;
+    }
+    queue.add(new int[] {sr, sc});
+    while (!queue.isEmpty()) {
+      final int[] curPos = queue.remove();
+      image[curPos[0]][curPos[1]] = newColor;
+      for (final int[] dir : DIRS) {
+        final int newRow = dir[0] + curPos[0], newCol = dir[1] + curPos[1];
+        if (newRow < rows && newRow >= 0 && newCol < cols && newCol >= 0 && image[newRow][newCol]
+            == replacedColor) {
+          queue.add(new int[] {newRow, newCol});
+        }
+      }
+    }
+    return image;
+  }
+}
