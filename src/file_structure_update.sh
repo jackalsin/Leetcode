2d55f66be32fccf49fd7b0a4c0acd174df965d4a
zhiwei.xin <zhiwei.xin@veritas.com>
2018-02-09 05:46:37 -0800

    Update file_structure_update.sh

--- src/file_structure_update.sh
+++ src/file_structure_update.sh
@@ -3,4 +3,6 @@ function update_directory_txt {
     find main -type d > directory.txt
 }
 
-update_directory_txt
+function update_directory {
+    while read -r line; do mkdir -p "$line" ; done < directory.txt
+}
