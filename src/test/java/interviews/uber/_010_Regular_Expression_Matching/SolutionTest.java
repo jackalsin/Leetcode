39875410ec638c6778907992571484646109c1b4
zhiwei.xin <zhiwei.xin@veritas.com>
2018-02-09 15:03:42 -0800

    Finish 010

---
+++ src/test/java/interviews/uber/_010_Regular_Expression_Matching/SolutionTest.java
@@ -0,0 +1,58 @@
+package interviews.uber._010_Regular_Expression_Matching;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SolutionTest {
+
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  public void isMatchWithEmptyString() {
+    assertTrue(solution.isMatch("", ""));
+  }
+
+  @Test
+  void isMatchWithSameString() {
+    assertTrue(solution.isMatch("abc", "abc"));
+  }
+
+  @Test
+  void isMatchWithAsteriskZeroLength() {
+    assertTrue(solution.isMatch("abc", "a*bc"));
+  }
+
+  @Test
+  void isMatchWithAsteriskMoreThanOneLength() {
+    assertTrue(solution.isMatch("aaabc", "a*bc"));
+  }
+
+  @Test
+  void isMatchWithDot() {
+    assertTrue(solution.isMatch("aabc", ".*bc"));
+  }
+
+  @Test
+  void isMatchWithSingleDot() {
+    assertTrue(solution.isMatch("aebc", "a.bc"));
+  }
+
+  @Test
+  void isMatchCaseOne() {
+    assertTrue(solution.isMatch("aaa", "ab*ac*a"));
+
+  }
+
+  @Test
+  void isMatchCaseTwo() {
+    assertTrue(solution.isMatch("aaa", "ab*a*c*a"));
+  }
+}
