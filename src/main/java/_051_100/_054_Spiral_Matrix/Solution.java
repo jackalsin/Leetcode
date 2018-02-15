c7ca4a23a106fc9d14f78604a391f4d9c3085234
jackalsin <jackal.sin@hotmail.com>
2018-02-15 15:26:18 +0800

    Finish 054

--- src/main/java/_051_100/_054_Spiral_Matrix/Solution.java
+++ src/main/java/_051_100/_054_Spiral_Matrix/Solution.java
@@ -39,9 +39,9 @@ public class Solution {
     return  result;
   }
 
-  public static void main(String[] args) {
-    System.out.println(3/2); // 1
-    System.out.println(5/2); // 2
-    System.out.println(-5/2); // -2
-  }
+//  public static void main(String[] args) {
+//    System.out.println(3/2); // 1
+//    System.out.println(5/2); // 2
+//    System.out.println(-5/2); // -2
+//  }
 }
---
+++ src/test/java/interviews/uber/_054_Spiral_Matrix/SolutionTest.java
@@ -0,0 +1,69 @@
+package interviews.uber._054_Spiral_Matrix;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import utils.TwoDimensionArray;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/15/2018.
+ */
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  void testEmpty() throws Exception {
+    assertEquals(Collections.emptyList(), solution.spiralOrder(new int[][] {}));
+  }
+
+  @Test
+  void testWith3By3() throws Exception {
+    assertEquals(Arrays.asList(1, 2, 3, 6, 9, 8, 7, 4, 5), solution.spiralOrder(get2dBoard(3, 3)));
+  }
+
+  @Test
+  void testWith5by5() throws Exception {
+    assertEquals(Arrays.asList(1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9,
+        14, 19, 18, 17, 12,
+        13), solution.spiralOrder(get2dBoard(5, 5)));
+  }
+
+  @Test
+  public void testWith4by4() throws Exception {
+    assertEquals(Arrays.asList(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10), solution
+        .spiralOrder(get2dBoard(4, 4)));
+  }
+
+  @Test
+  public void testWith1by2() throws Exception {
+    assertEquals(Arrays.asList(2, 3), solution.spiralOrder(new int[][] {{2, 3}}));
+  }
+
+  @Test
+  public void testWith3by1() throws Exception {
+    assertEquals(Arrays.asList(1, 2, 3), solution.spiralOrder(new int[][] {{1}, {2}, {3}}));
+  }
+
+  private int[][] get2dBoard(int rows, int cols) {
+    int[][] result = new int[rows][cols];
+    for (int row = 0; row < rows; row++) {
+      for (int col = 0; col < cols; col++) {
+        result[row][col] = row * cols + col + 1;
+      }
+    }
+    TwoDimensionArray.display2DimensionArray(result);
+    return result;
+  }
+
+}
