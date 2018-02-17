ee4a34f05f3d26945002b5724f6bd4a80e48f9fe
zhiwei.xin <zhiwei.xin@veritas.com>
2018-02-16 19:20:27 -0800

    Finish 078

--- src/main/java/interviews/uber/_076_Minimum_Window_Substring/Solution.java
+++ src/main/java/interviews/uber/_076_Minimum_Window_Substring/Solution.java
@@ -5,36 +5,45 @@ package interviews.uber._076_Minimum_Window_Substring;
  * @version 1.0 on 2/15/2018.
  */
 public class Solution {
-  private static final int N = 26;
-  private static final char BASE = 'A';
+  private static final int N = 256;
 
   /**
-   * TODO: Do it again
+   * Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+   * <p>
+   * For example,
+   * S = "ADOBECODEBANC"
+   * T = "ABC"
+   * Minimum window is "BANC".
+   * <p>
+   * Note:
+   * If there is no such window in S that covers all characters in T, return the empty string "".
+   * <p>
+   * If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
+   *
+   * @param s
+   * @param t
+   * @return
    */
   public String minWindow(String s, String t) {
     final int[] charCounts = new int[N];
     for (final char chr : t.toCharArray()) {
-      charCounts[chr - BASE]++;
+      charCounts[chr]++;
     }
-    int minLen = Integer.MAX_VALUE, minStart = 0, start = 0, end = 0, count = t.length();
-    while (end < s.length()) {
-      if (charCounts[s.charAt(end++) - BASE]-- > 0) {
-//        charCounts[s.charAt(end) - BASE]--;
+    int minLen = Integer.MAX_VALUE, start = 0, end = 0, minStart = 0, count = t.length();
+    for (; end < s.length(); end++) {
+      if (charCounts[s.charAt(end)]-- > 0) {
         count--;
       }
-
       while (count == 0) {
-        if (end - start < minLen) {
+        if (end - start + 1 < minLen) {
+          minLen = end - start + 1;
           minStart = start;
-          minLen = end - start;
         }
-        if (charCounts[s.charAt(start++) - BASE]++ == 0) {
-          count++;
-        }
-//        charCounts[s.charAt(start) - BASE]++;
-//        start++;
+        /* 非<code>t</code>里面的char，在++之前，会始终保持小于0
+         * 而 <code>== 0 </code>只可能是刚刚好是那个miss的char
+         */
+        if (charCounts[(int) s.charAt(start++)]++ == 0) count++;
       }
-//      end++;
     }
     return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
   }
--- src/test/java/interviews/uber/_076_Minimum_Window_Substring/SolutionTest.java
+++ src/test/java/interviews/uber/_076_Minimum_Window_Substring/SolutionTest.java
@@ -13,12 +13,19 @@ class SolutionTest {
   private Solution solution;
 
   @BeforeEach
-  void setUp() throws Exception {
+  void setUp() {
     solution = new Solution();
   }
 
+  /**
+   * |  A | D | O | B | E | C | O  |  D  |  E  |  B  |  A  |  N  |  C  |
+   * |  0 | 1 | 2 | 3 | 4 | 5 | 6  |  7  |  8  |  9  |  10 |  11 |  12 |
+   *
+   * @throws Exception
+   */
   @Test
-  void testWithOnlineCase() throws Exception {
-    assertEquals("BANC", solution.minWindow("ADOBECODEBANC", "ABC"));
+  void testWithOnlineCase() {
+    assertEquals("BANC", solution.minWindow(
+        "ADOBECODEBANC", "ABC"));
   }
 }
