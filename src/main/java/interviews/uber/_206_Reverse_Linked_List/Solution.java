79c74b34fa2ac91aa7f54376576ddc6fcd78cdc2
zhiwei.xin <zhiwei.xin@veritas.com>
2018-02-20 00:29:37 -0800

    Finish 206

---
+++ src/main/java/interviews/uber/_206_Reverse_Linked_List/Solution.java
@@ -0,0 +1,28 @@
+package interviews.uber._206_Reverse_Linked_List;
+
+import utils.ListNode;
+
+public class Solution {
+  /**
+   * Time complexity O(N)
+   *
+   * @param head
+   * @return
+   */
+  public ListNode reverseList(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+    final ListNode dummyHead = new ListNode(233);
+    dummyHead.next = head;
+    ListNode cur = dummyHead.next.next, tail = dummyHead.next;
+    while (cur != null) {
+      final ListNode newNext = dummyHead.next;
+      tail.next = cur.next;
+      dummyHead.next = cur;
+      cur.next = newNext;
+      cur = tail.next;
+    }
+    return dummyHead.next;
+  }
+}
--- src/test/java/interviews/uber/_206_Reverse_Linked_List/SolutionTest.java
+++ src/test/java/interviews/uber/_206_Reverse_Linked_List/SolutionTest.java
@@ -1,39 +1,40 @@
-package _201_250._206_Reverse_Linked_List;
+package interviews.uber._206_Reverse_Linked_List;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import utils.ListNode;
 import utils.ListNodes;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
 
-  @Before
-  public void setUp() throws Exception {
+  @BeforeEach
+  void setUp() {
     solution = new Solution();
   }
 
   @Test
-  public void testOneElement() {
+  void testOneElement() {
     ListNode input = ListNodes.getListOfNodes(new int[]{1});
     ListNode expected = ListNodes.getListOfNodes(new int[]{1});
     assertEquals(expected, solution.reverseList(input));
   }
 
   @Test
-  public void testTwoElements() {
+  void testTwoElements() {
     ListNode input = ListNodes.getListOfNodes(new int[]{1, 2});
     ListNode expected = ListNodes.getListOfNodes(new int[]{2, 1});
     assertEquals(expected, solution.reverseList(input));
   }
 
   @Test
-  public void testThreeElements() {
+  void testThreeElements() {
     ListNode input = ListNodes.getListOfNodes(new int[]{1, 2, 3});
     ListNode expected = ListNodes.getListOfNodes(new int[]{3, 2, 1});
     assertEquals(expected, solution.reverseList(input));
   }
+
 
 }
