ad4d21275613d1e0caa6e061568db7f5ec1ea6d6
zhiwei.xin <zhiwei.xin@veritas.com>
2018-02-20 00:20:16 -0800

    Finish 202

---
+++ src/main/java/interviews/uber/_202_Happy_Number/HashSetSolution.java
@@ -0,0 +1,33 @@
+package interviews.uber._202_Happy_Number;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class HashSetSolution implements Solution {
+  private final Set<Long> visited = new HashSet<>();
+
+  public boolean isHappy(int n) {
+    if (n <= 0) {
+      return false;
+    }
+    return isHappyHelper(n);
+  }
+
+  private boolean isHappyHelper(long n) {
+    if (n == 1) {
+      return true;
+    }
+    long res = 0;
+
+    while (n != 0) {
+      int digit = (int) (n % 10);
+      res += digit * digit;
+      n /= 10;
+    }
+    if (visited.contains(res)) {
+      return false;
+    }
+    visited.add(res);
+    return isHappyHelper(res);
+  }
+}
---
+++ src/main/java/interviews/uber/_202_Happy_Number/Solution.java
@@ -0,0 +1,6 @@
+package interviews.uber._202_Happy_Number;
+
+public interface Solution {
+
+  boolean isHappy(int n);
+}
