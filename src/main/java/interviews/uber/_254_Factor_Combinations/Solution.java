85b4ac8de5f085e30d108f349db5d531c06eba3f
zhiwei.xin <zhiwei.xin@veritas.com>
2018-02-21 02:47:27 -0800

    Add another cleaner solution for 254

--- src/main/java/interviews/uber/_254_Factor_Combinations/Solution.java
+++ src/main/java/interviews/uber/_254_Factor_Combinations/Solution.java
@@ -5,20 +5,27 @@ import java.util.List;
 
 public class Solution {
 
+  /**
+   * Time Complexity : O(N)
+   * O(n^(1/2) x n^(1/4) x ... x 1) 约等于 O（N）
+   *
+   * @param n
+   * @return
+   */
   public List<List<Integer>> getFactors(int n) {
     final List<List<Integer>> result = new ArrayList<>();
-    getFactors(result, new ArrayList<>(), n, 2);
+    getFactors(result, new ArrayList<>(), n, 1);
     return result;
   }
 
   private void getFactors(List<List<Integer>> result, final List<Integer> curPath, int n, int start) {
-    if (n <= 1) {
-      if (curPath.size() > 1) {
-        result.add(new ArrayList<>(curPath));
-      }
-      return;
+    if (start > 1) {
+      final List<Integer> toAdd = new ArrayList<>(curPath);
+      toAdd.add(n);
+      result.add(toAdd);
     }
-    for (int i = start; i <= n; i++) {
+
+    for (int i = Math.max(start, 2); i * i <= n; i++) {
       if (n % i == 0) {
         curPath.add(i);
         getFactors(result, curPath, n / i, i);
