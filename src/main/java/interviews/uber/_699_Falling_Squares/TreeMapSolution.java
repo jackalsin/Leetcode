0e1edf6ead1f9ac91b14e7fb048962c57a12b605
zhiwei.xin <zhiwei.xin@veritas.com>
2018-02-27 15:13:59 +0800

    Finish 699

--- src/main/java/interviews/uber/_699_Falling_Squares/TreeMapSolution.java
+++ src/main/java/interviews/uber/_699_Falling_Squares/TreeMapSolution.java
@@ -2,52 +2,48 @@ package interviews.uber._699_Falling_Squares;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.TreeMap;
+import java.util.TreeSet;
 
 public final class TreeMapSolution implements Solution {
+  private static final int DUMMY_VALUE = 23333;
 
   @Override
   public List<Integer> fallingSquares(int[][] positions) {
-    final TreeMap<int[], Integer> map = new TreeMap<>((x, y) -> {
-      return Integer.compare(x[0], y[0]);
-    });
-    map.put(new int[]{0, 1_000_000_000}, 0);
+    final TreeSet<Interval> treeSet = new TreeSet<>((x1, x2) -> Integer.compare(x1.start, x2.start));
+    treeSet.add(new Interval(0, 1_000_000_000, 0));
     final List<Integer> result = new ArrayList<>();
     int maxHeight = 0;
     for (final int[] pos : positions) {
-      final int left = pos[0], len = pos[1];
-      final int[] newKey = new int[]{left, left + len - 1};
-      final Map.Entry<int[], Integer> startEntry = map.floorEntry(newKey);
-      if (startEntry == null) {
-        // shouldn't arrive here
-        map.put(newKey, len);
-        maxHeight = Math.max(maxHeight, len);
-      } else {
-        map.remove(startEntry.getKey());
-        // startEntry.end <= len + left - 1
-        if (newKey[0] > startEntry.getKey()[0]) {
-          map.put(new int[]{startEntry.getKey()[0], newKey[0] - 1}, startEntry.getValue());
-          map.put(new int[]{newKey[0], startEntry.getKey()[1]}, startEntry.getValue());
-        }
-        Map.Entry<int[], Integer> curEntry = map.ceilingEntry(new int[]{newKey[0], startEntry.getKey()[1]});
-        int curHeight = startEntry.getValue();
-        while (curEntry != null && curEntry.getKey()[0] <= newKey[1]) { // reconsider
-          map.remove(curEntry.getKey());
-          curHeight = Math.max(curHeight, curEntry.getValue());
-          curEntry = map.ceilingEntry(new int[]{newKey[0]});
-        }
-
-        if (curEntry != null && curEntry.getKey()[1] < curEntry.getKey()[1]) {
-          map.put(new int[]{curEntry.getKey()[1] + 1, curEntry.getKey()[1]}, curEntry.getValue());
-        }
-        curHeight += len;
-        maxHeight = Math.max(maxHeight, curHeight);
-        map.put(newKey, curHeight);
+      final int left = pos[0], right = pos[0] + pos[1] - 1;
+      final Interval startInterval = treeSet.floor(new Interval(left, right, -1));
+      // using floor, which includes equals, startEntry should not be null
+      assert startInterval != null;
+      treeSet.remove(startInterval);
+      if (startInterval.start < left) {
+        treeSet.add(new Interval(startInterval.start, left - 1, startInterval.height));
       }
+      treeSet.add(new Interval(left, startInterval.end, startInterval.height));
+      int curHeight = startInterval.height;
+      Interval curInterval = treeSet.ceiling(new Interval(left, DUMMY_VALUE, DUMMY_VALUE));
+      assert curInterval != null;
+      while (curInterval != null && curInterval.end <= right) { // todo: consider equal
+        treeSet.remove(curInterval);
+        curHeight = Math.max(curHeight, curInterval.height);
+        curInterval = treeSet.ceiling(new Interval(left, DUMMY_VALUE, DUMMY_VALUE));
+      }
+
+      if (curInterval != null && right >= curInterval.start && right < curInterval.end) {
+        treeSet.remove(curInterval);
+        curHeight = Math.max(curHeight, curInterval.height);
+        treeSet.add(new Interval(right + 1, curInterval.end, curInterval.height));
+      }
+
+      // add new interval
+      curHeight += pos[1];
+      maxHeight = Math.max(curHeight, maxHeight);
       result.add(maxHeight);
-      // add assert checking the continuous interval.
+      treeSet.add(new Interval(left, right, curHeight));
     }
 
     return result;
@@ -62,7 +58,6 @@ public final class TreeMapSolution implements Solution {
       this.height = height;
     }
 
-
     @Override
     public boolean equals(Object o) {
       if (this == o) return true;
@@ -76,6 +71,15 @@ public final class TreeMapSolution implements Solution {
     @Override
     public int hashCode() {
       return Objects.hash(start, end, height);
+    }
+
+    @Override
+    public String toString() {
+      return "Interval{" +
+          "start=" + start +
+          ", end=" + end +
+          ", height=" + height +
+          '}';
     }
   }
 }
--- src/test/java/interviews/uber/_699_Falling_Squares/SolutionTest.java
+++ src/test/java/interviews/uber/_699_Falling_Squares/SolutionTest.java
@@ -25,9 +25,25 @@ class SolutionTest {
   }
 
   @Test
+  void testOnlineCase2() {
+    final int[][] input = {{100, 100}, {200, 100}};
+    final List<Integer> expected = List.of(100, 100);
+    final List<Integer> actual = solution.fallingSquares(input);
+    assertEquals(expected, actual);
+  }
+
+  @Test
   void testFailedCase1() {
     final int[][] input = {{2, 1}, {2, 9}, {1, 8}};
     final List<Integer> expected = List.of(1, 10, 18);
+    final List<Integer> actual = solution.fallingSquares(input);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void testFailedCase2() {
+    final int[][] input = {{6, 1}, {9, 2}, {2, 4}};
+    final List<Integer> expected = List.of(1, 2, 4);
     final List<Integer> actual = solution.fallingSquares(input);
     assertEquals(expected, actual);
   }
