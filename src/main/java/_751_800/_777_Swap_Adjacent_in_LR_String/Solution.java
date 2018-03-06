45f75bf982151e2e3eef9b1081313a03ec0720ca
jackalsin <jackal.sin@hotmail.com>
2018-03-06 05:49:26 -0800

    Finish 777

---
+++ src/main/java/_751_800/_777_Swap_Adjacent_in_LR_String/Solution.java
@@ -0,0 +1,43 @@
+package _751_800._777_Swap_Adjacent_in_LR_String;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/5/2018.
+ */
+public class Solution {
+  private static final String X = "X";
+  private static final char X_CHAR = 'X', L = 'L', R = 'R';
+
+  /**
+   * Time Complexity: O(N)
+   */
+  public boolean canTransform(String start, String end) {
+    if (!start.replace(X, "").equals(end.replace(X, ""))) {
+      return false;
+    }
+    for (int p1 = 0, p2 = 0; p1 <= start.length() && p2 <= end.length(); p1++, p2++) {
+      while (p1 < start.length() && start.charAt(p1) == X_CHAR) {
+        p1++;
+      }
+      while (p2 < end.length() && end.charAt(p2) == X_CHAR) {
+        p2++;
+      }
+      if (p1 == start.length() && p2 == end.length()) {
+        return true;
+      }
+      if (p1 == start.length() || p2 == end.length()) {
+        return false;
+      }
+
+      // L can be only moved to left, so p1 should be greater than or equals to p2
+      if (start.charAt(p1) == L && p1 < p2) {
+        return false;
+      }
+      if (end.charAt(p2) == R && p2 < p1) {
+        return false;
+      }
+
+    }
+    return false;
+  }
+}
---
+++ src/test/java/_751_800/_777_Swap_Adjacent_in_LR_String/SolutionTest.java
@@ -0,0 +1,33 @@
+package _751_800._777_Swap_Adjacent_in_LR_String;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/6/2018.
+ */
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final String start = "RXXLRXRXL", end = "XRLXXRRLX";
+    assertTrue(solution.canTransform(start, end));
+  }
+
+  @Test
+  void testFailedCase() {
+    final String start = "XXRXXLXXXX",
+        end = "XXXXRXXLXX";
+    assertFalse(solution.canTransform(start, end));
+  }
+}
