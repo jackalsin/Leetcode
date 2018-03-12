fe435ccc6c9c6d6d034d73b2fcca0e1f941175c7
jackalsin <jackal.sin@hotmail.com>
2018-03-12 01:38:47 -0700

    Add 621

---
+++ src/main/java/_601_650/_621_Task_Scheduler/MathSolution.java
@@ -0,0 +1,29 @@
+package _601_650._621_Task_Scheduler;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/12/2018.
+ */
+public class MathSolution {
+  /**
+   * https://leetcode.com/problems/task-scheduler/discuss/104496/concise-Java-Solution-O(N)-time-O(26)-space
+   */
+  public int leastInterval(char[] tasks, int n) {
+    final int[] counts = new int[26];
+    for (final char task : tasks) {
+      counts[task - 'A']++;
+    }
+    int max = 0, maxCount = 0; // how many max
+    for (final int count : counts) {
+      if (max == count) {
+        maxCount++;
+      } else if (max < count) {
+        max = count;
+        maxCount = 1;
+      }
+    }
+    /*首先最大值满足塞满不同的truck*， 然后剩下的再扔maxCount的最大值的字符*/
+    return Math.max(tasks.length, (max - 1) * (n + 1) + maxCount);
+  }
+}
+
---
+++ src/main/java/interviews/google/_621_Task_Scheduler/Solution.java
@@ -0,0 +1,9 @@
+package interviews.google._621_Task_Scheduler;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/11/2018.
+ */
+public class Solution {
+  // TODO: unimplemented
+}
