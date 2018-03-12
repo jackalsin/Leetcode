8d2b7e8ae589260863ae40a511d1c9a0e959bcea
jackalsin <jackal.sin@hotmail.com>
2018-03-12 09:18:14 -0700

    Add 337

---
+++ src/main/java/interviews/google/_337_House_Robber_III/Solution.java
@@ -0,0 +1,31 @@
+package interviews.google._337_House_Robber_III;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/12/2018.
+ */
+public class Solution {
+  // TOOD： 再来一道 Union Find
+  public int rob(TreeNode root) {
+    final int[] res = robHelper(root);
+    return Math.max(res[0], res[1]);
+  }
+
+  /**
+   * @return {maxNotUsingThis, maxUsingThis}
+   */
+  private int[] robHelper(TreeNode root) {
+    if (root == null) {
+      return new int[2];
+    }
+
+    final int[] left = robHelper(root.left),
+        right = robHelper(root.right), res = new int[2];
+
+    res[0] = Math.max(left[0], left[1]) + Math.max(right[1], right[0]);
+    res[1] = root.val + left[0] + right[0];
+    return res;
+  }
+}
