7baf05f9d006ae55679979805d3f055a7793fe66
zhiwei.xin <zhiwei.xin@veritas.com>
2018-03-15 19:58:25 -0700

    Finish 341

--- src/main/java/interviews/uber/_341_Flatten_Nested_List_Iterator/NestedIterator.java
+++ src/main/java/interviews/uber/_341_Flatten_Nested_List_Iterator/NestedIterator.java
@@ -2,46 +2,45 @@ package interviews.uber._341_Flatten_Nested_List_Iterator;
 
 import utils.nestedInteger.NestedInteger;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 /**
+ * Directly flatten integer performance:
+ *
+ * <p>44 / 44 test cases passed. Status: Accepted Runtime: 9 ms
+ *
  * @author jacka
  * @version 1.0 on 3/13/2018.
  */
 public final class NestedIterator implements Iterator<Integer> {
-  private int i = 0;
-  private final List<Integer> list = new ArrayList<>();
+  private final Stack<NestedInteger> stack = new Stack<>();
 
   public NestedIterator(List<NestedInteger> nestedList) {
-    for (final NestedInteger nestInteger : nestedList) {
-      list.addAll(nestedInteger(nestInteger));
+    for (int i = nestedList.size() - 1; i >= 0; i--) {
+      stack.push(nestedList.get(i));
     }
-  }
-
-  private List<Integer> nestedInteger(final NestedInteger nestedInt) {
-    final List<Integer> result = new ArrayList<>();
-    if (nestedInt.isInteger()) {
-      result.add(nestedInt.getInteger());
-    } else {
-      final List<NestedInteger> list = nestedInt.getList();
-      for (final NestedInteger nI : list) {
-        result.addAll(nestedInteger(nI));
-      }
-    }
-    return result;
   }
 
   @Override
   public Integer next() {
-    int res = list.get(i);
-    i++;
-    return res;
+    return stack.pop().getInteger();
   }
 
   @Override
   public boolean hasNext() {
-    return i != list.size();
+    while (!stack.isEmpty()) {
+      final NestedInteger resultNi = stack.peek();
+      if (resultNi.isInteger()) {
+        return true;
+      }
+      stack.pop();
+      List<NestedInteger> list = resultNi.getList();
+      for (int i = list.size() - 1; i >= 0; i--) {
+        stack.add(list.get(i));
+      }
+    }
+    return false;
   }
 }
