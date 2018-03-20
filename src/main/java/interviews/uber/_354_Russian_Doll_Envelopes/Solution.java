59d3148003171798555725f7d0e75ae062e0644e
jackalsin <jackal.sin@hotmail.com>
2018-03-19 22:07:54 -0700

    Add problem set

--- src/main/java/interviews/uber/_354_Russian_Doll_Envelopes/Solution.java
+++ src/main/java/interviews/uber/_354_Russian_Doll_Envelopes/Solution.java
@@ -8,26 +8,41 @@ import java.util.Comparator;
  * @version 1.0 on 3/15/2018.
  */
 public class Solution { // TODO:
+  /**
+   * 85 / 85 test cases passed.
+   * Status: Accepted
+   * Runtime: 22 ms
+   */
   public int maxEnvelopes(int[][] envelopes) {
     Arrays.sort(envelopes, new Comparator<>() {
       @Override
       public int compare(final int[] x, final int[] y) {
         int cmp = Integer.compare(x[0], y[0]);
         if (cmp == 0) {
-          return Integer.compare(x[1], y[1]);
+          return Integer.compare(y[1], x[1]);
         }
         return cmp;
       }
     });
 
-    int count = 0, prevWidth = 0, prevHeight = 0;
-    for (final int[] e : envelopes) {
-      if (e[0] > prevWidth && e[1] > prevHeight) {
-        count++;
-        prevWidth = e[0];
-        prevHeight = e[1];
+    final int[] tails = new int[envelopes.length];
+    if (envelopes.length == 0) {
+      return 0;
+    }
+    int right = 0;
+    tails[0] = envelopes[0][1];
+    for (int i = 1, envelopesLength = envelopes.length; i < envelopesLength; i++) {
+      int[] e = envelopes[i];
+      int insertPoint = Arrays.binarySearch(tails, 0, right + 1, e[1]);
+      if (insertPoint < 0) {
+        insertPoint = -(insertPoint + 1);
+      }
+
+      tails[insertPoint] = e[1];
+      if (insertPoint == right + 1) {
+        right++;
       }
     }
-    return count;
+    return right + 1;
   }
 }
---
+++ src/test/java/interviews/uber/_354_Russian_Doll_Envelopes/SolutionTest.java
@@ -0,0 +1,38 @@
+package interviews.uber._354_Russian_Doll_Envelopes;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/19/2018.
+ */
+class SolutionTest {
+
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase() throws Exception {
+    final int[][] input = {{5, 4}, {6, 4}, {6, 7}, {2, 3}};
+    assertEquals(3, solution.maxEnvelopes(input));
+  }
+
+  @Test
+  void testEmpty() throws Exception {
+    final int[][] input = {};
+    assertEquals(0, solution.maxEnvelopes(input));
+  }
+
+  @Test
+  void testOnlineCase2() throws Exception {
+    final int[][] input = {{5, 4}, {6, 5}, {6, 7}, {2, 3}, {7, 6}};
+    assertEquals(4, solution.maxEnvelopes(input));
+  }
+}
