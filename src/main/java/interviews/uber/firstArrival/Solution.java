9a99b17b0a426a4b117861220a9c5bbf399cf030
jackalsin <jackal.sin@hotmail.com>
2018-03-23 18:42:53 -0700

    Add problem Set

---
+++ src/main/java/interviews/uber/firstArrival/Solution.java
@@ -0,0 +1,24 @@
+package interviews.uber.firstArrival;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/23/2018.
+ */
+public class Solution {
+//Todo: unimplemented
+// http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=319414&extra=page%3D1%26filter%3Dsortid%26sortid
+// %3D311%26searchoption%5B3089%5D%5Bvalue%5D%5B2%5D%3D2%26searchoption%5B3089%5D%5Btype%5D%3Dcheckbox%26searchoption
+// %5B3090%5D%5Bvalue%5D%3D1%26searchoption%5B3090%5D%5Btype%5D%3Dradio%26searchoption%5B3046%5D%5Bvalue%5D%3D22
+// %26searchoption%5B3046%5D%5Btype%5D%3Dradio%26sortid%3D311
+// 现在有一大堆数据，每个数据是用户访问吴波官网的时间
+//  但是数据现在有问题，每条数据只有一个range，没有具体的时间
+//  比如，
+//  {用户A, 3点-4点}
+//  {用户B, 3点30-4点30}
+//  {用户C, 5点-6点}
+//  如果我要求返回所有第1个访问的客人，那么需要返回A和B，因为A和B有交集，所以A和B都有可能是第1位访客
+//  与此同时，A和B都有可能是第2个访问的客人
+//  这个数据结构并且需要支持高效的add和remove。因为黑车公司的司机随时都在动
+
+
+}
