0b02b81e1748eface15065e5076f35c2939b4684
zhiwei.xin <zhiwei.xin@veritas.com>
2018-03-27 18:39:30 -0700

    Finish 381

--- src/main/java/interviews/uber/_381_Insert_Delete_GetRandom_O_1_Duplicates_allowed/RandomizedCollection.java
+++ src/main/java/interviews/uber/_381_Insert_Delete_GetRandom_O_1_Duplicates_allowed/RandomizedCollection.java
@@ -1,66 +1,49 @@
 package interviews.uber._381_Insert_Delete_GetRandom_O_1_Duplicates_allowed;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
-public class RandomizedCollection { //TODO: 再来一遍
-  private final Map<Integer, Set<Integer>> valueToIndex = new HashMap<>();
+public class RandomizedCollection {
+  private final Map<Integer, Set<Integer>> valToIndex = new HashMap<>();
   private final List<Integer> values = new ArrayList<>();
   private final Random random = new Random();
-
-  /**
-   * Initialize your data structure here.
-   */
-  public RandomizedCollection() {
-
-  }
 
   /**
    * Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
    */
   public boolean insert(int val) {
-    if (valueToIndex.containsKey(val)) {
-      final Set<Integer> indexList = valueToIndex.get(val);
-      indexList.add(values.size());
-      values.add(val);
-      return false;
-    } else {
-      final Set<Integer> indexList = new LinkedHashSet<>();
-      valueToIndex.put(val, indexList);
-      indexList.add(values.size());
-      values.add(val);
-      return true;
-    }
+    final boolean res = !valToIndex.containsKey(val);
+    final Set<Integer> valIndex = valToIndex.getOrDefault(val, new HashSet<>());
+    valIndex.add(values.size());
+    values.add(val);
+    valToIndex.put(val, valIndex);
+    return res;
   }
 
   /**
    * Removes a value from the collection. Returns true if the collection contained the specified element.
-   * Only remove one of them if there are multiple.
    */
   public boolean remove(int val) {
-    if (valueToIndex.containsKey(val)) {
-      final Set<Integer> valIndexList = valueToIndex.get(val);
-      final int valIndex = valIndexList.iterator().next();
-      valIndexList.remove(valIndex);
+    if (valToIndex.containsKey(val)) {
+      final Set<Integer> valIndexSet = valToIndex.get(val);
+      final int valIndex = valIndexSet.iterator().next(), lastValIndex = values.size() - 1,
+          lastVal = values.get(values.size() - 1);
+      if (val == lastVal) {
+        valIndexSet.remove(lastValIndex);
+        values.remove(lastValIndex);
+      } else {
+        final Set<Integer> lastValIndexSet = valToIndex.get(lastVal);
+        // delete from index set
+        lastValIndexSet.remove(lastValIndex);
+        lastValIndexSet.add(valIndex);
+        valIndexSet.remove(valIndex);
 
-      if (valIndex != values.size() - 1) {
-        final int lastVal = values.get(values.size() - 1);
-        // update last value index
-        final Set<Integer> lastValIndexList = valueToIndex.get(lastVal);
-        lastValIndexList.remove(values.size() - 1);
-        lastValIndexList.add(valIndex);
-
-        // update values
+        // delete form value list
+        values.remove(lastValIndex);
         values.set(valIndex, lastVal);
       }
-      values.remove(values.size() - 1);
-      if (valIndexList.isEmpty()) {
-        valueToIndex.remove(val);
+
+      if (valIndexSet.isEmpty()) {
+        valToIndex.remove(val);
       }
       return true;
     } else {
@@ -72,6 +55,7 @@ public class RandomizedCollection { //TODO: 再来一遍
    * Get a random element from the collection.
    */
   public int getRandom() {
-    return values.get(random.nextInt(values.size()));
+    final int i = random.nextInt(values.size());
+    return values.get(i);
   }
 }
