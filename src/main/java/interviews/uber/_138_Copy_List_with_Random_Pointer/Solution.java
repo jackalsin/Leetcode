60917382325b06d5d908f60dc7352187575cbd7b
zhiwei.xin <zhiwei.xin@veritas.com>
2018-03-28 21:09:08 -0700

    Finish 138

--- src/main/java/interviews/uber/_138_Copy_List_with_Random_Pointer/Solution.java
+++ src/main/java/interviews/uber/_138_Copy_List_with_Random_Pointer/Solution.java
@@ -1,11 +1,19 @@
 package interviews.uber._138_Copy_List_with_Random_Pointer;
 
+import utils.RandomListNode;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import utils.RandomListNode;
-
-public class Solution { // TODO;
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ * int label;
+ * RandomListNode next, random;
+ * RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
   private final Map<RandomListNode, RandomListNode> oldToNew = new HashMap<>();
 
   public RandomListNode copyRandomList(RandomListNode head) {
@@ -15,9 +23,8 @@ public class Solution { // TODO;
     if (oldToNew.containsKey(head)) {
       return oldToNew.get(head);
     }
-    RandomListNode newHead = new RandomListNode(head.label);
+    final RandomListNode newHead = new RandomListNode(head.label);
     oldToNew.put(head, newHead);
-
     newHead.next = copyRandomList(head.next);
     newHead.random = copyRandomList(head.random);
     return newHead;
