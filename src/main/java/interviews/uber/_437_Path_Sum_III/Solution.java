99fad7d00a3fc87e3d40b05c935f9838903a9d8e
zhiwei.xin <zhiwei.xin@veritas.com>
2018-03-28 20:33:10 -0700

    Finish 437

--- src/main/java/interviews/uber/_437_Path_Sum_III/Solution.java
+++ src/main/java/interviews/uber/_437_Path_Sum_III/Solution.java
@@ -1,9 +1,43 @@
 package interviews.uber._437_Path_Sum_III;
 
+import utils.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author jacka
  * @version 1.0 on 3/23/2018.
  */
 public class Solution {
-// todo:
+  private final Map<Integer, Integer> preSumCount = new HashMap<>();
+
+  /**
+   * Time Complexity: O(N)
+   * 126 / 126 test cases passed.
+   * Status: Accepted
+   * Runtime: 18 ms
+   *
+   * @param root
+   * @param sum
+   * @return
+   */
+  public int pathSum(TreeNode root, int sum) {
+    preSumCount.put(0, 1);
+    return pathSum(root, sum, 0);
+  }
+
+  private int pathSum(final TreeNode root, final int target, int curSum) {
+    if (root == null) {
+      return 0;
+    }
+    curSum += root.val;
+    int count = preSumCount.getOrDefault(curSum - target, 0);
+
+    preSumCount.put(curSum, preSumCount.getOrDefault(curSum, 0) + 1);
+    count += pathSum(root.left, target, curSum);
+    count += pathSum(root.right, target, curSum);
+    preSumCount.put(curSum, preSumCount.getOrDefault(curSum, 0) - 1);
+    return count;
+  }
 }
