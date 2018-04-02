0205c28c12986b34ac6c0fc657cd174b29a6ad7c
jackalsin <jackal.sin@hotmail.com>
2018-04-01 19:12:58 -0700

    Finish 146

--- src/test/java/interviews/uber/_418_Sentence_Screen_Fitting/SolutionTest.java
+++ src/test/java/interviews/uber/_418_Sentence_Screen_Fitting/SolutionTest.java
@@ -1,45 +1,48 @@
-package _401_450._418_Sentence_Screen_Fitting;
+package interviews.uber._418_Sentence_Screen_Fitting;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * @author jacka
+ * @version 1.0 on 4/1/2018.
+ */
 class SolutionTest {
   private Solution solution;
 
   @BeforeEach
-  void setUp() throws Exception {
-    solution = new OptimizedSolution();
+  void setUp() {
+    solution = new Solution();
   }
 
   @Test
-  void testOnlineCase1() throws Exception {
+  void testOnlineCase1() {
     final String[] sentence = {"hello", "world"};
     assertEquals(1, solution.wordsTyping(sentence, 2, 8));
   }
 
   @Test
-  void testOnlineCase2() throws Exception {
+  void testOnlineCase2() {
     final String[] sentence = {"a", "bcd", "e"};
     assertEquals(2, solution.wordsTyping(sentence, 3, 6));
   }
 
   @Test
-  void testOnlineCase3() throws Exception {
+  void testOnlineCase3() {
     final String[] sentence = {"I", "had", "apple", "pie"};
     assertEquals(1, solution.wordsTyping(sentence, 4, 5));
   }
 
   @Test
-  void testFailedCase1() throws Exception {
+  void testFailedCase1() {
     final String[] sentence = {"try", "to", "be", "better"};
     assertEquals(5293333, solution.wordsTyping(sentence, 10000, 9001));
   }
 
   @Test
-  void testFailedCase2() throws Exception {
+  void testFailedCase2() {
     final String[] sentence = {"hello"};
     assertEquals(0, solution.wordsTyping(sentence, 10000, 1));
   }
@@ -47,6 +50,8 @@ class SolutionTest {
   @Test
   void testTLECase() {
     final String[] sentence = {"a", "b", "e"};
-    Assertions.assertEquals(66666666, solution.wordsTyping(sentence, 20000, 20000));
+    assertEquals(66666666, solution.wordsTyping(sentence, 20000, 20000));
   }
+
+
 }
