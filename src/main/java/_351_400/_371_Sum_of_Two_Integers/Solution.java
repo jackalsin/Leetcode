5711b051f873c441e8f4c05cebfbc9f47e3dde24
zhiwei.xin <zhiwei.xin@veritas.com>
2018-04-03 12:57:02 -0700

    Add 371

---
+++ src/main/java/_351_400/_371_Sum_of_Two_Integers/Solution.java
@@ -0,0 +1,12 @@
+package _351_400._371_Sum_of_Two_Integers;
+
+public class Solution {
+  public int getSum(int a, int b) {
+    while (b != 0) {
+      final int carry = (a & b);
+      a = a ^ b;
+      b = carry << 1;
+    }
+    return a;
+  }
+}
---
+++ src/test/java/_351_400/_371_Sum_of_Two_Integers/SolutionTest.java
@@ -0,0 +1,20 @@
+package _351_400._371_Sum_of_Two_Integers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void test1Plus1() {
+    assertEquals(4, solution.getSum(2, 2));
+  }
+}
