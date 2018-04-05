0eda32233d06aa7684a6945efeed11b6d3901b12
zhiwei.xin <zhiwei.xin@veritas.com>
2018-04-04 20:47:12 -0700

    Finish 383

---
+++ src/main/java/_351_400/_383_Ransom_Note/Solution.java
@@ -0,0 +1,19 @@
+package _351_400._383_Ransom_Note;
+
+public class Solution {
+
+  public boolean canConstruct(String ransomNote, String magazine) {
+    final int[] map = new int[128];
+    for (final char chr : magazine.toCharArray()) {
+      map[chr]++;
+    }
+    for (final char chr : ransomNote.toCharArray()) {
+      map[chr]--;
+      if (map[chr] < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
