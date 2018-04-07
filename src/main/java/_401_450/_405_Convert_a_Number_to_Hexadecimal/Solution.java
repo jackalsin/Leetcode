b16c9e99cae0f9d4c64f088cf490de69c9de0502
jackalsin <jackal.sin@hotmail.com>
2018-04-07 16:43:58 -0700

    Finish 405

---
+++ src/main/java/_401_450/_405_Convert_a_Number_to_Hexadecimal/Solution.java
@@ -0,0 +1,29 @@
+package _401_450._405_Convert_a_Number_to_Hexadecimal;
+
+import java.util.Arrays;
+
+public class Solution {
+  private static final char[] map = new char[16];
+
+  static {
+    for (int i = 0; i <= 9; i++) {
+      map[i] = (char) ('0' + i);
+    }
+    for (int i = 10; i <= 15; i++) {
+      map[i] = (char) ('a' + i - 10);
+    }
+    System.out.println(Arrays.toString(map));
+  }
+
+  public String toHex(int num) {
+    if (num == 0) {
+      return "0";
+    }
+    final StringBuilder sb = new StringBuilder();
+    while (num != 0) {
+      sb.append(map[(num & 0b1111)]);
+      num >>>= 4;
+    }
+    return sb.reverse().toString();
+  }
+}
---
+++ src/test/java/_401_450/_405_Convert_a_Number_to_Hexadecimal/SolutionTest.java
@@ -0,0 +1,30 @@
+package _401_450._405_Convert_a_Number_to_Hexadecimal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    assertEquals("1a", solution.toHex(26));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    assertEquals("ffffffff", solution.toHex(-1));
+  }
+
+  @Test
+  void testFailedOn0() {
+    assertEquals("0", solution.toHex(0));
+  }
+}
