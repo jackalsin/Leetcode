c86f9aea0d61557cc2725888269de19980c5e534
zhiwei.xin <zhiwei.xin@veritas.com>
2018-04-15 12:45:25 -0700

    Finish 439

---
+++ src/test/java/_401_450/_439_Ternary_Expression_Parser/SolutionTest.java
@@ -0,0 +1,31 @@
+package _401_450._439_Ternary_Expression_Parser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final String input = "T?2:3", expected = "2";
+    assertEquals(expected, solution.parseTernary(input));
+  }
+  @Test
+  void testOnlineCase2() {
+    final String input = "F?1:T?4:5", expected = "4";
+    assertEquals(expected, solution.parseTernary(input));
+  }
+  @Test
+  void testOnlineCase3() {
+    final String input = "T?T?F:5:3", expected = "F";
+    assertEquals(expected, solution.parseTernary(input));
+  }
+}
