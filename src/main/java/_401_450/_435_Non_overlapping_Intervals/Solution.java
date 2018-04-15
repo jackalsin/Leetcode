16234ce50591cf6c328843db33d1c7ea1df71f6d
zhiwei.xin <zhiwei.xin@veritas.com>
2018-04-14 21:19:02 -0700

    Finish 435

---
+++ src/main/java/_401_450/_435_Non_overlapping_Intervals/Solution.java
@@ -0,0 +1,43 @@
+package _401_450._435_Non_overlapping_Intervals;
+
+import utils.Interval;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution {
+  /**
+   * Time complexity: O(NlogN)
+   * <p>
+   * 18 / 18 test cases passed.
+   * Status: Accepted
+   * Runtime: 9 ms
+   * Submitted: 0 minutes ago
+   *
+   * @param intervals
+   * @return
+   */
+  public int eraseOverlapIntervals(Interval[] intervals) {
+    /*
+     * Proof:
+     * If one tasks end earlier, choose it, so there can be more task scheduled.
+     */
+    if (intervals.length == 0) return 0;
+
+    Arrays.sort(intervals, new Comparator<>() {
+      @Override
+      public int compare(Interval i, Interval j) {
+        return Integer.compare(i.end, j.end);
+      }
+    });
+
+    int count = 1, end = intervals[0].end;
+    for (int i = 1; i < intervals.length; i++) {
+      if (intervals[i].start >= end) {
+        end = intervals[i].end;
+        count++;
+      }
+    }
+    return intervals.length - count;
+  }
+}
