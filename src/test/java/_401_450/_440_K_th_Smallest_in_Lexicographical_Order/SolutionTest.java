107dbfb924850a55bfc5f2ef2be6168c66055d8d
zhiwei.xin <zhiwei.xin@veritas.com>
2018-04-15 22:24:29 -0700

    Finish 440

--- src/test/java/_401_450/_440_K_th_Smallest_in_Lexicographical_Order/SolutionTest.java
+++ src/test/java/_401_450/_440_K_th_Smallest_in_Lexicographical_Order/SolutionTest.java
@@ -22,4 +22,17 @@ class SolutionTest {
   void testOnlineCase2() {
     assertEquals(116, solution.findKthNumber(220, 20));
   }
+
+  @Test
+  void testFailedCase1() {
+    assertEquals(2, solution.findKthNumber(2, 2));
+  }
+
+  /**
+   * Overflow
+   */
+  @Test
+  void testFailedCase2() {
+    assertEquals(416126219, solution.findKthNumber(681692778, 351251360));
+  }
 }
