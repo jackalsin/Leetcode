e6f64c1f756e9b1b3c53ed84f0ced877c4a70da4
jackalsin <jackal.sin@hotmail.com>
2018-04-17 23:14:14 -0700

    Finish 438

---
+++ src/main/java/_401_450/_438_Find_All_Anagrams_in_a_String/SecondTry.java
@@ -0,0 +1,37 @@
+package _401_450._438_Find_All_Anagrams_in_a_String;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SecondTry implements Solution {
+  @Override
+  public List<Integer> findAnagrams(String s, String p) {
+    final List<Integer> result = new ArrayList<>();
+    int uniqueCount = 0;
+    final int[] counts = new int[26];
+    for (char chr : p.toCharArray()) {
+      if (counts[chr - 'a'] == 0) {
+        uniqueCount++;
+      }
+      counts[chr - 'a']++;
+    }
+
+    for (int start = 0, end = 0; end < s.length(); ) {
+      if (--counts[s.charAt(end++) - 'a'] == 0) {
+        uniqueCount--;
+      }
+
+      while (uniqueCount == 0) {
+        final char startChar = s.charAt(start);
+        if (++counts[startChar - 'a'] > 0) {
+          uniqueCount++;
+        }
+        if (end - start == p.length()) {
+          result.add(start);
+        }
+        start++;
+      }
+    }
+    return result;
+  }
+}
--- src/test/java/_401_450/_438_Find_All_Anagrams_in_a_String/SolutionTest.java
+++ src/test/java/_401_450/_438_Find_All_Anagrams_in_a_String/SolutionTest.java
@@ -13,7 +13,8 @@ class SolutionTest {
   @BeforeEach
   void setUp() {
 //    solution = new SampleSolution();
-    solution = new MySolution();
+//    solution = new MySolution();
+    solution = new SecondTry();
   }
 
   @Test
