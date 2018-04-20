5d0c3ae6129f38169d2e14db56f665b7e69b8af5
zhiwei.xin <zhiwei.xin@veritas.com>
2018-04-20 07:50:35 -0700

    Finish 455

---
+++ src/main/java/_451_500/_455_Assign_Cookies/Solution.java
@@ -0,0 +1,24 @@
+package _451_500._455_Assign_Cookies;
+
+import java.util.Arrays;
+
+public class Solution {
+
+  public int findContentChildren(int[] g, int[] s) {
+    int gI = 0, sI = 0;
+    for (final int[] c : new int[][]{g, s}) {
+      Arrays.sort(c);
+    }
+    while (gI < g.length) {
+      while (sI < s.length && g[gI] > s[sI]) {
+        sI++;
+      }
+      if (sI == s.length) {
+        return gI;
+      }
+      sI++;
+      gI++;
+    }
+    return gI;
+  }
+}
