d384e1ffad0a9fd1f1a2579e0142e4b004b433df
zhiwei.xin <zhiwei.xin@veritas.com>
2018-04-22 13:32:15 -0700

    Finish 458

---
+++ src/test/java/_451_500/_458_Poor_Pigs/SolutionTest.java
@@ -0,0 +1,25 @@
+package _451_500._458_Poor_Pigs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    assertEquals(5, solution.poorPigs(1000, 15, 60));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    assertEquals(10, solution.poorPigs(1000, 60, 60));
+  }
+}
