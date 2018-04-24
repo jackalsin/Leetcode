66874afded2387d41ec8f7024bbef89da4ce8b89
zhiwei.xin <zhiwei.xin@veritas.com>
2018-04-24 10:28:14 -0700

    Add Template for 466

---
+++ src/main/java/_451_500/_466_Count_The_Repetitions/BruteForceSolution.java
@@ -0,0 +1,13 @@
+package _451_500._466_Count_The_Repetitions;
+
+public final class BruteForceSolution implements Solution{
+  @Override
+  public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+    for (int i = 0; i < s1.length() ; i++) {
+      for (int j = 0; j < s2.length(); j++) {
+        return 0;
+      }
+    }
+    return 0;
+  }
+}
---
+++ src/main/java/_451_500/_466_Count_The_Repetitions/Solution.java
@@ -0,0 +1,5 @@
+package _451_500._466_Count_The_Repetitions;
+
+public interface Solution {
+  int getMaxRepetitions(String s1, int n1, String s2, int n2);
+}
