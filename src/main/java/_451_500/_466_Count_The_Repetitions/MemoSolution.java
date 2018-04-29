99745144286aa8f90cdb4bb7fce62bea136a49fe
jackalsin <jackal.sin@hotmail.com>
2018-04-29 12:37:45 -0700

    Add 466

---
+++ src/main/java/_451_500/_466_Count_The_Repetitions/MemoSolution.java
@@ -0,0 +1,31 @@
+package _451_500._466_Count_The_Repetitions;
+
+public class MemoSolution implements Solution {
+  @Override
+  public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+    final int[] repeatCount = new int[s2.length() + 1], nextIndex = new int[s2.length() + 1];
+    int j = 0, rep = 0;
+    for (int k = 1; k <= n1; k++) {
+      for (int i = 0; i < s1.length(); i++) {
+        if (s1.charAt(i) == s2.charAt(j)) {
+          j++;
+          if (j == s2.length()) {
+            j = 0;
+            rep++;
+          }
+        }
+      }
+      repeatCount[k] = rep;
+      nextIndex[k] = j;
+      for (int start = 0; start < k; start++) {
+        if (nextIndex[start] == j) { // used as a set.
+          int beforePattern = repeatCount[start];
+          int patternCount = (repeatCount[k] - repeatCount[start]) * ((n1 - start) / (k - start));
+          int afterPattern = repeatCount[start + (n1 - start) % (k - start)] - repeatCount[start];
+          return (beforePattern + patternCount + afterPattern) / n2;
+        }
+      }
+    }
+    return rep / n2;
+  }
+}
--- src/test/java/_451_500/_466_Count_The_Repetitions/BruteForceSolutionTest.java
+++ src/test/java/_451_500/_466_Count_The_Repetitions/BruteForceSolutionTest.java
@@ -10,7 +10,8 @@ class BruteForceSolutionTest {
 
   @BeforeEach
   void setUp() {
-    solution = new BruteForceSolution();
+//    solution = new BruteForceSolution();
+    solution = new MemoSolution();
   }
 
   @Test
@@ -19,12 +20,13 @@ class BruteForceSolutionTest {
     final int n1 = 4, n2 = 2;
     assertEquals(2, solution.getMaxRepetitions(s1, n1, s2, n2));
   }
+
   @Test
   void testOnlineCase2() {
     final String s1 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", s2 = "a";
     final int n1 = 1000000, n2 = 1;
 
-    assertEquals(2, solution.getMaxRepetitions(s1, n1, s2, n2));
+    assertEquals(100000000, solution.getMaxRepetitions(s1, n1, s2, n2));
   }
 }
 
