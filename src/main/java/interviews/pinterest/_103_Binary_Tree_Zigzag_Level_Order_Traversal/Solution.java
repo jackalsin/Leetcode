fb16ce1ec576772fb5eb42dcb6a807135da515c4
zhiwei.xin <zhiwei.xin@veritas.com>
2018-05-10 18:56:37 -0700

    Finish 103

--- src/main/java/interviews/pinterest/_103_Binary_Tree_Zigzag_Level_Order_Traversal/Solution.java
+++ src/main/java/interviews/pinterest/_103_Binary_Tree_Zigzag_Level_Order_Traversal/Solution.java
@@ -1,4 +1,39 @@
 package interviews.pinterest._103_Binary_Tree_Zigzag_Level_Order_Traversal;
 
+import utils.TreeNode;
+
+import java.util.*;
+
 public class Solution {
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    final List<List<Integer>> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+    boolean isReverse = false;
+    final Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      final List<Integer> curLine = new ArrayList<>();
+      result.add(curLine);
+      final int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        final TreeNode toRemove = queue.remove();
+        curLine.add(toRemove.val);
+        if (toRemove.left != null) {
+          queue.add(toRemove.left);
+        }
+        if (toRemove.right != null) {
+          queue.add(toRemove.right);
+        }
+      }
+
+      if (isReverse) {
+        Collections.reverse(curLine);
+      }
+      isReverse = !isReverse;
+
+    }
+    return result;
+  }
 }
---
+++ src/test/java/interviews/pinterest/_103_Binary_Tree_Zigzag_Level_Order_Traversal/SolutionTest.java
@@ -0,0 +1,55 @@
+package interviews.pinterest._103_Binary_Tree_Zigzag_Level_Order_Traversal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void test1Level() {
+    final TreeNode input = TreeNodes.getTreeLevelOrder(1);
+    final List<List<Integer>> expected = new ArrayList<>();
+    expected.add(Collections.singletonList(1));
+    assertEquals(expected, solution.zigzagLevelOrder(input));
+  }
+
+  @Test
+  void test2Levels() {
+    final TreeNode input = TreeNodes.getTreeLevelOrder(1, 2, 3);
+    final List<List<Integer>> expected = new ArrayList<>();
+    expected.add(Collections.singletonList(1));
+    expected.add(Arrays.asList(3, 2));
+    assertEquals(expected, solution.zigzagLevelOrder(input));
+  }
+
+  @Test
+  void test3Levels() {
+    final TreeNode input = TreeNodes.getTreeLevelOrder(1, 2, 3, 4, 5, 6, 7);
+    final List<List<Integer>> expected = new ArrayList<>();
+    expected.add(Collections.singletonList(1));
+    expected.add(Arrays.asList(3, 2));
+    expected.add(Arrays.asList(4, 5, 6, 7));
+    assertEquals(expected, solution.zigzagLevelOrder(input));
+  }
+
+  @Test
+  void testNullPointer() {
+    assertEquals(new ArrayList<>(), solution.zigzagLevelOrder(null));
+  }
+}
