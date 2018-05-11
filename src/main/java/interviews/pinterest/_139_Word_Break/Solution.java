8eb8aee439f3c7ee3a38535e995b813f86b94605
zhiwei.xin <zhiwei.xin@veritas.com>
2018-05-10 22:14:17 -0700

    Add 139

--- src/main/java/interviews/pinterest/_139_Word_Break/Solution.java
+++ src/main/java/interviews/pinterest/_139_Word_Break/Solution.java
@@ -1,4 +1,28 @@
 package interviews.pinterest._139_Word_Break;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Solution {
+
+  /**
+   * TODO: Write a gitbook
+   *
+   * @param s
+   * @param wordDict
+   * @return
+   */
+  public boolean wordBreak(String s, List<String> wordDict) {
+    final boolean[] hasWord = new boolean[s.length()];
+    final Set<String> wordSet = new HashSet<>(wordDict);
+    for (int start = s.length() - 1; start >= 0; start--) {
+      for (int end = start; end <= s.length(); end++) {
+        if (!hasWord[start]) {
+          hasWord[start] = (end == s.length() || hasWord[end]) && wordSet.contains(s.substring(start, end));
+        }
+      }
+    }
+    return hasWord[0];
+  }
 }
