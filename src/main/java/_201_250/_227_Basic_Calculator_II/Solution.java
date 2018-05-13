5415eeb422419fc7e05198ec53c11a7303c8d090
zhiwei.xin <zhiwei.xin@veritas.com>
2018-05-13 12:37:28 -0700

    Modify 227 to generic solution

--- src/main/java/_201_250/_227_Basic_Calculator_II/Solution.java
+++ src/main/java/_201_250/_227_Basic_Calculator_II/Solution.java
@@ -1,46 +1,9 @@
 package _201_250._227_Basic_Calculator_II;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 /**
  * @author jacka
  * @version 1.0 on 8/16/2017.
  */
-public class Solution {
-  public int calculate(String s) {
-    int num = 0;
-    char sign = '+';
-    Deque<Integer> stack = new ArrayDeque<>();
-
-    for (int i = 0; i < s.length(); ++i) {
-      char chr = s.charAt(i);
-      if (Character.isDigit(chr)) {
-        num = 10 * num + (chr - '0');
-      }
-
-      if (i == s.length() - 1 || (!Character.isDigit(chr) && chr != ' ')) {
-        calculate(stack, sign, num);
-        num = 0;
-        sign = chr;
-      }
-    }
-    int result = 0;
-    while (!stack.isEmpty()) {
-      result += stack.pop();
-    }
-    return result;
-  }
-
-  private static void calculate(Deque<Integer> stack, char sign, int num) {
-    if (sign == '+') {
-      stack.push(num);
-    } else if (sign == '-') {
-      stack.push(-num);
-    } else if (sign == '*') {
-      stack.push(stack.pop() * num);
-    } else if (sign == '/') {
-      stack.push(stack.pop() / num);
-    }
-  }
+public interface Solution {
+  int calculate(String s);
 }
--- src/test/java/_201_250/_227_Basic_Calculator_II/SolutionTest.java
+++ src/test/java/_201_250/_227_Basic_Calculator_II/SolutionTest.java
@@ -1,35 +1,36 @@
 package _201_250._227_Basic_Calculator_II;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author jacka
  * @version 1.0 on 8/17/2017.
  */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
-  @Before
-  public void setUp() throws Exception {
-    solution = new Solution();
+
+  @BeforeEach
+  void setUp() {
+    solution = new GenericSolution();
   }
 
   @Test
-  public void testOnline1() throws Exception {
+  void testOnline1() {
     final String input = "3+2*2";
     assertEquals(7, solution.calculate(input));
   }
 
   @Test
-  public void testOnline2() throws Exception {
+  void testOnline2() {
     final String input = " 3/2 ";
     assertEquals(1, solution.calculate(input));
   }
 
   @Test
-  public void testOnline3() throws Exception {
+  void testOnline3() {
     final String input = " 3+5 / 2 ";
     assertEquals(5, solution.calculate(input));
   }
