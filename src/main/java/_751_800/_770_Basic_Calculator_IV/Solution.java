03dbb96ef7b5896a160fdb64fca20200d75c3638
zhiwei.xin <zhiwei.xin@veritas.com>
2018-05-13 17:06:34 -0700

    Finish 770

---
+++ src/main/java/_751_800/_770_Basic_Calculator_IV/Solution.java
@@ -0,0 +1,7 @@
+package _751_800._770_Basic_Calculator_IV;
+
+import java.util.List;
+
+public interface Solution {
+  List<String> basicCalculatorIV(String expression, String[] evalvars, int[] evalints);
+}
