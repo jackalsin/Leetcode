7788e06d98223b2471d54a423d3dc16e53b576bf
zhiwei.xin <zhiwei.xin@veritas.com>
2018-05-14 22:09:22 -0700

    Finish 114

--- src/main/java/interviews/pinterest/_114_Flatten_Binary_Tree_to_Linked_List/Solution.java
+++ src/main/java/interviews/pinterest/_114_Flatten_Binary_Tree_to_Linked_List/Solution.java
@@ -1,4 +1,20 @@
 package interviews.pinterest._114_Flatten_Binary_Tree_to_Linked_List;
 
-public class Solution { // todo:
+import utils.TreeNode;
+
+public class Solution {
+  public void flatten(TreeNode root) {
+    if (root == null) {
+      return;
+    }
+    flatten(root.left);
+    flatten(root.right);
+    final TreeNode right = root.right;
+    root.right = root.left;
+    root.left = null;
+    while (root.right != null) {
+      root = root.right;
+    }
+    root.right = right;
+  }
 }
