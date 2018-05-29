d0621a42c6cff99cde986f2301cdf972c9c80568
zhiwei.xin <zhiwei.xin@veritas.com>
2018-05-29 13:27:09 -0700

    Finish 277

--- src/main/java/interviews/pinterest/_277_Find_the_Celebrity/Solution.java
+++ src/main/java/interviews/pinterest/_277_Find_the_Celebrity/Solution.java
@@ -1,4 +1,29 @@
 package interviews.pinterest._277_Find_the_Celebrity;
 
-public class Solution { // todo:
+import _251_300._277_Find_the_Celebrity.Relation;
+
+public class Solution extends Relation {
+  /**
+   * 171 / 171 test cases passed.
+   * Status: Accepted
+   * Runtime: 14 ms
+   *
+   * @param n
+   * @return
+   */
+  public int findCelebrity(int n) {
+    int candidate = 0;
+    for (int i = 1; i < n; i++) {
+      if (knows(candidate, i)) {
+        candidate = i;
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (i != candidate && (!knows(i, candidate) || knows(candidate, i))) {
+        return -1; // not found
+      }
+    }
+    return candidate;
+  }
 }
