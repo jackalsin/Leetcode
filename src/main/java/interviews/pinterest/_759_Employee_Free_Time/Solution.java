d67585163e1af1bcccea5e9d36c4625152ce44b3
zhiwei.xin <zhiwei.xin@veritas.com>
2018-06-02 21:28:33 -0700

    Finish 759

--- src/main/java/interviews/pinterest/_759_Employee_Free_Time/Solution.java
+++ src/main/java/interviews/pinterest/_759_Employee_Free_Time/Solution.java
@@ -1,4 +1,37 @@
 package interviews.pinterest._759_Employee_Free_Time;
 
-public class Solution { // TODO:
+import utils.Interval;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Solution {
+
+  public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+    final List<Interval> sortedIntervals = new ArrayList<>();
+    for (final List<Interval> e : schedule) {
+      sortedIntervals.addAll(e);
+    }
+    Collections.sort(sortedIntervals, new Comparator<Interval>() {
+      @Override
+      public int compare(Interval o1, Interval o2) {
+        return Integer.compare(o1.start, o2.start);
+      }
+    });
+
+    Interval candidate = sortedIntervals.get(0);
+    final List<Interval> result = new ArrayList<>();
+    for (Interval i : sortedIntervals) {
+      if (candidate.end < i.start) {
+        result.add(new Interval(candidate.end, i.start));
+        candidate = i;
+      } else {
+        candidate.end = Math.max(i.end, candidate.end);
+      }
+    }
+    return result;
+  }
+
 }
---
+++ src/test/java/interviews/pinterest/_759_Employee_Free_Time/SolutionTest.java
@@ -0,0 +1,47 @@
+package interviews.pinterest._759_Employee_Free_Time;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.Interval;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final List<List<Interval>> input = List.of(
+        List.of(new Interval(1, 2), new Interval(5, 6)),
+        List.of(new Interval(1, 3)),
+        List.of(new Interval(4, 10))
+    );
+
+    final List<Interval> expected = List.of(new Interval(3, 4));
+    final List<Interval> actual = solution.employeeFreeTime(input);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void testOnlineCase2() {
+    final List<List<Interval>> input = List.of(
+        List.of(new Interval(1, 3), new Interval(6, 7)),
+        List.of(new Interval(2, 4)),
+        List.of(new Interval(2, 5), new Interval(9, 12))
+    );
+
+    final List<Interval> expected = List.of(
+        new Interval(5, 6), new Interval(7, 9)
+    );
+    final List<Interval> actual = solution.employeeFreeTime(input);
+    assertEquals(expected, actual);
+  }
+
+}
