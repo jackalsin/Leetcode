dd88febc56b7eb5a5435a7b29ab42d5564a417f3
zhiwei.xin <zhiwei.xin@veritas.com>
2018-06-03 10:19:11 -0700

    Finish phone number and trie

---
+++ src/test/java/interviews/pinterest/phoneNumberAndTrie/SolutionTest.java
@@ -0,0 +1,28 @@
+package interviews.pinterest.phoneNumberAndTrie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final List<String> words = List.of("word", "abc", "mhi", "mhij");
+    final Set<String> expected = Set.of("mhi");
+    final String phoneNum = "644";
+    final List<String> actual = solution.search(words, phoneNum);
+    assertEquals(expected, new HashSet<>(actual));
+  }
+}
