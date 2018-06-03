22f633e9e7da8ede4cdd17036d5f8ef67331b369
zhiwei.xin <zhiwei.xin@veritas.com>
2018-06-02 22:34:37 -0700

    Finish 811

--- src/main/java/interviews/pinterest/_811_Subdomain_Visit_Count/Solution.java
+++ src/main/java/interviews/pinterest/_811_Subdomain_Visit_Count/Solution.java
@@ -1,21 +1,32 @@
 package interviews.pinterest._811_Subdomain_Visit_Count;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=381271&extra=page%3D1%26filter%3Dsortid%26sortid%3D311%26searchoption%5B3046%5D%5Bvalue%5D%3D33%26searchoption%5B3046%5D%5Btype%5D%3Dradio%26sortid%3D311
  */
-public class Solution { // todo:
-//  Q1: domain click count
-//
-////       String[] counts = {
-////       "900,google.com",. 1point3acres.com/bbs
-////       "60,mail.yahoo.com",
-////       "10,mobile.sports.yahoo.com",
-////       "40,sports.yahoo.com",
-////       "300,yahoo.com",
-////       "10,stackoverflow.com",
-////       "2,en.wikipedia.org",
-////       "1,es.wikipedia.org" };
-//
-//  Return click counts for each domain
-//  com=1320, stackoverflow.com=10, mail.yahoo.com=60, sports.yahoo.com=50,google.com=900, en.wikipedia.org=2, mobile.sports.yahoo.com=10, org=3, wikipedia.org=3, yahoo.com=410, es.wikipedia.org=1
+public class Solution {
+  public List<String> subdomainVisits(String[] cpdomains) {
+    final Map<String, Integer> domains = new HashMap<>();
+    for (final String cp : cpdomains) {
+      final String[] entry = cp.split(" ");
+      final int times = Integer.parseInt(entry[0]);
+      final String domain = entry[1];
+      for (int i = 0; i < entry[1].length(); i++) {
+        if (i == 0 || domain.charAt(i) == '.') {
+          final int startIndex = i == 0 ? 0 : i + 1;
+          final String subdomain = entry[1].substring(startIndex);
+          domains.put(subdomain, domains.getOrDefault(subdomain, 0) + times);
+        }
+      }
+    }
+    final List<String> result = new ArrayList<>();
+    for (final Map.Entry<String, Integer> entry : domains.entrySet()) {
+      result.add(entry.getValue() + " " + entry.getKey());
+    }
+    return result;
+  }
 }
