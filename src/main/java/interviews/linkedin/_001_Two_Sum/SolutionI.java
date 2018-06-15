c740ffa0019f1765e8684821e5e7df84fdddb669
jackalsin <jackal.sin@hotmail.com>
2018-06-15 09:24:35 -0700

    Finish 001

---
+++ src/main/java/interviews/linkedin/_001_Two_Sum/SolutionI.java
@@ -0,0 +1,22 @@
+package interviews.linkedin._001_Two_Sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/15/2017.
+ */
+public final class SolutionI implements Solution {
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> visited = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      if (visited.containsKey(target - nums[i])) {
+        return new int[]{i, visited.get(target - nums[i])};
+      } else {
+        visited.put(nums[i], i);
+      }
+    }
+    throw new IllegalStateException("No solution.");
+  }
+}
