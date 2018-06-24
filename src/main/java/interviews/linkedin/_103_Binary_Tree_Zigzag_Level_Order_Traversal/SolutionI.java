c009b4f449adec2ec851c0face62080cb0ae91d0
jackalsin <jackal.sin@hotmail.com>
2018-06-24 14:38:29 -0700

    Finish 102 / 103

---
+++ src/main/java/interviews/linkedin/_103_Binary_Tree_Zigzag_Level_Order_Traversal/SolutionI.java
@@ -0,0 +1,41 @@
+package interviews.linkedin._103_Binary_Tree_Zigzag_Level_Order_Traversal;
+
+import utils.TreeNode;
+
+import java.util.*;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/20/2017.
+ */
+public final class SolutionI implements Solution {
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.add(root);
+    boolean isInOrder = false;
+    while (!queue.isEmpty()) {
+      isInOrder = !isInOrder;
+      final int size = queue.size();
+      List<Integer> curLine = new ArrayList<>();
+      for (int i = 0; i < size; ++i) {
+        TreeNode curNode = queue.poll();
+        curLine.add(curNode.val);
+        if (curNode.left != null) {
+          queue.add(curNode.left);
+        }
+        if (curNode.right != null) {
+          queue.add(curNode.right);
+        }
+      }
+      if (!isInOrder) {
+        Collections.reverse(curLine);
+      }
+      result.add(curLine);
+    }
+    return result;
+  }
+}
