33f06d96b238cf7baf8030a0cd8cd3dac4c6ef7e
jackalsin <jackal.sin@hotmail.com>
2018-06-24 17:59:58 -0700

    Finish 104

---
+++ src/main/java/interviews/linkedin/_104_Maximum_Depth_of_Binary_Tree/SolutionII.java
@@ -0,0 +1,18 @@
+package interviews.linkedin._104_Maximum_Depth_of_Binary_Tree;
+
+import utils.TreeNode;
+
+/**
+ * @author Zhiwei.Xin
+ * @version 1.0 on 10/20/2017.
+ */
+public final class SolutionII implements Solution {
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+  }
+
+}
