6bf919933ecc341305f1709c5a0ffb0506a9eb2e
jackalsin <jackal.sin@hotmail.com>
2018-06-27 19:59:04 -0700

    Finish 254

---
+++ src/main/java/interviews/linkedin/_254_Factor_Combinations/SolutionII.java
@@ -0,0 +1,33 @@
+package interviews.linkedin._254_Factor_Combinations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/22/2017.
+ */
+public final class SolutionII implements Solution {
+  public List<List<Integer>> getFactors(int n) {
+    final List<List<Integer>> result = new ArrayList<>();
+    getFactors(result, n, 1, new ArrayList<>());
+    return result;
+  }
+
+  private static void getFactors(final List<List<Integer>> result, final int remain, int start, final List<Integer>
+      curPath) {
+    if (start != 1) {
+      curPath.add(remain);
+      result.add(new ArrayList<>(curPath));
+      curPath.remove(curPath.size() - 1);
+    }
+
+    for (int i = Math.max(2, start); i * i <= remain; i++) {
+      if (remain % i == 0) {
+        curPath.add(i);
+        getFactors(result, remain / i, i, curPath);
+        curPath.remove(curPath.size() - 1);
+      }
+    }
+  }
+}
--- src/test/java/interviews/linkedin/_254_Factor_Combinations/SolutionTest.java
+++ src/test/java/interviews/linkedin/_254_Factor_Combinations/SolutionTest.java
@@ -19,7 +19,7 @@ public class SolutionTest {
 
   @Before
   public void setUp() throws Exception {
-    solution = new Solution();
+    solution = new SolutionII();
   }
 
   @Test
