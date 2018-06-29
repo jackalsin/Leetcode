60f27aeddb62213f65fe006f7075b84b84989c8c
zhiwei.xin <zhiwei.xin@veritas.com>
2018-06-28 18:58:38 -0700

    Finish 364

--- src/main/java/interviews/linkedin/_364_Nested_List_Weight_Sum_II/Solution.java
+++ src/main/java/interviews/linkedin/_364_Nested_List_Weight_Sum_II/Solution.java
@@ -1,38 +1,14 @@
 package interviews.linkedin._364_Nested_List_Weight_Sum_II;
 
-import java.util.List;
-
 import utils.nestedInteger.NestedInteger;
+
+import java.util.List;
 
 /**
  * @author jacka
  * @version 1.0 on 10/22/2017.
  */
-public class Solution {
-  public int depthSumInverse(List<NestedInteger> nestedList) {
-    int depth = getDepth(nestedList);
-    return depthSumInverse(nestedList, depth);
-  }
+public interface Solution {
 
-  private int depthSumInverse(List<NestedInteger> nestedList, int depth) {
-    int result = 0;
-    for (NestedInteger nestedInteger : nestedList) {
-      if (nestedInteger.isInteger()) {
-        result += nestedInteger.getInteger() * depth;
-      } else {
-        result += depthSumInverse(nestedInteger.getList(), depth - 1);
-      }
-    }
-    return result;
-  }
-
-  private int getDepth(List<NestedInteger> nestedList) {
-    int depth = 1;
-    for (NestedInteger nestedInteger : nestedList) {
-      if (!nestedInteger.isInteger()) {
-        depth = Math.max(depth, getDepth(nestedInteger.getList()) + 1);
-      }
-    }
-    return depth;
-  }
+  int depthSumInverse(List<NestedInteger> nestedList);
 }
---
+++ src/test/java/interviews/linkedin/_364_Nested_List_Weight_Sum_II/SolutionIITest.java
@@ -0,0 +1,38 @@
+package interviews.linkedin._364_Nested_List_Weight_Sum_II;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.nestedInteger.NestedInteger;
+import utils.nestedInteger.NestedIntegerImpl;
+
+import java.util.List;
+
+import static java.util.List.of;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionIITest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new SolutionII();
+  }
+
+  @Test
+  void testOnlineCase() {
+    final List<NestedInteger> input = List.of(new NestedIntegerImpl(new NestedIntegerImpl(1),
+        new NestedIntegerImpl(1)), new NestedIntegerImpl(2), new NestedIntegerImpl(new
+        NestedIntegerImpl(1), new NestedIntegerImpl(1)));
+
+    assertEquals(8, solution.depthSumInverse(input));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    final List<NestedInteger> input = of(new NestedIntegerImpl(1), new NestedIntegerImpl(
+        new NestedIntegerImpl(4), new NestedIntegerImpl(new NestedIntegerImpl(6))
+    ));
+    assertEquals(17, solution.depthSumInverse(input));
+  }
+
+}
