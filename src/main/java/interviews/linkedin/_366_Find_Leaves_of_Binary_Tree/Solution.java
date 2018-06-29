7c6bc840bcc82cf91e790a40efb9a5024ff3b5b0
zhiwei.xin <zhiwei.xin@veritas.com>
2018-06-28 19:32:31 -0700

    Finish 366

--- src/main/java/interviews/linkedin/_366_Find_Leaves_of_Binary_Tree/Solution.java
+++ src/main/java/interviews/linkedin/_366_Find_Leaves_of_Binary_Tree/Solution.java
@@ -1,31 +1,13 @@
 package interviews.linkedin._366_Find_Leaves_of_Binary_Tree;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import utils.TreeNode;
+
+import java.util.List;
 
 /**
  * @author jacka
  * @version 1.0 on 10/22/2017.
  */
-public class Solution {
-  public List<List<Integer>> findLeaves(TreeNode root) {
-    final List<List<Integer>> result = new ArrayList<>();
-    helper(root, result);
-    return result;
-  }
-
-  private int helper(TreeNode root, List<List<Integer>> result) {
-    if (root == null) {
-      return -1;
-    } else {
-      int height = 1 + Math.max(helper(root.left, result), helper(root.right, result));
-      if (result.size() <= height) {
-        result.add(new ArrayList<>());
-      }
-      result.get(height).add(root.val);
-      return height;
-    }
-  }
+public interface Solution {
+  List<List<Integer>> findLeaves(TreeNode root);
 }
