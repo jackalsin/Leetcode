da135e6c6f5b5bf4dc2e432de9fe6b8d445d1a53
jackalsin <jackal.sin@hotmail.com>
2018-06-30 12:15:40 -0700

    Add 805

--- src/main/java/_801_850/_805_Split_Array_With_Same_Average/Solution.java
+++ src/main/java/_801_850/_805_Split_Array_With_Same_Average/Solution.java
@@ -1,11 +1,5 @@
 package _801_850._805_Split_Array_With_Same_Average;
 
-import java.util.Arrays;
-
-public class Solution {
-  public boolean splitArraySameAverage(int[] A) {
-    final int sum = Arrays.stream(A).sum();
-    // TODO; add 805
-    return false;
-  }
+public interface Solution {
+  boolean splitArraySameAverage(int[] A);
 }
