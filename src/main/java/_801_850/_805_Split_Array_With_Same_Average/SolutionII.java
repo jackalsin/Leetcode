34dce116fc5ba388a7bac4a6508b0f958a91e403
jackalsin <jackal.sin@hotmail.com>
2018-06-30 14:56:08 -0700

    Finish 805

---
+++ src/main/java/_801_850/_805_Split_Array_With_Same_Average/SolutionII.java
@@ -0,0 +1,8 @@
+package _801_850._805_Split_Array_With_Same_Average;
+
+public final class SolutionII implements Solution {
+  public boolean splitArraySameAverage(int[] A) {
+    return false; // TODO : Add solution
+  }
+
+}
