43fec5d2671933871e93d4ef7cb5235d90a69b9a
jackalsin <jackal.sin@hotmail.com>
2018-06-29 21:44:18 -0700

    Finish 311

--- src/main/java/interviews/linkedin/_311_Sparse_Matrix_Multiplication/SolutionII.java
+++ src/main/java/interviews/linkedin/_311_Sparse_Matrix_Multiplication/SolutionII.java
@@ -6,8 +6,27 @@ package interviews.linkedin._311_Sparse_Matrix_Multiplication;
  */
 public final class SolutionII implements Solution {
   public int[][] multiply(int[][] A, int[][] B) {
-    /* TODO: unimplemnts */
-    return null;
+    if (A == null || A.length == 0) {
+      return new int[][]{};
+    }
+    if (B == null || B.length == 0) {
+      return new int[][]{};
+    }
+
+    final int[][] C = new int[A.length][B[0].length];
+
+    for (int i = 0; i < A.length; i++) {
+      for (int k = 0; k < A[0].length; k++) {
+        if (A[i][k] == 0) {
+          continue;
+        }
+
+        for (int j = 0; j < B[0].length; j++) {
+          C[i][j] += A[i][k] * B[k][j];
+        }
+      }
+    }
+    return C;
   }
 
 }
