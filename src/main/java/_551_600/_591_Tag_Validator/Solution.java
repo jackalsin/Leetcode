c01f01c85f2609742de5d1f257a1b52023fb5d96
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-03 14:59:05 -0700

    Finish 591

--- src/main/java/_551_600/_591_Tag_Validator/Solution.java
+++ src/main/java/_551_600/_591_Tag_Validator/Solution.java
@@ -19,7 +19,9 @@ public class Solution {
     Deque<String> stack = new ArrayDeque<>();
     if (!code.startsWith("<")) return false;
     for (int i = 0; i < code.length(); ) {
-      if(i > 0 && stack.isEmpty()) return false;
+      if (i > 0 && stack.isEmpty()) {
+        return false;
+      }
       if (code.startsWith("</", i)) {
         final int rightEndIndex = code.indexOf('>', i);
         if (rightEndIndex == -1) return false;
@@ -31,11 +33,15 @@ public class Solution {
         i = rightEndIndex + 1;
       } else if (code.startsWith("<![CDATA[", i)) {
         final int rightEndIndex = code.indexOf(CDATA_RIGHT_MATCH, i);
-        if (rightEndIndex == -1) return false;
+        if (rightEndIndex == -1) {
+          return false;
+        }
         i = rightEndIndex + 3;
       } else if (code.startsWith("<", i)) {
         final int rightEndIndex = code.indexOf('>', i);
-        if (rightEndIndex == -1) return false;
+        if (rightEndIndex == -1) {
+          return false;
+        }
         String tagName = code.substring(i + 1, rightEndIndex);
         if (tagName.length() > 9 || tagName.length() == 0) {
           return false;
--- src/test/java/interviews/microsoft/_591_Tag_Validator/SolutionTest.java
+++ src/test/java/interviews/microsoft/_591_Tag_Validator/SolutionTest.java
@@ -1,81 +1,95 @@
-package _551_600._591_Tag_Validator;
+package interviews.microsoft._591_Tag_Validator;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * @author jacka
- * @version 1.0 on 11/5/2017.
- */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
-  @Before
-  public void setUp() throws Exception {
+
+  @BeforeEach
+  void setUp() throws Exception {
     solution = new Solution();
   }
 
   @Test
-  public void testFailedCase1() throws Exception {
+  void testFailedCase1() throws Exception {
     final String input = "<![CDATA[wahaha]]]><![CDATA[]> wahaha]]>";
     assertFalse(solution.isValid(input));
   }
 
   @Test
-  public void testTLESolution() throws Exception {
+  void testTLESolution() throws Exception {
     final String input = "<A><![CDATA[</A>]]123></A>";
     assertFalse(solution.isValid(input));
   }
 
 
   @Test
-  public void testOnlineCase1() throws Exception {
+  void testOnlineCase1() throws Exception {
     final String input = "<DIV>This is the first line <![CDATA[<div>]]></DIV>";
     assertTrue(solution.isValid(input));
   }
 
   @Test
-  public void testOnlineCase2() throws Exception {
+  void testOnlineCase2() throws Exception {
     final String input = "<DIV>>>  ![cdata[]] <![CDATA[<div>]>]]>]]>>]</DIV>";
     assertTrue(solution.isValid(input));
   }
+
   @Test
-  public void testOnlineCase3() throws Exception {
+  void testOnlineCase3() throws Exception {
     final String input = "<A>  <B> </A>   </B>";
     assertFalse(solution.isValid(input));
   }
+
   @Test
-  public void testOnlineCase4() throws Exception {
+  void testOnlineCase4() throws Exception {
     final String input = "<DIV>  div tag is not closed  <DIV>";
     assertFalse(solution.isValid(input));
   }
+
   @Test
-  public void testOnlineCase5() throws Exception {
+  void testOnlineCase5() throws Exception {
     final String input = "<DIV>  unmatched <  </DIV>";
     assertFalse(solution.isValid(input));
   }
 
   @Test
-  public void testOnlineCase6() throws Exception {
+  void testOnlineCase6() throws Exception {
     final String input = "<DIV> closed tags with invalid tag name  <b>123</b> </DIV>";
     assertFalse(solution.isValid(input));
   }
+
   @Test
-  public void testOnlineCase7() throws Exception {
+  void testOnlineCase7() throws Exception {
     final String input = "<DIV> unmatched tags with invalid tag name  </1234567890> and <CDATA[[]]>  </DIV>";
     assertFalse(solution.isValid(input));
   }
+
   @Test
-  public void testOnlineCase8() throws Exception {
+  void testOnlineCase8() throws Exception {
     final String input = "<DIV>  unmatched start tag <B>  and unmatched end tag </C>  </DIV>";
     assertFalse(solution.isValid(input));
   }
 
   @Test
-  public void testFailedCase2() throws Exception {
-    final String input = "<DIV>  a  </DIV>3";
+  void testFailedCase2() throws Exception {
+    final String input = "<A";
+    assertFalse(solution.isValid(input));
+  }
+
+  @Test
+  void testFailedCase3() throws Exception {
+    final String input = "<DIV><></></DIV>";
+    assertFalse(solution.isValid(input));
+  }
+
+  @Test
+  void testFailedCase4() throws Exception {
+    final String input = "<A></A>>";
     assertFalse(solution.isValid(input));
   }
 
