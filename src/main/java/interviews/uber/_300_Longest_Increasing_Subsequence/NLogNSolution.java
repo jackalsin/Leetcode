34a78c5648cd85e550dc29bda2275d1b17fb2486
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-06 08:43:02 -0700

    Update 300

--- src/main/java/interviews/uber/_300_Longest_Increasing_Subsequence/NLogNSolution.java
+++ src/main/java/interviews/uber/_300_Longest_Increasing_Subsequence/NLogNSolution.java
@@ -5,64 +5,41 @@ package interviews.uber._300_Longest_Increasing_Subsequence;
  * @version 1.0 on 3/16/2018.
  */
 public class NLogNSolution implements Solution {
+
   @Override
   public int lengthOfLIS(int[] nums) {
-    //    return example(nums);
-    return mySolution(nums);
-  }
-
-  public int mySolution(int[] nums) {
-    // tails is an array storing the smallest tail of all increasing subsequences with length i+1 in
-    // tails[i].
     if (nums == null || nums.length == 0) {
       return 0;
     }
-    int[] tails = new int[nums.length];
-    tails[0] = nums[0];
-    int right = 0;
-    for (int k = 1; k < nums.length; k++) {
-      int x = nums[k];
-      // find the last smaller than
-      int i = 0, j = right;
-      while (i < j) {
-        int m = (j - i + 1) / 2 + i;
-        if (tails[m] < x) {
-          i = m;
+
+    final int[] minTail = new int[nums.length];
+    minTail[0] = nums[0];
+    int end = 0;
+    for (int i = 1; i < nums.length; i++) {
+      int num = nums[i], left = 0, right = end;
+
+      // find lower than num;
+      while (left < right) {
+        final int mid = (right - left + 1) / 2 + left;
+        if (num > minTail[mid]) {
+          left = mid;
         } else {
-          j = m - 1;
+          right = mid - 1;
         }
       }
-      // all elements are not smaller than x
-      if (tails[i] >= x) {
-        i = -1;
+
+      // all the elements are greater than or equals
+      if (minTail[left] >= num) {
+        left = -1;
       }
-      // find the first not smaller than
-      tails[i + 1] = x;
-      if (i == right) {
-        right++;
+
+      // minTail[left] < nums[i] <= minTail[left + 1]
+      minTail[left + 1] = num;
+      if (left == end) { // is appending, rather than updating
+        end++;
       }
     }
-    return right + 1;
+    return end + 1;
   }
 
-  public int example(int[] nums) {
-    int[] tails = new int[nums.length];
-    int size = 0;
-    for (int x : nums) {
-      int i = 0, j = size;
-      while (i != j) {
-        int m = (i + j) / 2;
-        if (tails[m] < x) {
-          i = m + 1;
-        } else {
-          j = m;
-        }
-      }
-      tails[i] = x;
-      if (i == size) {
-        ++size;
-      }
-    }
-    return size;
-  }
 }
