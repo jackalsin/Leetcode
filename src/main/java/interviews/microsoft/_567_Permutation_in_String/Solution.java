232441d244e7f98e32a68135462ef958516e06d9
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-05 17:06:11 -0700

    Add 567

---
+++ src/main/java/interviews/microsoft/_567_Permutation_in_String/Solution.java
@@ -0,0 +1,20 @@
+package interviews.microsoft._567_Permutation_in_String;
+
+public class Solution {
+
+  public boolean checkInclusion(String s1, String s2) {
+    final int[] map = new int[26];
+    int count = 0;
+    for (char chr : s1.toCharArray()) {
+      if (map[chr - 'a'] == 0) {
+        count++;
+      }
+      map[chr - 'a']++;
+    }
+
+    for (int end = 0, start = 0; end < s2.length(); end++) {
+
+    }
+    return false;
+  }
+}
