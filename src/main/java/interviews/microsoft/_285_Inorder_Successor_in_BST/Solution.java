9fbe0db747ebcbb8dabdd6c1be4b4dff622cb5d9
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-06 21:33:30 -0700

    Finish 285

---
+++ src/main/java/interviews/microsoft/_285_Inorder_Successor_in_BST/Solution.java
@@ -0,0 +1,20 @@
+package interviews.microsoft._285_Inorder_Successor_in_BST;
+
+import utils.TreeNode;
+
+public class Solution {
+
+  public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+    TreeNode successor = null;
+    while (root != null) {
+      if (root.val <= p.val) {
+        root = root.right;
+      } else {
+        successor = root;
+        root = root.left;
+      }
+    }
+    return successor;
+  }
+
+}
---
+++ src/test/java/interviews/microsoft/_285_Inorder_Successor_in_BST/SolutionTest.java
@@ -0,0 +1,31 @@
+package interviews.microsoft._285_Inorder_Successor_in_BST;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOne() {
+    final TreeNode root = TreeNodes.getTreeLevelOrder(1);
+    assertNull(solution.inorderSuccessor(root, root));
+  }
+
+  @Test
+  void testThree() {
+    final TreeNode root = TreeNodes.getTreeLevelOrder(2, 1, 3);
+    assertEquals(root, solution.inorderSuccessor(root, root.left));
+  }
+
+}
