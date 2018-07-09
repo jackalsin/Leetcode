53cf7afa13ef29c00482c4929a2732e61f35b156
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-08 22:44:34 -0700

    Finish 235

---
+++ src/main/java/interviews/microsoft/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.java
@@ -0,0 +1,23 @@
+package interviews.microsoft._235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
+
+import utils.TreeNode;
+
+public class Solution {
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (p.val > q.val) {
+      return lowestCommonAncestor(root, q, p);
+    }
+
+    if (root.val < p.val) {
+      return lowestCommonAncestor(root.right, p, q);
+    } else if (root.val == p.val) {
+      return root;
+    } else if (root.val < q.val) {
+      return root;
+    } else if (root.val == q.val) {
+      return root;
+    } else {
+      return lowestCommonAncestor(root.left, p, q);
+    }
+  }
+}
