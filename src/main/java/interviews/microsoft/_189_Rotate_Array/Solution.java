985a0a62488f5b469092cde1b68d6a7ffabd337a
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-09 16:51:27 -0700

    Finish 189

---
+++ src/main/java/interviews/microsoft/_189_Rotate_Array/Solution.java
@@ -0,0 +1,20 @@
+package interviews.microsoft._189_Rotate_Array;
+
+public class Solution {
+  public void rotate(int[] nums, int k) {
+    k %= nums.length;
+    reverse(nums, nums.length - k, nums.length - 1);
+    reverse(nums, 0, nums.length - k - 1);
+    reverse(nums, 0, nums.length - 1);
+  }
+
+  private static void reverse(int[] nums, int i, int j) {
+    while (i < j) {
+      int tmp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = tmp;
+      i++;
+      j--;
+    }
+  }
+}
