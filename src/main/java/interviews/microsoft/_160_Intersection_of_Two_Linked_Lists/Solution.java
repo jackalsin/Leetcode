dd93c0cc7e2b4f3f9d76429f4ad3703647a5b8cc
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-10 15:06:35 -0700

    Finish 160

---
+++ src/main/java/interviews/microsoft/_160_Intersection_of_Two_Linked_Lists/Solution.java
@@ -0,0 +1,36 @@
+package interviews.microsoft._160_Intersection_of_Two_Linked_Lists;
+
+import utils.ListNode;
+
+public class Solution {
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    if (headB == null || headA == null) {
+      return null;
+    }
+    ListNode tail = headB;
+    while (tail.next != null) {
+      tail = tail.next;
+    }
+    tail.next = headB;
+
+    // head
+    ListNode slow = headA, fast = headA, entry = headA;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+
+      if (slow == fast) {
+        while (slow != entry) {
+          slow = slow.next;
+          entry = entry.next;
+        }
+        tail.next = null;
+        return entry;
+      }
+    }
+
+    // recover
+    tail.next = null;
+    return null;
+  }
+}
