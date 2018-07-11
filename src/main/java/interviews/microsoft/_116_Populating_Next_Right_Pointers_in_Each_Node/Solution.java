074fc0be546c0ad9b78bdbbc6600e29aa335b6e5
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-11 08:30:16 -0700

    Finish 116

---
+++ src/main/java/interviews/microsoft/_116_Populating_Next_Right_Pointers_in_Each_Node/Solution.java
@@ -0,0 +1,26 @@
+package interviews.microsoft._116_Populating_Next_Right_Pointers_in_Each_Node;
+
+import utils.TreeLinkNode;
+
+public class Solution { // TODO:
+
+  public void connect(TreeLinkNode root) {
+    if (root == null) {
+      return;
+    }
+    TreeLinkNode levelHead = root, curNode = root;
+    while (levelHead.left != null) {
+      curNode = levelHead;
+      while (curNode != null) {
+        curNode.left.next = curNode.right;
+        if (curNode.next != null) {
+          curNode.right.next = curNode.next.left;
+        }
+        curNode = curNode.next;
+      }
+
+      levelHead = levelHead.left;
+    }
+  }
+
+}
---
+++ src/test/java/interviews/microsoft/_116_Populating_Next_Right_Pointers_in_Each_Node/SolutionTest.java
@@ -0,0 +1,45 @@
+package interviews.microsoft._116_Populating_Next_Right_Pointers_in_Each_Node;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.TreeLinkNode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase() {
+    TreeLinkNode expected = new TreeLinkNode(1);
+    expected.left = new TreeLinkNode(2);
+    expected.right = new TreeLinkNode(3);
+    expected.left.next = expected.right;
+
+    expected.left.left = new TreeLinkNode(4);
+    expected.left.right = new TreeLinkNode(5);
+    expected.right.left = new TreeLinkNode(6);
+    expected.right.right = new TreeLinkNode(7);
+
+    expected.left.left.next = expected.left.right;
+    expected.left.right.next = expected.right.left;
+    expected.right.left.next = expected.right.right;
+
+    TreeLinkNode root = new TreeLinkNode(1);
+    root.left = new TreeLinkNode(2);
+    root.right = new TreeLinkNode(3);
+
+    root.left.left = new TreeLinkNode(4);
+    root.left.right = new TreeLinkNode(5);
+    root.right.left = new TreeLinkNode(6);
+    root.right.right = new TreeLinkNode(7);
+    solution.connect(root);
+
+    assertEquals(expected, root);
+  }
+}
