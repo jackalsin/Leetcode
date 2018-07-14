b7f7c06cf70603d2e120f50dbcce0e73bbffab17
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-13 22:23:51 -0700

    Finish 106

---
+++ src/main/java/interviews/microsoft/_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/Solution.java
@@ -0,0 +1,35 @@
+package interviews.microsoft._106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal;
+
+import utils.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+  public TreeNode buildTree(int[] inorder, int[] postorder) {
+    final Map<Integer, Integer> valToIndex = new HashMap<>();
+    for (int i = 0; i < inorder.length; i++) {
+      valToIndex.put(inorder[i], i);
+    }
+
+    return buildTree(valToIndex, inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+  }
+
+  private TreeNode buildTree(final Map<Integer, Integer> valToIndex, final int[] inorder, final int is, final int ie,
+                             int[] postorder, int ps, int pe) {
+    if (is > ie || ps > pe) {
+      return null;
+    }
+
+    final int rootIndex = valToIndex.get(postorder[pe]);
+    // pe 这一行满足 pe - ps = rootIndex - 1 - is,
+    final TreeNode left = buildTree(valToIndex, inorder, is, rootIndex - 1, postorder, ps, rootIndex - 1 - is + ps);
+    final TreeNode right = buildTree(valToIndex, inorder, rootIndex + 1, ie, postorder, rootIndex - is + ps, pe - 1);
+
+    final TreeNode root = new TreeNode(postorder[pe]);
+    root.left = left;
+    root.right = right;
+
+    return root;
+  }
+}
---
+++ src/test/java/interviews/microsoft/_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/SolutionTest.java
@@ -0,0 +1,26 @@
+package interviews.microsoft._106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void test3Levels() {
+    final int[] inorder = new int[]{1, 2, 3, 4, 5, 6, 7};
+    final int[] levelOrder = new int[]{4, 2, 6, 1, 3, 5, 7};
+    final int[] postOrder = new int[]{1, 3, 2, 5, 7, 6, 4};
+    final TreeNode root = TreeNodes.getTreeLevelOrder(levelOrder);
+    assertEquals(root, solution.buildTree(inorder, postOrder));
+  }
+}
