5a6514dbba088bb002b5299f70089a10568e4c82
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-14 14:45:38 -0700

    Finish 088

---
+++ src/main/java/interviews/microsoft/_088_Merge_Sorted_Array/Solution.java
@@ -0,0 +1,20 @@
+package interviews.microsoft._088_Merge_Sorted_Array;
+
+public class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int k = m + n - 1, i = m - 1, j = n - 1;
+    while (i >= 0 || j >= 0) {
+      if (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+          nums1[k--] = nums1[i--];
+        } else {
+          nums1[k--] = nums2[j--];
+        }
+      } else if (i < 0) {
+        nums1[k--] = nums2[j--];
+      } else {
+        nums1[k--] = nums1[i--];
+      }
+    }
+  }
+}
---
+++ src/test/java/interviews/microsoft/_088_Merge_Sorted_Array/SolutionTest.java
@@ -0,0 +1,42 @@
+package interviews.microsoft._088_Merge_Sorted_Array;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testBothFinish() {
+    final int[] nums1 = new int[]{1, 3, 5, 0, 0, 0};
+    final int[] nums2 = new int[]{2, 4, 6};
+    solution.merge(nums1, 3, nums2, 3);
+    assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, nums1);
+  }
+
+  @Test
+  void test1FinishFirst() {
+    final int[] nums1 = new int[]{1, 0, 0, 0};
+    final int[] nums2 = new int[]{-1, 2, 4};
+    final int[] expected = new int[]{-1, 1, 2, 4};
+    solution.merge(nums1, 1, nums2, 3);
+    assertArrayEquals(expected, nums1);
+  }
+
+  @Test
+  void test2FinishFirst() {
+    final int[] nums1 = new int[]{1, 3, 4, 0};
+    final int[] nums2 = new int[]{2};
+    final int[] expected = new int[]{1, 2, 3, 4};
+    solution.merge(nums1, 3, nums2, 1);
+    assertArrayEquals(expected, nums1);
+  }
+
+}
