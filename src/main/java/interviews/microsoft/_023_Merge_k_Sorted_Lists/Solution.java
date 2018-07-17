bcd85c22dd3802860aa8a4ecf71b541a71212322
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-17 12:40:21 -0700

    Finish 023

---
+++ src/main/java/interviews/microsoft/_023_Merge_k_Sorted_Lists/Solution.java
@@ -0,0 +1,37 @@
+package interviews.microsoft._023_Merge_k_Sorted_Lists;
+
+import utils.ListNode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Solution {
+  public ListNode mergeKLists(ListNode[] lists) {
+    final Queue<ListNode> queue = new PriorityQueue<>(new Comparator<ListNode>() {
+      @Override
+      public int compare(ListNode l1, ListNode l2) {
+        return Integer.compare(l1.val, l2.val);
+      }
+
+    });
+
+    for (ListNode listNode : lists) {
+      if (listNode != null) {
+        queue.add(listNode);
+      }
+    }
+
+    final ListNode dummy = new ListNode(1);
+    ListNode cur = dummy;
+    while (!queue.isEmpty()) {
+      final ListNode toRemove = queue.remove();
+      cur.next = new ListNode(toRemove.val);
+      if (toRemove.next != null) {
+        queue.add(toRemove.next);
+      }
+      cur = cur.next;
+    }
+    return dummy.next;
+  }
+}
