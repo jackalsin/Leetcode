584fac3b432cd33fc8829bc9718f4016f6a58b41
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-17 22:59:58 -0700

    Finish 871

---
+++ src/main/java/_851_900/_871_Minimum_Number_of_Refueling_Stops/Solution.java
@@ -0,0 +1,11 @@
+package _851_900._871_Minimum_Number_of_Refueling_Stops;
+
+public interface Solution {
+  /**
+   * @param target
+   * @param startFuel
+   * @param stations
+   * @return
+   */
+  int minRefuelStops(int target, int startFuel, int[][] stations);
+}
