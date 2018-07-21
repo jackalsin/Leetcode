8d4eb68113d39464640d4f129e5c59fb33c36a84
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-21 14:50:51 -0700

    Add slack notification
    Add slack notification

---
+++ src/main/java/_651_700/_653_Two_Sum_IV_Input_is_a_BST/Solution.java
@@ -0,0 +1,21 @@
+package _651_700._653_Two_Sum_IV_Input_is_a_BST;
+
+import utils.TreeNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+  private final Set<Integer> visited = new HashSet<>();
+
+  public boolean findTarget(TreeNode root, int k) {
+    if (root == null) {
+      return false;
+    }
+    if (visited.contains(k - root.val)) {
+      return true;
+    }
+    visited.add(root.val);
+    return findTarget(root.left, k) || findTarget(root.right, k);
+  }
+}
---
+++ src/test/java/_651_700/_653_Two_Sum_IV_Input_is_a_BST/SolutionTest.java
@@ -0,0 +1,32 @@
+package _651_700._653_Two_Sum_IV_Input_is_a_BST;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final TreeNode root = TreeNodes.getTreeLevelOrder(5, 3, 6, 2, 4, null, 7);
+    assertTrue(solution.findTarget(root, 9));
+  }
+
+
+  @Test
+  void testOnlineCase2() {
+    final TreeNode root = TreeNodes.getTreeLevelOrder(5, 3, 6, 2, 4, null, 7);
+    assertFalse(solution.findTarget(root, 28));
+  }
+
+}
