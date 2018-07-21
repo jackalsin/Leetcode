a8d25084cdb4a47ce69d18efafea00cf329b4ac4
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-21 14:25:30 -0700

    Finish 637

---
+++ src/test/java/_601_650/_637_Average_of_Levels_in_Binary_Tree/SolutionTest.java
@@ -0,0 +1,29 @@
+package _601_650._637_Average_of_Levels_in_Binary_Tree;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final TreeNode root = TreeNodes.getTreeLevelOrder(3, 9, 20, null, null, 15, 7);
+    final List<Double> expected = List.of(3d, 14.5, 11d);
+    final List<Double> actual = solution.averageOfLevels(root);
+    assertEquals(expected, actual);
+  }
+
+
+}
