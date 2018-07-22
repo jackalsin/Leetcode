2dd2cdf4f48bee3ac6d92b156ef4224df62ddbe6
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-22 14:51:56 -0700

    Finish 680

---
+++ src/main/java/_651_700/_680_Valid_Palindrome_II/Solution.java
@@ -0,0 +1,23 @@
+package _651_700._680_Valid_Palindrome_II;
+
+public class Solution {
+  public boolean validPalindrome(String s) {
+    return validPalindrome(s, 0, s.length() - 1, false);
+  }
+
+  private static boolean validPalindrome(String s, int left, int right, boolean hasDelete) {
+    while (left < right) {
+      if (s.charAt(left) == s.charAt(right)) {
+        left++;
+        right--;
+        continue;
+      }
+      if (hasDelete) {
+        return false;
+      }
+      return validPalindrome(s, left + 1, right, true) ||
+          validPalindrome(s, left, right - 1, true);
+    }
+    return true;
+  }
+}
---
+++ src/test/java/_651_700/_673_Number_of_Longest_Increasing_Subsequence/SolutionTest.java
@@ -0,0 +1,48 @@
+package _651_700._673_Number_of_Longest_Increasing_Subsequence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+//    solution = new MySolution();
+//    solution = new SegmentTreeSolutionRef();
+    solution = new SegmentTreeSolution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    // 1, 3, 5, 7
+    // 1, 3, 4, 7
+    final int[] nums = {1, 3, 5, 4, 7};
+    assertEquals(2, solution.findNumberOfLIS(nums));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    final int[] nums = {2, 2, 2, 2, 2};
+    assertEquals(5, solution.findNumberOfLIS(nums));
+  }
+
+  @Test
+  void testOnlineCase3() {
+    // 1, 2, 4, 5, 7
+    // 1, 2, 3, 5, 7
+    // 1, 2, 3, 4, 7
+    final int[] nums = {1, 2, 4, 3, 5, 4, 7, 2};
+    assertEquals(3, solution.findNumberOfLIS(nums));
+  }
+
+  @Test
+  void testOnlineCase4() {
+    // 1, 2, 4
+    // 1, 2, 3
+    final int[] nums = {1, 2, 4, 3};
+    assertEquals(2, solution.findNumberOfLIS(nums));
+  }
+}
