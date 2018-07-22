16e8ddd37ea42d3f6d1a0c832e36e454b65761c5
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-21 18:21:11 -0700

    Finish 670

---
+++ src/main/java/_651_700/_670_Maximum_Swap/Solution.java
@@ -0,0 +1,36 @@
+package _651_700._670_Maximum_Swap;
+
+public class Solution {
+  /**
+   * 111 / 111 test cases passed.
+   * Status: Accepted
+   * Runtime: 5 ms
+   *
+   * @param num
+   * @return
+   */
+  public int maximumSwap(final int num) {
+    assert num >= 0;
+    final char[] chars = String.valueOf(num).toCharArray();
+    int[] digitToPos = new int[10];
+    for (int i = 0; i < chars.length; i++) {
+      digitToPos[chars[i] - '0'] = i;
+    }
+
+    for (int i = 0; i < chars.length; i++) {
+      final int digit = chars[i] - '0';
+      for (int k = 9; k > digit; k--) {
+        if (digitToPos[k] > i) {
+          char tmp = chars[digitToPos[k]];
+          chars[digitToPos[k]] = chars[i];
+          chars[i] = tmp;
+
+          return Integer.valueOf(String.valueOf(chars));
+        }
+      }
+    }
+
+    return num;
+  }
+
+}
---
+++ src/test/java/_651_700/_670_Maximum_Swap/SolutionTest.java
@@ -0,0 +1,31 @@
+package _651_700._670_Maximum_Swap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    assertEquals(7236, solution.maximumSwap(2736));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    assertEquals(9972, solution.maximumSwap(9972));
+  }
+
+  @Test
+  void testOnlineCase3() {
+    assertEquals(9972, solution.maximumSwap(9927));
+  }
+
+}
