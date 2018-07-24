ae75d4b8c6e5901029aa58e59f33ecce0ca2d26c
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-23 22:16:27 -0700

    Finish 750

---
+++ src/test/java/_701_750/_750_Number_Of_Corner_Rectangles/SolutionITest.java
@@ -0,0 +1,43 @@
+package _701_750._750_Number_Of_Corner_Rectangles;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionITest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new SolutionI();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final int[][] nums = {
+        {1, 0, 0, 1, 0},
+        {0, 0, 1, 0, 1},
+        {0, 0, 0, 1, 0},
+        {1, 0, 1, 0, 1}
+    };
+    assertEquals(1, solution.countCornerRectangles(nums));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    final int[][] nums = {
+        {1, 1, 1},
+        {1, 1, 1},
+        {1, 1, 1}
+    };
+    assertEquals(9, solution.countCornerRectangles(nums));
+  }
+
+  @Test
+  void testOnlineCase3() {
+    final int[][] nums = {{1, 1, 1, 1}};
+    assertEquals(0, solution.countCornerRectangles(nums));
+  }
+
+}
