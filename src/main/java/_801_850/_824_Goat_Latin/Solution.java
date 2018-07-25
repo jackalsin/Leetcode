dbcfd3a59e08807b8378ad2b70fa38c5e876271e
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-25 08:46:00 -0700

    Finish 824

---
+++ src/main/java/_801_850/_824_Goat_Latin/Solution.java
@@ -0,0 +1,43 @@
+package _801_850._824_Goat_Latin;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+  private static final Set<Character> VOWEL = new HashSet<>(
+      Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+  );
+
+  public String toGoatLatin(String S) {
+    int wordCount = 0;
+    final StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < S.length(); i++) {
+      final char chr = S.charAt(i);
+      if (chr == ' ') {
+        sb.append(' ');
+      } else {
+        wordCount++;
+        int start = i;
+        while (i + 1 < S.length() && S.charAt(i + 1) != ' ') {
+          i++;
+        }
+        String word = S.substring(start, i + 1);
+        if (VOWEL.contains(chr)) {
+          sb.append(word);
+        } else {
+          sb.append(word.substring(1)).append(word.charAt(0));
+        }
+
+        sb.append("ma");
+        // append a
+        for (int aCount = 0; aCount < wordCount; aCount++) {
+          sb.append('a');
+        }
+      }
+
+    }
+    return sb.toString();
+  }
+
+}
---
+++ src/test/java/_801_850/_824_Goat_Latin/SolutionTest.java
@@ -0,0 +1,29 @@
+package _801_850._824_Goat_Latin;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final String input = "I speak Goat Latin",
+        expected = "Imaa peaksmaaa oatGmaaaa atinLmaaaaa";
+    assertEquals(expected, solution.toGoatLatin(input));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    final String input = "The quick brown fox jumped over the lazy dog",
+        expected = "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa";
+    assertEquals(expected, solution.toGoatLatin(input));
+  }
+}
