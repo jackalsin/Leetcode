c9b861226c86ceaef1054dc327bff158624d4fc5
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-26 20:03:20 -0700

    Finish 013

---
+++ src/main/java/interviews/facebook/_013_Roman_to_Integer/Solution.java
@@ -0,0 +1,45 @@
+package interviews.facebook._013_Roman_to_Integer;
+
+public class Solution {
+
+  private static final int I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000;
+
+  public int romanToInt(String s) {
+    int res = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.startsWith("IX", i) || s.startsWith("IV", i)) {
+        res -= 1;
+      } else if (s.startsWith("XL", i) || s.startsWith("XC", i)) {
+        res -= 10;
+      } else if (s.startsWith("CD", i) || s.startsWith("CM", i)) {
+        res -= 100;
+      } else {
+        final char chr = s.charAt(i);
+        switch (chr) {
+          case 'I':
+            res += I;
+            break;
+          case 'V':
+            res += V;
+            break;
+          case 'X':
+            res += X;
+            break;
+          case 'L':
+            res += L;
+            break;
+          case 'C':
+            res += C;
+            break;
+          case 'D':
+            res += D;
+            break;
+          case 'M':
+            res += M;
+            break;
+        }
+      }
+    }
+    return res;
+  }
+}
---
+++ src/test/java/interviews/facebook/_013_Roman_to_Integer/SolutionTest.java
@@ -0,0 +1,22 @@
+package interviews.facebook._013_Roman_to_Integer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOneDigit() {
+    assertEquals(9, solution.romanToInt("IX"));
+  }
+
+
+}
