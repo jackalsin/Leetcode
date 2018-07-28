16884be760ec779e234dcf0f6d47f4a9fbda1435
zhiwei.xin <zhiwei.xin@veritas.com>
2018-07-28 15:20:42 -0700

    Finish 080

---
+++ src/main/java/_051_100/_080_Remove_Duplicates_from_Sorted_Array_II/OnePassSolution.java
@@ -0,0 +1,17 @@
+package _051_100._080_Remove_Duplicates_from_Sorted_Array_II;
+
+/**
+ * @author jacka
+ * @version 1.0 on 5/14/2017.
+ */
+public final class OnePassSolution implements Solution {
+  public int removeDuplicates(int[] nums) {
+    int nextPtr = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (i < 2 || nums[i] != nums[nextPtr - 2]) {
+        nums[nextPtr++] = nums[i];
+      }
+    }
+    return nextPtr;
+  }
+}
---
+++ src/main/java/_051_100/_080_Remove_Duplicates_from_Sorted_Array_II/TwoPassSolution.java
@@ -0,0 +1,29 @@
+package _051_100._080_Remove_Duplicates_from_Sorted_Array_II;
+
+/**
+ * @author jacka
+ * @version 1.0 on 5/14/2017.
+ */
+public final class TwoPassSolution implements Solution {
+  public int removeDuplicates(int[] nums) {
+    int left = 0;
+    int right = 0;
+    while (right < nums.length) {
+      int count = getCount(nums, right);
+      int step = count >= 2 ? 2 : 1;
+      while (step-- > 0) {
+        nums[left++] = nums[right];
+      }
+      right += count;
+    }
+    return left; //check 0;
+
+  }
+
+  int getCount(int[] nums, int rightIndex) {
+    int curVal = nums[rightIndex];
+    int count = 0;
+    while (rightIndex < nums.length && curVal == nums[rightIndex++]) count++;
+    return count;
+  }
+}
