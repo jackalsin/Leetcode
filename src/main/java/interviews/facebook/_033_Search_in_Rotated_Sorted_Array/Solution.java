1c4cc6adbdd196314e454180d5e8a8a55f39706d
jackalsin <jackal.sin@hotmail.com>
2018-07-29 08:24:28 -0700

    Finish 033

---
+++ src/main/java/interviews/facebook/_033_Search_in_Rotated_Sorted_Array/Solution.java
@@ -0,0 +1,40 @@
+package interviews.facebook._033_Search_in_Rotated_Sorted_Array;
+
+public class Solution {
+  public int search(int[] nums, int target) {
+    int left = 0, right = nums.length - 1;
+    while (left <= right) {
+      final int mid = (right - left) / 2 + left;
+      if (nums[mid] < nums[right]) {
+        if (nums[mid] <= target && target <= nums[right]) {
+          return binarySearch(nums, target, mid, right);
+        } else {
+          right = mid - 1;
+        }
+      } else {
+        if (nums[left] <= target && target <= nums[mid]) {
+          return binarySearch(nums, target, left, mid);
+        } else {
+          left = mid + 1;
+        }
+      }
+    }
+
+    return -1;
+  }
+
+  private static int binarySearch(final int[] nums, final int target, int left, int right) {
+    while (left <= right) {
+      final int mid = (right - left) / 2 + left;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+}
