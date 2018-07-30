8d1fc87f6ee36887da3c7ce21d003de515186a07
jackalsin <jackal.sin@hotmail.com>
2018-07-29 18:58:37 -0700

    Finish 067

--- src/test/java/_051_100/_067_Add_Binary/SolutionTest.java
+++ src/test/java/_051_100/_067_Add_Binary/SolutionTest.java
@@ -1,7 +1,7 @@
 package _051_100._067_Add_Binary;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,29 +9,30 @@ import static org.junit.Assert.assertEquals;
  * @author jacka
  * @version 1.0 on 4/26/2017.
  */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
-  @Before
-  public void setUp() throws Exception {
+
+  @BeforeEach
+  void setUp() {
     solution = new Solution();
   }
 
   @Test
-  public void testWithNoCarry() throws Exception {
+  void testWithNoCarry() {
     String a = "1";
     String b = "1";
     assertEquals("10", solution.addBinary(a, b));
   }
 
   @Test
-  public void testWithCarry() throws Exception {
+  void testWithCarry() {
     String a = "1";
     String b = "0";
     assertEquals("1", solution.addBinary(a, b));
   }
 
   @Test
-  public void testWithDifferentLens() throws Exception {
+  void testWithDifferentLens() {
     String a = "11";
     String b = "1";
     assertEquals("100", solution.addBinary(a, b));
