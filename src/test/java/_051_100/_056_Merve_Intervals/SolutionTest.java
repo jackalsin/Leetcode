b240abfa22804053794673dafbe5d59a9a895c91
jackalsin <jackal.sin@hotmail.com>
2018-07-29 18:33:59 -0700

    Finish 056

--- src/test/java/_051_100/_056_Merve_Intervals/SolutionTest.java
+++ src/test/java/_051_100/_056_Merve_Intervals/SolutionTest.java
@@ -1,11 +1,10 @@
 package _051_100._056_Merve_Intervals;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.Interval;
 
 import java.util.List;
-
-import utils.Interval;
 
 import static org.junit.Assert.assertEquals;
 import static utils.Util.getIntervals;
@@ -14,18 +13,19 @@ import static utils.Util.getIntervals;
  * @author Zhiwei.Xin
  * @version 1.0 on 4/20/2017.
  */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
-  @Before
-  public void setUp() throws Exception {
+
+  @BeforeEach
+  void setUp() {
     solution = new Solution();
   }
 
   @Test
-  public void testWithOnlineCase1() throws Exception {
-    List<Interval> actual = solution.merge(getIntervals(new int[][]{ {1,3}, {2, 6}, {8, 10}, {15,
-    18}}));
-    assertEquals(getIntervals(new int[][]{{1,6}, {8, 10}, {15, 18}}), actual);
+  void testWithOnlineCase1() {
+    List<Interval> actual = solution.merge(getIntervals(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15,
+        18}}));
+    assertEquals(getIntervals(new int[][]{{1, 6}, {8, 10}, {15, 18}}), actual);
   }
 
 
