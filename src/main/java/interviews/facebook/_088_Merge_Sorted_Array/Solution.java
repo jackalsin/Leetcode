18fec33375b03c2242ae87fccab49550343cbd48
jackalsin <jackal.sin@hotmail.com>
2018-07-30 18:25:04 -0700

    Finish 088

---
+++ src/main/java/interviews/facebook/_088_Merge_Sorted_Array/Solution.java
@@ -0,0 +1,19 @@
+package interviews.facebook._088_Merge_Sorted_Array;
+
+public class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    for (int i = m - 1, j = n - 1, k = m + n - 1; i >= 0 || j >= 0; k--) {
+      if (i >= 0 && j >= 0) {
+        if (nums1[i] < nums2[j]) {
+          nums1[k] = nums2[j--];
+        } else {
+          nums1[k] = nums1[i--];
+        }
+      } else if (i >= 0) {
+        nums1[k] = nums1[i--];
+      } else {
+        nums1[k] = nums2[j--];
+      }
+    }
+  }
+}
