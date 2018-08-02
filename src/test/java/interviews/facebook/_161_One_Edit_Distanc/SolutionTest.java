ab7746892725596b85a722361b467ec2f52a9fd6
jackalsin <jackal.sin@hotmail.com>
2018-08-01 21:51:53 -0700

    Finish 161

---
+++ src/test/java/interviews/facebook/_161_One_Edit_Distanc/SolutionTest.java
@@ -0,0 +1,36 @@
+package interviews.facebook._161_One_Edit_Distanc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testEmptyWithEmpty() {
+    assertFalse(solution.isOneEditDistance("", ""));
+  }
+
+  @Test
+  void test1With0() {
+    assertTrue(solution.isOneEditDistance("1", ""));
+  }
+
+  @Test
+  void test2With1() {
+    assertTrue(solution.isOneEditDistance("12", "1"));
+  }
+
+  @Test
+  void test4With4() {
+    assertTrue(solution.isOneEditDistance("1234", "1233"));
+  }
+}
