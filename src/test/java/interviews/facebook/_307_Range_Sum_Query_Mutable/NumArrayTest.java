7e83f03b78c20d6d3578d6de45b0ad5a30602c08
jackalsin <jackal.sin@hotmail.com>
2018-08-05 14:49:34 -0700

    Finish 307

---
+++ src/test/java/interviews/facebook/_307_Range_Sum_Query_Mutable/NumArrayTest.java
@@ -0,0 +1,18 @@
+package interviews.facebook._307_Range_Sum_Query_Mutable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NumArrayTest {
+  private NumArray solution;
+
+  @Test
+  void testOnlineCase1() {
+    final int[] input = {1, 3, 5};
+    solution = new NumArray(input);
+    assertEquals(9, solution.sumRange(0, 2));
+    solution.update(1, 2);
+    assertEquals(8, solution.sumRange(0, 2));
+  }
+}
