1298ad4844215ab5a7afe04870017b79b132465c
jackalsin <jackal.sin@hotmail.com>
2018-08-05 19:57:02 -0700

    Finish 493

---
+++ src/main/java/interviews/facebook/_493_Reverse_Pairs/BinaryIndexTreeSolution.java
@@ -0,0 +1,60 @@
+package interviews.facebook._493_Reverse_Pairs;
+
+import java.util.Arrays;
+
+
+public final class BinaryIndexTreeSolution implements Solution {
+
+  public int reversePairs(int[] nums) {
+    final int[] tree = new int[nums.length + 1];
+    final int[] sorted = new int[nums.length];
+    System.arraycopy(nums, 0, sorted, 0, nums.length);
+    Arrays.sort(sorted);
+    int res = 0;
+
+    // BIT[i] indicates the count of element in sorted
+    // that has been inserted to BIT
+    for (int j = 0; j < nums.length; j++) {
+      // 这个index之后的都符合
+      final int index = index(sorted, 2L * nums[j] + 1);
+      res += search(tree, index);
+      insert(tree, index(sorted, nums[j]));
+    }
+
+    return res;
+  }
+
+  private void insert(int[] tree, int i) {
+    ++i;
+    while (i > 0) {
+      tree[i] += 1;
+      i -= i & -i;
+    }
+  }
+
+  private int search(int[] tree, int i) {
+    ++i;
+    int res = 0;
+    while (i < tree.length) {
+      res += tree[i];
+      i += i & -i;
+    }
+    return res;
+  }
+
+  private int index(int[] nums, long val) {
+    int left = 0, right = nums.length - 1;
+    while (left <= right) {
+      final int mid = (right - left) / 2 + left;
+      if (nums[mid] == val) {
+        return mid;
+      } else if (nums[mid] < val) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return left + 1;
+  }
+
+}
---
+++ src/test/java/interviews/facebook/_493_Reverse_Pairs/BinaryIndexTreeSolutionTest.java
@@ -0,0 +1,36 @@
+package interviews.facebook._493_Reverse_Pairs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class BinaryIndexTreeSolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+//    solution = new MergeSortSolution();
+    solution = new BinaryIndexTreeSolution();
+//    solution = new BinaryIndexTreeSolutionSample();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final int[] nums = {1, 3, 2, 3, 1};
+    assertEquals(2, solution.reversePairs(nums));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    final int[] nums = {2, 4, 3, 5, 1};
+    assertEquals(3, solution.reversePairs(nums));
+  }
+
+  @Test
+  void testOnlineCase3() {
+    final int[] nums = {5, 4, 3, 2, 1};
+    assertEquals(4, solution.reversePairs(nums));
+  }
+
+}
