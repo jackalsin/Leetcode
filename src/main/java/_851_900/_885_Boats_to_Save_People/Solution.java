e3254a2dbaa891bfc2c6c553917f47cef5f1ea60
jackalsin <jackal.sin@hotmail.com>
2018-08-06 08:30:27 -0700

    Finish 885

---
+++ src/main/java/_851_900/_885_Boats_to_Save_People/Solution.java
@@ -0,0 +1,39 @@
+package _851_900._885_Boats_to_Save_People;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution {
+  public int numRescueBoats(int[] people, int limit) {
+    final TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+
+    for (int p : people) {
+      treeMap.put(p, treeMap.getOrDefault(p, 0) + 1);
+    }
+
+    int count = 0, curBoatWeight = 0, curBoatPeople = 0;
+
+    while (!treeMap.isEmpty()) {
+      final Map.Entry<Integer, Integer> weightToCount = treeMap.floorEntry(limit - curBoatWeight);
+      if (weightToCount == null || curBoatPeople == 2) {
+        count++;
+        curBoatWeight = 0;
+        curBoatPeople = 0;
+      } else {
+        if (weightToCount.getValue() == 1) {
+          treeMap.remove(weightToCount.getKey());
+        } else {
+          treeMap.put(weightToCount.getKey(), weightToCount.getValue() - 1);
+        }
+        curBoatWeight += weightToCount.getKey();
+        curBoatPeople++;
+      }
+    }
+
+    if (curBoatWeight != 0) {
+      count++;
+    }
+
+    return count;
+  }
+}
---
+++ src/test/java/_851_900/_885_Boats_to_Save_People/SolutionTest.java
@@ -0,0 +1,41 @@
+package _851_900._885_Boats_to_Save_People;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final int[] input = {1, 2};
+    assertEquals(1, solution.numRescueBoats(input, 3));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    final int[] input = {3, 2, 2, 1};
+    assertEquals(3, solution.numRescueBoats(input, 3));
+  }
+
+  @Test
+  void testOnlineCase3() {
+    final int[] input = {3, 5, 3, 4};
+    assertEquals(4, solution.numRescueBoats(input, 5));
+  }
+
+  @Test
+  void testOnlineCase4() {
+    final int[] input = {3, 2, 3, 2, 2};
+    assertEquals(3, solution.numRescueBoats(input, 6));
+  }
+
+
+}
