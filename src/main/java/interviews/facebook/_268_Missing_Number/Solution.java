10db8771493f8e9c85a38b53ca8d995389c9f64a
jackalsin <jackal.sin@hotmail.com>
2018-08-06 08:36:59 -0700

    Finish 268

---
+++ src/main/java/interviews/facebook/_268_Missing_Number/Solution.java
@@ -0,0 +1,15 @@
+package interviews.facebook._268_Missing_Number;
+
+public class Solution {
+  public int missingNumber(int[] nums) {
+    int res = 0;
+    final int n = nums.length;
+    for (int i = 1; i <= n; i++) {
+      res ^= i;
+    }
+    for (int num : nums) {
+      res ^= num;
+    }
+    return res;
+  }
+}
