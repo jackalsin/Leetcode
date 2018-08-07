a916ec40cbe5e37c98b9058ff22ffc55ef2bd629
jackalsin <jackal.sin@hotmail.com>
2018-08-06 20:23:10 -0700

    Refactor 273

--- src/main/java/_251_300/_273_Integer_to_English_Words/Solution.java
+++ src/main/java/_251_300/_273_Integer_to_English_Words/Solution.java
@@ -4,50 +4,6 @@ package _251_300._273_Integer_to_English_Words;
  * @author Zhiwei.Xin
  * @version 1.0 on 8/30/2017.
  */
-public class Solution {
-  private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-  private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-  private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
-
-  public String numberToWords(int num) {
-    if (num == 0) return "Zero";
-    StringBuilder sb = new StringBuilder();
-    int power = 3;
-    while (num != 0) {
-      int divident = getPower(power);
-      String cur1000 = helper(num / divident);
-      if (!cur1000.isEmpty()) {
-        sb.append(cur1000);
-        if (!THOUSANDS[power].isEmpty()) {
-          sb.append(" ").append(THOUSANDS[power]);
-        }
-      }
-      num %= divident;
-      power--;
-    }
-
-    return sb.substring(1);
-  }
-
-  private int getPower(int power) {
-    int result = 1;
-    while (power-- != 0) {
-      result *= 1000;
-    }
-    return result;
-  }
-
-  private String helper(int num) {
-    if (num == 0) {
-      return "";
-    } else if (num < 20) {
-      return " " + LESS_THAN_20[num];
-    } else if (num < 100) {
-      return " " + TENS[num / 10] + helper(num % 10);
-    } else if (num < 1000) {
-      return " " + LESS_THAN_20[num / 100] + " Hundred" + helper(num % 100);
-    } else {
-      throw new IllegalStateException("Over 1000");
-    }
-  }
+public interface Solution {
+  String numberToWords(int num);
 }
--- src/test/java/_251_300/_273_Integer_to_English_Words/SolutionTest.java
+++ src/test/java/_251_300/_273_Integer_to_English_Words/SolutionTest.java
@@ -13,46 +13,46 @@ public class SolutionTest {
   private Solution solution;
   @Before
   public void setUp() throws Exception {
-    solution = new Solution();
+    solution = new IntuitiveSolution();
   }
 
   @Test
-  public void testZero() throws Exception {
+  public void testZero() {
     assertEquals("Zero", solution.numberToWords(0));
   }
 
   @Test
-  public void testOne() throws Exception {
+  public void testOne() {
     assertEquals("One", solution.numberToWords(1));
   }
 
   @Test
-  public void test27() throws Exception {
+  public void test27() {
     assertEquals("Twenty Seven", solution.numberToWords(27));
   }
   @Test
-  public void test127() throws Exception {
+  public void test127() {
     assertEquals("One Hundred Twenty Seven", solution.numberToWords(127));
   }
 
   @Test
-  public void test1227() throws Exception {
+  public void test1227() {
     assertEquals("One Thousand Two Hundred Twenty Seven", solution.numberToWords(1227));
   }
 
   @Test
-  public void test1234567() throws Exception {
+  public void test1234567() {
     assertEquals("One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven",
         solution.numberToWords(1234567));
   }
 
   @Test
-  public void testMillion() throws Exception {
+  public void testMillion() {
     assertEquals("One Million", solution.numberToWords(1_000_000));
   }
 
   @Test
-  public void testMillion2Hundred() throws Exception {
+  public void testMillion2Hundred() {
     assertEquals("One Million Two Hundred", solution.numberToWords(1_000_200));
   }
 }
