42b85b4a7601a80338c71990c975730fd31e7c43
jackalsin <jackal.sin@hotmail.com>
2018-08-11 08:48:43 -0700

    Merge to master

---
+++ src/main/java/interviews/facebook/_274_H_Index/Solution.java
@@ -0,0 +1,24 @@
+package interviews.facebook._274_H_Index;
+
+public class Solution {
+  public int hIndex(int[] citations) {
+    final int n = citations.length;
+    final int[] buckets = new int[citations.length + 1];
+    for (int c : citations) {
+      if (c >= n) buckets[n]++;
+      else {
+        buckets[c]++;
+      }
+    }
+
+    int counts = 0;
+    for (int h = buckets.length - 1; h >= 0; h--) {
+      counts += buckets[h];
+      if (counts >= h) {
+        return h;
+      }
+    }
+
+    throw new IllegalStateException("You shouldn't be here");
+  }
+}
---
+++ src/main/java/interviews/microsoft/noodleEssence/_088_Merge_Sorted_Array/Solution.java
@@ -0,0 +1,20 @@
+package interviews.microsoft.noodleEssence._088_Merge_Sorted_Array;
+
+public class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int i = m - 1, j = n - 1;
+    for (int k = m + n - 1; k >= 0; k--) {
+      if (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+          nums1[k] = nums1[i--];
+        } else {
+          nums1[k] = nums2[j--];
+        }
+      } else if (i >= 0) {
+        nums1[k] = nums1[i--];
+      } else {
+        nums1[k] = nums2[j--];
+      }
+    }
+  }
+}
