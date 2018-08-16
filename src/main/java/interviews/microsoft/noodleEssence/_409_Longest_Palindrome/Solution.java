cd95be406058a829153d1a43956e4456038ee18c
jackalsin <jackal.sin@hotmail.com>
2018-08-15 20:29:06 -0700

    Finish 409

--- src/main/java/interviews/microsoft/noodleEssence/_409_Longest_Palindrome/Solution.java
+++ src/main/java/interviews/microsoft/noodleEssence/_409_Longest_Palindrome/Solution.java
@@ -1,4 +1,22 @@
 package interviews.microsoft.noodleEssence._409_Longest_Palindrome;
 
-public class Solution { /// TODO:
+public class Solution {
+  public int longestPalindrome(String s) {
+    final int[] counts = new int[256];
+    int maxLen = 0;
+    int oddCount = 0;
+    for (char chr : s.toCharArray()) {
+      if (counts[chr] == 0) {
+        counts[chr]++;
+        oddCount++;
+      } else {
+        maxLen += 2;
+        counts[chr]--;
+        oddCount--;
+      }
+    }
+
+    return maxLen + (oddCount != 0 ? 1 : 0);
+  }
+
 }
---
+++ src/test/java/interviews/microsoft/noodleEssence/_409_Longest_Palindrome/SolutionTest.java
@@ -0,0 +1,21 @@
+package interviews.microsoft.noodleEssence._409_Longest_Palindrome;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase() {
+    final String input = "abccccdd";
+    assertEquals(7, solution.longestPalindrome(input));
+  }
+}
