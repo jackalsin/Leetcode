ddd36dbcfc08a52d08028f21052160d8835f4d6f
jackalsin <jackal.sin@hotmail.com>
2018-08-17 22:21:53 -0700

    Finish secondLargestBST

---
+++ src/main/java/interviews/microsoft/noodleEssence/econdLargestInBST/Solution.java
@@ -0,0 +1,33 @@
+package interviews.microsoft.noodleEssence.econdLargestInBST;
+
+import utils.TreeNode;
+
+public class Solution {
+
+  /**
+   * @param root
+   * @return
+   */
+  public int getSecondLargest(final TreeNode root) {
+    if (root == null) {
+      throw new NullPointerException();
+    }
+    TreeNode max = root, prev = null;
+    while (max.right != null) {
+      prev = max;
+      max = max.right;
+    }
+
+    if (max.left != null) { // find max in max.left
+      TreeNode cur = max.left;
+      while (cur.right != null) {
+        cur = cur.right;
+      }
+      return cur.val;
+    } else if (prev == null) {
+      throw new IllegalStateException("Not second larges");
+    } else {
+      return prev.val;
+    }
+  }
+}
