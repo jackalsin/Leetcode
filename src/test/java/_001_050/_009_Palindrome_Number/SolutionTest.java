23aa21413cc555b15f95117ecf0b90ac328151b1
jackalsin <jackal.sin@hotmail.com>
2018-08-18 20:02:22 -0700

    Add 009

--- src/test/java/_001_050/_009_Palindrome_Number/SolutionTest.java
+++ src/test/java/_001_050/_009_Palindrome_Number/SolutionTest.java
@@ -1,7 +1,7 @@
 package _001_050._009_Palindrome_Number;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -10,47 +10,47 @@ import static org.junit.Assert.assertTrue;
  * @author jacka
  * @version 1.0 on 2017/1/13.
  */
-public class SolutionTest {
+class SolutionTest {
 
   private Solution solution;
 
-  @Before
-  public void setUp() throws Exception {
+  @BeforeEach
+  void setUp() throws Exception {
     solution = new Solution();
   }
 
   @Test
-  public void testMinValue() throws Exception {
+  void testMinValue() throws Exception {
     assertFalse(solution.isPalindrome(Integer.MIN_VALUE));
   }
 
   @Test
-  public void testMaxValue() throws Exception {
+  void testMaxValue() throws Exception {
     assertFalse(solution.isPalindrome(Integer.MAX_VALUE));
   }
 
   @Test
-  public void testZero() throws Exception {
+  void testZero() throws Exception {
     assertTrue(solution.isPalindrome(0));
   }
 
   @Test
-  public void testSingleDigit() throws Exception {
+  void testSingleDigit() throws Exception {
     assertTrue(solution.isPalindrome(1));
   }
 
   @Test
-  public void testTwoDigits() throws Exception {
+  void testTwoDigits() throws Exception {
     assertTrue(solution.isPalindrome(11));
   }
 
   @Test
-  public void testThreeDigits() throws Exception {
+  void testThreeDigits() throws Exception {
     assertTrue(solution.isPalindrome(121));
   }
 
   @Test
-  public void testFourDigits() throws Exception {
+  void testFourDigits() throws Exception {
     assertTrue(solution.isPalindrome(1221));
   }
 }
