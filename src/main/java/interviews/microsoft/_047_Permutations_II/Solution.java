d56f74993af9e5e596cbf2c9a6b2a6854b674cb9
jackalsin <jackal.sin@hotmail.com>
2018-08-19 11:42:37 -0700

    Finish 047

--- src/main/java/interviews/microsoft/_047_Permutations_II/Solution.java
+++ src/main/java/interviews/microsoft/_047_Permutations_II/Solution.java
@@ -1,34 +1,8 @@
 package interviews.microsoft._047_Permutations_II;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class Solution {
-  public List<List<Integer>> permuteUnique(int[] nums) { // TODO: Add Documents with combination
-    final List<List<Integer>> result = new ArrayList<>();
-    Arrays.sort(nums);
-    permuteUnique(result, nums, new boolean[nums.length], new ArrayList<>());
-    return result;
-  }
-
-  private static void permuteUnique(final List<List<Integer>> result, final int[] nums, boolean[] visited,
-                                    final List<Integer> curPath) {
-    if (curPath.size() == nums.length) {
-      result.add(new ArrayList<>(curPath));
-      return;
-    }
-    for (int i = 0; i < nums.length; i++) {
-      if (visited[i]) continue;
-      /* if the same value, you can only use iff the previous one is used */
-      if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
-      curPath.add(nums[i]);
-      visited[i] = true;
-      permuteUnique(result, nums, visited, curPath);
-      visited[i] = false;
-      curPath.remove(curPath.size() - 1);
-    }
-
-  }
+public interface Solution {
+  List<List<Integer>> permuteUnique(int[] nums);
 }
 
---
+++ src/main/java/interviews/microsoft/_047_Permutations_II/SolutionII.java
@@ -0,0 +1,34 @@
+package interviews.microsoft._047_Permutations_II;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public final class SolutionII implements Solution {
+  public List<List<Integer>> permuteUnique(int[] nums) {
+    final List<List<Integer>> result = new ArrayList<>();
+    if (nums == null || nums.length == 0) {
+      return result;
+    }
+    Arrays.sort(nums);
+    dfs(result, new ArrayList<>(), nums, new boolean[nums.length]);
+    return result;
+  }
+
+  private void dfs(final List<List<Integer>> result, final List<Integer> curPath, final int[] nums, final boolean[] used) {
+    if (curPath.size() == nums.length) {
+      result.add(new ArrayList<>(curPath));
+      return;
+    }
+    for (int i = 0; i < nums.length; i++) {
+      if (used[i]) continue;
+      if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+      curPath.add(nums[i]);
+      used[i] = true;
+      dfs(result, curPath, nums, used);
+      used[i] = false;
+      curPath.remove(curPath.size() - 1);
+    }
+  }
+}
+
