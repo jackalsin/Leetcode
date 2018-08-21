3193dec517173f76fa98a9c3e4c7acec56a36bf7
jackalsin <jackal.sin@hotmail.com>
2018-08-20 19:04:39 -0700

    Finish 468

---
+++ src/main/java/interviews/microsoft/noodleEssence/_468_Validate_IP_Address/Solution.java
@@ -0,0 +1,81 @@
+package interviews.microsoft.noodleEssence._468_Validate_IP_Address;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+  private static final String IPV4 = "IPv4", IPV6 = "IPv6", NEITHER = "Neither";
+  private static final Set<Character> VALID_HEX = new HashSet<>();
+
+  static {
+    for (char chr = '0'; chr <= '9'; chr++) {
+      VALID_HEX.add(chr);
+    }
+    for (char chr = 'a'; chr <= 'f'; chr++) {
+      VALID_HEX.add(chr);
+    }
+    for (char chr = 'A'; chr <= 'F'; chr++) {
+      VALID_HEX.add(chr);
+    }
+  }
+
+  public String validIPAddress(String IP) {
+    if (IP == null) {
+      return NEITHER;
+    }
+    if (IP.contains(".")) {
+      return (IP.charAt(IP.length() - 1) != '.' && ipv4Validate(IP)) ? IPV4 : NEITHER;
+    } else if (IP.contains(":")) {
+      return (IP.charAt(IP.length() - 1) != ':' && ipv6Validate(IP)) ? IPV6 : NEITHER;
+    }
+    return NEITHER;
+  }
+
+  private static boolean ipv4Validate(String ip) {
+    final String[] items = ip.split("\\.");
+    if (items.length != 4) {
+      return false;
+    }
+    for (String item : items) {
+      int num = 0;
+      if (item.length() == 0 || item.length() > 3) {
+        return false;
+      }
+      if (item.length() == 2 || item.length() == 3) {
+        if (item.charAt(0) == '0') {
+          return false;
+        }
+      }
+      for (char chr : item.toCharArray()) {
+        if (!Character.isDigit(chr)) {
+          return false;
+        }
+        num = num * 10 + chr - '0';
+      }
+      if (num >= 256) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static boolean ipv6Validate(String ip) {
+    final String[] items = ip.split(":");
+    if (items.length != 8) {
+      return false;
+    }
+    for (String item : items) {
+      if (item.length() == 0 || item.length() > 4) {
+        return false;
+      }
+
+      for (char chr : item.toCharArray()) {
+        if (!VALID_HEX.contains(chr)) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
---
+++ src/test/java/interviews/microsoft/noodleEssence/_468_Validate_IP_Address/SolutionTest.java
@@ -0,0 +1,54 @@
+package interviews.microsoft.noodleEssence._468_Validate_IP_Address;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private static final String IPV4 = "IPv4", IPV6 = "IPv6", NEITHER = "Neither";
+
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final String ipv4 = "172.16.254.1";
+    assertEquals(IPV4, solution.validIPAddress(ipv4));
+  }
+
+  @Test
+  void testOnlineCaseIPv4Leading0() {
+    final String ip = "172.16.254.01";
+    assertEquals(NEITHER, solution.validIPAddress(ip));
+  }
+
+  @Test
+  void testOnlineCaseIPv6() {
+    final String ip = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
+    assertEquals(IPV6, solution.validIPAddress(ip));
+  }
+
+  @Test
+  void testOnlineCaseIPv6Extra0() {
+    final String ip = "02001:0db8:85a3:0000:0000:8a2e:0370:7334";
+    assertEquals(NEITHER, solution.validIPAddress(ip));
+  }
+
+  @Test
+  void testFailedCase1IPv6() {
+    final String ip = "2001:0db8:85a3:0:0:8A2E:0370:7334:";
+    assertEquals(NEITHER, solution.validIPAddress(ip));
+  }
+
+  @Test
+  void testFailedCase2IPv4() {
+    final String ip = "12..33.4";
+    assertEquals(NEITHER, solution.validIPAddress(ip));
+  }
+
+}
