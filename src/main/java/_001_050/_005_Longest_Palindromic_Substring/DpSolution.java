cc7d1a42e09f1d7860d49c6477fd4f12f15cd55b
zhiwei.xin <zhiwei.xin@veritas.com>
2018-08-25 15:49:48 -0700

    Finish 005 Dp Solution

---
+++ src/main/java/_001_050/_005_Longest_Palindromic_Substring/DpSolution.java
@@ -0,0 +1,27 @@
+package _001_050._005_Longest_Palindromic_Substring;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/11/2017.
+ */
+public final class DpSolution implements Solution {
+  @Override
+  public String longestPalindrome(String s) {
+    if (s == null || s.length() == 0) {
+      return "";
+    }
+    final boolean[][] dp = new boolean[s.length()][s.length()];
+    String res = "";
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+      for (int j = i; j < s.length(); j++) {
+        dp[i][j] = (2 >= j - i || dp[i + 1][j - 1]) && (s.charAt(i) == s.charAt(j));
+
+        if (dp[i][j] && (j - i + 1) > res.length()) {
+          res = s.substring(i, j + 1);
+        }
+      }
+    }
+    return res;
+  }
+}
--- src/main/java/_001_050/_005_Longest_Palindromic_Substring/Solution.java
+++ src/main/java/_001_050/_005_Longest_Palindromic_Substring/Solution.java
@@ -4,35 +4,8 @@ package _001_050._005_Longest_Palindromic_Substring;
  * @author jacka
  * @version 1.0 on 1/11/2017.
  */
-public class Solution {
-  // when needing global variables
-  private int maxLen = 0;
-  private int left = 0;
+public interface Solution {
 
-  public String longestPalindrome(String s) {
-    if (s == null) {
-      return "";
-    } else if (s.length() < 2) {
-      return s;
-    } else {
-      for (int i = 0; i < s.length(); i++) {
-        scanLeftRight(s, i, i);
-        scanLeftRight(s, i, i + 1);
-      }
-      return s.substring(left, left + maxLen);
-    }
-  }
+  String longestPalindrome(String s);
 
-  private void scanLeftRight(String s, int left, int right) {
-    while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-      left--;
-      right++;
-    }
-//    int curLen = left - right + 1 - 2
-    int curLen = right - left - 1;
-    if (maxLen < curLen) {
-      this.left = left + 1;
-      maxLen = curLen;
-    }
-  }
 }
