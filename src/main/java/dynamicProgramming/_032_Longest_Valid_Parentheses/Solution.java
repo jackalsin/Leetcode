981e6462f21e1dbc3e7688f4d1486329abaddb16
zhiwei.xin <zhiwei.xin@veritas.com>
2018-08-25 17:07:24 -0700

    Finish 032 dp
    Finish 032 dp

---
+++ src/main/java/dynamicProgramming/_032_Longest_Valid_Parentheses/Solution.java
@@ -0,0 +1,30 @@
+package dynamicProgramming._032_Longest_Valid_Parentheses;
+
+public class Solution {
+
+  /**
+   * TODO: Add to gitbook
+   *
+   * @param s
+   * @return
+   */
+  public int longestValidParentheses(String s) {
+    final int[] dp = new int[s.length() + 1];
+    int res = 0, leftCount = 0;
+    for (int i = 0; i < s.length(); i++) {
+      final char chr = s.charAt(i);
+      if (chr == '(') {
+        leftCount++;
+      } else {
+        if (leftCount > 0) {
+          dp[i + 1] = dp[i] + 2; // (())
+          dp[i + 1] += dp[i + 1 - dp[i + 1]]; //()()
+          res = Math.max(res, dp[i + 1]);
+          leftCount--;
+        }
+      }
+    }
+//    System.out.println(Arrays.toString(dp));
+    return res;
+  }
+}
--- src/test/java/_001_050/_032_Longest_Valid_Parentheses/SolutionTest.java
+++ src/test/java/_001_050/_032_Longest_Valid_Parentheses/SolutionTest.java
@@ -1,7 +1,8 @@
 package _001_050._032_Longest_Valid_Parentheses;
 
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,30 +10,31 @@ import static org.junit.Assert.assertEquals;
  * @author jacka
  * @version 1.0 on 2017/2/8.
  */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
-  @Before
-  public void setUp() throws Exception {
+
+  @BeforeEach
+  void setUp() throws Exception {
     solution = new Solution();
   }
 
   @Test
-  public void testWithAllValid() throws Exception {
+  void testWithAllValid() throws Exception {
     assertEquals(8, solution.longestValidParentheses("(()()())"));
   }
 
   @Test
-  public void testWithLeadingValid() throws Exception {
+  void testWithLeadingValid() throws Exception {
     assertEquals(8, solution.longestValidParentheses("(()()()()"));
   }
 
   @Test
-  public void testWithTailingValid() throws Exception {
+  void testWithTailingValid() throws Exception {
     assertEquals(8, solution.longestValidParentheses("()()()())"));
   }
 
   @Test
-  public void testWithAllInvalid() throws Exception {
+  void testWithAllInvalid() throws Exception {
     assertEquals(0, solution.longestValidParentheses(")("));
   }
 }
