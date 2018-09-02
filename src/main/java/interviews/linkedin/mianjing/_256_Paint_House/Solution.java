1e7d3816c16cea7f2cf8bb8e34dc61c9283a2adb
zhiwei.xin <zhiwei.xin@veritas.com>
2018-09-02 13:35:06 -0700

    Finish 256

--- src/main/java/interviews/linkedin/mianjing/_256_Paint_House/Solution.java
+++ src/main/java/interviews/linkedin/mianjing/_256_Paint_House/Solution.java
@@ -1,4 +1,21 @@
 package interviews.linkedin.mianjing._256_Paint_House;
 
 public class Solution { // TODO: follow up 输出所有结果
+  public int minCost(int[][] costs) {
+    final int n = costs.length;
+    final int[][] minCosts = new int[n + 1][3];
+    for (int i = 1; i <= n; ++i) {
+
+      minCosts[i][0] = Math.min(minCosts[i - 1][1], minCosts[i - 1][2]) + costs[i - 1][0];
+      minCosts[i][1] = Math.min(minCosts[i - 1][0], minCosts[i - 1][2]) + costs[i - 1][1];
+      minCosts[i][2] = Math.min(minCosts[i - 1][0], minCosts[i - 1][1]) + costs[i - 1][2];
+    }
+
+    int minCost = Integer.MAX_VALUE;
+    for (int i = 0; i < 3; i++) {
+      minCost = Math.min(minCost, minCosts[n][i]);
+    }
+
+    return minCost;
+  }
 }
