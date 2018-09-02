65675dd8b5b0923ffc0e9aefbcc2cf36b9d84369
jackalsin <jackal.sin@hotmail.com>
2018-09-02 09:00:51 -0700

    Finish 898

---
+++ src/test/java/_851_900/_898_Bitwise_ORs_of_Subarrays/SolutionTest.java
@@ -0,0 +1,30 @@
+package _851_900._898_Bitwise_ORs_of_Subarrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    assertEquals(1, solution.subarrayBitwiseORs(new int[]{1}));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    assertEquals(3, solution.subarrayBitwiseORs(new int[]{1, 1, 2}));
+  }
+
+  @Test
+  void testOnlineCase3() {
+    assertEquals(6, solution.subarrayBitwiseORs(new int[]{1, 2, 4}));
+  }
+}
