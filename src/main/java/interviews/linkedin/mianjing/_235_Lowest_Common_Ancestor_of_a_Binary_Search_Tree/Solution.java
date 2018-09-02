d34ae581ed0c70bb06dd3d088a6c4b2e88308353
zhiwei.xin <zhiwei.xin@veritas.com>
2018-09-02 11:52:18 -0700

    Finish 236

--- src/main/java/interviews/linkedin/mianjing/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.java
+++ src/main/java/interviews/linkedin/mianjing/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.java
@@ -1,4 +1,18 @@
 package interviews.linkedin.mianjing._235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
 
-public class Solution { // TODO：
+import utils.TreeNode;
+
+public class Solution {
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (p.val > q.val) {
+      return lowestCommonAncestor(root, q, p);
+    }
+    if (root.val < p.val) {
+      return lowestCommonAncestor(root.right, q, p);
+    } else if (p.val <= root.val && root.val <= q.val) {
+      return root;
+    } else {
+      return lowestCommonAncestor(root.left, q, p);
+    }
+  }
 }
--- src/test/java/interviews/linkedin/mianjing/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/SolutionTest.java
+++ src/test/java/interviews/linkedin/mianjing/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/SolutionTest.java
@@ -1,5 +1,4 @@
-package _201_250._235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
-
+package interviews.linkedin.mianjing._235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,18 +6,13 @@ import utils.TreeNode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-/**
- * @author jacka
- * @version 1.0 on 8/18/2017.
- */
 class SolutionTest {
   private Solution solution;
 
   private TreeNode root = new TreeNode(6);
 
   @BeforeEach
-  void setUp() {
+  void setUp() throws Exception {
     solution = new Solution();
     root.left = new TreeNode(2);
     root.left.left = new TreeNode(0);
@@ -31,23 +25,23 @@ class SolutionTest {
   }
 
   @Test
-  void testPEqualsQRoot() {
+  void testPEqualsQRoot() throws Exception {
     assertEquals(root, solution.lowestCommonAncestor(root, root, root));
   }
 
   @Test
-  void testPEqualsQNonRoot() {
+  void testPEqualsQNonRoot() throws Exception {
     assertEquals(root.left, solution.lowestCommonAncestor(root, root.left, root.left));
   }
 
   @Test
-  void testParent() {
+  void testParent() throws Exception {
     assertEquals(root.left.right, solution.lowestCommonAncestor(root, root.left.right,
         root.left.right.right));
   }
 
   @Test
-  void testRegular() {
+  void testRegular() throws Exception {
     assertEquals(root.left, solution.lowestCommonAncestor(root, root.left.left,
         root.left.right.right));
   }
