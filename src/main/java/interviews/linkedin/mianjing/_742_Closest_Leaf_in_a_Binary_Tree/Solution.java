321df35c0e11257f0b3cf281d358c7e510543c66
jackalsin <jackal.sin@hotmail.com>
2018-09-04 22:44:50 -0700

    742 with wrong solution

--- src/main/java/interviews/linkedin/mianjing/_742_Closest_Leaf_in_a_Binary_Tree/Solution.java
+++ src/main/java/interviews/linkedin/mianjing/_742_Closest_Leaf_in_a_Binary_Tree/Solution.java
@@ -1,4 +1,39 @@
 package interviews.linkedin.mianjing._742_Closest_Leaf_in_a_Binary_Tree;
 
-public class Solution { // TODO
+import utils.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Solution {
+
+  public int findClosestLeaf(TreeNode root, int k) {
+    if (root == null) {
+      return 0;
+    }
+    final Queue<TreeNode> q = new ArrayDeque<>();
+    q.add(root);
+    int minDiff = Integer.MAX_VALUE, minCandidate = root.val; // any value is fine
+    while (!q.isEmpty()) {
+      final TreeNode toRemove = q.remove();
+      if (toRemove.left == null && toRemove.right == null) {
+        final int curDiff = Math.abs(toRemove.val - k);
+        if (curDiff < minDiff) {
+          minDiff = curDiff;
+          minCandidate = toRemove.val;
+        }
+      } else {
+
+        if (toRemove.left != null) {
+          q.add(toRemove.left);
+        }
+        if (toRemove.right != null) {
+          q.add(toRemove.right);
+        }
+      }
+    }
+
+    assert minDiff != Integer.MAX_VALUE;
+    return minCandidate;
+  }
 }
