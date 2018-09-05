28b3d594eeb0ce68c19ff24b393b0e5a6ba60370
jackalsin <jackal.sin@hotmail.com>
2018-09-04 21:20:12 -0700

    Finish Random Generator

--- src/main/java/interviews/linkedin/mianjing/randomGennerator/Solution.java
+++
@@ -1,7 +0,0 @@
-package interviews.linkedin.mianjing.randomGennerator;
-
-public class Solution {
-  // https://stackoverflow.com/questions/1986859/unbiased-random-number-generator-using-a-biased-one
-
-
-}
