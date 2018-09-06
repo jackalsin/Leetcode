8e097732d7381d5f5c1efb06896d4b3d448a9add
jackalsin <jackal.sin@hotmail.com>
2018-09-05 22:46:53 -0700

    Finish 611

---
+++ src/main/java/_601_650/_611_Valid_Triangle_Number/Solution.java
@@ -0,0 +1,30 @@
+package _601_650._611_Valid_Triangle_Number;
+
+import java.util.Arrays;
+
+/**
+ * @author zhiwei.xin
+ * @version 1.0 on 9/5/18
+ */
+public class Solution {
+  public int triangleNumber(int[] nums) {
+    if (nums == null || nums.length <= 2) {
+      return 0;
+    }
+    final int n = nums.length;
+    Arrays.sort(nums);
+    int count = 0;
+    for (int i = n - 1; i >= 2; --i) {
+      int l = 0, r = i - 1;
+      while (l < r) {
+        if (nums[l] + nums[r] > nums[i]) {
+          count += r - l;
+          r--;
+        } else {
+          ++l;
+        }
+      }
+    }
+    return count;
+  }
+}
---
+++ src/main/java/interviews/linkedin/_611_Valid_Triangle_Number/Solution.java
@@ -0,0 +1,13 @@
+package interviews.linkedin._611_Valid_Triangle_Number;
+
+/**
+ * @author zhiwei.xin
+ * @version 1.0 on 9/5/18
+ */
+public class Solution {
+  // todo: revisit
+  public int triangleNumber(int[] nums) {
+    return 0;
+  }
+
+}
