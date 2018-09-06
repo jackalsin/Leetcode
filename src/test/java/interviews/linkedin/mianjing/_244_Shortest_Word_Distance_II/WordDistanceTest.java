942d486e5c90c5d6965e4ec937689eaa66e820e9
jackalsin <jackal.sin@hotmail.com>
2018-09-05 22:02:12 -0700

    Finish 244

---
+++ src/test/java/interviews/linkedin/mianjing/_244_Shortest_Word_Distance_II/WordDistanceTest.java
@@ -0,0 +1,23 @@
+package interviews.linkedin.mianjing._244_Shortest_Word_Distance_II;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author zhiwei.xin
+ * @version 1.0 on 9/5/18
+ */
+class WordDistanceTest {
+
+
+  @Test
+  void testOnline1() {
+    final String[] words = {"practice", "makes", "perfect", "coding", "makes"};
+    final WordDistance solution = new WordDistance(words);
+
+    assertEquals(3, solution.shortest("coding", "practice"));
+    assertEquals(1, solution.shortest("makes", "coding"));
+  }
+
+}
