09c167d2f83f5693b00c10ba90b9072633c551c2
jackalsin <jackal.sin@hotmail.com>
2018-09-06 08:40:15 -0700

    Finish 235

---
+++ src/main/java/interviews/linkedin/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.java
@@ -0,0 +1,26 @@
+package interviews.linkedin._235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
+
+import utils.TreeNode;
+
+/**
+ * @author zhiwei.xin
+ * @version 1.0 on 9/6/18
+ */
+public class Solution {
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (p.val > q.val) {
+      return lowestCommonAncestor(root, q, p);
+    }
+    if (root == null || root == q || root == p) {
+      return root;
+    }
+
+    if (root.val < p.val) {
+      return lowestCommonAncestor(root.right, q, p);
+    } else if (root.val > q.val) {
+      return lowestCommonAncestor(root.left, q, p);
+    } else {
+      return root;
+    }
+  }
+}
---
+++ src/test/java/interviews/linkedin/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/SolutionTest.java
@@ -0,0 +1,53 @@
+package interviews.linkedin._235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.TreeNode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author zhiwei.xin
+ * @version 1.0 on 9/6/18
+ */
+class SolutionTest {
+  private Solution solution;
+
+  private TreeNode root = new TreeNode(6);
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+    root.left = new TreeNode(2);
+    root.left.left = new TreeNode(0);
+    root.left.right = new TreeNode(4);
+    root.left.right.left = new TreeNode(3);
+    root.left.right.right = new TreeNode(5);
+    root.right = new TreeNode(8);
+    root.right.left = new TreeNode(7);
+    root.right.right = new TreeNode(9);
+  }
+
+  @Test
+  void testPEqualsQRoot() {
+    assertEquals(root, solution.lowestCommonAncestor(root, root, root));
+  }
+
+  @Test
+  void testPEqualsQNonRoot() {
+    assertEquals(root.left, solution.lowestCommonAncestor(root, root.left, root.left));
+  }
+
+  @Test
+  void testParent() {
+    assertEquals(root.left.right, solution.lowestCommonAncestor(root, root.left.right,
+        root.left.right.right));
+  }
+
+  @Test
+  void testRegular() {
+    assertEquals(root.left, solution.lowestCommonAncestor(root, root.left.left,
+        root.left.right.right));
+  }
+
+}
