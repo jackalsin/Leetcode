f5174f19d935290b648eea9ab53063a19fc643f5
jackalsin <jackal.sin@hotmail.com>
2018-09-07 15:25:50 -0700

    Add 040

--- src/test/java/_001_050/_040_Combination_Sum_II/SolutionTest.java
+++ src/test/java/_001_050/_040_Combination_Sum_II/SolutionTest.java
@@ -1,7 +1,7 @@
 package _001_050._040_Combination_Sum_II;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,22 +14,23 @@ import static org.junit.Assert.assertEquals;
  * @author jacka
  * @version 1.0 on 2/18/2017.
  */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
-  @Before
-  public void setUp() throws Exception {
+
+  @BeforeEach
+  void setUp() {
     solution = new Solution();
   }
 
   @Test
-  public void testWithOnlineCase() throws Exception {
+  void testWithOnlineCase() {
     List<List<Integer>> expect = new ArrayList<>();
     expect.add(Arrays.asList(1, 7));
     expect.add(Arrays.asList(1, 2, 5));
     expect.add(Arrays.asList(2, 6));
     expect.add(Arrays.asList(1, 1, 6));
 
-    List<List<Integer>> actual = solution.combinationSum(new int[] {10, 1, 2, 7, 6, 1, 5}, 8);
+    List<List<Integer>> actual = solution.combinationSum(new int[]{10, 1, 2, 7, 6, 1, 5}, 8);
     System.out.println("acutal:\n " + actual);
     assertEquals(expect.size(), actual.size());
     assertEquals(new HashSet<>(expect), new HashSet<>(actual));
@@ -37,11 +38,11 @@ public class SolutionTest {
   }
 
   @Test
-  public void testWithTripleElements() throws Exception {
+  void testWithTripleElements() {
     List<List<Integer>> expect = new ArrayList<>();
     expect.add(Arrays.asList(1, 1, 6));
 
-    List<List<Integer>> actual = solution.combinationSum(new int[] {1, 1, 1, 6, 10}, 8);
+    List<List<Integer>> actual = solution.combinationSum(new int[]{1, 1, 1, 6, 10}, 8);
     System.out.println("acutal:\n " + actual);
     assertEquals(expect.size(), actual.size());
     assertEquals(new HashSet<>(expect), new HashSet<>(actual));
