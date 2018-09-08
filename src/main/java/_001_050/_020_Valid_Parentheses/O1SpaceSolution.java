62d21480f165e4f4eed5402e7f2b59856f6449ff
jackalsin <jackal.sin@hotmail.com>
2018-09-08 16:56:56 -0700

    Finish 020

---
+++ src/main/java/_001_050/_020_Valid_Parentheses/O1SpaceSolution.java
@@ -0,0 +1,28 @@
+package _001_050._020_Valid_Parentheses;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/23/2017.
+ */
+public final class O1SpaceSolution implements Solution {
+  public boolean isValid(String s) {
+    final char[] chars = s.toCharArray();
+    int top = -1;
+    for (int i = 0; i < chars.length; ++i) {
+      if (top == -1 || !isMatch(chars[i], chars[top])) {
+        ++top;
+        chars[top] = chars[i];
+      } else {
+        --top;
+      }
+    }
+    return top == -1;
+  }
+
+  private static boolean isMatch(char a, char b) {
+    if (a == ')' && b == '(') return true;
+    if (a == '}' && b == '{') return true;
+    if (a == ']' && b == '[') return true;
+    return false;
+  }
+}
---
+++ src/test/java/_001_050/_020_Valid_Parentheses/O1SpaceSolutionTest.java
@@ -0,0 +1,56 @@
+package _001_050._020_Valid_Parentheses;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class O1SpaceSolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    solution = new O1SpaceSolution();
+  }
+
+  @Test
+  void testEmptyString() throws Exception {
+    assertTrue(solution.isValid(""));
+  }
+
+  @Test
+  void testIsValid1() throws Exception {
+    assertTrue(solution.isValid("()"));
+  }
+
+  @Test
+  void testIsValid2() throws Exception {
+    assertTrue(solution.isValid("()[]"));
+  }
+
+  @Test
+  void testIsValid3() throws Exception {
+    assertTrue(solution.isValid("()[]{}"));
+  }
+
+  @Test
+  void testIsValid4() throws Exception {
+    assertTrue(solution.isValid("([]{})"));
+  }
+
+  @Test
+  void testIsValid5() throws Exception {
+    assertFalse(solution.isValid("([)]"));
+  }
+
+  @Test
+  void testIsValid6() throws Exception {
+    assertFalse(solution.isValid("{(})"));
+  }
+
+  @Test
+  void testIsValid7() throws Exception {
+    assertFalse(solution.isValid("["));
+  }
+}
