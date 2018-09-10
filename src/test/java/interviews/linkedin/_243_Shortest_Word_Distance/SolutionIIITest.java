7e7f32b7a8e9458e071f45c370f02175e3329f7a
zhiwei.xin <zhiwei.xin@veritas.com>
2018-09-10 16:36:10 -0700

    Finish 243

--- src/test/java/interviews/linkedin/_243_Shortest_Word_Distance/SolutionIIITest.java
+++ src/test/java/interviews/linkedin/_243_Shortest_Word_Distance/SolutionIIITest.java
@@ -1,31 +1,26 @@
-package _201_250._243_Shortest_Word_Distance;
-
+package interviews.linkedin._243_Shortest_Word_Distance;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * @author Zhiwei.Xin
- * @version 1.0 on 8/21/2017.
- */
-class SolutionTest {
+class SolutionIIITest {
   private Solution solution;
 
   @BeforeEach
-  void setUp() throws Exception {
-    solution = new Solution();
+  void setUp() {
+    solution = new SolutionIII();
   }
 
   @Test
-  void testOnlineCase1() throws Exception {
+  void testOnlineCase1() {
     final String[] input = new String[]{"practice", "makes", "perfect", "coding", "makes"};
     assertEquals(3, solution.shortestDistance(input, "coding", "practice"));
   }
 
   @Test
-  void testOnlineCase2() throws Exception {
+  void testOnlineCase2() {
     final String[] input = new String[]{"practice", "makes", "perfect", "coding", "makes"};
     assertEquals(1, solution.shortestDistance(input, "makes", "coding"));
   }
