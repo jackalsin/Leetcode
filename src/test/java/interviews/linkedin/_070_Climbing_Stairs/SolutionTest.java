6851b34263d25b4c892edba30c4a6872c258065f
zhiwei.xin <zhiwei.xin@veritas.com>
2018-09-10 19:41:51 -0700

    Finish 070

---
+++ src/test/java/interviews/linkedin/_070_Climbing_Stairs/SolutionTest.java
@@ -0,0 +1,35 @@
+package interviews.linkedin._070_Climbing_Stairs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  void testWith1() throws Exception {
+    assertEquals(1, solution.climbStairs(1));
+  }
+
+  @Test
+  void testWith2() throws Exception {
+    assertEquals(2, solution.climbStairs(2));
+  }
+
+  @Test
+  void testWith3() throws Exception {
+    assertEquals(3, solution.climbStairs(3));
+  }
+
+  @Test
+  void testWith4() throws Exception {
+    assertEquals(5, solution.climbStairs(4));
+  }
+}
