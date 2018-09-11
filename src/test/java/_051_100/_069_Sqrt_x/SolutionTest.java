d34bcc44264af206ab0a8b3acd2e008475679b0a
zhiwei.xin <zhiwei.xin@veritas.com>
2018-09-10 18:24:56 -0700

    Finish 069

--- src/test/java/_051_100/_069_Sqrt_x/SolutionTest.java
+++ src/test/java/_051_100/_069_Sqrt_x/SolutionTest.java
@@ -1,7 +1,8 @@
 package _051_100._069_Sqrt_x;
 
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,37 +10,41 @@ import static org.junit.Assert.assertEquals;
  * @author jacka
  * @version 1.0 on 4/27/2017.
  */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
-  @Before
-  public void setUp() throws Exception {
+
+  @BeforeEach
+  void setUp() throws Exception {
     solution = new Solution();
   }
 
   @Test
-  public void test1() throws Exception {
+  void test1() throws Exception {
     assertEquals(1, solution.mySqrt(1));
   }
 
   @Test
-  public void test2() throws Exception {
+  void test2() throws Exception {
     assertEquals(1, solution.mySqrt(2));
   }
 
   @Test
-  public void test3() throws Exception {
+  void test3() throws Exception {
     assertEquals(1, solution.mySqrt(3));
   }
+
   @Test
-  public void test4() throws Exception {
+  void test4() throws Exception {
     assertEquals(2, solution.mySqrt(4));
   }
+
   @Test
-  public void test5() throws Exception {
+  void test5() throws Exception {
     assertEquals(2, solution.mySqrt(5));
   }
+
   @Test
-  public void test2147395599() throws Exception {
+  void test2147395599() throws Exception {
     assertEquals(46339, solution.mySqrt(2147395599));
   }
 }
---
+++ src/test/java/interviews/linkedin/_069_Sqrt_x/SolutionTest.java
@@ -0,0 +1,45 @@
+package interviews.linkedin._069_Sqrt_x;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  void test1() throws Exception {
+    assertEquals(1, solution.mySqrt(1));
+  }
+
+  @Test
+  void test2() throws Exception {
+    assertEquals(1, solution.mySqrt(2));
+  }
+
+  @Test
+  void test3() throws Exception {
+    assertEquals(1, solution.mySqrt(3));
+  }
+
+  @Test
+  void test4() throws Exception {
+    assertEquals(2, solution.mySqrt(4));
+  }
+
+  @Test
+  void test5() throws Exception {
+    assertEquals(2, solution.mySqrt(5));
+  }
+
+  @Test
+  void test2147395599() throws Exception {
+    assertEquals(46339, solution.mySqrt(2147395599));
+  }
+}
