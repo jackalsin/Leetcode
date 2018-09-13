acc8a2683a25bf331c5fc37306551fdb37a80c5d
jackalsin <jackal.sin@hotmail.com>
2018-09-12 23:17:25 -0700

    Finish 636

---
+++ src/test/java/interviews/linkedin/_636_Exclusive_Time_of_Functions/SolutionTest.java
@@ -0,0 +1,30 @@
+package interviews.linkedin._636_Exclusive_Time_of_Functions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() throws Exception {
+    final int n = 2;
+    final List<String> logs = List.of(
+        "0:start:0",
+        "1:start:2",
+        "1:end:5",
+        "0:end:6");
+
+    final int[] output = {3, 4}, actual = solution.exclusiveTime(n, logs);
+    assertArrayEquals(output, actual);
+  }
+}
