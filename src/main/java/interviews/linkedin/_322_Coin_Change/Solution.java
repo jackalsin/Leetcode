e907789098edef0d40f2a90ab6d12ee04eaa2d40
zhiwei.xin <zhiwei.xin@veritas.com>
2018-09-16 11:09:53 -0700

    Finish 322

---
+++ src/main/java/interviews/linkedin/_322_Coin_Change/Solution.java
@@ -0,0 +1,10 @@
+package interviews.linkedin._322_Coin_Change;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public interface Solution {
+
+  int coinChange(int[] coins, int amount);
+}
