e196b20e60e6709bb25eb57fd9278eef32ba48d7
jackalsin <jackal.sin@hotmail.com>
2018-09-16 19:46:57 -0700

    Finish 259

--- src/test/java/interviews/linkedin/_259_3Sum_Smaller/SolutionTest.java
+++ src/test/java/interviews/linkedin/_259_3Sum_Smaller/SolutionTest.java
@@ -1,15 +1,10 @@
-package _251_300._259_3Sum_Smaller;
-
+package interviews.linkedin._259_3Sum_Smaller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * @author jacka
- * @version 1.0 on 8/27/2017.
- */
 class SolutionTest {
   private Solution solution;
 
@@ -23,4 +18,5 @@ class SolutionTest {
     final int[] input = new int[]{-2, 0, 1, 3};
     assertEquals(2, solution.threeSumSmaller(input, 2));
   }
+
 }
