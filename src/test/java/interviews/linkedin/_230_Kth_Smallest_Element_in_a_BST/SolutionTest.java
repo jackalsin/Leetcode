a14dc5a1050121a1a534c9cef2fb6bd873e2c36f
jackalsin <jackal.sin@hotmail.com>
2018-09-17 20:11:55 -0700

    Finish 230

--- src/test/java/interviews/linkedin/_230_Kth_Smallest_Element_in_a_BST/SolutionTest.java
+++ src/test/java/interviews/linkedin/_230_Kth_Smallest_Element_in_a_BST/SolutionTest.java
@@ -1,17 +1,12 @@
-package _201_250._230_Kth_Smallest_Element_in_a_BST;
-
+package interviews.linkedin._230_Kth_Smallest_Element_in_a_BST;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import utils.TreeNode;
 import utils.TreeNodes;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * @author jacka
- * @version 1.0 on 8/17/2017.
- */
 class SolutionTest {
   private Solution solution;
 
@@ -31,7 +26,7 @@ class SolutionTest {
     TreeNode root = TreeNodes.getTreeLevelOrder(4, 2, 6, 1, 3, 5, 7);
     for (int i = 1; i <= 7; i++) {
       System.out.print("k = " + i);
-      assertEquals("k = " + i + " pass", i, solution.kthSmallest(root, i));
+      assertEquals(i, solution.kthSmallest(root, i));
       System.out.println(" pass");
     }
   }
