f5487312a2e66a24c033f38a847cd0f323f0e3f7
zhiwei.xin <zhiwei.xin@veritas.com>
2018-09-17 18:54:41 -0700

    Add push o1 pop o N Solution

---
+++ src/test/java/interviews/linkedin/_225_Implement_Stack_using_Queues/PushO1PopONTest.java
@@ -0,0 +1,23 @@
+package interviews.linkedin._225_Implement_Stack_using_Queues;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PushO1PopONTest {
+  private MyStack stack;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    stack = new PushO1PopON();
+  }
+
+  @Test
+  void testFailed1() throws Exception {
+    stack.push(1);
+    stack.push(2);
+    assertEquals(2, stack.top());
+  }
+
+}
