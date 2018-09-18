1dc441c1688b29f2821b881d3b502a96a453ed49
jackalsin <jackal.sin@hotmail.com>
2018-09-17 20:35:02 -0700

    Finish 167

---
+++ src/main/java/interviews/linkedin/_167_Two_Sum_II_Input_array_is_sorted/Solution.java
@@ -0,0 +1,20 @@
+package interviews.linkedin._167_Two_Sum_II_Input_array_is_sorted;
+
+import java.util.Arrays;
+
+public class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    for (int left = 0, right = nums.length - 1; left < right; ) {
+      final int sum = nums[left] + nums[right];
+      if (sum < target) {
+        left++;
+      } else if (sum == target) {
+        return new int[]{left + 1, right + 1};
+      } else {
+        right--;
+      }
+    }
+
+    throw new IllegalArgumentException("In array " + Arrays.toString(nums));
+  }
+}
