319f61202ea2fcc68bc9fe312f5d8411e54d3203
jackalsin <jackal.sin@hotmail.com>
2018-09-18 22:10:37 -0700

    Add 002

---
+++ src/main/java/interviews/linkedin/_002_Add_Two_Numbers/Solution.java
@@ -0,0 +1,31 @@
+package interviews.linkedin._002_Add_Two_Numbers;
+
+import utils.ListNode;
+
+public class Solution {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    final ListNode dummy = new ListNode(0);
+    ListNode cur = dummy;
+
+    int carry = 0;
+    while (carry != 0 || l1 != null || l2 != null) {
+      final int num1 = l1 == null ? 0 : l1.val, num2 = l2 == null ? 0 : l2.val, sum = num1 + num2 + carry, curDigit = sum % 10;
+      carry = sum / 10;
+
+      cur.next = new ListNode(curDigit);
+
+      cur = cur.next;
+
+      if (l1 != null) {
+        l1 = l1.next;
+      }
+
+      if (l2 != null) {
+        l2 = l2.next;
+      }
+    }
+
+    return dummy.next;
+  }
+
+}
