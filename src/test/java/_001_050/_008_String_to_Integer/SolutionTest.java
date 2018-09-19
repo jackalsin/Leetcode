3c9491665e39527259767e769b692430f386d987
jackalsin <jackal.sin@hotmail.com>
2018-09-18 22:14:48 -0700

    Finish 008

--- src/test/java/_001_050/_008_String_to_Integer/SolutionTest.java
+++ src/test/java/_001_050/_008_String_to_Integer/SolutionTest.java
@@ -1,7 +1,8 @@
 package _001_050._008_String_to_Integer;
 
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,37 +10,37 @@ import static org.junit.Assert.assertEquals;
  * @author jacka
  * @version 1.0 on 2017/1/13.
  */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
 
-  @Before
-  public void setUp() throws Exception {
+  @BeforeEach
+  void setUp() throws Exception {
     solution = new Solution();
   }
 
   @Test
-  public void testMaxInteger() throws Exception {
+  void testMaxInteger() throws Exception {
     assertEquals(Integer.MAX_VALUE, solution.myAtoi(String.valueOf(Integer.MAX_VALUE)));
   }
 
   @Test
-  public void testMinInteger() throws Exception {
+  void testMinInteger() throws Exception {
     assertEquals(Integer.MIN_VALUE, solution.myAtoi(String.valueOf(Integer.MIN_VALUE)));
   }
 
   @Test
-  public void testMinIntegerPlusOne() throws Exception {
+  void testMinIntegerPlusOne() throws Exception {
     assertEquals(Integer.MIN_VALUE + 1,
         solution.myAtoi(String.valueOf(Integer.MIN_VALUE + 1)));
   }
 
   @Test
-  public void testZero() throws Exception {
+  void testZero() throws Exception {
     assertEquals(0, solution.myAtoi(String.valueOf("0")));
   }
 
   @Test
-  public void testUpOverflow() throws Exception {
+  void testUpOverflow() throws Exception {
     assertEquals(Integer.MAX_VALUE, solution.myAtoi(String.valueOf("2147483648")));
   }
 }
