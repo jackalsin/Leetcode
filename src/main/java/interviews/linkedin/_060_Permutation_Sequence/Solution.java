aa58d095e312a1e2a3cfb4558b9709ea260cc219
jackalsin <jackal.sin@hotmail.com>
2018-09-18 22:17:23 -0700

    Finish 060

---
+++ src/main/java/interviews/linkedin/_060_Permutation_Sequence/Solution.java
@@ -0,0 +1,35 @@
+package interviews.linkedin._060_Permutation_Sequence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+  private static final int[] FACTORIAL = new int[10];
+
+  static {
+    int tmp = 1;
+    for (int i = 1; i <= 9; ++i) {
+      tmp *= i;
+      FACTORIAL[i] = tmp;
+    }
+  }
+
+  public String getPermutation(int n, int k) {
+    assert n >= 1 && n <= 9;
+    final StringBuilder sb = new StringBuilder();
+    final List<Integer> candidates = new ArrayList<>();
+    for (int i = 1; i <= n; ++i) {
+      candidates.add(i);
+    }
+    k--;
+    for (int i = 1, remainDigits = n - 1; i <= n; ++i, --remainDigits) {
+      final int perCount = FACTORIAL[remainDigits];
+      final int index = perCount == 0 ? 0 : (k / perCount);
+      k -= index * perCount;
+      sb.append(candidates.get(index));
+      candidates.remove(index);
+    }
+    return sb.toString();
+  }
+}
--- src/test/java/_051_100/_060_Permutation_Sequence/SolutionTest.java
+++ src/test/java/_051_100/_060_Permutation_Sequence/SolutionTest.java
@@ -1,7 +1,8 @@
 package _051_100._060_Permutation_Sequence;
 
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,31 +10,32 @@ import static org.junit.Assert.assertEquals;
  * @author jacka
  * @version 1.0 on 4/22/2017.
  */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
-  @Before
-  public void setUp() throws Exception {
+
+  @BeforeEach
+  void setUp() throws Exception {
     solution = new Solution();
   }
 
   @Test
-  public void testWith3And1() throws Exception {
+  void testWith3And1() throws Exception {
     assertEquals("123", solution.getPermutation(3, 1));
   }
 
   @Test
-  public void testWith3And2() throws Exception {
+  void testWith3And2() throws Exception {
     assertEquals("132", solution.getPermutation(3, 2));
   }
 
   @Test
-  public void testWith3And3() throws Exception {
-    assertEquals("213", solution.getPermutation(3,3 ));
+  void testWith3And3() throws Exception {
+    assertEquals("213", solution.getPermutation(3, 3));
   }
 
   @Test
-  public void testWith3And4() throws Exception {
-    assertEquals("231", solution.getPermutation(3,4));
+  void testWith3And4() throws Exception {
+    assertEquals("231", solution.getPermutation(3, 4));
   }
 
 }
