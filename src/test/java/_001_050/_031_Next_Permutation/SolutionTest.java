563ae16e7a36c3a2139b46adb3b38f72276df6a3
jackalsin <jackal.sin@hotmail.com>
2018-09-18 22:16:15 -0700

    Finish 031

--- src/test/java/_001_050/_031_Next_Permutation/SolutionTest.java
+++ src/test/java/_001_050/_031_Next_Permutation/SolutionTest.java
@@ -1,7 +1,8 @@
 package _001_050._031_Next_Permutation;
 
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 
@@ -9,26 +10,27 @@ import static org.junit.Assert.assertArrayEquals;
  * @author jacka
  * @version 1.0 on 1/31/2017.
  */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
-  @Before
-  public void setUp() throws Exception {
+
+  @BeforeEach
+  void setUp() throws Exception {
     solution = new Solution();
   }
 
   @Test
-  public void testWithRegularElement() throws Exception {
-    final int[] input = new int[] {1, 4, 6, 5, 3, 2};
+  void testWithRegularElement() throws Exception {
+    final int[] input = new int[]{1, 4, 6, 5, 3, 2};
     solution.nextPermutation(input);
-    final int[] result = new int[] {1, 5, 2, 3, 4, 6};
+    final int[] result = new int[]{1, 5, 2, 3, 4, 6};
     assertArrayEquals(result, input);
   }
 
   @Test
-  public void testWithDuplicatedElement() throws Exception {
-    final int[] input = new int[] {1, 5, 1};
+  void testWithDuplicatedElement() throws Exception {
+    final int[] input = new int[]{1, 5, 1};
     solution.nextPermutation(input);
-    final int[] result = new int[] {5, 1, 1};
+    final int[] result = new int[]{5, 1, 1};
     assertArrayEquals(result, input);
   }
 
