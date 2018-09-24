b7f747d6d5026e2a599e5d52fffa7795f0665ca4
zhiwei.xin <zhiwei.xin@veritas.com>
2018-09-24 11:34:27 -0700

    Finish 053

---
+++ src/main/java/interviews/linkedin/_053_Maximum_Subarray/SolutionIII.java
@@ -0,0 +1,19 @@
+package interviews.linkedin._053_Maximum_Subarray;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/17/2017.
+ */
+public final class SolutionIII implements Solution {
+  public int maxSubArray(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    int maxSoFar = nums[0], maxEndHere = nums[0];
+    for (int i = 1; i < nums.length; ++i) {
+      maxEndHere = Math.max(maxEndHere + nums[i], nums[i]);
+      maxSoFar = Math.max(maxSoFar, maxEndHere);
+    }
+    return maxSoFar;
+  }
+}
