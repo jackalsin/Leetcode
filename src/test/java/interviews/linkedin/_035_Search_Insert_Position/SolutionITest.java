dd4483d66510dec6c204de425679cbaf37e138c3
jackalsin <jackal.sin@hotmail.com>
2018-09-24 19:58:30 -0700

    Finish 035

---
+++ src/test/java/interviews/linkedin/_035_Search_Insert_Position/SolutionITest.java
@@ -0,0 +1,53 @@
+package interviews.linkedin._035_Search_Insert_Position;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionITest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    solution = new SolutionI();
+  }
+
+  // non-exist value
+  @Test
+  void testWithLeadingValue() throws Exception {
+    final int[] input = new int[]{1, 3, 5, 7, 9};
+    assertEquals(0, solution.searchInsert(input, 0));
+  }
+
+  @Test
+  void testWithTrailingValue() throws Exception {
+    final int[] input = new int[]{1, 3, 5, 7, 9};
+    assertEquals(5, solution.searchInsert(input, 10));
+  }
+
+  @Test
+  void testWithMidMissingValue() throws Exception {
+    final int[] input = new int[]{1, 3, 5, 7, 9};
+    assertEquals(3, solution.searchInsert(input, 6));
+  }
+  //test for existing value
+
+  @Test
+  void testWith0thValue() throws Exception {
+    final int[] input = new int[]{1, 3, 5, 7, 9};
+    assertEquals(0, solution.searchInsert(input, 1));
+  }
+
+  @Test
+  void testWithLastValue() throws Exception {
+    final int[] input = new int[]{1, 3, 5, 7, 9};
+    assertEquals(4, solution.searchInsert(input, 9));
+  }
+
+  @Test
+  void testWithMidValue() throws Exception {
+    final int[] input = new int[]{1, 3, 5, 7, 9};
+    assertEquals(2, solution.searchInsert(input, 5));
+  }
+}
