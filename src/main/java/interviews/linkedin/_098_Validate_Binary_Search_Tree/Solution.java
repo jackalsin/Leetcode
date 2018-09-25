5e273e5f1c16c78f061dc4c9c5b0d609daf8c809
jackalsin <jackal.sin@hotmail.com>
2018-09-24 19:57:37 -0700

    Finish 010

---
+++ src/main/java/interviews/linkedin/_098_Validate_Binary_Search_Tree/Solution.java
@@ -0,0 +1,29 @@
+package interviews.linkedin._098_Validate_Binary_Search_Tree;
+
+import utils.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+  public boolean isValidBST(TreeNode root) {
+    final Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode prev = null;
+    while (root != null || !stack.isEmpty()) {
+      if (root != null) {
+        stack.push(root);
+        root = root.left;
+      } else {
+        root = stack.pop();
+        if (prev != null) {
+          if (root.val <= prev.val) {
+            return false;
+          }
+        }
+        prev = root;
+        root = root.right;
+      }
+    }
+    return true;
+  }
+}
