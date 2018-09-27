c2b79273986f5494a62f75e8de54eac59f437fab
jackalsin <jackal.sin@hotmail.com>
2018-09-27 11:14:16 -0700

    Finish 245

---
+++ src/test/java/interviews/linkedin/_245_Shortest_Word_Distance_III/SolutionIVTest.java
@@ -0,0 +1,22 @@
+package interviews.linkedin._245_Shortest_Word_Distance_III;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionIVTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    solution = new SolutionIV();
+  }
+
+  @Test
+  void testMakesAndMakes() throws Exception {
+    final String[] input = new String[]{"practice", "makes", "perfect", "coding", "makes"};
+    assertEquals(1, solution.shortestWordDistance(input, "makes", "coding"));
+    assertEquals(3, solution.shortestWordDistance(input, "makes", "makes"));
+  }
+}
