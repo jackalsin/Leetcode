6f6a846e275782fa5a5870566a492c51a1836fc4
jackalsin <jackal.sin@hotmail.com>
2018-10-03 00:08:32 -0700

    Finish 154

--- src/main/java/interviews/linkedin/_154_Find_Minimum_in_Rotated_Sorted_Array_II/Solution.java
+++ src/main/java/interviews/linkedin/_154_Find_Minimum_in_Rotated_Sorted_Array_II/Solution.java
@@ -1,4 +1,19 @@
 package interviews.linkedin._154_Find_Minimum_in_Rotated_Sorted_Array_II;
 
-public class Solution { // TODO:
+public class Solution {
+  public int findMin(int[] nums) {
+    int left = 0, right = nums.length - 1;
+    while (left < right) {
+      final int mid = (right - left) / 2 + left;
+      if (nums[mid] == nums[right]) {
+        right--;
+      } else if (nums[mid] < nums[right]) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return nums[left];
+  }
 }
--- src/test/java/interviews/linkedin/_154_Find_Minimum_in_Rotated_Sorted_Array_II/SolutionTest.java
+++ src/test/java/interviews/linkedin/_154_Find_Minimum_in_Rotated_Sorted_Array_II/SolutionTest.java
@@ -1,47 +1,53 @@
-package _151_200._154_Find_Minimum_in_Rotated_Sorted_Array_II;
+package interviews.linkedin._154_Find_Minimum_in_Rotated_Sorted_Array_II;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * @author jacka
- * @version 1.0 on 7/17/2017.
- */
 class SolutionTest {
   private Solution solution;
 
   @BeforeEach
-  void setUp() throws Exception {
+  void setUp() {
     solution = new Solution();
   }
 
   // non duplicated test cases
   @Test
-  void testOneElement() throws Exception {
+  void testOneElement() {
     final int[] input = new int[]{1};
     assertEquals(1, solution.findMin(input));
   }
 
   @Test
-  void testTwoElement() throws Exception {
+  void testTwoElement() {
     final int[] input = new int[]{1, 2};
     assertEquals(1, solution.findMin(input));
   }
 
   @Test
-  void testTwoElements() throws Exception {
+  void testTwoElements() {
     final int[] input = new int[]{2, 1};
     assertEquals(1, solution.findMin(input));
   }
 
   @Test
-  void test5Elements() throws Exception {
+  void test5Elements() {
     final int[] input = new int[]{2, 3, 4, 0, 1};
     assertEquals(0, solution.findMin(input));
   }
 
   // duplicated test cases
+  @Test
+  void testDup1() {
+    final int[] input = new int[]{2, 2, 2, 0, 1};
+    assertEquals(0, solution.findMin(input));
+  }
+
+  void testDup2() {
+    final int[] input = new int[]{1, 3, 3};
+    assertEquals(1, solution.findMin(input));
+  }
 
 }
