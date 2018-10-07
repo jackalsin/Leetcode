815069aa63079dd0b63d9471549684075700f04b
jackalsin <jackal.sin@hotmail.com>
2018-10-07 09:31:40 -0700

    Finish 518

--- src/main/java/interviews/linkedin/_518_Coin_Change_2/Solution.java
+++ src/main/java/interviews/linkedin/_518_Coin_Change_2/Solution.java
@@ -1,16 +1,5 @@
 package interviews.linkedin._518_Coin_Change_2;
 
-public class Solution {
-  // TODO: Revisit
-  public int change(int amount, int[] coins) {
-    final int[] dp = new int[amount + 1];
-    dp[0] = 1;
-
-    for (int coin : coins) {
-      for (int i = coin; i <= amount; ++i) {
-        dp[i] += dp[i - coin];
-      }
-    }
-    return dp[amount];
-  }
+public interface Solution {
+  int change(int amount, int[] coins) ;
 }
