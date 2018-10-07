6271bd7235706f89c5dc0ea33c12dc630248b117
jackalsin <jackal.sin@hotmail.com>
2018-10-06 21:30:34 -0700

    Finish 516

--- src/main/java/interviews/linkedin/_516_Longest_Palindromic_Subsequence/Solution.java
+++ src/main/java/interviews/linkedin/_516_Longest_Palindromic_Subsequence/Solution.java
@@ -3,27 +3,17 @@ package interviews.linkedin._516_Longest_Palindromic_Subsequence;
 public class Solution {
   // TODO: revisit
   public int longestPalindromeSubseq(String s) {
-    // dp[i][j] s[i ... j] longest subseq
-
-    if (s == null || s.length() == 0) {
-      return 0;
-    }
-    final int rows = s.length(), cols = s.length();
-    final int[][] dp = new int[rows][cols];
-
-    for (int row = rows - 1; row >= 0; --row) {
-      dp[row][row] = 1;
-      for (int col = row + 1; col < cols; ++col) {
-        dp[row][col] = dp[row + 1][col - 1];
-        if (s.charAt(row) == s.charAt(col)) {
-          dp[row][col] += 2;
-        } else {
-          dp[row][col] = Math.max(dp[row][col], dp[row + 1][col]);
-          dp[row][col] = Math.max(dp[row][col], dp[row][col - 1]);
+    final int[][] dp = new int[s.length()][s.length()];
+    for (int i = s.length() - 1; i >= 0; --i) {
+      dp[i][i] = 1;
+      for (int j = i + 1; j < s.length(); ++j) {
+        if (s.charAt(i) == s.charAt(j)) {
+          dp[i][j] = dp[i + 1][j - 1] + 2;
         }
+        dp[i][j] = Math.max(dp[i][j], dp[i + 1][j]);
+        dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
       }
     }
-//    TwoDimensionArray.display(dp);
-    return dp[0][rows - 1];
+    return dp[0][s.length() - 1];
   }
 }
