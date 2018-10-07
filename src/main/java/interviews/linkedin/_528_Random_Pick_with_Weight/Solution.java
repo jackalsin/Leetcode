605c0dd02bfd9e45f218249a78a4929e51ec11ca
jackalsin <jackal.sin@hotmail.com>
2018-10-07 10:52:38 -0700

    Finish 528

--- src/main/java/interviews/linkedin/_528_Random_Pick_with_Weight/Solution.java
+++ src/main/java/interviews/linkedin/_528_Random_Pick_with_Weight/Solution.java
@@ -1,8 +1,30 @@
 package interviews.linkedin._528_Random_Pick_with_Weight;
 
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
+
 /**
  * @author zhiwei.xin
  * @version 1.0 on 9/13/18
  */
-public class Solution { // TODO:
+public class Solution {
+  private final TreeMap<Integer, Integer> upperToIndex = new TreeMap<>();
+  private final Random rand = new Random();
+  private int sum;
+
+  public Solution(int[] w) {
+    int sum = 0;
+    for (int i = 0; i < w.length; ++i) {
+      sum += w[i];
+      upperToIndex.put(sum, i);
+    }
+    this.sum = sum;
+  }
+
+  public int pickIndex() {
+    final int i = rand.nextInt(sum);
+    final Map.Entry<Integer, Integer> entry = upperToIndex.higherEntry(i);
+    return entry.getValue();
+  }
 }
