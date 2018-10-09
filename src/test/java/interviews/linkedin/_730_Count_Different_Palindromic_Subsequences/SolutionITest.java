d741392dc54b7adaa09d4f757f6559f4d97c42b5
jackalsin <jackal.sin@hotmail.com>
2018-10-08 21:52:32 -0700

    Finish 730

---
+++ src/test/java/interviews/linkedin/_730_Count_Different_Palindromic_Subsequences/SolutionITest.java
@@ -0,0 +1,27 @@
+package interviews.linkedin._730_Count_Different_Palindromic_Subsequences;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionITest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    solution = new SolutionI();
+  }
+
+  @Test
+  void testOnlineCase1() throws Exception {
+    final String input = "bccb";
+    assertEquals(6, solution.countPalindromicSubsequences(input));
+  }
+
+  @Test
+  void testOnlineCase2() throws Exception {
+    final String input = "abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba";
+    assertEquals(104860361, solution.countPalindromicSubsequences(input));
+  }
+}
