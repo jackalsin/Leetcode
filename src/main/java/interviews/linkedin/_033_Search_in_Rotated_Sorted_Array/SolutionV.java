843ad1581104bc6c354b58b127c6428306f5d0a4
jackalsin <jackal.sin@hotmail.com>
2018-10-12 21:51:24 -0700

    Finish 033

---
+++ src/main/java/interviews/linkedin/_033_Search_in_Rotated_Sorted_Array/SolutionV.java
@@ -0,0 +1,43 @@
+package interviews.linkedin._033_Search_in_Rotated_Sorted_Array;
+
+public final class SolutionV implements Solution {
+  @Override
+  public int search(int[] nums, int target) {
+    if (nums == null || nums.length == 0) {
+      return -1;
+    }
+    int left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+      final int mid = left + (right - left) / 2;
+      if (nums[mid] < nums[right]) {
+        if (nums[mid] <= target && target <= nums[right]) {
+          return binarySearch(nums, target, mid, right);
+        } else {
+          right = mid - 1;
+        }
+      } else {
+        if (nums[left] <= target && target <= nums[mid]) {
+          return binarySearch(nums, target, left, mid);
+        } else {
+          left = mid + 1;
+        }
+      }
+    }
+    return -1;
+  }
+
+  private static int binarySearch(final int[] nums, final int target, int left, int right) {
+    while (left <= right) {
+      final int mid = (right - left) / 2 + left;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return -1;
+  }
+}
