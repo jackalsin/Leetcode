2caac90343d95dac1ccafa368700c2bf0d45c231
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-14 19:00:10 -0700

    Finish 255

---
+++ src/main/java/interviews/linkedin/_255_Verify_Preorder_Sequence_in_Binary_Search_Tree/SolutionIII.java
@@ -0,0 +1,27 @@
+package interviews.linkedin._255_Verify_Preorder_Sequence_in_Binary_Search_Tree;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public final class SolutionIII implements Solution {
+
+  public boolean verifyPreorder(int[] preorder) {
+    long preVal = Long.MIN_VALUE;
+
+    final Deque<Integer> stack = new ArrayDeque<>();
+
+    for (int num : preorder) {
+
+      if (num < preVal) {
+        return false;
+      }
+
+      while (!stack.isEmpty() && stack.peek() < num) {
+        preVal = stack.pop();
+      }
+      stack.push(num);
+    }
+    return true;
+  }
+
+}
