5ecbb207201b43cd0879a4443ae2b7f0fc301c74
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-18 20:04:42 -0700

    Finish 547 with DFS and UnionFind

---
+++ src/main/java/_501_550/_547_Friend_Circles/DFSSolution.java
@@ -0,0 +1,28 @@
+package _501_550._547_Friend_Circles;
+
+public final class DFSSolution implements Solution {
+  @Override
+  public int findCircleNum(int[][] M) {
+    final int n = M.length;
+    assert 1 <= n && n <= 200;
+    int count = 0;
+    final boolean[] visited = new boolean[n];
+    for (int i = 0; i < n; i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        dfs(visited, M, i);
+        count++;
+      }
+    }
+    return count;
+  }
+
+  private static void dfs(final boolean[] visited, final int[][] M, final int start) {
+    for (int i = 0; i < M.length; i++) {
+      if (start != i && !visited[i] && M[i][start] == 1) {
+        visited[i] = true;
+        dfs(visited, M, i);
+      }
+    }
+  }
+}
