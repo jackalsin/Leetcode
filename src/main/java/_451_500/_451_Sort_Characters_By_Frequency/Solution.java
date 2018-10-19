00b30df82b93cf8359e96ee6cc9043ad95ecf955
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-19 10:37:59 -0700

    Finish 451

---
+++ src/main/java/_451_500/_451_Sort_Characters_By_Frequency/Solution.java
@@ -0,0 +1,5 @@
+package _451_500._451_Sort_Characters_By_Frequency;
+
+public interface Solution {
+  String frequencySort(String s);
+}
---
+++ src/main/java/_451_500/_451_Sort_Characters_By_Frequency/SolutionII.java
@@ -0,0 +1,38 @@
+package _451_500._451_Sort_Characters_By_Frequency;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SolutionII implements Solution {
+
+  @Override
+  public String frequencySort(String s) {
+    final int[] counts = new int[256];
+    final char[] chars = s.toCharArray();
+
+    for (char chr : chars) {
+      counts[chr]++;
+    }
+    final List<Character>[] buckets = new List[s.length() + 1];
+    for (int i = 0; i < counts.length; i++) {
+      final char chr = (char) i;
+      final int count = counts[i];
+      if (buckets[count] == null) {
+        buckets[count] = new ArrayList<>();
+      }
+      buckets[count].add(chr);
+    }
+    final StringBuilder sb = new StringBuilder();
+    for (int count = buckets.length - 1; count >= 0; count--) {
+      if (buckets[count] == null) {
+        continue;
+      }
+      for (char chr : buckets[count]) {
+        for (int i = 0; i < count; i++) {
+          sb.append(chr);
+        }
+      }
+    }
+    return sb.toString();
+  }
+}
