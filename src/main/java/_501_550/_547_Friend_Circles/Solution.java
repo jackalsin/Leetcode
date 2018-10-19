50262d5be91119addf9658d6c1ab7a2bcf142bb6
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-18 19:36:28 -0700

    add 547

---
+++ src/main/java/_501_550/_547_Friend_Circles/Solution.java
@@ -0,0 +1,6 @@
+package _501_550._547_Friend_Circles;
+
+public interface Solution {
+  int findCircleNum(int[][] M);
+
+}
---
+++ src/main/java/interviews/linkedin/_547_Friend_Circles/Solution.java
@@ -0,0 +1,6 @@
+package interviews.linkedin._547_Friend_Circles;
+
+public interface Solution {
+  // TODO:
+  int findCircleNum(int[][] M);
+}
