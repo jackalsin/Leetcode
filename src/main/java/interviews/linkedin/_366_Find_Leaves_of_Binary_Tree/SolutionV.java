fa61524df9393b76a1f8c896a536c4b0e43e74cb
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-21 13:19:48 -0700

    Finish 273

---
+++ src/main/java/interviews/linkedin/_366_Find_Leaves_of_Binary_Tree/SolutionV.java
@@ -0,0 +1,12 @@
+package interviews.linkedin._366_Find_Leaves_of_Binary_Tree;
+
+import utils.TreeNode;
+
+import java.util.List;
+
+public class SolutionV implements Solution {
+  @Override
+  public List<List<Integer>> findLeaves(TreeNode root) {
+    return null;
+  }
+}
