0a51dfde7364b81803c6c51882c162221e9fabf2
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-20 23:22:46 -0700

    Finish 341

--- src/main/java/interviews/linkedin/_341_Flatten_Nested_List_Iterator/NestedIterator.java
+++ src/main/java/interviews/linkedin/_341_Flatten_Nested_List_Iterator/NestedIterator.java
@@ -1,13 +1,11 @@
 package interviews.linkedin._341_Flatten_Nested_List_Iterator;
 
-import utils.nestedInteger.NestedInteger;
-
-import java.util.*;
+import java.util.Iterator;
 
 /**
  * @author zhiwei.xin
  * @version 1.0 on 9/5/18
  */
 public interface NestedIterator extends Iterator<Integer> {
-
+// TODO: Revisit
 }
