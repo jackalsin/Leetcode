f1f79bbb7a79a0899a1d460cef49765260648810
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-24 10:48:18 -0700

    Finish 039

---
+++ src/main/java/interviews/linkedin/_039_Combination_Sum/SolutionI.java
@@ -0,0 +1,31 @@
+package interviews.linkedin._039_Combination_Sum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SolutionI implements Solution {
+  @Override
+  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    final List<List<Integer>> result = new ArrayList<>();
+    final List<Integer> curPath = new ArrayList<>();
+    dfs(result, candidates, target, curPath, 0, 0);
+    return result;
+  }
+
+  private static void dfs(final List<List<Integer>> result, final int[] candidates, final int target,
+                          final List<Integer> curPath, final int start, final int sum) {
+    if (sum > target) {
+      return;
+    } else if (sum == target) {
+
+      result.add(new ArrayList<>(curPath));
+      return;
+    }
+
+    for (int i = start; i < candidates.length; ++i) {
+      curPath.add(candidates[i]);
+      dfs(result, candidates, target, curPath, i, sum + candidates[i]);
+      curPath.remove(curPath.size() - 1);
+    }
+  }
+}
---
+++ src/test/java/interviews/linkedin/_039_Combination_Sum/SolutionITest.java
@@ -0,0 +1,33 @@
+package interviews.linkedin._039_Combination_Sum;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionITest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new SolutionI();
+  }
+
+  @Test
+  void testWithExampleCase() throws Exception {
+    List<List<Integer>> expect = new ArrayList<>();
+    expect.add(Arrays.asList(2, 2, 3));
+    expect.add(Collections.singletonList(7));
+
+    List<List<Integer>> actual = solution.combinationSum(new int[]{2, 3, 6, 7}, 7);
+    assertEquals(expect.size(), actual.size());
+    assertEquals(new HashSet<>(expect), new HashSet<>(actual));
+
+  }
+}
