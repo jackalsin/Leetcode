0d074fd30aabbed9fa12f078e940701bf75a5c5d
jackalsin <jackal.sin@hotmail.com>
2018-10-23 21:44:43 -0700

    Finish 033

--- src/main/java/interviews/linkedin/_033_Search_in_Rotated_Sorted_Array/SolutionV.java
+++ src/main/java/interviews/linkedin/_033_Search_in_Rotated_Sorted_Array/SolutionV.java
@@ -1,15 +1,14 @@
 package interviews.linkedin._033_Search_in_Rotated_Sorted_Array;
 
 public final class SolutionV implements Solution {
-  @Override
+
   public int search(int[] nums, int target) {
     if (nums == null || nums.length == 0) {
       return -1;
     }
     int left = 0, right = nums.length - 1;
-
-    while (left <= right) {
-      final int mid = left + (right - left) / 2;
+    while (left <= right) { // TODO: equals
+      final int mid = (right - left) / 2 + left;
       if (nums[mid] < nums[right]) {
         if (nums[mid] <= target && target <= nums[right]) {
           return binarySearch(nums, target, mid, right);
@@ -24,18 +23,19 @@ public final class SolutionV implements Solution {
         }
       }
     }
+
     return -1;
   }
 
   private static int binarySearch(final int[] nums, final int target, int left, int right) {
     while (left <= right) {
       final int mid = (right - left) / 2 + left;
-      if (nums[mid] == target) {
+      if (target == nums[mid]) {
         return mid;
-      } else if (nums[mid] < target) {
-        left = mid + 1;
-      } else {
+      } else if (target < nums[mid]) {
         right = mid - 1;
+      } else {
+        left = mid + 1;
       }
     }
     return -1;
