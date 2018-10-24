d195ec506a4d839ae9b8b30a09e9a4c33276bfd1
jackalsin <jackal.sin@hotmail.com>
2018-10-23 20:47:12 -0700

    Finish 020

---
+++ src/main/java/interviews/linkedin/_020_Valid_Parentheses/Solution.java
@@ -0,0 +1,5 @@
+package interviews.linkedin._020_Valid_Parentheses;
+
+public interface Solution {
+  boolean isValid(String s);
+}
---
+++ src/main/java/interviews/linkedin/_020_Valid_Parentheses/SolutionI.java
@@ -0,0 +1,35 @@
+package interviews.linkedin._020_Valid_Parentheses;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class SolutionI implements Solution {
+  private static final Map<Character, Character> MAP = new HashMap<>() {{
+    put('}', '{');
+    put(')', '(');
+    put(']', '[');
+  }};
+
+  @Override
+  public boolean isValid(String s) {
+    final Deque<Character> stack = new ArrayDeque<>();
+
+    for (char chr : s.toCharArray()) {
+      if (chr == '(' || chr == '{' || chr == '[') {
+        stack.push(chr);
+      } else {
+        if (stack.isEmpty()) {
+          return false;
+        }
+        final char matchChr = MAP.get(chr);
+        if (stack.pop() != matchChr) {
+          return false;
+        }
+      }
+    }
+
+    return stack.isEmpty();
+  }
+}
