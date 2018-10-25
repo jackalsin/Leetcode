a581eaa0b55cb3cf1ea8ac97cc7d63d60b481689
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-24 17:40:09 -0700

    Finish 695

---
+++ src/main/java/interviews/linkedin/_695_Max_Area_of_Island/Solution.java
@@ -0,0 +1,6 @@
+package interviews.linkedin._695_Max_Area_of_Island;
+
+public interface Solution {
+  int maxAreaOfIsland(int[][] grid);
+
+}
---
+++ src/test/java/interviews/linkedin/_695_Max_Area_of_Island/SolutionITest.java
@@ -0,0 +1,31 @@
+package interviews.linkedin._695_Max_Area_of_Island;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionITest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new SolutionI();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final int[][] input = {
+        {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+        {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+    };
+
+    assertEquals(6, solution.maxAreaOfIsland(input));
+  }
+}
