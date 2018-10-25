275a51b56f1289b8ab37987bc58f6ec45319e5ae
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-24 19:46:03 -0700

    Finish 671

--- src/main/java/interviews/linkedin/_671_Second_Minimum_Node_In_a_Binary_Tree/Solution.java
+++ src/main/java/interviews/linkedin/_671_Second_Minimum_Node_In_a_Binary_Tree/Solution.java
@@ -6,29 +6,13 @@ import utils.TreeNode;
  * @author jacka
  * @version 1.0 on 10/23/2017.
  */
-public class Solution {
+public interface Solution {
 
-  public int findSecondMinimumValue(TreeNode root) {
-    if (root == null) {
-      return -1;
-    }
-    return minVal(root, root.val);
-  }
-
-  private int minVal(TreeNode root, int min) {
-    if (root == null) {
-      return -1;
-    }
-    if (root.val != min) {
-      return root.val;
-    }
-    int left = minVal(root.left, min), right = minVal(root.right, min);
-    if (left == -1) {
-      return right;
-    }
-    if (right == -1) {
-      return left;
-    }
-    return Math.min(left, right);
-  }
+  /**
+   * TODO: Revisit
+   *
+   * @param root
+   * @return
+   */
+  int findSecondMinimumValue(TreeNode root);
 }
