b13b17b5f8e23d729acb3971d3e1dec803fb0a06
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-26 15:04:52 -0700

    Finish 714

---
+++ src/main/java/interviews/linkedin/_714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee/Solution.java
@@ -0,0 +1,6 @@
+package interviews.linkedin._714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee;
+
+public interface Solution {
+  int maxProfit(int[] prices, int fee);
+
+}
---
+++ src/test/java/interviews/linkedin/_714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee/SolutionITest.java
@@ -0,0 +1,22 @@
+package interviews.linkedin._714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionITest {
+  private Solution solution;
+
+  @BeforeEach
+  void setup() {
+    solution = new SolutionI();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final int[] prices = {1, 3, 2, 8, 4, 9};
+    assertEquals(8, solution.maxProfit(prices, 2));
+  }
+
+}
