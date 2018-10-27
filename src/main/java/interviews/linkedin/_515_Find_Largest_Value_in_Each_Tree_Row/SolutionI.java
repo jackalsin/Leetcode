64a7e04b133332f1a64d5ab5e5db1f1cc4fa0066
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-27 14:18:11 -0700

    Finish 515

---
+++ src/main/java/interviews/linkedin/_515_Find_Largest_Value_in_Each_Tree_Row/SolutionI.java
@@ -0,0 +1,40 @@
+package interviews.linkedin._515_Find_Largest_Value_in_Each_Tree_Row;
+
+import utils.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/23/2017.
+ */
+public class SolutionI {
+  public List<Integer> largestValues(TreeNode root) {
+    final List<Integer> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+    final Queue<TreeNode> q = new ArrayDeque<>();
+    q.add(root);
+    while (!q.isEmpty()) {
+      final int size = q.size();
+      int max = Integer.MIN_VALUE;
+      for (int i = 0; i < size; ++i) {
+        final TreeNode toRemove = q.remove();
+        max = Math.max(max, toRemove.val);
+        if (toRemove.left != null) {
+          q.add(toRemove.left);
+        }
+        if (toRemove.right != null) {
+          q.add(toRemove.right);
+        }
+      }
+      result.add(max);
+    }
+    return result;
+  }
+
+}
