91f65acab252abf0a96e000a8a142dd022273666
zhiwei.xin <zhiwei.xin@veritas.com>
2018-10-27 14:52:56 -0700

    Finish 468

--- src/test/java/interviews/linkedin/_468_Validate_IP_Address/SolutionIITest.java
+++ src/test/java/interviews/linkedin/_468_Validate_IP_Address/SolutionIITest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SolutionTest {
+class SolutionIITest {
   private Solution solution;
 
   @BeforeEach
   void setUp() {
-    solution = new Solution();
+    solution = new SolutionII();
   }
 
   @Test
