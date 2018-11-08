f68b49848eb26a39524204b3296df521af69e4fe
zhiwei.xin <zhiwei.xin@veritas.com>
2018-11-07 17:06:20 -0800

    Update 710

---
+++ src/test/java/_701_750/_710_Random_Pick_with_Blacklist/SolutionTest.java
@@ -0,0 +1,51 @@
+package _701_750._710_Random_Pick_with_Blacklist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private static final int MAX_COUNT = 1_000_000;
+  private static final double BIAS = 1E-3;
+
+  @Test
+  void testOnlineCase1() {
+    final int N = 4, blacklists[] = {};
+    final Solution solution = new Solution(N, blacklists);
+    final int[] counts = new int[N];
+    for (int i = 0; i < MAX_COUNT; ++i) {
+      counts[solution.pick()]++;
+    }
+
+    for (int i = 0; i < N; ++i) {
+      assertEquals(0.25, counts[i] / (double) MAX_COUNT, BIAS);
+    }
+  }
+
+  @Test
+  void testOnlineCase2() {
+    final int N = 3, blacklists[] = {1};
+    final Solution solution = new Solution(N, blacklists);
+    final int[] counts = new int[N];
+    for (int i = 0; i < MAX_COUNT; ++i) {
+      counts[solution.pick()]++;
+    }
+
+    assertEquals(0.5d, counts[0] / (double) MAX_COUNT, BIAS);
+    assertEquals(0.5d, counts[2] / (double) MAX_COUNT, BIAS);
+  }
+
+  @Test
+  void testOnlineCase3() {
+    final int N = 4, blacklists[] = {2};
+    final Solution solution = new Solution(N, blacklists);
+    final int[] counts = new int[N];
+    for (int i = 0; i < MAX_COUNT; ++i) {
+      counts[solution.pick()]++;
+    }
+
+    assertEquals(1 / 3d, counts[0] / (double) MAX_COUNT, BIAS);
+    assertEquals(1 / 3d, counts[1] / (double) MAX_COUNT, BIAS);
+    assertEquals(1 / 3d, counts[3] / (double) MAX_COUNT, BIAS);
+  }
+}
