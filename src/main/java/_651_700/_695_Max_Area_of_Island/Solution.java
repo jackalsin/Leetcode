b056e2f8f291bf9eedbd87859273984189758e81
zhiwei.xin <zhiwei.xin@veritas.com>
2018-11-10 18:30:47 -0800

    Finish 695

--- src/main/java/_651_700/_695_Max_Area_of_Island/Solution.java
+++ src/main/java/_651_700/_695_Max_Area_of_Island/Solution.java
@@ -1,36 +1,31 @@
 package _651_700._695_Max_Area_of_Island;
 
 public class Solution {
-  int max = 0, count = 0;
-
   public int maxAreaOfIsland(int[][] grid) {
-    if (grid == null || grid.length == 0) {
-      return max;
+    if (grid == null || grid.length == 0 || grid[0].length == 0) {
+      return 0;
     }
     final int rows = grid.length, cols = grid[0].length;
-    for (int row = 0; row < rows; ++row) {
-      for (int col = 0; col < cols; ++col) {
+    int max = 0;
+    for (int row = 0; row < rows; row++) {
+      for (int col = 0; col < cols; col++) {
         if (grid[row][col] == 1) {
-          count = 0;
-          dfs(grid, rows, cols, row, col);
-          max = Math.max(count, max);
+          max = Math.max(max, dfs(grid, rows, cols, row, col));
         }
       }
+
     }
     return max;
   }
 
-  private void dfs(final int[][] grid, final int rows, final int cols, int row, int col) {
-    if (row >= rows || row < 0 || col < 0 || col >= cols || grid[row][col] == 0) {
-      return;
-    }
-
-    count++;
+  private static int dfs(final int[][] grid, final int rows, final int cols, final int row, final int col) {
+    if (row < 0 || col < 0 || col >= cols || row >= rows || grid[row][col] == 0)
+      return 0;
     grid[row][col] = 0;
-    dfs(grid, rows, cols, row + 1, col);
-    dfs(grid, rows, cols, row - 1, col);
-    dfs(grid, rows, cols, row, col + 1);
-    dfs(grid, rows, cols, row, col - 1);
+    return 1 + dfs(grid, rows, cols, row + 1, col) +
+        dfs(grid, rows, cols, row - 1, col) +
+        dfs(grid, rows, cols, row, col + 1) +
+        dfs(grid, rows, cols, row, col - 1);
   }
 
 }
