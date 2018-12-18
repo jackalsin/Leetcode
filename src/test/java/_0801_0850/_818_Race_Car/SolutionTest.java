38709c9be797415fd993140b9fa01f120eb430e9
jackalsin <jackal.sin@hotmail.com>
2018-12-17 21:39:14 -0800

    818 Error

---
+++ src/test/java/_0801_0850/_818_Race_Car/SolutionTest.java
@@ -0,0 +1,30 @@
+package _0801_0850._818_Race_Car;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    assertEquals(2, solution.racecar(3));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    assertEquals(5, solution.racecar(6));
+  }
+
+  @Test
+  void testOnlineCase3() {
+    assertEquals(7, solution.racecar(5));
+  }
+}
