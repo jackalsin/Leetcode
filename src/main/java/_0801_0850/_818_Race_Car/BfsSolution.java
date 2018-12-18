3de77786789ffbdc4352fd622ceda1fdaf6f5a4a
jackalsin <jackal.sin@hotmail.com>
2018-12-18 08:25:30 -0800

    Finish 818 BFS solution

---
+++ src/main/java/_0801_0850/_818_Race_Car/BfsSolution.java
@@ -0,0 +1,50 @@
+package _0801_0850._818_Race_Car;
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
+public final class BfsSolution implements Solution {
+
+  public int racecar(int target) {
+    assert 1 <= target && target <= 10000;
+    final Queue<int[]> q = new ArrayDeque<>();
+    q.add(new int[]{0, 1});
+    final Set<String> visited = new HashSet<>();
+    visited.add(getKey(0, 1));
+    int level = 0;
+    while (!q.isEmpty()) {
+      final int size = q.size();
+      for (int i = 0; i < size; i++) {
+        final int[] toRemove = q.remove();
+        final int pos = toRemove[0], speed = toRemove[1];
+        if (pos == target) {
+          return level;
+        }
+
+        // continue positive
+        final int[] nextAccelerate = {pos + speed, speed * 2};
+        if (!visited.contains(getKey(nextAccelerate[0], nextAccelerate[1]))
+            && nextAccelerate[0] > 0 && nextAccelerate[0] < 2 * target) {
+          q.add(nextAccelerate);
+          visited.add(getKey(nextAccelerate[0], nextAccelerate[1]));
+        }
+        final int[] nextReverse = {pos, speed > 0 ? -1 : 1};
+        if (!visited.contains(getKey(nextReverse[0], nextReverse[1]))
+            && nextReverse[0] > 0 && nextReverse[0] < 2 * target) {
+          q.add(nextReverse);
+          visited.add(getKey(nextReverse[0], nextReverse[1]));
+        }
+      }
+      level++;
+    }
+    return -1; // should never reachable
+  }
+
+  private static String getKey(int pos, int speed) { // todo: add new document for this
+    return pos + " " + speed;
+  }
+
+
+}
--- src/main/java/_0801_0850/_818_Race_Car/Solution.java
+++ src/main/java/_0801_0850/_818_Race_Car/Solution.java
@@ -1,29 +1,7 @@
 package _0801_0850._818_Race_Car;
 
-public final class Solution {
+public interface Solution {
 
-  public int racecar(int target) { // todo:
-    assert 1 <= target && target <= 10000;
-    final int[][] dp = new int[target * 2 + 1][2 * target + 1];
-    return racecar(dp, target, 1);
-  }
-
-  private static int racecar(final int[][] dp, final int target, final int speed) {
-    assert target >= 0;
-    if (target == 0) {
-      return 0;
-    } else if (dp[target][speed] != 0) {
-      return dp[target][speed];
-    }
-    int minSteps = Integer.MAX_VALUE;
-    if (target >= speed) {
-      minSteps = Math.min(minSteps, 1 + racecar(dp, target - speed, speed * 2));
-      minSteps = Math.min(minSteps, 3 + racecar(dp, target - speed, 1));
-    } else {
-      minSteps = Math.min(minSteps, 2 + racecar(dp, speed - target, 1));
-    }
-    dp[target][speed] = minSteps;
-    return minSteps;
-  }
+  int racecar(int target);
 
 }
