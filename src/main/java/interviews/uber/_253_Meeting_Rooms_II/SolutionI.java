3b70a9def9ab366bb8619afac65d21c4f545074e
jackalsin <jackal.sin@hotmail.com>
2018-12-23 19:41:05 -0800

    Add 253 untested

--- src/main/java/interviews/uber/_253_Meeting_Rooms_II/SolutionI.java
+++ src/main/java/interviews/uber/_253_Meeting_Rooms_II/SolutionI.java
@@ -2,44 +2,41 @@ package interviews.uber._253_Meeting_Rooms_II;
 
 import utils.Interval;
 
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-public final class SolutionI implements Solution {
+public final class SolutionI implements Solution { // TODO: untested
   public int minMeetingRooms(Interval[] intervals) {
-    final TreeMap<Interval, Integer> map = new TreeMap<>(new Comparator<Interval>() {
+    if (intervals == null || intervals.length == 0) {
+      return 0;
+    }
+
+    Arrays.sort(intervals, new Comparator<Interval>() {
       @Override
       public int compare(Interval o1, Interval o2) {
-        return Integer.compare(o1.end, o2.end);
+        return Integer.compare(o1.start, o2.start);
       }
     });
-    int minMeetingRoom = 0;
-    map.put(new Interval(0, Integer.MAX_VALUE), 0);
-    for (Interval i : intervals) {
-      i.end--; // make inclusive
-      final Map<Interval, Integer> tailMap = new TreeMap<>(map.tailMap(
-          new Interval(i.start, i.start)));
-      for (Map.Entry<Interval, Integer> e : tailMap.entrySet()) {
-        final Interval interval = e.getKey();
-        final int count = e.getValue();
-        if (interval.start > i.end) break;
-
-        final int intervalStart = interval.start, intervalEnd = interval.end,
-            iStart = i.start, iEnd = i.end;
-        map.remove(interval);
-
-        if (intervalStart < iStart) {
-          map.put(new Interval(intervalStart, iStart - 1), count);
+    final Queue<Integer> pq = new PriorityQueue<>(
+        new Comparator<Integer>() {
+          @Override
+          public int compare(Integer o1, Integer o2) {
+            return Integer.compare(o1, o2);
+          }
         }
-        if (iEnd < intervalEnd) {
-          map.put(new Interval(iEnd + 1, intervalEnd), count);
-        }
-        // i.start <= interval.start && interval.end <= i.end;
-        map.put(new Interval(Math.max(iStart, intervalStart), Math.min(iEnd, intervalEnd)), count + 1);
-        minMeetingRoom = Math.max(minMeetingRoom, count + 1);
+    );
+    int min = 1;
+    pq.add(intervals[0].end);
+    for (int i = 1; i < intervals.length; i++) {
+      if (!pq.isEmpty() && intervals[i].start >= pq.peek()) {
+        pq.remove();
       }
+
+      pq.add(intervals[i].end);
+      min = Math.max(min, pq.size());
     }
-    return minMeetingRoom;
-  }
+    return min;
+  } // end of method
 }
