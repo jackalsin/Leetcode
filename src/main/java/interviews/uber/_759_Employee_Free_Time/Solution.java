77528bcd2cf841c29c2f534fa510397658730bb3
jackalsin <jackal.sin@hotmail.com>
2019-01-01 21:59:01 -0800

    Add 759

--- src/main/java/interviews/uber/_759_Employee_Free_Time/Solution.java
+++ src/main/java/interviews/uber/_759_Employee_Free_Time/Solution.java
@@ -2,49 +2,41 @@ package interviews.uber._759_Employee_Free_Time;
 
 import utils.Interval;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
   public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-    final Queue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-      /**
-       * @param o1 [listIndex, elem index]
-       * @param o2
-       * @return
-       */
-      @Override
-      public int compare(int[] o1, int[] o2) {
-        final Interval i1 = schedule.get(o1[0]).get(o1[1]), i2 = schedule.get(o2[0]).get(o2[1]);
-        return Integer.compare(i1.start, i2.start);
+    final int size = getSize(schedule);
+    final int[] starts = new int[size], ends = new int[size];
+    {
+      int index = 0;
+      for (List<Interval> s : schedule) {
+        for (Interval i : s) {
+          starts[index] = i.start;
+          ends[index++] = i.end;
+        }
       }
-    });
+    }
+    Arrays.sort(starts);
+    Arrays.sort(ends);
     final List<Interval> result = new ArrayList<>();
-    for (int i = 0; i < schedule.size(); i++) {
-      final List<Interval> s = schedule.get(i);
-      if (!s.isEmpty()) {
-        pq.add(new int[]{i, 0});
+    for (int i = 1; i < starts.length; i++) {
+      final int prevEnd = ends[i - 1], start = starts[i];
+      if (prevEnd < start) {
+        result.add(new Interval(prevEnd, start));
       }
-    }
-
-    Interval prev = new Interval(0, 1);
-    while (!pq.isEmpty()) {
-      final int[] toRemove = pq.remove();
-      final List<Interval> curList = schedule.get(toRemove[0]);
-      final Interval curInterval = curList.get(toRemove[1]);
-      if (prev.end >= curInterval.start) {
-        prev.end = Math.max(prev.end, curInterval.end);
-      } else {
-        result.add(prev);
-        prev = new Interval(curInterval.end, curInterval.end + 1);
-      }
-      if (toRemove[1] + 1 < curList.size()) {
-        pq.add(new int[]{toRemove[0], toRemove[1] + 1});
-      }
-    }
-    // todo: doubts
-    if (result.size() > 0 && result.get(result.size() - 1).start != prev.start) {
-      result.add(prev);
     }
     return result;
+  }
+
+
+  private static int getSize(final List<List<Interval>> schedule) {
+    int size = 0;
+    for (final List<Interval> s : schedule) {
+      size += s.size();
+    }
+    return size;
   }
 }
