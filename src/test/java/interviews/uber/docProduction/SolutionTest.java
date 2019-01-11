d49b581661cb538512fe677446f2542c3bc87a4d
jackalsin <jackal.sin@hotmail.com>
2019-01-10 23:08:07 -0800

    Add Dot Production

---
+++ src/test/java/interviews/uber/docProduction/SolutionTest.java
@@ -0,0 +1,21 @@
+package interviews.uber.docProduction;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final String a = "abc", b = "def";
+    assertEquals(32, solution.docProduction(a, b));
+  }
+}
