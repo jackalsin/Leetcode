d72b26e531c8b3b90faeb963781a3541b3682606
jackalsin <jackal.sin@hotmail.com>
2019-01-13 14:03:36 -0800

    Add 347

--- src/main/java/interviews/uber/_347_Top_K_Frequent_Elements/Solution.java
+++ src/main/java/interviews/uber/_347_Top_K_Frequent_Elements/Solution.java
@@ -1,29 +1,39 @@
 package interviews.uber._347_Top_K_Frequent_Elements;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Solution {
   public List<Integer> topKFrequent(int[] nums, int k) {
-    final Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
-
-    for (int n : nums) {
-      frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+    final Map<Integer, Integer> freqMap = new HashMap<>();
+    for (int num : nums) {
+      freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
     }
 
-    final List<List<Integer>> buckets = new ArrayList<>();
-    for (int i = 0; i < nums.length + 1; i++) {
-      buckets.add(new ArrayList<>());
+    final Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
+        new Comparator<Map.Entry<Integer, Integer>>() {
+          @Override
+          public int compare(Map.Entry<Integer, Integer> e1, Map.Entry<Integer, Integer> e2) {
+            return Integer.compare(e1.getValue(), e2.getValue());
+          }
+        }
+    );
+
+    for (Map.Entry<Integer, Integer> e : freqMap.entrySet()) {
+      pq.add(e);
+      if (pq.size() > k) {
+        pq.remove();
+      }
     }
 
-    for (final Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-      buckets.get(entry.getValue()).add(entry.getKey());
-    }
-    final List<Integer> result = new ArrayList<>();
-    for (int i = buckets.size() - 1; i >= 0 && result.size() < k; i--) {
-      result.addAll(buckets.get(i));
+    final LinkedList<Integer> result = new LinkedList<>();
+    while (!pq.isEmpty()) {
+      result.add(pq.remove().getKey());
     }
     return result;
   }
