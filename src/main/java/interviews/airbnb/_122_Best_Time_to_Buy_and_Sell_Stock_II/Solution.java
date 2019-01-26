078d33a3fe32c5b46d1dd7d4bc4b3fb91b16a2d5
jackal.sin <jackal.sin@hotmail.com>
2019-01-26 11:23:31 -0800

    Finish 122

---
+++ src/main/java/interviews/airbnb/_122_Best_Time_to_Buy_and_Sell_Stock_II/Solution.java
@@ -0,0 +1,15 @@
+package interviews.airbnb._122_Best_Time_to_Buy_and_Sell_Stock_II;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/26/2019.
+ */
+public class Solution {
+  public int maxProfit(int[] prices) {
+    int maxProfit = 0;
+    for (int i = 1; i < prices.length; i++) {
+      maxProfit += Math.max(0, prices[i] - prices[i - 1]);
+    }
+    return maxProfit;
+  }
+}
