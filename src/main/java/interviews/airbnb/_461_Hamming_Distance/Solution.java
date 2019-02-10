65a9ad64f2597e290cfe308aa55b1ba76be4c13e
jackal.sin <jackal.sin@hotmail.com>
2019-02-09 20:07:16 -0800

    Finish 461

---
+++ src/main/java/interviews/airbnb/_461_Hamming_Distance/Solution.java
@@ -0,0 +1,18 @@
+package interviews.airbnb._461_Hamming_Distance;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/9/2019.
+ */
+public class Solution {
+  public int hammingDistance(int x, int y) {
+    int dist = 0;
+    for (int i = (1 << 31); i != 0; i >>>= 1) {
+      final int one = x & i, that = y & i;
+      if (one != that) {
+        dist++;
+      }
+    }
+    return dist;
+  }
+}
