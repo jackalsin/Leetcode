3b36f7f2fd3446b7c1ba5503f27c59fda6d44e49
jackal.sin <jackal.sin@hotmail.com>
2019-02-10 09:58:28 -0800

    Fix 269 Bug Test

--- src/test/java/_0251_0300/_269_Alien_Dictionary/SolutionTest.java
+++ src/test/java/_0251_0300/_269_Alien_Dictionary/SolutionTest.java
@@ -1,8 +1,8 @@
 package _0251_0300._269_Alien_Dictionary;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
