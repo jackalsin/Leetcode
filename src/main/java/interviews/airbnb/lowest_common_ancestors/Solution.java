3b7fe6f8a18973e1abfabdc2eff183fc9d491619
jackal.sin <jackal.sin@hotmail.com>
2019-02-18 13:00:41 -0800

    Add lowest common ancestors

---
+++ src/main/java/interviews/airbnb/lowest_common_ancestors/Solution.java
@@ -0,0 +1,10 @@
+package interviews.airbnb.lowest_common_ancestors;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/17/2019.
+ */
+public interface Solution {
+
+  String commonAncestors(String word1, String word2);
+}
---
+++ src/test/java/interviews/airbnb/lowest_common_ancestors/SolutionITest.java
@@ -0,0 +1,29 @@
+package interviews.airbnb.lowest_common_ancestors;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/18/2019.
+ */
+class SolutionITest {
+  private Solution solution;
+
+  @Test
+  void testOnlineCase1() {
+    final String[][] words = {
+        {"earth", "america"},
+        {"america", "south america", "north america"},
+        {"north america", "canada", "us"},
+        {"canada", "ontario", "quebec", "calgary"},
+        {"us", "california"}
+    };
+    solution = new SolutionI(words);
+
+    assertEquals("north america", solution.commonAncestors("us", "canada"));
+    assertEquals("north america", solution.commonAncestors("california", "canada"));
+    assertEquals("north america", solution.commonAncestors("california", "ontario"));
+  }
+}
