bc53ed9eb51aa4b8b9083dc928bd8638e040c7c1
jackal.sin <jackal.sin@hotmail.com>
2019-02-20 18:36:41 -0800

    Finish 252

--- src/main/java/_0251_0300/_252_Meeting_Rooms/Solution.java
+++ src/main/java/_0251_0300/_252_Meeting_Rooms/Solution.java
@@ -9,7 +9,7 @@ import java.util.Comparator;
  * @author Zhiwei.Xin
  * @version 1.0 on 8/25/2017.
  */
-public class Solution {
+public final class Solution {
   /**
    * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
    * For example,
@@ -22,12 +22,14 @@ public class Solution {
   public boolean canAttendMeetings(Interval[] intervals) {
     Arrays.sort(intervals, new Comparator<Interval>() {
       @Override
-      public int compare(Interval i1, Interval i2) {
-        return Integer.compare(i1.start, i2.start);
+      public int compare(Interval o1, Interval o2) {
+        return Integer.compare(o1.start, o2.start);
       }
     });
-    for (int i = 1; i < intervals.length; ++i) {
-      if(intervals[i].start < intervals[i - 1].end) {
+
+    for (int i = 1; i < intervals.length; i++) {
+      final Interval cur = intervals[i], prev = intervals[i - 1];
+      if (prev.end > cur.start) {
         return false;
       }
     }
--- src/test/java/_0251_0300/_252_Meeting_Rooms/SolutionTest.java
+++ src/test/java/_0251_0300/_252_Meeting_Rooms/SolutionTest.java
@@ -1,7 +1,7 @@
 package _0251_0300._252_Meeting_Rooms;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import utils.Interval;
 
 import static org.junit.Assert.assertFalse;
@@ -11,35 +11,35 @@ import static org.junit.Assert.assertTrue;
  * @author Zhiwei.Xin
  * @version 1.0 on 8/25/2017.
  */
-public class SolutionTest {
+class SolutionTest {
   private Solution solution;
 
-  @Before
-  public void setUp() throws Exception {
+  @BeforeEach
+  void setUp() throws Exception {
     solution = new Solution();
   }
 
   @Test
-  public void testEmpty() throws Exception {
-    final Interval[] input = new Interval[] {};
+  void testEmpty() throws Exception {
+    final Interval[] input = new Interval[]{};
     assertTrue(solution.canAttendMeetings(input));
   }
 
   @Test
-  public void testOne() throws Exception {
-    final Interval[] input = new Interval[] {new Interval(3, 6)};
+  void testOne() throws Exception {
+    final Interval[] input = new Interval[]{new Interval(3, 6)};
     assertTrue(solution.canAttendMeetings(input));
   }
 
   @Test
-  public void testMulti() throws Exception {
-    final Interval[] input = new Interval[] {new Interval(3, 6), new Interval(6, 20)};
+  void testMulti() throws Exception {
+    final Interval[] input = new Interval[]{new Interval(3, 6), new Interval(6, 20)};
     assertTrue(solution.canAttendMeetings(input));
   }
 
   @Test
-  public void testNegative() throws Exception {
-    final Interval[] input = new Interval[] {new Interval(3, 6), new Interval(3, 20)};
+  void testNegative() throws Exception {
+    final Interval[] input = new Interval[]{new Interval(3, 6), new Interval(3, 20)};
     assertFalse(solution.canAttendMeetings(input));
   }
 
