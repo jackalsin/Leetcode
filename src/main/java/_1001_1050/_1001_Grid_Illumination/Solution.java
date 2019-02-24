254500a25d6c83e6ca254d710f88e62fb2e8f6be
jackal.sin <jackal.sin@hotmail.com>
2019-02-23 22:31:14 -0800

    Finish 1001

--- src/main/java/_1001_1050/_1001_Grid_Illumination/Solution.java
+++ src/main/java/_1001_1050/_1001_Grid_Illumination/Solution.java
@@ -1,7 +1,9 @@
 package _1001_1050._1001_Grid_Illumination;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author jacka
@@ -17,7 +19,7 @@ public class Solution {
   public int[] gridIllumination(int N, int[][] lamps, int[][] queries) {
     final Map<Integer, Integer> rows = new HashMap<>(), cols = new HashMap<>(),
         dia = new HashMap<>(), antiDia = new HashMap<>();
-    final Map<Cell, Integer> lampsCount = new HashMap<>();
+    final Set<Cell> lampSet = new HashSet<>();
 
     for (int[] l : lamps) {
       final int row = l[0], col = l[1];
@@ -26,7 +28,7 @@ public class Solution {
       cols.put(col, cols.getOrDefault(col, 0) + 1);
       dia.put(row - col, dia.getOrDefault(row - col, 0) + 1);
       antiDia.put(row + col, antiDia.getOrDefault(row + col, 0) + 1);
-      lampsCount.put(cell, lampsCount.getOrDefault(cell, 0) + 1);
+      lampSet.add(cell);
     }
     final int[] ans = new int[queries.length];
     for (int i = 0; i < queries.length; i++) {
@@ -40,12 +42,11 @@ public class Solution {
         for (final int[] d : DIRS) {
           final int row = q[0] + d[0], col = q[1] + d[1];
           final Cell cell = new Cell(row, col);
-          if (lampsCount.containsKey(cell)) {
+          if (lampSet.remove(cell)) {
             decreaseAndRemoveIfZero(rows, row);
             decreaseAndRemoveIfZero(cols, col);
             decreaseAndRemoveIfZero(dia, row - col);
             decreaseAndRemoveIfZero(antiDia, row + col);
-            decreaseAndRemoveIfZero(lampsCount, cell);
           }
         }
       }
