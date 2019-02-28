e25d996174d53aabefd6d3d761f32cb3b90b0966
jackal.sin <jackal.sin@hotmail.com>
2019-02-27 22:14:09 -0800

    Finish List of List Iterator

---
+++ src/main/java/interviews/airbnb/listOfListIterator/Solution.java
@@ -0,0 +1,44 @@
+package interviews.airbnb.listOfListIterator;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/26/2019.
+ */
+public final class Solution implements Iterator<Integer> {
+  private Iterator<List<Integer>> rowItr;
+  private Iterator<Integer> colItr;
+
+  public Solution(final List<List<Integer>> input) {
+    rowItr = input.iterator();
+    colItr = Collections.emptyIterator();
+  }
+
+  @Override
+  public boolean hasNext() {
+    while ((colItr == null || !colItr.hasNext()) && rowItr.hasNext()) {
+      final List<Integer> colItrCandidate = rowItr.next();
+      colItr = colItrCandidate == null ? null : colItrCandidate.iterator();
+    }
+    return colItr != null && colItr.hasNext();
+  }
+
+  @Override
+  public Integer next() {
+    if (!hasNext()) {
+      throw new NoSuchElementException();
+    }
+    return colItr.next();
+  }
+
+  @Override
+  public void remove() {
+    if (colItr != null) {
+      colItr.remove();
+    }
+  }
+}
