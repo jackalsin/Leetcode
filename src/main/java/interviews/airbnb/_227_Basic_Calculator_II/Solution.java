05a506341bb7d196a994be0b2187dcccb0c88646
jackal.sin <jackal.sin@hotmail.com>
2019-03-02 15:59:01 -0800

    Finish 227

--- src/main/java/interviews/airbnb/_227_Basic_Calculator_II/Solution.java
+++ src/main/java/interviews/airbnb/_227_Basic_Calculator_II/Solution.java
@@ -7,31 +7,26 @@ package interviews.airbnb._227_Basic_Calculator_II;
 public class Solution {
   public int calculate(String s) {
     int l1 = 0, o1 = 1, l2 = 1, o2 = 1;
+
     for (int i = 0; i < s.length(); i++) {
       final char chr = s.charAt(i);
-      if (chr == ' ') {
-        continue;
-      } else if (Character.isDigit(chr)) {
+      if (Character.isDigit(chr)) {
         int num = chr - '0';
         while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
           num = num * 10 + s.charAt(i + 1) - '0';
           i++;
         }
-        l2 = o2 == 1 ? (l2 * num) : (l2 / num);
-      } else if (chr == '+') {
-        l1 += o1 * l2;
-        o1 = 1;
-        l2 = 1;
-        o2 = 1;
-      } else if (chr == '-') {
-        l1 += o1 * l2;
-        o1 = -1;
-        l2 = 1;
+        l2 = o2 == 1 ? l2 * num : l2 / num;
         o2 = 1;
       } else if (chr == '*') {
         o2 = 1;
       } else if (chr == '/') {
         o2 = -1;
+      } else if (chr == '+' || chr == '-') {
+        l1 = l1 + o1 * l2;
+        o1 = chr == '+' ? 1 : -1;
+        l2 = 1;
+        o2 = 1;
       }
     }
     return l1 + o1 * l2;
