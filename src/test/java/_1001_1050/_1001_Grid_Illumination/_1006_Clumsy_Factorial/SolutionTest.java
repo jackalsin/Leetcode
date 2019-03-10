91b8a7b9aca365c75d2db317ff11c2d07351fb86
jackal.sin <jackal.sin@hotmail.com>
2019-03-09 19:22:07 -0800

    Finish 1006

---
+++ src/test/java/_1001_1050/_1001_Grid_Illumination/_1006_Clumsy_Factorial/SolutionTest.java
@@ -0,0 +1,29 @@
+package _1001_1050._1001_Grid_Illumination._1006_Clumsy_Factorial;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/9/2019.
+ */
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    assertEquals(7, solution.clumsy(4));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    assertEquals(12, solution.clumsy(10));
+  }
+}
