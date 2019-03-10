d619a598f0c041d631f04fb982daad4bd53916ee
jackal.sin <jackal.sin@hotmail.com>
2019-03-09 16:17:56 -0800

    Unfinish test embeded JSON

---
+++ src/main/java/interviews/airbnb/testJustification/Col.java
@@ -0,0 +1,15 @@
+package interviews.airbnb.testJustification;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/9/2019.
+ */
+public final class Col {
+  public final int width;
+  public final String str;
+
+  public Col(String str, int width) {
+    this.width = width;
+    this.str = str;
+  }
+}
