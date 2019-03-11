e87626d6d24e9701ec6c8720358da41b84fd6875
jackal.sin <jackal.sin@hotmail.com>
2019-03-10 20:17:57 -0700

    Finish Hibert Curve

--- src/main/java/interviews/airbnb/hilbertCurve/Solution.java
+++ src/main/java/interviews/airbnb/hilbertCurve/Solution.java
@@ -14,7 +14,30 @@ public class Solution {
    如果是rotation的话，原坐标需要互换，(x, y) => (y, x)然后乘以以上
   */
   public int hilbertCurve(int x, int y, int iter) {
-    return 0;
+    if (iter < 0) {
+      throw new IllegalArgumentException();
+    }
+    if (x < 0) {
+      throw new IllegalArgumentException();
+    }
+    if (y < 0) {
+      throw new IllegalArgumentException();
+    }
+
+    if (iter == 0) {
+      return 1;
+    }
+    final int midVal = 1 << (iter - 1);
+    final int num = midVal * midVal;
+    if (x >= midVal && y >= midVal) { // right top
+      return 2 * num + hilbertCurve(x - midVal, y - midVal, iter - 1);
+    } else if (y >= midVal) { // top left
+      return num + hilbertCurve(x, y - midVal, iter - 1);
+    } else if (x < midVal) { // left bottom
+      return hilbertCurve(y, x, iter - 1);
+    } else { // right bottom
+      return 3 * num + hilbertCurve(midVal - 1 - y, 2 * midVal - 1 - x, iter - 1);
+    }
   }
 
   public int sampleSolution(final int x, final int y, final int iter) {
