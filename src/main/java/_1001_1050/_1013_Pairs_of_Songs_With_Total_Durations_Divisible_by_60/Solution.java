a4477f8bef023b8bd77252a5b34d9b270fdfc445
jackalsin <jackal.sin@hotmail.com>
2019-03-17 14:06:11 -0700

    Finish 1013

---
+++ src/main/java/_1001_1050/_1013_Pairs_of_Songs_With_Total_Durations_Divisible_by_60/Solution.java
@@ -0,0 +1,13 @@
+package _1001_1050._1013_Pairs_of_Songs_With_Total_Durations_Divisible_by_60;
+
+public class Solution {
+  public int numPairsDivisibleBy60(int[] time) {
+    final int[] count = new int[60];
+    int sum = 0;
+    for (int t : time) {
+      sum += count[(60 - t % 60) % 60];
+      count[t % 60]++;
+    }
+    return sum;
+  }
+}
---
+++ src/test/java/_1001_1050/_1013_Pairs_of_Songs_With_Total_Durations_Divisible_by_60/SolutionTest.java
@@ -0,0 +1,27 @@
+package _1001_1050._1013_Pairs_of_Songs_With_Total_Durations_Divisible_by_60;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final int[] input = {30, 20, 150, 100, 40};
+    assertEquals(3, solution.numPairsDivisibleBy60(input));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    final int[] input = {60, 60, 60};
+    assertEquals(3, solution.numPairsDivisibleBy60(input));
+  }
+}
