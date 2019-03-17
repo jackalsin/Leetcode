64fd09dd5aab0c10d62be6a5839377c8033bdd0a
jackalsin <jackal.sin@hotmail.com>
2019-03-17 11:19:36 -0700

    Finish 1012

---
+++ src/main/java/_1001_1050/_1012_Complement_of_Base_10_Integer/Solution.java
@@ -0,0 +1,9 @@
+package _1001_1050._1012_Complement_of_Base_10_Integer;
+
+public final class Solution {
+  public int bitwiseComplement(int N) {
+    int x = 1;
+    while (x < N) x = 2 * x + 1;
+    return x - N;
+  }
+}
