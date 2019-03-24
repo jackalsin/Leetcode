2c5afa0f06b940bad078c9cd5577f64f42d17413
jackalsin <jackal.sin@hotmail.com>
2019-03-24 15:09:37 -0700

    Finish 1022

--- src/main/java/_1001_1050/_1022_Smallest_Integer_Divisible_by_K/Solution.java
+++ src/main/java/_1001_1050/_1022_Smallest_Integer_Divisible_by_K/Solution.java
@@ -3,16 +3,18 @@ package _1001_1050._1022_Smallest_Integer_Divisible_by_K;
 public class Solution {
   public int smallestRepunitDivByK(final int K) {
     final boolean[] visited = new boolean[K];
-    int n = 1, len = 1;
-    while (n % K != 0) {
-      final int next = (n % K) * 10 + 1;
-      if (visited[next % K]) {
+    int n = 1, len = 0;
+    while (true) {
+      final int mod = (n * 10 + 1) % K;
+      len++;
+      if (mod == 0) {
+        return len;
+      }
+      if (visited[mod]) {
         return -1;
       }
-      visited[next % K] = true;
-      n = next;
-      len++;
+      visited[mod] = true;
+      n = mod;
     }
-    return len;
   }
 }
