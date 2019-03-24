2b3bbea0e16ccd297aba5e594eba7bc458c6bc7f
jackalsin <jackal.sin@hotmail.com>
2019-03-24 14:28:30 -0700

    Finish 1021

---
+++ src/main/java/_1001_1050/_1021_Best_Sightseeing_Pair/Solution.java
@@ -0,0 +1,12 @@
+package _1001_1050._1021_Best_Sightseeing_Pair;
+
+public class Solution {
+  public int maxScoreSightseeingPair(int[] A) {
+    int score = -1;
+    for (int j = 1, maxAi = A[0]; j < A.length; j++) {
+      score = Math.max(score, A[j] - j + maxAi);
+      maxAi = Math.max(maxAi, A[j] + j);
+    }
+    return score;
+  }
+}
---
+++ src/test/java/_1001_1050/_1021_Best_Sightseeing_Pair/SolutionTest.java
@@ -0,0 +1,22 @@
+package _1001_1050._1021_Best_Sightseeing_Pair;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final int[] input = {8, 1, 5, 2, 6};
+    assertEquals(11, solution.maxScoreSightseeingPair(input));
+  }
+}
