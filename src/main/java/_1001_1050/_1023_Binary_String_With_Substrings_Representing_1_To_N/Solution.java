fcf0ebdad8804e44727f1bf70f24bc57066cb5dc
jackalsin <jackal.sin@hotmail.com>
2019-03-24 20:02:12 -0700

    Update 1023

--- src/main/java/_1001_1050/_1023_Binary_String_With_Substrings_Representing_1_To_N/Solution.java
+++ src/main/java/_1001_1050/_1023_Binary_String_With_Substrings_Representing_1_To_N/Solution.java
@@ -1,13 +1,30 @@
 package _1001_1050._1023_Binary_String_With_Substrings_Representing_1_To_N;
 
 public class Solution {
-  public boolean queryString(String S, int N) {
-    for (int i = N; i > N / 2; i--) {
-      final String s = Integer.toBinaryString(i);
-      if (!S.contains(s)) {
-        return false;
+  public boolean queryString(String s, int N) {
+//    for (int i = N; i > N / 2; i--) {
+//      final String s = Integer.toBinaryString(i);
+//      if (!S.contains(s)) {
+//        return false;
+//      }
+//    }
+//    return true;
+
+    boolean[] seen = new boolean[N + 1];
+    int seenCnt = 0;
+    final int n = s.length();
+    for (int i = 0; i < n; i++) {
+      if (s.charAt(i) == '0') continue;
+      for (int j = i, num = 0; num <= N && j < n; j++) {
+        num = (num << 1) + s.charAt(j) - '0';
+        if (num >= 1 && num <= N && !seen[num]) {
+          seen[num] = true;
+          seenCnt++;
+          if (seenCnt == N)
+            return true;
+        }
       }
     }
-    return true;
+    return false;
   }
 }
