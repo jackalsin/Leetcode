268576a25e5e5b0e04b7419c12cd6218729bf847
jackalsin <jackal.sin@hotmail.com>
2019-03-31 13:46:53 -0700

    Finish 224

--- src/main/java/interviews/airbnb/_224_Basic_Calculator/Solution.java
+++ src/main/java/interviews/airbnb/_224_Basic_Calculator/Solution.java
@@ -5,29 +5,32 @@ import java.util.Deque;
 
 public final class Solution {
   public int calculate(String s) {
-    final Deque<int[]> stack = new ArrayDeque<>();
-    int l1 = 0, o1 = 1, l2 = 0;
+    if (s == null || s.length() == 0) {
+      return 0;
+    }
+    final Deque<Integer> stack = new ArrayDeque<>();
+    int l1 = 0, sign = 1;
     for (int i = 0; i < s.length(); i++) {
       final char chr = s.charAt(i);
       if (chr == ' ') continue;
       if (Character.isDigit(chr)) {
-        int num = (chr - '0');
+        int num = chr - '0';
         while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
           num = num * 10 + s.charAt(i + 1) - '0';
           i++;
         }
-        l1 = l1 + o1 * num;
-      } else if (chr == '+') {
-        o1 = 1;
-      } else if (chr == '-') {
-        o1 = -1;
+        l1 = l1 + sign * num;
+      } else if (chr == '+' || chr == '-') {
+        sign = chr == '+' ? 1 : -1;
       } else if (chr == '(') {
-        stack.push(new int[]{l1, o1});
+        stack.push(l1);
+        stack.push(sign);
+        sign = 1;
         l1 = 0;
-        o1 = 1;
       } else if (chr == ')') {
-        final int[] toRemove = stack.pop();
-        l1 = toRemove[0] + toRemove[1] * l1;
+        final int prevSign = stack.pop(), prevL1 = stack.pop();
+        l1 = prevL1 + prevSign * l1;
+        sign = 1;
       }
     }
     return l1;
