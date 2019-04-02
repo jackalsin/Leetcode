e84869afab3489174818be0dc60efe39aadddb17
jackalsin <jackal.sin@hotmail.com>
2019-04-01 18:29:25 -0700

    Finish Search Median In Large File

---
+++ src/test/java/interviews/airbnb/searchMedianInLargeFile/SolutionITest.java
@@ -0,0 +1,37 @@
+package interviews.airbnb.searchMedianInLargeFile;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/5/2019.
+ */
+class SolutionITest {
+  private Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new SolutionI();
+  }
+
+  @Test
+  void testOnlineCase1() {
+    final int[] nums = {2, 2, 3, 4, 5, 5, 6};
+    assertEquals(4, solution.findMedian(nums));
+  }
+
+  @Test
+  void testOnlineCase2() {
+    final int[] nums = {1};
+    assertEquals(1, solution.findMedian(nums));
+  }
+
+  @Test
+  void testOnlineCase3() {
+    final int[] nums = {1, 2};
+    assertEquals(1.5, solution.findMedian(nums));
+  }
+}
