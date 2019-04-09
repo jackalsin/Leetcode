6c41088cbb3946bd9a11be7c1132bd5ac082d1a8
jackalsin <jackal.sin@hotmail.com>
2019-04-08 20:41:22 -0700

    Finish 040

--- src/main/java/interviews/airbnb/_040_Combination_Sum_II/Solution.java
+++ src/main/java/interviews/airbnb/_040_Combination_Sum_II/Solution.java
@@ -10,15 +10,20 @@ import java.util.List;
  */
 public final class Solution {
   public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-    Arrays.sort(candidates);
     final List<List<Integer>> result = new ArrayList<>();
+    if (candidates == null || candidates.length == 0) {
+      return result;
+    }
+    Arrays.sort(candidates);
     dfs(result, candidates, target, new ArrayList<>(), 0);
     return result;
   }
 
-  private static void dfs(final List<List<Integer>> result, final int[] candidates, final int target,
-                          final List<Integer> curPath, final int start) {
-    if (target < 0) return;
+  private static void dfs(List<List<Integer>> result, final int[] candidates, final int target,
+                          final List<Integer> curPath, int start) {
+    if (target < 0) {
+      return;
+    }
     if (target == 0) {
       result.add(new ArrayList<>(curPath));
       return;
@@ -28,8 +33,8 @@ public final class Solution {
     }
 
     for (int i = start; i < candidates.length; i++) {
-      if (target < candidates[i]) return; // performance 提高到100%
-      if (i > start && candidates[i - 1] == candidates[i]) continue;
+      if (candidates[i] > target) return;
+      if (i != start && candidates[i] == candidates[i - 1]) continue;
       curPath.add(candidates[i]);
       dfs(result, candidates, target - candidates[i], curPath, i + 1);
       curPath.remove(curPath.size() - 1);
