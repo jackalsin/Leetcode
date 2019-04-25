4956a310528c04691fddf7f4348c2bf20ab2a559
jackalsin <jackal.sin@hotmail.com>
2019-04-24 22:11:03 -0700

    Finish 039

--- src/main/java/interviews/airbnb/_039_Combination_Sum/Solution.java
+++ src/main/java/interviews/airbnb/_039_Combination_Sum/Solution.java
@@ -7,26 +7,29 @@ import java.util.List;
  * @author jacka
  * @version 1.0 on 1/22/2019.
  */
-public class Solution {
+public final class Solution {
   public List<List<Integer>> combinationSum(int[] candidates, int target) {
     final List<List<Integer>> result = new ArrayList<>();
-    dfs(result, target, candidates, new ArrayList<>(), 0);
+    dfs(result, candidates, target, new ArrayList<>(), 0);
     return result;
   }
 
-  private static void dfs(final List<List<Integer>> result, final int target, final int[] candidates,
-                          final List<Integer> curPath, final int start) {
-    if (target < 0) {
+  private static void dfs(final List<List<Integer>> result, final int[] candidates, final int remain,
+                          final List<Integer> curPath, final int startIndex) {
+    if (remain < 0) {
       return;
     }
-    if (target == 0) {
+    if (remain == 0) {
       result.add(new ArrayList<>(curPath));
       return;
     }
+    if (startIndex == candidates.length) {
+      return;
+    }
 
-    for (int i = start; i < candidates.length; i++) {
+    for (int i = startIndex; i < candidates.length; i++) {
       curPath.add(candidates[i]);
-      dfs(result, target - candidates[i], candidates, curPath, i);
+      dfs(result, candidates, remain - candidates[i], curPath, i);
       curPath.remove(curPath.size() - 1);
     }
   }
