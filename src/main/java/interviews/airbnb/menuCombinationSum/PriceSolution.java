65890aba5648ea20189e9fb8a0ad9d7a5b83067b
jackalsin <jackal.sin@hotmail.com>
2019-05-03 07:53:38 -0700

    Finish price solution

---
+++ src/main/java/interviews/airbnb/menuCombinationSum/PriceSolution.java
@@ -0,0 +1,18 @@
+package interviews.airbnb.menuCombinationSum;
+
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/18/2019.
+ */
+public interface PriceSolution {
+
+  /**
+   * @param itemToPrice item to price (can be zero)
+   * @param sum         target sum
+   * @param delta       allowed precise bias
+   * @return a list of combinations that satisfies sum of price whose bias less than or equals <tt>delta</tt>
+   */
+  List<List<Double>> combinationSum(List<Double> itemToPrice, final double sum, final double delta);
+}
