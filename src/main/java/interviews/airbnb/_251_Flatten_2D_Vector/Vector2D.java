0950ec57b2988a4ca8a6aaf3d41457c008e2f159
jackalsin <jackal.sin@hotmail.com>
2019-05-04 12:08:44 -0700

    Finish 251

--- src/main/java/interviews/airbnb/_251_Flatten_2D_Vector/Vector2D.java
+++ src/main/java/interviews/airbnb/_251_Flatten_2D_Vector/Vector2D.java
@@ -7,20 +7,22 @@ package interviews.airbnb._251_Flatten_2D_Vector;
 public final class Vector2D {
   private int row = 0, col = 0;
   private final int[][] v;
+  private final int rows;
 
   public Vector2D(int[][] v) {
     this.v = v;
+    this.rows = v.length;
   }
 
   public int next() {
-    hasNext();
+    assert hasNext();
     return v[row][col++];
   }
 
   public boolean hasNext() {
-    while (row < v.length && col == v[row].length) {
-      row++;
+    while (row < rows && col == v[row].length) {
       col = 0;
+      row++;
     }
     return row != v.length && col < v[row].length;
   }
