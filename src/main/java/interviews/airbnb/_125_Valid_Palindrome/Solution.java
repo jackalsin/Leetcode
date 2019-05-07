c56f2494b026be0c12f9b988d2daa12de0e33d21
jackalsin <jackal.sin@hotmail.com>
2019-05-07 08:12:24 -0700

    Finish 125

--- src/main/java/interviews/airbnb/_125_Valid_Palindrome/Solution.java
+++ src/main/java/interviews/airbnb/_125_Valid_Palindrome/Solution.java
@@ -4,46 +4,22 @@ package interviews.airbnb._125_Valid_Palindrome;
  * @author jacka
  * @version 1.0 on 1/26/2019.
  */
-public class Solution {
-  private int left, right;
-
+public final class Solution {
   public boolean isPalindrome(String s) {
     if (s == null || s.length() == 0) {
       return true;
     }
-    left = 0;
-    right = s.length() - 1;
-    nextLeft(s);
-    nextRight(s);
-    while (left < right) {
-      final char leftChar = s.charAt(left), rightChar = s.charAt(right);
-      if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+    for (int left = 0, right = s.length() - 1; left < right; ) {
+      while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+      while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+      final char leftChar = Character.toLowerCase(s.charAt(left)),
+          rightChar = Character.toLowerCase(s.charAt(right));
+      if (leftChar != rightChar) {
         return false;
       }
       left++;
       right--;
-      nextLeft(s);
-      nextRight(s);
     }
     return true;
   }
-
-  private void nextLeft(String s) {
-    while (left < s.length()) {
-      final char chr = s.charAt(left);
-      if (Character.isLetterOrDigit(chr)) break;
-      left++;
-    }
-  }
-
-  private void nextRight(final String s) {
-    while (right >= 0) {
-      final char chr = s.charAt(right);
-      if (Character.isLetterOrDigit(chr)) {
-        break;
-      }
-      right--;
-    }
-  }
-
 }
