4db71a38b5645a2d59c6b68a4205c04cb66b2dae
jackalsin <jackal.sin@hotmail.com>
2019-06-04 09:33:37 -0700

    Finish 125

--- src/main/java/interviews/airbnb/_125_Valid_Palindrome/Solution.java
+++ src/main/java/interviews/airbnb/_125_Valid_Palindrome/Solution.java
@@ -6,12 +6,15 @@ package interviews.airbnb._125_Valid_Palindrome;
  */
 public final class Solution {
   public boolean isPalindrome(String s) {
-    if (s == null || s.length() == 0) {
+    if (s == null || s.isEmpty()) {
       return true;
     }
     for (int left = 0, right = s.length() - 1; left < right; ) {
       while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
       while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+      if (left >= right) {
+        break;
+      }
       final char leftChar = Character.toLowerCase(s.charAt(left)),
           rightChar = Character.toLowerCase(s.charAt(right));
       if (leftChar != rightChar) {
