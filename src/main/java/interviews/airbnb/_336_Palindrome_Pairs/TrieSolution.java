4e37eb7648ca35b855b94e2a8a031c7f9b9d6e2c
jackalsin <jackal.sin@hotmail.com>
2019-06-04 21:02:20 -0700

    Finish 336

--- src/main/java/interviews/airbnb/_336_Palindrome_Pairs/TrieSolution.java
+++ src/main/java/interviews/airbnb/_336_Palindrome_Pairs/TrieSolution.java
@@ -11,53 +11,48 @@ import java.util.List;
 public final class TrieSolution implements Solution {
   private static final int N = 26;
   private final Node root = new Node();
+  private final List<List<Integer>> result = new ArrayList<>();
 
   public List<List<Integer>> palindromePairs(String[] words) {
-    final List<List<Integer>> result = new ArrayList<>();
-    if (words == null || words.length == 0) {
-      return result;
-    }
     for (int i = 0; i < words.length; i++) {
-      String word = words[i];
+      final String word = words[i];
       insert(root, word, i, word.length() - 1);
     }
     for (int i = 0; i < words.length; i++) {
-      search(result, root, words[i], i, 0);
+      search(root, words[i], i, 0);
     }
     return result;
   }
 
-  private static void search(final List<List<Integer>> result, final Node root, final String word, final int indexInWords, int i) {
+  private void search(final Node root, final String word, final int indexInWords, final int i) {
     if (i == word.length()) {
-      // current word, palindrome, curPath
-      for (int index : root.index) {
-        if (index != indexInWords) {
-          result.add(Arrays.asList(indexInWords, index));
+      for (int otherIndex : root.indexes) {
+        if (indexInWords != otherIndex) {
+          result.add(Arrays.asList(indexInWords, otherIndex));
         }
       }
       return;
-    } // end of i == word.length();
-
-    // curPath, palindrome, word ending here
-    if (root.indexInWords != -1 && indexInWords != root.indexInWords && isPalindrome(word, i, word.length() - 1)) {
+    } // end of i == words.length();
+    if (isPalindrome(word, i, word.length() - 1) && root.indexInWords != -1 && indexInWords != root.indexInWords) {
       result.add(Arrays.asList(indexInWords, root.indexInWords));
     }
+
     final char chr = word.charAt(i);
     final int chrIndex = chr - 'a';
     if (root.next[chrIndex] == null) {
       return;
     }
-    search(result, root.next[chrIndex], word, indexInWords, i + 1);
+    search(root.next[chrIndex], word, indexInWords, i + 1);
   }
 
-  private static void insert(final Node root, final String word, int indexInWords, final int i) {
+  private void insert(final Node root, final String word, final int indexInWords, final int i) {
     if (i == -1) {
       root.indexInWords = indexInWords;
-      root.index.add(indexInWords);
+      root.indexes.add(indexInWords);
       return;
     }
     if (isPalindrome(word, 0, i)) {
-      root.index.add(indexInWords);
+      root.indexes.add(indexInWords);
     }
     final char chr = word.charAt(i);
     final int chrIndex = chr - 'a';
@@ -67,9 +62,11 @@ public final class TrieSolution implements Solution {
     insert(root.next[chrIndex], word, indexInWords, i - 1);
   }
 
-  private static boolean isPalindrome(final String word, final int left, final int right) {
-    for (int i = left, j = right; i < j; i++, j--) {
-      if (word.charAt(i) != word.charAt(j)) {
+
+  private static boolean isPalindrome(final String str, int left, int right) {
+    for (; left < right; left++, right--) {
+      final char leftChar = str.charAt(left), rightChar = str.charAt(right);
+      if (leftChar != rightChar) {
         return false;
       }
     }
@@ -80,8 +77,8 @@ public final class TrieSolution implements Solution {
     private final Node[] next = new Node[N];
     private int indexInWords = -1;
     /**
-     * index of words that after this char (exclusive) are
+     * below this node, the index of words is palindrome
      */
-    private final List<Integer> index = new ArrayList<>();
+    private final List<Integer> indexes = new ArrayList<>();
   }
 }
