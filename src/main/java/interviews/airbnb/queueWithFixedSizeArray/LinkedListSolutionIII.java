8e2d0ba1a6969ebecaa251d6dee49c17ef26893f
jackalsin <jackal.sin@hotmail.com>
2019-06-04 19:06:03 -0700

    Finish fix width implement queue

---
+++ src/main/java/interviews/airbnb/queueWithFixedSizeArray/LinkedListSolutionIII.java
@@ -0,0 +1,38 @@
+package interviews.airbnb.queueWithFixedSizeArray;
+
+public final class LinkedListSolutionIII implements MyQueue {
+  private static final int N = 5;
+  private Node head = new Node(), tail = head;
+  private int start = 0, end = 0;
+
+  public void push(int x) {
+    tail.vals[end++] = x;
+    if (end == N) {
+      tail.next = new Node();
+      tail = tail.next;
+      end = 0;
+    }
+  }
+
+  public int pop() {
+    final int res = head.vals[start++];
+    if (start == N) {
+      start = 0;
+      head = head.next;
+    }
+    return res;
+  }
+
+  public int peek() {
+    return head.vals[start];
+  }
+
+  public boolean empty() {
+    return head == tail && start == end;
+  }
+
+  private static final class Node {
+    private final int[] vals = new int[N];
+    private Node next;
+  }
+}
