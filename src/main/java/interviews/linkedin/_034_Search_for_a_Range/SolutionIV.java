87f53ba6758aa72fe6cb1530c286b96c4c69a792
jackalsin <jackal.sin@hotmail.com>
2019-06-25 21:38:14 -0700

    Finish 034

--- src/main/java/interviews/linkedin/_034_Search_for_a_Range/SolutionIV.java
+++ src/main/java/interviews/linkedin/_034_Search_for_a_Range/SolutionIV.java
@@ -1,40 +1,41 @@
 package interviews.linkedin._034_Search_for_a_Range;
 
-public class SolutionIV implements Solution {
+public final class SolutionIV implements Solution {
+
+  @Override
   public int[] searchRange(int[] nums, int target) {
-    if (nums == null || nums.length == 0) {
+    if (nums == null || nums.length == 0) { // TODO: redo failed here
       return new int[]{-1, -1};
     }
-    return new int[]{first(nums, target, 0, nums.length - 1),
-        last(nums, target, 0, nums.length - 1)};
+    return new int[]{first(nums, target), last(nums, target)};
   }
 
-  private static int first(final int[] nums, final int target, int left, int right) {
+  private static int last(final int[] nums, final int target) {
+    int left = 0, right = nums.length - 1;
     while (left < right) {
-      final int mid = (right - left) / 2 + left;
-      if (nums[mid] >= target) {
-        right = mid;
-      } else {
-        left = mid + 1;
-      }
-    }
-
-    if (nums[left] == target) {
-      return left;
-    }
-    return -1;
-  }
-
-  private static int last(final int[] nums, final int target, int left, int right) {
-    while (left < right) {
-      final int mid = (right - left + 1) / 2 + left;
+      final int mid = left + (right - left + 1) / 2;
       if (nums[mid] <= target) {
         left = mid;
       } else {
         right = mid - 1;
       }
     }
+    if (nums[left] == target) {
+      return left;
+    }
+    return -1;
+  }
 
+  private static int first(final int[] nums, final int target) {
+    int left = 0, right = nums.length - 1;
+    while (left < right) {
+      final int mid = left + (right - left) / 2;
+      if (nums[mid] >= target) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
     if (nums[left] == target) {
       return left;
     }
