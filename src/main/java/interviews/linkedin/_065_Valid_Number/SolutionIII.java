280e658addbd6c7cf64c1ebe1b9ee386b0f56319
jackalsin <jackal.sin@hotmail.com>
2019-06-28 18:39:31 -0700

    Finish 065

--- src/main/java/interviews/linkedin/_065_Valid_Number/SolutionIII.java
+++ src/main/java/interviews/linkedin/_065_Valid_Number/SolutionIII.java
@@ -1,11 +1,13 @@
 package interviews.linkedin._065_Valid_Number;
 
 public class SolutionIII implements Solution {
-  private static final String REG_NUMBER = "((\\d+(\\.\\d*)?)|(\\d*\\.\\d+))";
+  private static final String REGEX = "[+-]?(([\\d]+(\\.\\d*)?)|([\\d]*\\.\\d+))([eE][+-]?\\d+)?";
 
-  @Override
   public boolean isNumber(String s) {
+    if (s == null || s.isEmpty()) {
+      return false;
+    }
     s = s.trim();
-    return s.matches("[+-]?" + REG_NUMBER + "(" + "[Ee][+-]?\\d+)?");
+    return s.matches(REGEX);
   }
 }
