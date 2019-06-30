2b4e6e07614c0bd2d73d6c56cb9fa70ab78af3d3
jackalsin <jackal.sin@hotmail.com>
2019-06-30 10:40:08 -0700

    Finish 088

--- src/main/java/interviews/linkedin/_088_Merge_Sorted_Array/Solution.java
+++ src/main/java/interviews/linkedin/_088_Merge_Sorted_Array/Solution.java
@@ -1,16 +1,22 @@
 package interviews.linkedin._088_Merge_Sorted_Array;
 
 public class Solution {
+  /**
+   * TODO: Redo
+   *
+   * @param nums1
+   * @param m
+   * @param nums2
+   * @param n
+   */
   public void merge(int[] nums1, int m, int[] nums2, int n) {
-    for (int k = m + n - 1, i = m - 1, j = n - 1; k >= 0; --k) {
-      if (i < 0) {
-        nums1[k] = nums2[j--];
-      } else if (j < 0) {
-        nums1[k] = nums1[i--];
-      } else if (nums1[i] < nums2[j]) {
-        nums1[k] = nums2[j--];
+    for (int i = m - 1, j = n - 1, k = m + n - 1; k >= 0; k--) {
+      if (j < 0 || (i >= 0 && nums1[i] > nums2[j])) {
+        nums1[k] = nums1[i];
+        i--;
       } else {
-        nums1[k] = nums1[i--];
+        nums1[k] = nums2[j];
+        j--;
       }
     }
   }
