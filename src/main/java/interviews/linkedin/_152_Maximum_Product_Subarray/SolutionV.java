24c0133f09a5e17b9c22d1a8f8d36cd41118b30a
jackalsin <jackal.sin@hotmail.com>
2019-07-06 11:51:16 -0700

    Finish 152

--- src/main/java/interviews/linkedin/_152_Maximum_Product_Subarray/SolutionV.java
+++ src/main/java/interviews/linkedin/_152_Maximum_Product_Subarray/SolutionV.java
@@ -9,16 +9,24 @@ public final class SolutionV implements Solution {
     if (nums == null || nums.length == 0) {
       return 0;
     }
-
-    int maxSoFar = nums[0], maxEndHere = nums[0], minEndHere = nums[0];
+    int minProdEndHere = nums[0],
+        maxProdEndHere = nums[0],
+        maxSoFar = nums[0];
     for (int i = 1; i < nums.length; i++) {
-      final int prevMaxEndHere = maxEndHere, prevMinEndHere = minEndHere;
-      maxEndHere = Math.max(prevMaxEndHere * nums[i], prevMinEndHere * nums[i]);
-      maxEndHere = Math.max(maxEndHere, nums[i]);
-      minEndHere = Math.min(prevMaxEndHere * nums[i], prevMinEndHere * nums[i]);
-      minEndHere = Math.min(minEndHere, nums[i]);
-      maxSoFar = Math.max(maxSoFar, maxEndHere);
+      final int prevMinProdEndHere = minProdEndHere,
+          prevMaxProdEndHere = maxProdEndHere;
+      minProdEndHere = min(prevMinProdEndHere * nums[i], prevMaxProdEndHere * nums[i], nums[i]);
+      maxProdEndHere = max(prevMinProdEndHere * nums[i], prevMaxProdEndHere * nums[i], nums[i]);
+      maxSoFar = Math.max(maxSoFar, maxProdEndHere);
     }
     return maxSoFar;
+  }
+
+  private static int min(int a, int b, int c) {
+    return Math.min(Math.min(a, b), c);
+  }
+
+  private static int max(int a, int b, int c) {
+    return Math.max(Math.max(a, b), c);
   }
 }
--- src/test/java/interviews/linkedin/_152_Maximum_Product_Subarray/SolutionTest.java
+++ src/test/java/interviews/linkedin/_152_Maximum_Product_Subarray/SolutionTest.java
@@ -1,37 +1,46 @@
 package interviews.linkedin._152_Maximum_Product_Subarray;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.Assert.assertEquals;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author jacka
  * @version 1.0 on 10/21/2017.
  */
-public class SolutionTest {
-  private Solution solution;
+class SolutionTest {
 
-  @Before
-  public void setUp() throws Exception {
-    solution = new SolutionII();
-  }
-
-  @Test
-  public void testFailedNegative() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testFailedNegative(Solution solution) {
     final int[] nums = {-2};
     assertEquals(-2, solution.maxProduct(nums));
   }
 
-  @Test
-  public void testOnlineCase() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase(Solution solution) {
     final int[] nums = {2, 3, -2, 4};
     assertEquals(6, solution.maxProduct(nums));
   }
 
-  @Test
-  public void testZero() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testZero(Solution solution) {
     final int[] nums = {0, 2};
     assertEquals(2, solution.maxProduct(nums));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII(),
+        new SolutionIV(),
+        new SolutionV()
+    );
   }
 }
