0b773a4f71cbd2994cae2f493de4fe60912b27bd
jackalsin <jackal.sin@hotmail.com>
2019-07-07 13:42:48 -0700

    Finish 187

--- src/test/java/interviews/linkedin/_187_Repeated_DNA_Sequences/SolutionTest.java
+++ src/test/java/interviews/linkedin/_187_Repeated_DNA_Sequences/SolutionTest.java
@@ -1,34 +1,39 @@
 package interviews.linkedin._187_Repeated_DNA_Sequences;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author jacka
  * @version 1.0 on 10/21/2017.
  */
-public class SolutionTest {
-  private Solution solution;
+class SolutionTest {
 
-  @Before
-  public void setUp() throws Exception {
-    solution = new SolutionII();
-  }
-
-  @Test
-  public void testAAAAAAAAAAA() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testAAAAAAAAAAA(Solution solution) {
     final String input = "AAAAAAAAAAA";
     assertEquals(List.of("AAAAAAAAAA"), solution.findRepeatedDnaSequences(input));
   }
 
-  @Test
-  public void testAAAAACCCCCAAAAACCCCCCAAAAAGGGTTT() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testAAAAACCCCCAAAAACCCCCCAAAAAGGGTTT(Solution solution) {
     final String input = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
     assertEquals(List.of("AAAAACCCCC", "CCCCCAAAAA"), solution.findRepeatedDnaSequences(input));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII()
+    );
   }
 
 }
