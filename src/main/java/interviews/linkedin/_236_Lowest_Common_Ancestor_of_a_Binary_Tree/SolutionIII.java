5a8ab7b9c9fa03db4ce214fc9e8ffd7c7abc3f18
jackalsin <jackal.sin@hotmail.com>
2019-07-08 19:21:47 -0700

    Finish 236

--- src/main/java/interviews/linkedin/_236_Lowest_Common_Ancestor_of_a_Binary_Tree/SolutionIII.java
+++ src/main/java/interviews/linkedin/_236_Lowest_Common_Ancestor_of_a_Binary_Tree/SolutionIII.java
@@ -7,20 +7,23 @@ import utils.TreeNode;
  * @version 1.0 on 10/22/2017.
  */
 public final class SolutionIII implements Solution {
+
+  @Override
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
     if (root == null || root == p || root == q) {
       return root;
-    }
-    final TreeNode left = lowestCommonAncestor(root.left, p, q),
-        right = lowestCommonAncestor(root.right, p, q);
-    if (left == null && right == null) {
-      return null;
-    } else if (left == null && right != null) {
-      return right;
-    } else if (left != null && right == null) {
-      return left;
     } else {
-      return root;
+      final TreeNode left = lowestCommonAncestor(root.left, p, q),
+          right = lowestCommonAncestor(root.right, p, q);
+      if (left == null && right == null) {
+        return null;
+      } else if (left == null) {
+        return right;
+      } else if (right == null) {
+        return left;
+      } else {
+        return root;
+      }
     }
   }
 }
