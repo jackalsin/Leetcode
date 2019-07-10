0789bdeda1755a9e68e176b3b3d14a0728acaba6
jackalsin <jackal.sin@hotmail.com>
2019-07-09 21:07:33 -0700

    Finish 057

---
+++ src/main/java/interviews/linkedin/_057_Insert_Interval/April152019Solution.java
@@ -0,0 +1,9 @@
+package interviews.linkedin._057_Insert_Interval;
+
+/**
+ * @author jacka
+ * @version 1.0 on 7/9/2019
+ */
+public interface April152019Solution {
+  int[][] insert(int[][] intervals, int[] newInterval);
+}
---
+++ src/test/java/interviews/linkedin/_057_Insert_Interval/April152019SolutionTest.java
@@ -0,0 +1,41 @@
+package interviews.linkedin._057_Insert_Interval;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 7/9/2019
+ */
+class April152019SolutionTest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testFailedCase1(April152019Solution solution) {
+    final int[] newInterval = {5, 7};
+    final int[][] expected = {newInterval};
+    assertArrayEquals(expected, solution.insert(new int[][]{}, newInterval));
+  }
+
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testFailedCase2(April152019Solution solution) {
+    final int[] newInterval = {2, 5};
+    final int[][] expected = {{1, 5}, {6, 9}},
+        actual = solution.insert(
+            new int[][]{{1, 3}, {6, 9}},
+            newInterval);
+    assertArrayEquals(expected, actual);
+  }
+
+  static Stream<April152019Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionIV()
+    );
+  }
+}
