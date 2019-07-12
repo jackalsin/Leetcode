ad7b7dfcc884cfb6dfce64934c1ab3e8ee6e6b7a
jackalsin <jackal.sin@hotmail.com>
2019-07-11 20:14:37 -0700

    Finish 256

--- src/test/java/interviews/linkedin/_256_Paint_House/SolutionIITest.java
+++ src/test/java/interviews/linkedin/_256_Paint_House/SolutionIITest.java
@@ -1,33 +1,31 @@
 package interviews.linkedin._256_Paint_House;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionIITest {
 
-  private Solution solution;
-
-  @BeforeEach
-  void setUp() throws Exception {
-    solution = new SolutionII();
-  }
-
-  @Test
-  void testEmpty() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testEmpty(Solution solution) {
     final int[][] costs = new int[][]{};
     assertEquals(0, solution.minCost(costs));
   }
 
-  @Test
-  void testOne() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOne(Solution solution) {
     final int[][] costs = new int[][]{{7, 2, 6}};
     assertEquals(2, solution.minCost(costs));
   }
 
-  @Test
-  void testNormal() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testNormal(Solution solution) {
     final int[][] costs = new int[][]{
         {4, 5, 6},
         {1, 20, 21}
@@ -35,8 +33,9 @@ class SolutionIITest {
     assertEquals(6, solution.minCost(costs));
   }
 
-  @Test
-  void testNormal2() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testNormal2(Solution solution) {
     final int[][] costs = new int[][]{
         {4, 5, 6},
         {1, 20, 21},
@@ -45,13 +44,22 @@ class SolutionIITest {
     assertEquals(26, solution.minCost(costs));
   }
 
-  @Test
-  void testNormal3() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testNormal3(Solution solution) {
     final int[][] costs = new int[][]{
         {17, 2, 17},
         {16, 16, 5},
         {14, 3, 19}
     };
     assertEquals(10, solution.minCost(costs));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII()
+    );
   }
 }
