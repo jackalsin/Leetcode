193b483824e2ecf688e29c4c0e5f2ab9c365fc37
jackalsin <jackal.sin@hotmail.com>
2019-07-20 12:29:46 -0700

    Finish 339

---
+++ src/main/java/interviews/linkedin/_339_Nested_List_Weight_Sum/SolutionIV.java
@@ -0,0 +1,27 @@
+package interviews.linkedin._339_Nested_List_Weight_Sum;
+
+import utils.nestedInteger.NestedInteger;
+
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 7/20/2019
+ */
+public final class SolutionIV implements Solution {
+  public int depthSum(List<NestedInteger> nestedList) {
+    return depthSum(nestedList, 1);
+  }
+
+  private static int depthSum(final List<NestedInteger> nestedList, final int d) {
+    int res = 0;
+    for (NestedInteger ni : nestedList) {
+      if (ni.isInteger()) {
+        res += d * ni.getInteger();
+      } else {
+        res += depthSum(ni.getList(), d + 1);
+      }
+    }
+    return res;
+  }
+}
