be5540778fb35527c07797fab5aabbf2bd22e7a3
jackalsin <jackal.sin@hotmail.com>
2019-07-20 17:41:44 -0700

    Finish 366

--- src/test/java/interviews/linkedin/_366_Find_Leaves_of_Binary_Tree/SolutionTest.java
+++ src/test/java/interviews/linkedin/_366_Find_Leaves_of_Binary_Tree/SolutionTest.java
@@ -1,24 +1,21 @@
 package interviews.linkedin._366_Find_Leaves_of_Binary_Tree;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import utils.TreeNode;
 import utils.TreeNodes;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SolutionVTest {
-  private Solution solution;
+class SolutionTest {
 
-  @BeforeEach
-  void setup() {
-    solution = new SolutionV();
-  }
 
-  @Test
-  void testOnlineCase1() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
     final TreeNode root = TreeNodes.getTreeLevelOrder(1, 2, 3, 4, 5);
     final List<List<Integer>> actual = solution.findLeaves(root);
     final List<List<Integer>> expected = List.of(
@@ -27,5 +24,16 @@ class SolutionVTest {
         List.of(1)
     );
     assertEquals(expected, actual);
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII(),
+        new SolutionIV(),
+        new SolutionV(),
+        new SolutionVI()
+    );
   }
 }
