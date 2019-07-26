794a5f0dc37e6aa252f22446c3c614cf1b3b3524
jackalsin <jackal.sin@hotmail.com>
2019-07-25 20:46:55 -0700

    Finish 616

--- src/test/java/interviews/linkedin/_611_Valid_Triangle_Number/SolutionITest.java
+++ src/test/java/interviews/linkedin/_611_Valid_Triangle_Number/SolutionITest.java
@@ -1,33 +1,40 @@
 package interviews.linkedin._611_Valid_Triangle_Number;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionITest {
-  private Solution solution;
 
-  @BeforeEach
-  void setup() {
-    solution = new SolutionI();
-  }
-
-  @Test
-  void testOnlineCase1() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
     final int[] input = {2, 2, 3, 4};
     assertEquals(3, solution.triangleNumber(input));
   }
 
-  @Test
-  void testDuplicate1() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testDuplicate1(Solution solution) {
     final int[] input = {2, 2, 2, 3, 4};
     assertEquals(7, solution.triangleNumber(input));
   }
 
-  @Test
-  void testFailed2() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testFailed2(Solution solution) {
     final int[] input = {0, 1, 0};
     assertEquals(0, solution.triangleNumber(input));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII()
+    );
   }
 }
