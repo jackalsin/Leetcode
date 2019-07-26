0075a6f10724699e00cccca657fe1a74cb551487
jackalsin <jackal.sin@hotmail.com>
2019-07-25 22:05:31 -0700

    Finish 516

---
+++ src/test/java/interviews/linkedin/_516_Longest_Palindromic_Subsequence/SolutionTest.java
@@ -0,0 +1,32 @@
+package interviews.linkedin._516_Longest_Palindromic_Subsequence;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    assertEquals(4, solution.longestPalindromeSubseq("bbbab"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
+    assertEquals(2, solution.longestPalindromeSubseq("cbbd"));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII(),
+        new SolutionIV()
+    );
+  }
+}
