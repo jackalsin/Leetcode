e5d0e97831cd428171b7c7f1440da1f419f70364
jackalsin <jackal.sin@hotmail.com>
2019-07-28 21:58:08 -0700

    Finish 1028

---
+++ src/main/java/interviews/linkedin/_1028_Recover_a_Tree_From_Preorder_Traversal/Solution.java
@@ -0,0 +1,11 @@
+package interviews.linkedin._1028_Recover_a_Tree_From_Preorder_Traversal;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 7/28/2019
+ */
+public interface Solution {
+  TreeNode recoverFromPreorder(String S);
+}
