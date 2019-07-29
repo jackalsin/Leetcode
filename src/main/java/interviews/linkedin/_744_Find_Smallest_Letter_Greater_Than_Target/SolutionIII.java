bec898b4bbe03e213d9ffc7227ae82e2f6a34b37
jackalsin <jackal.sin@hotmail.com>
2019-07-28 19:45:48 -0700

    Finish 744

---
+++ src/main/java/interviews/linkedin/_744_Find_Smallest_Letter_Greater_Than_Target/SolutionIII.java
@@ -0,0 +1,28 @@
+package interviews.linkedin._744_Find_Smallest_Letter_Greater_Than_Target;
+
+/**
+ * @author jacka
+ * @version 1.0 on 7/28/2019
+ */
+public final class SolutionIII implements Solution {
+  @Override
+  public char nextGreatestLetter(char[] letters, char target) {
+    if (letters == null || letters.length == 0) {
+      return target;
+    }
+    int left = 0, right = letters.length - 1;
+    while (left < right) {
+      final int mid = left + (right - left) / 2;
+      if (letters[mid] > target) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    if (letters[left] > target) {
+      return letters[left];
+    }
+    // every element is smaller than target
+    return letters[0];
+  }
+}
--- src/test/java/interviews/linkedin/_744_Find_Smallest_Letter_Greater_Than_Target/SolutionTest.java
+++ src/test/java/interviews/linkedin/_744_Find_Smallest_Letter_Greater_Than_Target/SolutionTest.java
@@ -1,65 +1,76 @@
 package interviews.linkedin._744_Find_Smallest_Letter_Greater_Than_Target;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SolutionITest {
+class SolutionTest {
 
-  private Solution solution;
-
-  @BeforeEach
-  void setup() {
-    solution = new SolutionI();
-  }
-
-  @Test
-  void testOnlineCase1() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
     final char[] letters = {'c', 'f', 'j'};
     final char target = 'a';
     assertEquals('c', solution.nextGreatestLetter(letters, target));
   }
 
-  @Test
-  void testOnlineCase2() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
     final char[] letters = {'c', 'f', 'j'};
     final char target = 'c';
     assertEquals('f', solution.nextGreatestLetter(letters, target));
   }
 
-  @Test
-  void testOnlineCase3() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase3(Solution solution) {
     final char[] letters = {'c', 'f', 'j'};
     final char target = 'd';
     assertEquals('f', solution.nextGreatestLetter(letters, target));
   }
 
-  @Test
-  void testOnlineCase4() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase4(Solution solution) {
     final char[] letters = {'c', 'f', 'j'};
     final char target = 'g';
     assertEquals('j', solution.nextGreatestLetter(letters, target));
   }
 
-  @Test
-  void testOnlineCase5() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase5(Solution solution) {
     final char[] letters = {'c', 'f', 'j'};
     final char target = 'a';
     assertEquals('c', solution.nextGreatestLetter(letters, target));
   }
 
-  @Test
-  void testOnlineCase6() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase6(Solution solution) {
     final char[] letters = {'c', 'f', 'j'};
     final char target = 'j';
     assertEquals('c', solution.nextGreatestLetter(letters, target));
   }
 
-  @Test
-  void testOnlineCase7() {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase7(Solution solution) {
     final char[] letters = {'c', 'f', 'j'};
     final char target = 'k';
     assertEquals('c', solution.nextGreatestLetter(letters, target));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII()
+    );
   }
 }
