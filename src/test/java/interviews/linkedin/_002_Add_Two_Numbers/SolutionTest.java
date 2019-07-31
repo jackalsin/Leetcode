5e3e3fa856dbf532a3c34804100da7630d48df38
jackalsin <jackal.sin@hotmail.com>
2019-07-30 20:51:34 -0700

    Finish 002

--- src/test/java/interviews/linkedin/_002_Add_Two_Numbers/SolutionTest.java
+++ src/test/java/interviews/linkedin/_002_Add_Two_Numbers/SolutionTest.java
@@ -1,8 +1,11 @@
 package interviews.linkedin._002_Add_Two_Numbers;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import utils.ListNode;
 import utils.ListNodes;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,13 +17,19 @@ class SolutionTest {
 
   private static final ListNode NUM_38 = ListNodes.getListOfNodes(new int[]{8, 3});
 
-  @Test
-  void addTwoNumbers() throws Exception {
-    Solution solution = new Solution();
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void addTwoNumbers(Solution solution) throws Exception {
     assertEquals(ListNodes.getListOfNodes(new int[]{0, 0, 2, 1}),
         solution.addTwoNumbers(NUM_432, NUM_768));
     assertEquals(ListNodes.getListOfNodes(new int[]{0, 7, 4}),
         solution.addTwoNumbers(NUM_432, NUM_38));
+  }
 
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII()
+    );
   }
 }
