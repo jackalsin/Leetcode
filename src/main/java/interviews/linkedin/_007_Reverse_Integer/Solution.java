b73a7ae9a52d2342e7b2b1a9ddcf065795e12c66
jackalsin <jackal.sin@hotmail.com>
2019-07-30 21:22:41 -0700

    Finish 007

--- src/main/java/interviews/linkedin/_007_Reverse_Integer/Solution.java
+++ src/main/java/interviews/linkedin/_007_Reverse_Integer/Solution.java
@@ -1,24 +1,5 @@
 package interviews.linkedin._007_Reverse_Integer;
 
-public class Solution {
-  public int reverse(int x) {
-    if (x == 0) {
-      return 0;
-    }
-    int sign = x > 0 ? 1 : -1;
-    if (x == Integer.MIN_VALUE) {
-      return 0;
-    }
-    x = Math.abs(x);
-    int num = 0;
-    while (x != 0) {
-      final int curDigit = x % 10;
-      if (num > Integer.MAX_VALUE / 10 || (num == Integer.MAX_VALUE / 10 && curDigit > Integer.MAX_VALUE % 10))
-        return 0;
-      num = num * 10 + curDigit;
-      x /= 10;
-    }
-
-    return sign * num;
-  }
+public interface Solution {
+  int reverse(int x);
 }
--- src/test/java/interviews/linkedin/_007_Reverse_Integer/SolutionTest.java
+++ src/test/java/interviews/linkedin/_007_Reverse_Integer/SolutionTest.java
@@ -1,51 +1,59 @@
 package interviews.linkedin._007_Reverse_Integer;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
-  private Solution solution;
-
-  @BeforeEach
-  void setUp() throws Exception {
-    solution = new Solution();
-  }
-
-  @Test
-  void reverseZero() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void reverseZero(Solution solution) {
     assertEquals(0, solution.reverse(0));
   }
 
-  @Test
-  void reverseNegativeSingleDigit() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void reverseNegativeSingleDigit(Solution solution) {
     assertEquals(-1, solution.reverse(-1));
   }
 
-  @Test
-  void reverseNegativeTwoDigits() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void reverseNegativeTwoDigits(Solution solution) {
     assertEquals(-12, solution.reverse(-21));
   }
 
-  @Test
-  void reversePositiveSingleDigit() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void reversePositiveSingleDigit(Solution solution) {
     assertEquals(1, solution.reverse(1));
   }
 
-  @Test
-  void reversePositiveTwoDigits() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void reversePositiveTwoDigits(Solution solution) {
     assertEquals(12, solution.reverse(21));
   }
 
-  @Test
-  void reverseMinValue() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void reverseMinValue(Solution solution) {
     assertEquals(0, solution.reverse(Integer.MIN_VALUE));
   }
 
-  @Test
-  void reverseOverflow() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void reverseOverflow(Solution solution) {
     assertEquals(-2143847412, solution.reverse(-2147483412));
   }
 
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII()
+    );
+  }
 }
