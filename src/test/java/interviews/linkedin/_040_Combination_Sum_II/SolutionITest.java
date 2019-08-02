cbf4159c71ea821635346658faca53b1f9ccce6a
jackalsin <jackal.sin@hotmail.com>
2019-08-01 20:15:54 -0700

    Finish SolutionIII

--- src/test/java/interviews/linkedin/_040_Combination_Sum_II/SolutionITest.java
+++ src/test/java/interviews/linkedin/_040_Combination_Sum_II/SolutionITest.java
@@ -1,12 +1,13 @@
 package interviews.linkedin._040_Combination_Sum_II;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,15 +16,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @version 1.0 on 9/7/18
  */
 class SolutionITest {
-  private Solution solution;
 
-  @BeforeEach
-  void setUp() {
-    solution = new SolutionI();
-  }
-
-  @Test
-  void testWithOnlineCase() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testWithOnlineCase(Solution solution) {
     List<List<Integer>> expect = new ArrayList<>();
     expect.add(Arrays.asList(1, 7));
     expect.add(Arrays.asList(1, 2, 5));
@@ -36,14 +32,22 @@ class SolutionITest {
 
   }
 
-  @Test
-  void testWithTripleElements() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testWithTripleElements(Solution solution) {
     List<List<Integer>> expect = new ArrayList<>();
     expect.add(Arrays.asList(1, 1, 6));
 
     List<List<Integer>> actual = solution.combinationSum2(new int[]{1, 1, 1, 6, 10}, 8);
     assertEquals(expect.size(), actual.size());
     assertEquals(new HashSet<>(expect), new HashSet<>(actual));
+  }
 
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII()
+    );
   }
 }
