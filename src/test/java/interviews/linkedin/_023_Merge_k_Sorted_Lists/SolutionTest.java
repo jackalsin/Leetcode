d42efdfb9e16e51f5560151e58a6d191ead5ff06
jackalsin <jackal.sin@hotmail.com>
2019-08-02 19:44:30 -0700

    Finish 021

--- src/test/java/interviews/linkedin/_023_Merge_k_Sorted_Lists/SolutionTest.java
+++ src/test/java/interviews/linkedin/_023_Merge_k_Sorted_Lists/SolutionTest.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SolutionIITest {
+class SolutionTest {
   @ParameterizedTest
   @MethodSource("solutionProvider")
   void testOnlineCase1(Solution solution) {
