5e881963c275b4c23adc7550877496ba61d63228
jackalsin <jackal.sin@hotmail.com>
2019-08-02 20:46:45 -0700

    Finish 015

--- src/main/java/interviews/linkedin/_015_3Sum/Solution.java
+++ src/main/java/interviews/linkedin/_015_3Sum/Solution.java
@@ -1,37 +1,13 @@
 package interviews.linkedin._015_3Sum;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class Solution {
-  public List<List<Integer>> threeSum(int[] nums) {
-    final List<List<Integer>> result = new ArrayList<>();
-    Arrays.sort(nums);
-    for (int i = 0; i < nums.length; ++i) {
-      if (i > 0 && nums[i - 1] == nums[i]) continue; // i duplicate
-      for (int j = i + 1, k = nums.length - 1; j < k; ) {
-        final int sum = nums[i] + nums[j] + nums[k];
-        if (sum == 0) {
-          result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-          // skip all the duplicates
-          while (j + 1 < k && nums[j + 1] == nums[j]) {
-            j++;
-          }
-          j++;
-          while (k - 1 > j && nums[k - 1] == nums[k]) {
-            k--;
-          }
-          k--;
-
-        } else if (sum < 0) {
-          j++;
-        } else {
-          --k;
-        }
-      }
-    }
-    return result;
-  }
-
+public interface Solution {
+  /**
+   * TO: Revisit
+   *
+   * @param nums
+   * @return
+   */
+  List<List<Integer>> threeSum(int[] nums);
 }
--- src/test/java/interviews/linkedin/_015_3Sum/SolutionTest.java
+++ src/test/java/interviews/linkedin/_015_3Sum/SolutionTest.java
@@ -1,28 +1,22 @@
 package interviews.linkedin._015_3Sum;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
 
-  private static final int[] ARRAY_1 = new int[]{1, 2, 3, 4, 5};
-  private static final int[] ARRAY_2 = new int[]{-4, -1, -1, 0, 1, 2};
-  private Solution solution;
 
-  @BeforeEach
-  void setUp() throws Exception {
-    solution = new Solution();
-  }
-
-  @Test
-  void threeSum() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void threeSum(Solution solution) {
     final List<List<Integer>> expected = List.of(
         List.of(-1, 0, 1),
         List.of(-1, -1, 2)
@@ -32,8 +26,9 @@ class SolutionTest {
     assertEquals(new HashSet<>(expected), new HashSet<>(actual));
   }
 
-  @Test
-  void threeSumWithDipAnsSymmetric() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void threeSumWithDipAnsSymmetric(Solution solution) {
     List<List<Integer>> sampleAnswer = new ArrayList<>();
     sampleAnswer.add(Arrays.asList(-4, 2, 2));
     sampleAnswer.add(Arrays.asList(-2, -2, 4));
@@ -45,6 +40,25 @@ class SolutionTest {
     assertEquals(
         new HashSet<>(sampleAnswer),
         new HashSet<>(solution.threeSum(new int[]{-4, -2, -2, -1, -1, 0, 1, 1, 2, 2, 4}))
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void threeSumWithDupAnsSymmetric(Solution solution) {
+    List<List<Integer>> sampleAnswer = new ArrayList<>();
+    sampleAnswer.add(Arrays.asList(-1, -1, 2));
+    sampleAnswer.add(Arrays.asList(-1, 0, 1));
+    assertEquals(
+        new HashSet<>(sampleAnswer),
+        new HashSet<>(solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4}))
+    );
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII()
     );
   }
 }
