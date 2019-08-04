f74270eb9d54c27a54b3c5e78ea193b6b9004401
jackalsin <jackal.sin@hotmail.com>
2019-08-03 17:41:41 -0700

    Finish 100

--- src/main/java/interviews/linkedin/_100_Same_Tree/Solution.java
+++ src/main/java/interviews/linkedin/_100_Same_Tree/Solution.java
@@ -6,16 +6,6 @@ import utils.TreeNode;
  * @author zhiwei.xin
  * @version 1.0 on 9/5/18
  */
-public class Solution {
-  public boolean isSameTree(TreeNode p, TreeNode q) {
-    if (p == null) {
-      return q == null;
-    }
-    if (q == null) {
-      return false;
-    }
-    return q.val == p.val
-        && isSameTree(p.left, q.left)
-        && isSameTree(p.right, q.right);
-  }
+public interface Solution {
+  boolean isSameTree(TreeNode p, TreeNode q);
 }
--- src/test/java/interviews/linkedin/_100_Same_Tree/SolutionTest.java
+++ src/test/java/interviews/linkedin/_100_Same_Tree/SolutionTest.java
@@ -1,17 +1,40 @@
 package interviews.linkedin._100_Same_Tree;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author zhiwei.xin
  * @version 1.0 on 9/5/18
  */
 class SolutionTest {
-  private Solution solution;
-
-  @BeforeEach
-  void setup() {
-    solution = new Solution();
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testValid(Solution solution) {
+    final TreeNode p = TreeNodes.getTreeLevelOrder(2, 1, 3),
+        q = TreeNodes.getTreeLevelOrder(2, 1, 3);
+    assertTrue(solution.isSameTree(p, q));
   }
 
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testInvalid(Solution solution) {
+    final TreeNode p = TreeNodes.getTreeLevelOrder(1, 2, null),
+        q = TreeNodes.getTreeLevelOrder(1, null, 2);
+    assertFalse(solution.isSameTree(p, q));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII()
+    );
+  }
 }
