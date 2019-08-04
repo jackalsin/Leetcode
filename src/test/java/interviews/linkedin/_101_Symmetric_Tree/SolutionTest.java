b7294cfc5f2710a6f9b694f51a1f74f4e0663db4
jackalsin <jackal.sin@hotmail.com>
2019-08-03 17:55:02 -0700

    Finish 101

--- src/test/java/interviews/linkedin/_101_Symmetric_Tree/SolutionTest.java
+++ src/test/java/interviews/linkedin/_101_Symmetric_Tree/SolutionTest.java
@@ -1,9 +1,11 @@
 package interviews.linkedin._101_Symmetric_Tree;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import utils.TreeNode;
 import utils.TreeNodes;
+
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -12,30 +14,34 @@ import static org.junit.Assert.assertTrue;
  * @author jacka
  * @version 1.0 on 10/19/2017.
  */
-public class SolutionTest {
-  private Solution solution;
+class SolutionTest {
 
-  @Before
-  public void setUp() throws Exception {
-    solution = new SolutionII();
-  }
-
-  @Test
-  public void testOnlineCase() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase(Solution solution) {
     TreeNode root = TreeNodes.getTreeLevelOrder(1, 2, 2, 3, 4, 4, 3);
     assertTrue(solution.isSymmetric(root));
   }
 
-  @Test
-  public void testThreeLevelTree() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testThreeLevelTree(Solution solution) {
     TreeNode root = TreeNodes.getTreeLevelOrder(1, 2, 2, 3, 4, 4, 3, 5, 6, 7, 8, 8, 7, 6, 5);
     assertTrue(solution.isSymmetric(root));
   }
 
-  @Test
-  public void testFailed1() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testFailed1(Solution solution) {
     TreeNode root = TreeNodes.getTreeLevelOrder(1, 2, 2, null, 3, null, 3);
     assertFalse(solution.isSymmetric(root));
   }
 
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII()
+    );
+  }
 }
