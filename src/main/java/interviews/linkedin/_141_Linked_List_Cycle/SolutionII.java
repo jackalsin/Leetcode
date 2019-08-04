114fc3f94035a2a2982d411c7727218209b92fd3
jackalsin <jackal.sin@hotmail.com>
2019-08-03 18:41:43 -0700

    Finish 141

---
+++ src/main/java/interviews/linkedin/_141_Linked_List_Cycle/SolutionII.java
@@ -0,0 +1,22 @@
+package interviews.linkedin._141_Linked_List_Cycle;
+
+import utils.ListNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/3/2019
+ */
+public final class SolutionII implements Solution {
+  @Override
+  public boolean hasCycle(ListNode head) {
+    ListNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
