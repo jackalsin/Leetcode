dc98f856062343991fb88366f7d58280e89fea49
jackalsin <jackal.sin@hotmail.com>
2019-08-03 18:21:51 -0700

    Finish 112

---
+++ src/main/java/interviews/linkedin/_112_Path_Sum/SolutionII.java
@@ -0,0 +1,21 @@
+package interviews.linkedin._112_Path_Sum;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/3/2019
+ */
+public final class SolutionII implements Solution {
+  @Override
+  public boolean hasPathSum(TreeNode root, int sum) {
+    if (root == null) {
+      return false;
+    }
+    if (root.left == null && root.right == null) {
+      return sum == root.val;
+    }
+    return hasPathSum(root.left, sum - root.val)
+        || hasPathSum(root.right, sum - root.val);
+  }
+}
