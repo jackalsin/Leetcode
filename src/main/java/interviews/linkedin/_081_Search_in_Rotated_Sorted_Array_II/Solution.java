7d742f5b81093d645ab3de0bbacf2e7ea56b1439
jackalsin <jackal.sin@hotmail.com>
2019-08-04 20:18:01 -0700

    Finish 081

--- src/main/java/interviews/linkedin/_081_Search_in_Rotated_Sorted_Array_II/Solution.java
+++ src/main/java/interviews/linkedin/_081_Search_in_Rotated_Sorted_Array_II/Solution.java
@@ -1,5 +1,12 @@
 package interviews.linkedin._081_Search_in_Rotated_Sorted_Array_II;
 
 public interface Solution {
+  /**
+   * TODO: revisit
+   *
+   * @param nums
+   * @param target
+   * @return
+   */
   boolean search(int[] nums, int target);
 }
