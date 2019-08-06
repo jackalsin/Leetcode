6d68417395f0ca34a422c66b138334b120a11cbd
jackalsin <jackal.sin@hotmail.com>
2019-08-05 19:52:16 -0700

    Finish 198

---
+++ src/main/java/interviews/linkedin/_198_House_Robber/SolutionIII.java
@@ -0,0 +1,22 @@
+package interviews.linkedin._198_House_Robber;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/5/2019
+ */
+public final class SolutionIII implements Solution {
+  @Override
+  public int rob(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    int maxRobEndHere = nums[0], maxNotRobEndHere = 0;
+    for (int i = 1; i < nums.length; i++) {
+      final int prevRob = maxRobEndHere,
+          prevNotRob = maxNotRobEndHere;
+      maxRobEndHere = prevNotRob + nums[i];
+      maxNotRobEndHere = Math.max(maxNotRobEndHere, prevRob);
+    }
+    return Math.max(maxNotRobEndHere, maxRobEndHere);
+  }
+}
--- src/test/java/interviews/linkedin/_198_House_Robber/SolutionIITest.java
+++ src/test/java/interviews/linkedin/_198_House_Robber/SolutionIITest.java
@@ -1,21 +1,26 @@
 package interviews.linkedin._198_House_Robber;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionIITest {
-  private Solution solution;
 
-  @BeforeEach
-  void setUp() {
-    solution = new SolutionII();
-  }
-
-  @Test
-  void testOnlineCase1() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
     final int[] nums = {1, 2, 3, 1};
     assertEquals(4, solution.rob(nums));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII()
+    );
   }
 }
