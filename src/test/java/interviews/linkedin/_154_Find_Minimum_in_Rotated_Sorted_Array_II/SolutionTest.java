6407370b46c5cff0fd282ac23c67ffe31f0911ca
jackalsin <jackal.sin@hotmail.com>
2019-08-07 08:34:49 -0700

    Finish 154

--- src/test/java/interviews/linkedin/_154_Find_Minimum_in_Rotated_Sorted_Array_II/SolutionTest.java
+++ src/test/java/interviews/linkedin/_154_Find_Minimum_in_Rotated_Sorted_Array_II/SolutionTest.java
@@ -1,53 +1,61 @@
 package interviews.linkedin._154_Find_Minimum_in_Rotated_Sorted_Array_II;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
-  private Solution solution;
-
-  @BeforeEach
-  void setUp() {
-    solution = new Solution();
-  }
-
   // non duplicated test cases
-  @Test
-  void testOneElement() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOneElement(Solution solution) {
     final int[] input = new int[]{1};
     assertEquals(1, solution.findMin(input));
   }
 
-  @Test
-  void testTwoElement() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testTwoElement(Solution solution) {
     final int[] input = new int[]{1, 2};
     assertEquals(1, solution.findMin(input));
   }
 
-  @Test
-  void testTwoElements() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testTwoElements(Solution solution) {
     final int[] input = new int[]{2, 1};
     assertEquals(1, solution.findMin(input));
   }
 
-  @Test
-  void test5Elements() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void test5Elements(Solution solution) {
     final int[] input = new int[]{2, 3, 4, 0, 1};
     assertEquals(0, solution.findMin(input));
   }
 
   // duplicated test cases
-  @Test
-  void testDup1() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testDup1(Solution solution) {
     final int[] input = new int[]{2, 2, 2, 0, 1};
     assertEquals(0, solution.findMin(input));
   }
 
-  void testDup2() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testDup2(Solution solution) {
     final int[] input = new int[]{1, 3, 3};
     assertEquals(1, solution.findMin(input));
   }
 
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII()
+    );
+  }
 }
