b7ba86c13e876a48e43324a960e05ccb373c39c3
jackalsin <jackal.sin@hotmail.com>
2019-08-06 20:47:41 -0700

    Finish 226

--- src/main/java/interviews/linkedin/_226_Invert_Binary_Tree/Solution.java
+++ src/main/java/interviews/linkedin/_226_Invert_Binary_Tree/Solution.java
@@ -2,15 +2,6 @@ package interviews.linkedin._226_Invert_Binary_Tree;
 
 import utils.TreeNode;
 
-public class Solution {
-  public TreeNode invertTree(TreeNode root) {
-    if (root == null) {
-      return null;
-    }
-    final TreeNode left = root.left, right = root.right;
-    root.left = invertTree(right);
-    root.right = invertTree(left);
-    return root;
-  }
-
+public interface Solution {
+  TreeNode invertTree(TreeNode root);
 }
--- src/test/java/interviews/linkedin/_226_Invert_Binary_Tree/SolutionTest.java
+++ src/test/java/interviews/linkedin/_226_Invert_Binary_Tree/SolutionTest.java
@@ -1,33 +1,36 @@
 package interviews.linkedin._226_Invert_Binary_Tree;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import utils.TreeNode;
 import utils.TreeNodes;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
-  private Solution solution;
 
-  @BeforeEach
-  void setUp() throws Exception {
-    solution = new Solution();
-  }
-
-  @Test
-  void testOneNode() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOneNode(Solution solution) {
     TreeNode expected = new TreeNode(1);
     TreeNode actual = solution.invertTree(expected);
     assertEquals(expected, actual);
   }
 
-  @Test
-  void test3LevelNodes() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void test3LevelNodes(Solution solution) {
     TreeNode input = TreeNodes.getTreeLevelOrder(4, 2, 7, 1, 3, 6, 9);
     TreeNode actual = solution.invertTree(input);
     TreeNode expected = TreeNodes.getTreeLevelOrder(4, 7, 2, 9, 6, 3, 1);
     assertEquals(expected, actual);
   }
 
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
 }
