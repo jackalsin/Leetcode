dfc2f722aed85fc2f6aad713308bdb9205749d41
jackalsin <jackal.sin@hotmail.com>
2019-08-09 20:24:55 -0700

    Finish 270

--- src/main/java/interviews/linkedin/_270_Closest_Binary_Search_Tree_Value/Solution.java
+++ src/main/java/interviews/linkedin/_270_Closest_Binary_Search_Tree_Value/Solution.java
@@ -2,16 +2,14 @@ package interviews.linkedin._270_Closest_Binary_Search_Tree_Value;
 
 import utils.TreeNode;
 
-public class Solution {
+public interface Solution {
 
-  public int closestValue(TreeNode root, double target) {
-
-    final TreeNode child = root.val < target ? root.right : root.left;
-    if (child == null) {
-      return root.val;
-    }
-    final int childMin = closestValue(child, target);
-
-    return (Math.abs(root.val - target) < Math.abs(childMin - target)) ? root.val : childMin;
-  }
+  /**
+   * TODO: Revisit
+   *
+   * @param root
+   * @param target
+   * @return
+   */
+  int closestValue(TreeNode root, double target);
 }
--- src/test/java/interviews/linkedin/_270_Closest_Binary_Search_Tree_Value/SolutionTest.java
+++ src/test/java/interviews/linkedin/_270_Closest_Binary_Search_Tree_Value/SolutionTest.java
@@ -1,31 +1,36 @@
 package interviews.linkedin._270_Closest_Binary_Search_Tree_Value;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import utils.TreeNode;
 import utils.TreeNodes;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
-  private Solution solution;
 
-  @BeforeEach
-  void setup() {
-    solution = new Solution();
-  }
-
-  @Test
-  void testOnlineCase1() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
     final TreeNode root = TreeNodes.getTreeLevelOrder(new int[]{4, 2, 5, 1, 3});
     final double target = 3.714286;
     assertEquals(4, solution.closestValue(root, target));
   }
 
-  @Test
-  void testOnlineCase2() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
     final TreeNode root = TreeNodes.getTreeLevelOrder(new int[]{0});
     final double target = Integer.MAX_VALUE;
     assertEquals(0, solution.closestValue(root, target));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII()
+    );
   }
 }
