2ddef394c2e89154944fde1e69cdc02d3f6a7466
jackalsin <jackal.sin@hotmail.com>
2019-08-11 16:34:37 -0700

    Finish 347

---
+++ src/main/java/interviews/linkedin/_347_Top_K_Frequent_Elements/Solution.java
@@ -0,0 +1,18 @@
+package interviews.linkedin._347_Top_K_Frequent_Elements;
+
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/11/2019
+ */
+public interface Solution {
+  /**
+   * TODO: revisit
+   *
+   * @param nums
+   * @param k
+   * @return
+   */
+  List<Integer> topKFrequent(int[] nums, int k);
+}
