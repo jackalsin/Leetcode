d46a12e715298e0d6decb0044b9a087ba87228ac
jackalsin <jackal.sin@hotmail.com>
2019-08-11 11:56:27 -0700

    Finish 344

--- src/main/java/interviews/linkedin/_344_Reverse_String/Solution.java
+++ src/main/java/interviews/linkedin/_344_Reverse_String/Solution.java
@@ -1,20 +1,5 @@
 package interviews.linkedin._344_Reverse_String;
 
-public class Solution {
-  public String reverseString(String s) {
-    if (s == null) {
-      return null;
-    }
-    final char[] chars = s.toCharArray();
-    int left = 0, right = s.length() - 1;
-    while (left < right) {
-      char tmp = chars[left];
-      chars[left] = chars[right];
-      chars[right] = tmp;
-      left++;
-      right--;
-    }
-    return String.valueOf(chars);
-  }
-
+public interface Solution {
+  void reverseString(char[] s);
 }
