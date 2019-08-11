a1db17159b042a210620db4a0c1a7b6f0366e0d2
jackalsin <jackal.sin@hotmail.com>
2019-08-11 12:02:09 -0700

    Finish 541

---
+++ src/main/java/interviews/linkedin/_541_Reverse_String_II/Solution.java
@@ -0,0 +1,9 @@
+package interviews.linkedin._541_Reverse_String_II;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/11/2019
+ */
+public interface Solution {
+  String reverseStr(String s, int k);
+}
---
+++ src/test/java/interviews/linkedin/_541_Reverse_String_II/SolutionITest.java
@@ -0,0 +1,28 @@
+package interviews.linkedin._541_Reverse_String_II;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/11/2019
+ */
+class SolutionITest {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final String s = "abcdefg", expected = "bacdfeg";
+    final int k = 2;
+    assertEquals(expected, solution.reverseStr(s, k));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
