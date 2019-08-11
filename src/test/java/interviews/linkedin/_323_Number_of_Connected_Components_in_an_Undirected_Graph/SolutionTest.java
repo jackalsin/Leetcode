ef0bf0c684a2a6025e7de9054512be2cf7348c55
jackalsin <jackal.sin@hotmail.com>
2019-08-11 11:20:47 -0700

    Finish 323

--- src/test/java/interviews/linkedin/_323_Number_of_Connected_Components_in_an_Undirected_Graph/SolutionTest.java
+++ src/test/java/interviews/linkedin/_323_Number_of_Connected_Components_in_an_Undirected_Graph/SolutionTest.java
@@ -1,30 +1,42 @@
 package interviews.linkedin._323_Number_of_Connected_Components_in_an_Undirected_Graph;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
-  private Solution solution;
 
-  @BeforeEach
-  void setUp() throws Exception {
-    solution = new Solution();
-  }
-
-  @Test
-  void testOnlineCase1() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
     final int n = 5;
     final int[][] edges = {{0, 1}, {1, 2}, {3, 4}};
     assertEquals(2, solution.countComponents(5, edges));
   }
 
-  @Test
-  void testOnlineCase2() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
     final int n = 5;
     final int[][] edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}};
     assertEquals(1, solution.countComponents(5, edges));
   }
 
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase3(Solution solution) {
+    final int n = 5;
+    final int[][] edges = {{0, 1}, {1, 2}, {0, 2}, {3, 4}};
+    assertEquals(2, solution.countComponents(5, edges));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII()
+    );
+  }
 }
