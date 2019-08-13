7cfb1bfe13c98d55b79c14d7fdb83c6858ba65de
jackalsin <jackal.sin@hotmail.com>
2019-08-12 22:58:31 -0700

    Finish 367

---
+++ src/main/java/interviews/linkedin/_367_Valid_Perfect_Square/SolutionIII.java
@@ -0,0 +1,27 @@
+package interviews.linkedin._367_Valid_Perfect_Square;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/12/2019
+ */
+public final class SolutionIII implements Solution {
+  @Override
+  public boolean isPerfectSquare(int num) {
+    if (num <= 0) {
+      return false;
+    }
+    int left = 1, right = num;
+    while (left <= right) {
+      final int mid = left + (right - left) / 2;
+      if (num % mid == 0 && num / mid == mid) {
+        return true;
+      }
+      if (num / mid > mid) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return false;
+  }
+}
--- src/test/java/interviews/linkedin/_367_Valid_Perfect_Square/SolutionTest.java
+++ src/test/java/interviews/linkedin/_367_Valid_Perfect_Square/SolutionTest.java
@@ -1,26 +1,38 @@
 package interviews.linkedin._367_Valid_Perfect_Square;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * @author jacka
  * @version 1.0 on 10/23/2017.
  */
-public class SolutionTest {
-  private Solution solution;
+class SolutionTest {
 
-  @Before
-  public void setUp() throws Exception {
-    solution = new SolutionII();
-  }
-
-  //
-  @Test
-  public void test16Solution() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void test16Solution(Solution solution) {
     final int num = 16;
     assertTrue(solution.isPerfectSquare(num));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void test14Solution(Solution solution) {
+    final int num = 14;
+    assertFalse(solution.isPerfectSquare(num));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII()
+    );
   }
 }
