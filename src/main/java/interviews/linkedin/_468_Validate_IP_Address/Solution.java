2347af4c6ff1e2d23b369b0de91cbe19f8030b1d
jackalsin <jackal.sin@hotmail.com>
2019-08-15 09:44:48 -0700

    Finish 468

--- src/main/java/interviews/linkedin/_468_Validate_IP_Address/Solution.java
+++ src/main/java/interviews/linkedin/_468_Validate_IP_Address/Solution.java
@@ -3,5 +3,11 @@ package interviews.linkedin._468_Validate_IP_Address;
 public interface Solution {
   String IPV4 = "IPv4", IPV6 = "IPv6", NEITHER = "Neither";
 
+  /**
+   * TODO: Revisit
+   *
+   * @param IP
+   * @return
+   */
   String validIPAddress(String IP);
 }
