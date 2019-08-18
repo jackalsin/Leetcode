53590170df647897f9a9b15c5c15b73f4e57a648
jackalsin <jackal.sin@hotmail.com>
2019-08-18 12:57:17 -0700

    Finish 695

--- src/test/java/interviews/linkedin/_695_Max_Area_of_Island/SolutionITest.java
+++ src/test/java/interviews/linkedin/_695_Max_Area_of_Island/SolutionITest.java
@@ -1,20 +1,17 @@
 package interviews.linkedin._695_Max_Area_of_Island;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionITest {
-  private Solution solution;
 
-  @BeforeEach
-  void setup() {
-    solution = new SolutionI();
-  }
-
-  @Test
-  void testOnlineCase1() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
     final int[][] input = {
         {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
@@ -27,5 +24,12 @@ class SolutionITest {
     };
 
     assertEquals(6, solution.maxAreaOfIsland(input));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII()
+    );
   }
 }
