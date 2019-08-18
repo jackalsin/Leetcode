2fdf4f8beb5815ecddcac02ebb9cc1503d4753f0
jackalsin <jackal.sin@hotmail.com>
2019-08-18 15:50:28 -0700

    Finish 701

---
+++ src/main/java/interviews/linkedin/_701_Insert_into_a_Binary_Search_Tree/Solution.java
@@ -0,0 +1,12 @@
+package interviews.linkedin._701_Insert_into_a_Binary_Search_Tree;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/18/2019
+ */
+public interface Solution {
+  TreeNode insertIntoBST(TreeNode root, int val);
+
+}
---
+++ src/test/java/interviews/linkedin/_701_Insert_into_a_Binary_Search_Tree/SolutionITest.java
@@ -0,0 +1,31 @@
+package interviews.linkedin._701_Insert_into_a_Binary_Search_Tree;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/18/2019
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final TreeNode root = TreeNodes.getTreeLevelOrder(4, 2, 7, 1, 3);
+    final TreeNode expected = TreeNodes.getTreeLevelOrder(4, 2, 7, 1, 3, 5);
+    assertEquals(expected, solution.insertIntoBST(root, 5));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
