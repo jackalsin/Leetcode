691515e16bc2a080b861b5cb35587e170f291797
jackalsin <jackal.sin@hotmail.com>
2019-08-20 19:49:18 -0700

    Finish 890

---
+++ src/main/java/interviews/linkedin/_890_Find_and_Replace_Pattern/Solution.java
@@ -0,0 +1,18 @@
+package interviews.linkedin._890_Find_and_Replace_Pattern;
+
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/20/2019
+ */
+public interface Solution {
+  /**
+   * TODO: Revisit
+   *
+   * @param words
+   * @param pattern
+   * @return
+   */
+  List<String> findAndReplacePattern(String[] words, String pattern);
+}
