b00ec2bf12bf17e18d8f7d751c1e8206dd62fceb
jackalsin <jackal.sin@hotmail.com>
2019-08-21 09:01:31 -0700

    Finish 034

--- src/test/java/interviews/linkedin/_034_Search_for_a_Range/SolutionTest.java
+++ src/test/java/interviews/linkedin/_034_Search_for_a_Range/SolutionTest.java
@@ -1,37 +1,53 @@
 package interviews.linkedin._034_Search_for_a_Range;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.Assert.assertArrayEquals;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
  * @author jacka
  * @version 1.0 on 10/16/2017.
  */
-public class SolutionTest {
-  private Solution solution;
-
-  @Before
-  public void setUp() throws Exception {
-    solution = new SolutionII();
-  }
-
-  @Test
-  public void testOnlineCase() throws Exception {
+class SolutionTest {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase(Solution solution) {
     final int[] input = {2, 2};
     assertArrayEquals(new int[]{0, 1}, solution.searchRange(input, 2));
   }
 
-  @Test
-  public void testOnlineCase2() throws Exception {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
     final int[] input = {5, 7, 7, 8, 8, 10};
     assertArrayEquals(new int[]{3, 4}, solution.searchRange(input, 8));
   }
 
-  @Test
-  public void testFailedCase() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testFailedCase(Solution solution) {
     final int[] input = {1};
     assertArrayEquals(new int[]{0, 0}, solution.searchRange(input, 1));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testFailedCase2(Solution solution) {
+    final int[] input = {5, 7, 7, 8, 8, 10};
+    assertArrayEquals(new int[]{-1, -1}, solution.searchRange(input, 6));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII(),
+        new SolutionIV(),
+        new SolutionV()
+    );
   }
 }
