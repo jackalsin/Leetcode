b78deb4fb41842716b4ad21343ce74da2c770d65
jackalsin <jackal.sin@hotmail.com>
2019-08-24 15:42:35 -0700

    Finish 076

--- src/test/java/interviews/linkedin/_076_Minimum_Window_Substring/SolutionTest.java
+++ src/test/java/interviews/linkedin/_076_Minimum_Window_Substring/SolutionTest.java
@@ -1,7 +1,9 @@
 package interviews.linkedin._076_Minimum_Window_Substring;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,26 +11,39 @@ import static org.junit.Assert.assertEquals;
  * @author jacka
  * @version 1.0 on 10/19/2017.
  */
-public class SolutionTest {
-  private Solution solution;
-
-  @Before
-  public void setUp() throws Exception {
-    solution = new SolutionII();
-//    solution = new SolutionI();
-  }
-
-  @Test
-  public void testA() throws Exception {
+class SolutionTest {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testA(Solution solution) {
     final String s = "a";
     final String t = "a";
     assertEquals(s, solution.minWindow(s, t));
   }
 
-  @Test
-  public void testADOBECODEBANC() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testAAndAA(Solution solution) {
+    final String s = "a";
+    final String t = "aa";
+    assertEquals("", solution.minWindow(s, t));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testADOBECODEBANC(Solution solution) {
     final String s = "ADOBECODEBANC";
     final String t = "ABC";
     assertEquals("BANC", solution.minWindow(s, t));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII(),
+        new SolutionIV(),
+        new SolutionV(),
+        new SolutionVI()
+    );
   }
 }
