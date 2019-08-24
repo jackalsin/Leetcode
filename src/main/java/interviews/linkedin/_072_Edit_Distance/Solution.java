e007d8e369caea41dda0c96d75c3dfd4d67966c8
jackalsin <jackal.sin@hotmail.com>
2019-08-24 14:42:49 -0700

    Finish 072

--- src/main/java/interviews/linkedin/_072_Edit_Distance/Solution.java
+++ src/main/java/interviews/linkedin/_072_Edit_Distance/Solution.java
@@ -1,5 +1,12 @@
 package interviews.linkedin._072_Edit_Distance;
 
 public interface Solution {
+  /**
+   * TODO: Revisit
+   *
+   * @param word1
+   * @param word2
+   * @return
+   */
   int minDistance(String word1, String word2);
 }
