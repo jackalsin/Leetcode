a5d1fe2caac785c59afc9e83dd1d886aa54c4cef
jackalsin <jackal.sin@hotmail.com>
2019-08-25 10:49:44 -0700

    Finish 100

--- src/test/java/interviews/linkedin/_100_Same_Tree/SolutionTest.java
+++ src/test/java/interviews/linkedin/_100_Same_Tree/SolutionTest.java
@@ -34,7 +34,8 @@ class SolutionTest {
   static Stream<Solution> solutionProvider() {
     return Stream.of(
         new SolutionI(),
-        new SolutionII()
+        new SolutionII(),
+        new SolutionIII()
     );
   }
 }
