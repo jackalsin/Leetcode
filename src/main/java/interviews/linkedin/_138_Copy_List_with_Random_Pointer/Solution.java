1ba6eb1e2140a4a5938e111c5db346ad0925284c
jackalsin <jackal.sin@hotmail.com>
2019-08-26 09:45:40 -0700

    Finish 138

--- src/main/java/interviews/linkedin/_138_Copy_List_with_Random_Pointer/Solution.java
+++ src/main/java/interviews/linkedin/_138_Copy_List_with_Random_Pointer/Solution.java
@@ -3,5 +3,11 @@ package interviews.linkedin._138_Copy_List_with_Random_Pointer;
 import utils._138_Copy_List_with_Random_Pointer.Node;
 
 public interface Solution {
+  /**
+   * TODO: Revisit O 1 Solution
+   *
+   * @param head
+   * @return
+   */
   Node copyRandomList(Node head);
 }
