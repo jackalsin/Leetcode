936c09ce38f4ab5f33ba105a3b3aab5cdcc7b259
jackalsin <jackal.sin@hotmail.com>
2019-08-26 08:55:24 -0700

    Finish 128

--- src/main/java/interviews/linkedin/_128_Longest_Consecutive_Sequence/Solution.java
+++ src/main/java/interviews/linkedin/_128_Longest_Consecutive_Sequence/Solution.java
@@ -5,5 +5,11 @@ package interviews.linkedin._128_Longest_Consecutive_Sequence;
  * @version 1.0 on 8/3/2019
  */
 public interface Solution {
+  /**
+   * TODO: Revisit
+   *
+   * @param nums
+   * @return
+   */
   int longestConsecutive(int[] nums);
 }
