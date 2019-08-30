922d557a0b94577eab5bc6b7c51039186d247ed5
jackalsin <jackal.sin@hotmail.com>
2019-08-29 20:56:40 -0700

    Finish 173

--- src/test/java/interviews/linkedin/_173_Binary_Search_Tree_Iterator/BSTIteratorTest.java
+++ src/test/java/interviews/linkedin/_173_Binary_Search_Tree_Iterator/BSTIteratorTest.java
@@ -1,9 +1,12 @@
 package interviews.linkedin._173_Binary_Search_Tree_Iterator;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import utils.TreeNode;
 import utils.TreeNodes;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,24 +14,25 @@ import static org.junit.Assert.assertEquals;
  * @author jacka
  * @version 1.0 on 10/21/2017.
  */
-public class BSTIteratorTest {
-  @Before
-  public void setUp() throws Exception {
-  }
+class BSTIteratorTest {
 
-  @Test
-  public void test1Null2() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void test1Null2(Class<BSTIterator> solutionClass) throws NoSuchMethodException, IllegalAccessException,
+      InvocationTargetException, InstantiationException {
     final TreeNode root = TreeNodes.getTreeLevelOrder(1, null, 2);
-    BSTIterator bstIterator = new BSTIteratorII(root);
+    BSTIterator bstIterator = solutionClass.getConstructor(TreeNode.class).newInstance(root);
     assertEquals(1, bstIterator.next());
     assertEquals(2, bstIterator.next());
   }
 
-  @Test
-  public void test1To15() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void test1To15(Class<BSTIterator> solutionClass) throws NoSuchMethodException, IllegalAccessException,
+      InvocationTargetException, InstantiationException {
     final TreeNode root = TreeNodes.getTreeLevelOrder(8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11,
         13, 15);
-    BSTIterator bstIterator = new BSTIteratorII(root);
+    BSTIterator bstIterator = solutionClass.getConstructor(TreeNode.class).newInstance(root);
     assertEquals(1, bstIterator.next());
     assertEquals(2, bstIterator.next());
     assertEquals(3, bstIterator.next());
@@ -44,5 +48,14 @@ public class BSTIteratorTest {
     assertEquals(13, bstIterator.next());
     assertEquals(14, bstIterator.next());
     assertEquals(15, bstIterator.next());
+  }
+
+  static Stream<Class> solutionProvider() {
+    return Stream.of(
+        BSTIteratorI.class,
+        BSTIteratorII.class,
+        BSTIteratorIII.class,
+        BSTIteratorIV.class
+    );
   }
 }
