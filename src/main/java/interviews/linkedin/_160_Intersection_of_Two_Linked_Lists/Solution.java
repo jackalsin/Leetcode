6a81e93a9b5a35aedb00d2be6f7a696f809c84fc
jackalsin <jackal.sin@hotmail.com>
2019-08-31 13:48:44 -0700

    Finish 160

--- src/main/java/interviews/linkedin/_160_Intersection_of_Two_Linked_Lists/Solution.java
+++ src/main/java/interviews/linkedin/_160_Intersection_of_Two_Linked_Lists/Solution.java
@@ -2,21 +2,13 @@ package interviews.linkedin._160_Intersection_of_Two_Linked_Lists;
 
 import utils.ListNode;
 
-public class Solution {
-  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-    ListNode a = headA, b = headB;
-    while (a != b) {
-      if (a == null) {
-        a = headB;
-      } else {
-        a = a.next;
-      }
-      if (b == null) {
-        b = headA;
-      } else {
-        b = b.next;
-      }
-    }
-    return a;
-  }
+public interface Solution {
+  /**
+   * TODO: Revisit
+   *
+   * @param headA
+   * @param headB
+   * @return
+   */
+  ListNode getIntersectionNode(ListNode headA, ListNode headB);
 }
--- src/test/java/interviews/linkedin/_160_Intersection_of_Two_Linked_Lists/SolutionTest.java
+++ src/test/java/interviews/linkedin/_160_Intersection_of_Two_Linked_Lists/SolutionTest.java
@@ -1,26 +1,28 @@
 package interviews.linkedin._160_Intersection_of_Two_Linked_Lists;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import utils.ListNode;
 import utils.ListNodes;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
-  private Solution solution;
-
-  @BeforeEach
-  void setUp() {
-    solution = new Solution();
-  }
-
-  @Test
-  void testOnlineCase() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase(Solution solution) {
     ListNode headA = ListNodes.getListOfNodes(new int[]{1, 2, 3, 4, 5});
     ListNode headB = ListNodes.getListOfNodes(new int[]{-1, -2, -3});
     headB.next.next.next = headA.next.next;
     assertEquals(headA.next.next, solution.getIntersectionNode(headA, headB));
   }
 
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SwapSolution(),
+        new FloydCycleSolution()
+    );
+  }
 }
