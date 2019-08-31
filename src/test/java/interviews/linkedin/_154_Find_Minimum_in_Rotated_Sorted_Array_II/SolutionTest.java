c7d07da8fa7044da1ae8997ae02fa7dd74a77f40
jackalsin <jackal.sin@hotmail.com>
2019-08-31 14:39:56 -0700

    Finish 154

--- src/test/java/interviews/linkedin/_154_Find_Minimum_in_Rotated_Sorted_Array_II/SolutionTest.java
+++ src/test/java/interviews/linkedin/_154_Find_Minimum_in_Rotated_Sorted_Array_II/SolutionTest.java
@@ -55,7 +55,8 @@ class SolutionTest {
   static Stream<Solution> solutionProvider() {
     return Stream.of(
         new SolutionI(),
-        new SolutionII()
+        new SolutionII(),
+        new SolutionIII()
     );
   }
 }
