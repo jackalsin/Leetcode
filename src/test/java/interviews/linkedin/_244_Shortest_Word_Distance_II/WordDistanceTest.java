e3bb56634af269105c9cc7435dbf9b065ba5b14b
jackalsin <jackal.sin@hotmail.com>
2019-08-31 21:16:35 -0700

    Finish 244

--- src/test/java/interviews/linkedin/_244_Shortest_Word_Distance_II/WordDistanceTest.java
+++ src/test/java/interviews/linkedin/_244_Shortest_Word_Distance_II/WordDistanceTest.java
@@ -1,7 +1,9 @@
 package interviews.linkedin._244_Shortest_Word_Distance_II;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,17 +11,38 @@ import static org.junit.Assert.assertEquals;
  * @author jacka
  * @version 1.0 on 10/22/2017.
  */
-public class WordDistanceTest {
-  @Before
-  public void setUp() throws Exception {
-  }
+class WordDistanceTest {
 
-  @Test
-  public void testOnlineCase() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase(Class<WordDistance> solutionClass) throws Exception {
     final String[] words = {"practice", "makes", "perfect", "coding", "makes"};
-    final WordDistance wordDistance = new WordDistanceII(words);
+    final WordDistance wordDistance = solutionClass
+        .getConstructor(String[].class)
+        .newInstance((Object) words);
     assertEquals(3, wordDistance.shortest("coding", "practice"));
     assertEquals(1, wordDistance.shortest("coding", "makes"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Class<WordDistance> solutionClass) throws Exception {
+    final String[] words = {"a", "b"};
+    final WordDistance wordDistance = solutionClass
+        .getConstructor(String[].class)
+        .newInstance((Object) words);
+    assertEquals(1, wordDistance.shortest("a", "b"));
+  }
+
+  static Stream<Class> solutionProvider() {
+    return Stream.of(
+        WordDistanceI.class,
+        WordDistanceII.class,
+        WordDistanceIII.class,
+        WordDistanceIV.class,
+        WordDistanceV.class,
+        WordDistanceVI.class
+    );
   }
 
 }
