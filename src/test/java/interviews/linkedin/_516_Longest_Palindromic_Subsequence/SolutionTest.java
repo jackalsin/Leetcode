13eaed3fa221d06ca4828d087a5b132f802ac974
jackalsin <jackal.sin@hotmail.com>
2019-09-09 20:33:19 -0700

    Finish 516

--- src/test/java/interviews/linkedin/_516_Longest_Palindromic_Subsequence/SolutionTest.java
+++ src/test/java/interviews/linkedin/_516_Longest_Palindromic_Subsequence/SolutionTest.java
@@ -26,7 +26,8 @@ class SolutionTest {
         new SolutionI(),
         new SolutionII(),
         new SolutionIII(),
-        new SolutionIV()
+        new SolutionIV(),
+        new SolutionV()
     );
   }
 }
