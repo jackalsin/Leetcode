7928d194e81963dd10ea6e31c6f4cc7795b1857c
jackalsin <jackal.sin@hotmail.com>
2019-09-13 19:20:41 -0700

    finish 647

--- src/test/java/interviews/linkedin/_647_Palindromic_Substrings/SolutionTest.java
+++ src/test/java/interviews/linkedin/_647_Palindromic_Substrings/SolutionTest.java
@@ -23,7 +23,8 @@ class SolutionTest {
   static Stream<Solution> solutionProvider() {
     return Stream.of(
         new SolutionI(),
-        new SolutionII()
+        new SolutionII(),
+        new SolutionIII()
     );
   }
 }
