79095228234965254623fde31de2b87da923c676
jackalsin <jackal.sin@hotmail.com>
2019-10-02 19:35:47 -0700

    Finish 057

--- src/main/java/interviews/linkedin/_057_Insert_Interval/SolutionV.java
+++ src/main/java/interviews/linkedin/_057_Insert_Interval/SolutionV.java
@@ -10,21 +10,25 @@ import java.util.List;
 public final class SolutionV implements April152019Solution {
   @Override
   public int[][] insert(int[][] intervals, int[] newInterval) {
-    if (intervals == null) {
-      return new int[][]{};
+    if (intervals == null || intervals.length == 0) {
+      return new int[][]{newInterval};
     }
     final List<int[]> result = new ArrayList<>();
     int i = 0;
     while (i < intervals.length && intervals[i][1] < newInterval[0]) {
-      result.add(intervals[i++]);
+      result.add(intervals[i]);
+      i++;
     }
+
     while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-      newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-      newInterval[1] = Math.max(newInterval[1], intervals[i++][1]);
+      newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+      newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+      i++;
     }
     result.add(newInterval);
     while (i < intervals.length) {
-      result.add(intervals[i++]);
+      result.add(intervals[i]);
+      ++i;
     }
     return result.toArray(new int[][]{});
   }
