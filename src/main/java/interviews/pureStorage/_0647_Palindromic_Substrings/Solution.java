1d62963fa2b857a9a525ef1b6276af2f067c0f2c
jackalsin <jackal.sin@hotmail.com>
2019-10-07 21:27:24 -0700

    Finish 0647

---
+++ src/main/java/interviews/pureStorage/_0647_Palindromic_Substrings/Solution.java
@@ -0,0 +1,9 @@
+package interviews.pureStorage._0647_Palindromic_Substrings;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/7/2019
+ */
+public interface Solution {
+  int countSubstrings(String s);
+}
---
+++ src/test/java/interviews/pureStorage/_0647_Palindromic_Substrings/SolutionITest.java
@@ -0,0 +1,28 @@
+package interviews.pureStorage._0647_Palindromic_Substrings;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/7/2019
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final String s = "fdsklf";
+    assertEquals(6, solution.countSubstrings(s));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
