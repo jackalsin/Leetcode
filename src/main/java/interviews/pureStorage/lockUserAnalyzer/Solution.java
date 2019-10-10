58229a75cacaf49be2b48d091ce183ee31cee52c
jackalsin <jackal.sin@hotmail.com>
2019-10-09 22:34:15 -0700

    Finish lock user

---
+++ src/main/java/interviews/pureStorage/lockUserAnalyzer/Solution.java
@@ -0,0 +1,9 @@
+package interviews.pureStorage.lockUserAnalyzer;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/9/2019
+ */
+public interface Solution {
+  int isValidLockSeq(final String[] seq);
+}
