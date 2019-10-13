85c7e6e4080c44bff73dbaa354f98a95b62d9d5f
jackalsin <jackal.sin@hotmail.com>
2019-10-12 18:07:42 -0700

    finish 863

---
+++ src/main/java/_0851_0900/_863_All_Nodes_Distance_K_in_Binary_Tree/Solution.java
@@ -0,0 +1,13 @@
+package _0851_0900._863_All_Nodes_Distance_K_in_Binary_Tree;
+
+import utils.TreeNode;
+
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/11/2019
+ */
+public interface Solution {
+  List<Integer> distanceK(TreeNode root, TreeNode target, int K);
+}
