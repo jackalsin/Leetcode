ca8e1f2d2bcc8ac42e18d6c97726a4c69ef41e78
jackalsin <jackal.sin@hotmail.com>
2019-10-13 18:02:19 -0700

    Finish 560

---
+++ src/main/java/interviews/oracle/_560_Subarray_Sum_Equals_K/Solution.java
@@ -0,0 +1,9 @@
+package interviews.oracle._560_Subarray_Sum_Equals_K;
+
+/**
+ * @author jacka
+ * @version 1.0 on 12/20/2017.
+ */
+public interface Solution {
+  int subarraySum(int[] nums, int k);
+}
