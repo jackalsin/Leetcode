b50c41bdcf80c06efbf4ea6d30d9d0181bb4ca81
jackalsin <jackal.sin@hotmail.com>
2019-10-15 19:25:40 -0700

    Finish 238

---
+++ src/main/java/interviews/oracle/_238_Product_of_Array_Except_Self/Solution.java
@@ -0,0 +1,15 @@
+package interviews.oracle._238_Product_of_Array_Except_Self;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/15/2019
+ */
+public interface Solution {
+  /**
+   * TODO:
+   *
+   * @param nums
+   * @return
+   */
+  int[] productExceptSelf(int[] nums);
+}
---
+++ src/test/java/interviews/oracle/_238_Product_of_Array_Except_Self/SolutionTest.java
@@ -0,0 +1,29 @@
+package interviews.oracle._238_Product_of_Array_Except_Self;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/15/2019
+ */
+class SolutionTest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final int[] input = new int[]{1, 2, 3, 4};
+    final int[] expected = new int[]{24, 12, 8, 6};
+    assertArrayEquals(expected, solution.productExceptSelf(input));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
