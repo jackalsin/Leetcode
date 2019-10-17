d3a277535a351846f081147b6c2b15cd2d64f38e
jackalsin <jackal.sin@hotmail.com>
2019-10-16 20:53:09 -0700

    Finish 236

---
+++ src/main/java/interviews/oracle/_236_Lowest_Common_Ancestor_of_a_Binary_Tree/Solution.java
@@ -0,0 +1,12 @@
+package interviews.oracle._236_Lowest_Common_Ancestor_of_a_Binary_Tree;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/16/2019
+ */
+public interface Solution {
+  TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q);
+
+}
---
+++ src/test/java/interviews/oracle/_236_Lowest_Common_Ancestor_of_a_Binary_Tree/SolutionITest.java
@@ -0,0 +1,44 @@
+package interviews.oracle._236_Lowest_Common_Ancestor_of_a_Binary_Tree;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/16/2019
+ */
+class SolutionITest {
+  private TreeNode root = TreeNodes.getTreeLevelOrder(3, 5, 1, 6, 2, 0, 8);
+
+  @BeforeEach
+  void setUp() {
+    root.left.right.left = new TreeNode(7);
+    root.left.right.right = new TreeNode(4);
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    assertEquals(3, solution.lowestCommonAncestor(root, root.left, root.right).val);
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
+    assertEquals(5, solution.lowestCommonAncestor(root, root.left, root.left.right.right).val);
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+
+}
