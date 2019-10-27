c635c7ba8029dbe6be77067b00537c09d06101eb
jackalsin <jackal.sin@hotmail.com>
2019-10-26 17:50:29 -0700

    Finish game engine

---
+++ src/main/java/interviews/oracle/pockerGameEngine/PokerGameEngine.java
@@ -0,0 +1,10 @@
+package interviews.oracle.pockerGameEngine;
+
+/**
+ * @author jacka
+ * @version 1.0 on 10/24/2019
+ */
+public interface PokerGameEngine {
+  // TODO: 然后来了个发牌的OOD，shuffle用java自带的API就可以，面试官主要看对java的内置API的熟练度，中途说了如果已经有了API，就不需要写code自己再实现。
+  // TODO: 后系统设计能接受不同类型的扑克游戏，再问问怎么分布式，work flow，后又让写2小算法，洗牌，和，5张牌判断3张牌数字一样。BQ两三个。
+}
