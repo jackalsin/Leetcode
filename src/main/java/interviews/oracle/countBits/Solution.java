a1da6ec7d1531822c4b7a61a12205660bf431280
jackalsin <jackal.sin@hotmail.com>
2019-11-02 18:17:50 -0700

    Finish bit count

---
+++ src/main/java/interviews/oracle/countBits/Solution.java
@@ -0,0 +1,9 @@
+package interviews.oracle.countBits;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/2/2019
+ */
+public interface Solution {
+  int bitCount(int n);
+}
---
+++ src/test/java/interviews/oracle/countBits/SolutionITest.java
@@ -0,0 +1,29 @@
+package interviews.oracle.countBits;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/2/2019
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    for (int i = 0; i < 1_000_000; i++) {
+      assertEquals(Integer.bitCount(i), solution.bitCount(i));
+    }
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
