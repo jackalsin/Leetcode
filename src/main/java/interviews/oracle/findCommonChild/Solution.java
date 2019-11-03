a43f2b6a7a9c83f2be5238ccb9a662376f37c3b0
jackalsin <jackal.sin@hotmail.com>
2019-11-03 15:34:28 -0800

    Finish find common child

---
+++ src/main/java/interviews/oracle/findCommonChild/Solution.java
@@ -0,0 +1,24 @@
+package interviews.oracle.findCommonChild;
+
+import utils.TreeNode;
+
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/3/2019
+ */
+public interface Solution {
+  /**
+   * Input: (A, X), return C or B
+   * <p>
+   * Follow up 是return most RECENT common descent
+   *
+   * @param rootA
+   * @param rootB
+   * @return
+   */
+  List<Integer> getCommonChild(final TreeNode rootA, final TreeNode rootB);
+
+  int getMostRecentCommonChild(final TreeNode rootA, final TreeNode rootB);
+}
