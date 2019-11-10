4ef0544cc817f871a98f443f4033103d61ccc831
jackalsin <jackal.sin@hotmail.com>
2019-11-09 21:25:21 -0800

    Finish 005

---
+++ src/test/java/interviews/oracle/_005_Longest_Palindromic_Substring/SolutionITest.java
@@ -0,0 +1,56 @@
+package interviews.oracle._005_Longest_Palindromic_Substring;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/9/2019
+ */
+class SolutionITest {
+
+  private static final String ABCD = "abcd";
+  private static final String ABBA = "abba";
+  private static final String ACA = "aca";
+  private static final String ABCDAD = "abcdad";
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void longestPalindromeEndOddAnswer(Solution solution) {
+    assertEquals("dad", solution.longestPalindrome(ABCDAD));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void longestPalindromeFullOddAnswer(Solution solution) {
+    assertEquals("aca", solution.longestPalindrome(ACA));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void longestPalindromeFullEvenAnswer(Solution solution) {
+    assertEquals("abba", solution.longestPalindrome(ABBA));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void longestPalindromeSingleCharAnswer(Solution solution) {
+    Set<String> ans = new HashSet<>(Arrays.asList("a", "b", "c", "d"));
+    String expect = solution.longestPalindrome(ABCD);
+    assertTrue(ans.contains(expect));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
