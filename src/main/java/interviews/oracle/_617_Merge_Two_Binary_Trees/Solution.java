bd64bab80e2c34bdc0cdb2431cc89cc07e969b45
jackalsin <jackal.sin@hotmail.com>
2019-11-24 11:27:01 -0800

    Finish 617

---
+++ src/main/java/interviews/oracle/_617_Merge_Two_Binary_Trees/Solution.java
@@ -0,0 +1,11 @@
+package interviews.oracle._617_Merge_Two_Binary_Trees;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/24/2019
+ */
+public interface Solution {
+  TreeNode mergeTrees(TreeNode t1, TreeNode t2);
+}
---
+++ src/main/java/interviews/oracle/_617_Merge_Two_Binary_Trees/SolutionI.java
@@ -0,0 +1,24 @@
+package interviews.oracle._617_Merge_Two_Binary_Trees;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/24/2019
+ */
+public final class SolutionI implements Solution {
+  @Override
+  public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+    if (t1 == null && t2 == null) {
+      return null;
+    } else if (t1 == null) {
+      return t2;
+    } else if (t2 == null) {
+      return t1;
+    }
+    final TreeNode root = new TreeNode(t1.val + t2.val);
+    root.left = mergeTrees(t1.left, t2.left);
+    root.right = mergeTrees(t1.right, t2.right);
+    return root;
+  }
+}
