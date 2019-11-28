8cbc5c1c5e41130c4eed443e6a8d9a954c3e23a6
jackalsin <jackal.sin@hotmail.com>
2019-11-27 20:30:58 -0800

    Finish 011

---
+++ src/main/java/interviews/oracle/flowFree/Solution.java
@@ -0,0 +1,9 @@
+package interviews.oracle.flowFree;
+
+/**
+ * @author jacka
+ * @version 1.0 on 11/10/2019
+ */
+public interface Solution {
+  char[][] solve(final int size, final int[] p1, final int[]... p);
+}
--- src/test/java/interviews/oracle/_011_Container_With_Most_Water/SolutionITest.java
+++ src/test/java/interviews/oracle/_011_Container_With_Most_Water/SolutionITest.java
@@ -36,7 +36,8 @@ class SolutionITest {
 
   static Stream<Solution> solutionProvider() {
     return Stream.of(
-        new SolutionI()
+        new SolutionI(),
+        new SolutionII()
     );
   }
 }
