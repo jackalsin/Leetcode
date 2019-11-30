71928f4e8c8a50050a12f50e587cd2b71cfef527
jackalsin <jackal.sin@hotmail.com>
2019-11-30 15:27:01 -0800

    Finish 417

---
+++ src/main/java/interviews/oracle/_417_Pacific_Atlantic_Water_Flow/DfsSolution.java
@@ -0,0 +1,59 @@
+package interviews.oracle._417_Pacific_Atlantic_Water_Flow;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author zhixi
+ * @version 1.0 on 11/30/2019
+ */
+public final class DfsSolution implements Solution {
+  private static final int[][] DIRS = {
+      {0, 1}, {0, -1}, {1, 0}, {-1, 0}
+  };
+
+  public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+    final List<List<Integer>> result = new ArrayList<>();
+    if (matrix == null || matrix.length == 0) {
+      return result;
+    }
+    final int rows = matrix.length, cols = matrix[0].length;
+    final boolean[][] pacific = new boolean[rows][cols],
+        atlantic = new boolean[rows][cols];
+    for (int row = 0; row < rows; ++row) {
+      pacific[row][0] = true;
+      dfs(matrix, pacific, row, 0);
+      atlantic[row][cols - 1] = true;
+      dfs(matrix, atlantic, row, cols - 1);
+    }
+    for (int col = 0; col < cols; ++col) {
+      pacific[0][col] = true;
+      dfs(matrix, pacific, 0, col);
+      atlantic[rows - 1][col] = true;
+      dfs(matrix, atlantic, rows - 1, col);
+    }
+//    display(pacific);
+//    display(atlantic);
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
+        if (pacific[i][j] && atlantic[i][j]) {
+          result.add(Arrays.asList(i, j));
+        }
+      }
+    }
+    return result;
+  }
+
+  private static void dfs(final int[][] matrix, final boolean[][] visited, final int row, final int col) {
+    visited[row][col] = true;
+    final int curHeight = matrix[row][col], rows = matrix.length, cols = matrix[0].length;
+    for (final int[] dir : DIRS) {
+      final int nextRow = dir[0] + row, nextCol = dir[1] + col;
+      if (nextRow < 0 || nextCol < 0 || nextRow >= rows || nextCol >= cols || visited[nextRow][nextCol] || curHeight > matrix[nextRow][nextCol]) {
+        continue;
+      }
+      dfs(matrix, visited, nextRow, nextCol);
+    }
+  }
+}
--- src/test/java/interviews/oracle/_417_Pacific_Atlantic_Water_Flow/SolutionTest.java
+++ src/test/java/interviews/oracle/_417_Pacific_Atlantic_Water_Flow/SolutionTest.java
@@ -13,12 +13,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @author jacka
  * @version 1.0 on 11/29/2019
  */
-class BfsSolutionTest {
+class SolutionTest {
 
   @ParameterizedTest
   @MethodSource("solutionProvider")
   public void testOnlineCase1(Solution solution) {
-    final int[][] matrix = {{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}};
+    final int[][] matrix = {
+        {1, 2, 2, 3, 5},
+        {3, 2, 3, 4, 4},
+        {2, 4, 5, 3, 1},
+        {6, 7, 1, 4, 5},
+        {5, 1, 1, 2, 4}
+    };
     final List<List<Integer>> expected = List.of(
         List.of(0, 4), List.of(1, 3), List.of(1, 4), List.of(2, 2), List.of(3, 0), List.of(3, 1),
         List.of(4, 0));
@@ -41,7 +47,8 @@ class BfsSolutionTest {
 
   static Stream<Solution> solutionProvider() {
     return Stream.of(
-        new BfsSolution()
+        new BfsSolution(),
+        new DfsSolution()
     );
   }
 }
