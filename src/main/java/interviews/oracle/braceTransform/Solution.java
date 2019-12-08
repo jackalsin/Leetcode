c92825685dec6f39889d28e6c50c8b23f56b795c
jackalsin <jackal.sin@hotmail.com>
2019-12-07 21:06:56 -0800

    Finish brace transform

---
+++ src/main/java/interviews/oracle/braceTransform/Solution.java
@@ -0,0 +1,14 @@
+package interviews.oracle.braceTransform;
+
+/**
+ * @author jacka
+ * @version 1.0 on 12/7/2019
+ */
+public interface Solution {
+  /**
+   * )21(21)a1( => -121021a11
+   * matched => 0
+   * ) => -1, ( => 1
+   */
+  String transform(final String input);
+}
---
+++ src/main/java/interviews/oracle/braceTransform/SolutionI.java
@@ -0,0 +1,52 @@
+package interviews.oracle.braceTransform;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author jacka
+ * @version 1.0 on 12/7/2019
+ */
+public final class SolutionI implements Solution {
+  @Override
+  public String transform(String input) {
+    if (input == null || input.isEmpty()) {
+      return input;
+    }
+    final char[] chars = input.toCharArray();
+    final StringBuilder sb = new StringBuilder();
+    final Deque<Integer> stack = new ArrayDeque<>();
+    for (int i = 0; i < chars.length; ++i) {
+      final char chr = chars[i];
+      if (chr == '(') {
+        stack.push(i);
+      } else if (chr == ')') {
+        if (stack.isEmpty()) {
+          stack.push(i);
+        } else {
+          if (chars[stack.peek()] == '(') { // match
+            sb.setCharAt(stack.pop(), '0');
+            continue;
+          } else {
+            stack.push(i);
+          }
+        }
+      }
+      sb.append(chr);
+    }
+
+    final StringBuilder res = new StringBuilder();
+    for (int i = 0; i < sb.length(); ++i) {
+      final char chr = sb.charAt(i);
+      if (chr == '(') {
+        res.append("1");
+      } else if (chr == ')') {
+        res.append("-1");
+      } else {
+        res.append(chr);
+      }
+    }
+
+    return res.toString();
+  }
+}
