5c5b41413951b25cab645bd904edcb8c883fe68e
jackalsin <jackal.sin@hotmail.com>
2019-12-14 22:36:36 -0800

    Finish 814

---
+++ src/main/java/interviews/hulu/_814_Binary_Tree_Pruning/Solution.java
@@ -0,0 +1,11 @@
+package interviews.hulu._814_Binary_Tree_Pruning;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 12/14/2019
+ */
+public interface Solution {
+  TreeNode pruneTree(TreeNode root);
+}
---
+++ src/test/java/_0801_0850/_814_Binary_Tree_Pruning/SolutionITest.java
@@ -0,0 +1,45 @@
+package _0801_0850._814_Binary_Tree_Pruning;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 12/14/2019
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final TreeNode root = new TreeNode(1);
+    root.right = TreeNodes.getTreeLevelOrder(0, 0, 1);
+    final TreeNode expected = new TreeNode(1);
+    expected.right = new TreeNode(0);
+    expected.right.right = new TreeNode(1);
+    assertEquals(expected, solution.pruneTree(root));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
+    final TreeNode root = new TreeNode(1);
+    root.right = TreeNodes.getTreeLevelOrder(0, 0, 1);
+    final TreeNode expected = new TreeNode(1);
+    expected.right = new TreeNode(0);
+    expected.right.right = new TreeNode(1);
+    assertEquals(expected, solution.pruneTree(root));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
