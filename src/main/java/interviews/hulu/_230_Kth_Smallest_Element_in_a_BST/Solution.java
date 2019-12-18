95db6be3e7423ab3a7ba5efad297db9eb7cd77ec
jackalsin <jackal.sin@hotmail.com>
2019-12-17 20:03:55 -0800

    Finish 230

---
+++ src/main/java/interviews/hulu/_230_Kth_Smallest_Element_in_a_BST/Solution.java
@@ -0,0 +1,11 @@
+package interviews.hulu._230_Kth_Smallest_Element_in_a_BST;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 12/17/2019
+ */
+public interface Solution {
+  int kthSmallest(TreeNode root, int k);
+}
