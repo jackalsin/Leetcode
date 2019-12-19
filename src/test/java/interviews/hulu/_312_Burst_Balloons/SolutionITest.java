deb48e79684b30e37fe698997c96177a57351031
jackalsin <jackal.sin@hotmail.com>
2019-12-18 20:43:44 -0800

    Finish 312

--- src/test/java/interviews/hulu/_312_Burst_Balloons/SolutionITest.java
+++ src/test/java/interviews/hulu/_312_Burst_Balloons/SolutionITest.java
@@ -1,4 +1,4 @@
-package _0301_0350._312_Burst_Balloons;
+package interviews.hulu._312_Burst_Balloons;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,19 +9,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author jacka
- * @version 1.0 on 9/18/2017.
+ * @version 1.0 on 12/18/2019
  */
-public class SolutionTest {
-  static Stream<Solution> solutionProvider() {
-    return Stream.of(
-        new Solution()
-    );
-  }
+class SolutionITest {
 
   @ParameterizedTest
   @MethodSource("solutionProvider")
   public void onlineCase(Solution solution) throws Exception {
     final int[] input = {3, 1, 5, 8};
     assertEquals(167, solution.maxCoins(input));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
   }
 }
