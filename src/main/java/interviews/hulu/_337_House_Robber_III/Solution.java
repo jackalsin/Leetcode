93f502313e00073825a4f6c34f72f70ec599a862
jackalsin <jackal.sin@hotmail.com>
2019-12-18 22:28:09 -0800

    Finish 337

---
+++ src/main/java/interviews/hulu/_337_House_Robber_III/Solution.java
@@ -0,0 +1,11 @@
+package interviews.hulu._337_House_Robber_III;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 12/18/2019
+ */
+public interface Solution {
+  int rob(TreeNode root);
+}
--- src/test/java/interviews/hulu/_337_House_Robber_III/SolutionITest.java
+++ src/test/java/interviews/hulu/_337_House_Robber_III/SolutionITest.java
@@ -1,4 +1,4 @@
-package _0301_0350._337_House_Robber_III;
+package interviews.hulu._337_House_Robber_III;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,13 +11,9 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * @author jacka
- * @version 1.0 on 10/5/2017.
+ * @version 1.0 on 12/18/2019
  */
-public class SolutionTest {
-  static Stream<Solution> solutionProvider() {
-    return Stream.of(new Solution());
-  }
-
+class SolutionITest {
   @ParameterizedTest
   @MethodSource("solutionProvider")
   public void testOnlineCase(Solution solution) throws Exception {
@@ -30,5 +26,11 @@ public class SolutionTest {
   public void testOnlineCase2(Solution solution) throws Exception {
     TreeNode root = TreeNodes.getTreeLevelOrder(3, 4, 5, 1, 3, null, 1);
     assertEquals(9, solution.rob(root));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
   }
 }
