3e8c8df3463e65b93be822b03d3abcb004fbaba5
jackalsin <jackal.sin@hotmail.com>
2020-01-06 19:58:48 -0800

    Finish 218

--- src/test/java/interviews/uber/_218_The_Skyline_Problem/SolutionITest.java
+++ src/test/java/interviews/uber/_218_The_Skyline_Problem/SolutionITest.java
@@ -1,42 +1,40 @@
-package _0201_0250._218_The_Skyline_Problem;
+package interviews.uber._218_The_Skyline_Problem;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * @author jacka
- * @version 1.0 on 8/15/2017.
+ * @version 1.0 on 1/6/2020
  */
-public class SolutionTest {
+class SolutionITest {
 
   @ParameterizedTest
   @MethodSource("solutionProvider")
   public void testFailedCase(Solution solution) {
     final int[][] input = new int[][]{{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}};
-    final List<int[]> expected = Arrays.asList(new int[]{2, 10}, new int[]{3, 15}, new int[]{7, 12},
-        new int[]{12, 0}, new int[]{15, 10}, new int[]{20, 8}, new int[]{24, 0});
-    solution.getSkyline(input);
+    final List<List<Integer>> expected = List.of(List.of(2, 10), List.of(3, 15), List.of(7, 12),
+        List.of(12, 0), List.of(15, 10), List.of(20, 8), List.of(24, 0)), actual = solution.getSkyline(input);
+    assertEquals(expected, actual);
   }
 
   @ParameterizedTest
   @MethodSource("solutionProvider")
   public void testFailedTestCase(Solution solution) {
     final int[][] input = new int[][]{{0, 3, 3}, {1, 5, 3}, {2, 4, 3}, {3, 7, 3}};
-    final List<int[]> expected = Arrays.asList(
-        new int[]{0, 3}, new int[]{7, 0});
-    final List<int[]> actual = solution.getSkyline(input);
-    for (int[] child : actual) {
-      System.out.println(Arrays.toString(child));
-    }
+    final List<List<Integer>> expected = List.of(
+        List.of(0, 3), List.of(7, 0)), actual = solution.getSkyline(input);
+    assertEquals(expected, actual);
   }
 
   static Stream<Solution> solutionProvider() {
     return Stream.of(
-        new Solution()
+        new SolutionI()
     );
   }
 }
