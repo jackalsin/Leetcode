0f0d52ec6703a43e91b32ffda7bbdf49856387bd
jackalsin <jackal.sin@hotmail.com>
2020-01-07 22:29:56 -0800

    Finish 332

--- src/main/java/_0301_0350/_332_Reconstruct_Itinerary/Solution.java
+++ src/main/java/_0301_0350/_332_Reconstruct_Itinerary/Solution.java
@@ -1,13 +1,21 @@
 package _0301_0350._332_Reconstruct_Itinerary;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * @author jacka
  * @version 1.0 on 10/1/2017.
  */
 public class Solution {
-
+  // TODO:
+  // 考虑到每条边走一次的解法一定存在，那么就可以apply
+  // https://en.wikipedia.org/wiki/Eulerian_path#Hierholzer's_algorithm
+  // 此时要么是所有点degree和为0，要么顶多两个点是奇数
   public List<String> findItinerary(String[][] tickets) {
     Map<String, Queue<String>> fromToMapping = new HashMap<>();
     for (String[] it : tickets) {
--- src/test/java/_0301_0350/_332_Reconstruct_Itinerary/SolutionTest.java
+++ src/test/java/_0301_0350/_332_Reconstruct_Itinerary/SolutionTest.java
@@ -1,38 +1,41 @@
 package _0301_0350._332_Reconstruct_Itinerary;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author jacka
  * @version 1.0 on 10/4/2017.
  */
 public class SolutionTest {
-  private Solution solution;
 
-  @Before
-  public void setUp() throws Exception {
-    solution = new Solution();
-  }
-
-  @Test
-  public void testOnlineCase1() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase1(Solution solution) {
     final String[][] tickets = {{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"}};
     final List<String> expected = List.of("JFK", "MUC", "LHR", "SFO", "SJC");
     assertEquals(expected, solution.findItinerary(tickets));
   }
 
 
-  @Test
-  public void testOnlineCase2() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase2(Solution solution) {
     final String[][] tickets = {{"JFK", "SFO"}, {"JFK", "ATL"}, {"SFO", "ATL"},
         {"ATL", "JFK"}, {"ATL", "SFO"}};
     final List<String> expected = List.of("JFK", "ATL", "JFK", "SFO", "ATL", "SFO");
     assertEquals(expected, solution.findItinerary(tickets));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new Solution()
+    );
   }
 
 }
