4e5746a5b947102f2d9b0e57a257755b3d4d3f4d
jackalsin <jackal.sin@hotmail.com>
2020-02-08 11:21:42 -0800

    Add 449

--- src/main/java/utils/TreeNode.java
+++ src/main/java/utils/TreeNode.java
@@ -41,8 +41,8 @@ public final class TreeNode {
   public String toString() {
     return "TreeNode{" +
         "val=" + val +
-//        ", left=" + left +
-//        ", right=" + right +
+        ", left=" + left +
+        ", right=" + right +
         '}';
   }
 }
---
+++ src/test/java/interviews/hulu/_449_Serialize_and_Deserialize_BST/CodecITest.java
@@ -0,0 +1,64 @@
+package interviews.hulu._449_Serialize_and_Deserialize_BST;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/8/2020
+ */
+class CodecITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Class<Codec> solutionClass) throws NoSuchMethodException, IllegalAccessException,
+      InvocationTargetException, InstantiationException {
+    Codec solution = solutionClass.getConstructor().newInstance();
+    final TreeNode root = TreeNodes.getTreeLevelOrder(2, 1, 3);
+    final TreeNode actual = solution.deserialize(solution.serialize(root));
+    assertEquals(root, actual);
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Class<Codec> solutionClass) throws NoSuchMethodException, IllegalAccessException,
+      InvocationTargetException, InstantiationException {
+    Codec solution = solutionClass.getConstructor().newInstance();
+    final TreeNode root = TreeNodes.getTreeLevelOrder(2, null, 3);
+    final TreeNode actual = solution.deserialize(solution.serialize(root));
+    assertEquals(root, actual);
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase3(Class<Codec> solutionClass) throws NoSuchMethodException, IllegalAccessException,
+      InvocationTargetException, InstantiationException {
+    Codec solution = solutionClass.getConstructor().newInstance();
+    final TreeNode root = TreeNodes.getTreeLevelOrder(2, 1);
+    final TreeNode actual = solution.deserialize(solution.serialize(root));
+    assertEquals(root, actual);
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testFailedCase1(Class<Codec> solutionClass) throws NoSuchMethodException, IllegalAccessException,
+      InvocationTargetException, InstantiationException {
+    Codec solution = solutionClass.getConstructor().newInstance();
+    final TreeNode root = null;
+    final TreeNode actual = solution.deserialize(solution.serialize(root));
+    assertEquals(root, actual);
+  }
+
+  static Stream<Class> solutionProvider() {
+    return Stream.of(
+        CodecI.class
+    );
+  }
+}
