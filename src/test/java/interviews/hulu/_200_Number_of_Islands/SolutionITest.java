643620b55a7151c77d9162f466904222c606a131
jackalsin <jackal.sin@hotmail.com>
2020-02-08 11:43:23 -0800

    Add 200

--- src/test/java/interviews/hulu/_200_Number_of_Islands/SolutionITest.java
+++ src/test/java/interviews/hulu/_200_Number_of_Islands/SolutionITest.java
@@ -47,7 +47,8 @@ class SolutionITest {
 
   static Stream<Solution> solutionProvider() {
     return Stream.of(
-        new SolutionI()
+        new SolutionI(),
+        new SolutionII()
     );
   }
 }
