548a7c475ede903e3a5c78e37541cc84f27d5900
jackalsin <jackal.sin@hotmail.com>
2020-02-09 23:53:12 -0800

    Add max Ratio

---
+++ src/main/java/interviews/hulu/maxRatio/SolutionI.java
@@ -0,0 +1,37 @@
+package interviews.hulu.maxRatio;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/8/2020
+ */
+public final class SolutionI implements Solution {
+  @Override
+  public double maxRatio(final double[][] points) {
+    final Map<Double, Double[]> xToMinMax = new HashMap<>();
+    for (final double[] p : points) {
+      xToMinMax.putIfAbsent(p[0], new Double[]{Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY});
+      final Double[] update = xToMinMax.get(p[0]);
+      update[0] = Math.min(update[0], p[1]);
+      update[1] = Math.max(update[1], p[1]);
+    }
+    final List<Map.Entry<Double, Double[]>> allPoints = new ArrayList<>(xToMinMax.entrySet());
+    allPoints.sort((x, y) -> Double.compare(x.getKey(), y.getKey()));
+    Map.Entry<Double, Double[]> prev = null;
+    double max = Double.NEGATIVE_INFINITY;
+    for (final Map.Entry<Double, Double[]> e : xToMinMax.entrySet()) {
+      if (prev != null) {
+        final double dx = e.getKey() - prev.getKey(),
+            dy = e.getValue()[1] - prev.getValue()[0],
+            ratio = dy / dx;
+        max = Math.max(max, ratio);
+      }
+      prev = e;
+    }
+    return max;
+  }
+}
---
+++ src/test/java/interviews/hulu/maxRatio/SolutionITest.java
@@ -0,0 +1,32 @@
+package interviews.hulu.maxRatio;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/9/2020
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final double[][] input = {
+        {-1, -1},
+        {-2, -5},
+        {4, 5}
+    };
+    assertEquals(4d, solution.maxRatio(input), 1E-6);
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
