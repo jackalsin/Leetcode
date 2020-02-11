282ec001c7fd4f3e3d0384e8900ca912a4807980
jackalsin <jackal.sin@hotmail.com>
2020-02-11 09:20:38 -0800

    Add 122

---
+++ src/main/java/interviews/hulu/_122_Best_Time_to_Buy_and_Sell_Stock_II/Solution.java
@@ -0,0 +1,11 @@
+package interviews.hulu._122_Best_Time_to_Buy_and_Sell_Stock_II;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/11/2020
+ */
+public interface Solution {
+
+  int maxProfit(int[] prices);
+
+}
