2d71bebdd03e6de7b1e68a63d29b51237b6b4115
jackalsin <jackal.sin@hotmail.com>
2020-02-11 01:22:19 -0800

    Add 121

--- src/test/java/_0101_0150/_121_Best_Time_to_Buy_and_Sell_Stock/SolutionTest.java
+++ src/test/java/_0101_0150/_121_Best_Time_to_Buy_and_Sell_Stock/SolutionTest.java
@@ -1,7 +1,9 @@
 package _0101_0150._121_Best_Time_to_Buy_and_Sell_Stock;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,20 +12,23 @@ import static org.junit.Assert.assertEquals;
  * @version 1.0 on 6/24/2017.
  */
 public class SolutionTest {
-  private Solution solution;
-  @Before
-  public void setUp() throws Exception {
-    solution = new Solution();
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new Solution()
+    );
   }
 
-  @Test
-  public void testOnlineCase() throws Exception {
-    final int[] prices = new int[] {7, 1, 5, 3, 6, 4};
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase(Solution solution) {
+    final int[] prices = new int[]{7, 1, 5, 3, 6, 4};
     assertEquals(5, solution.maxProfit(prices));
   }
-  @Test
-  public void testOnlineCaseZeroProfit() throws Exception {
-    final int[] prices = new int[] {7, 6, 5, 4, 3, 1};
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCaseZeroProfit(Solution solution) {
+    final int[] prices = new int[]{7, 6, 5, 4, 3, 1};
     assertEquals(0, solution.maxProfit(prices));
   }
 
