ac77719e55b78a47b46725404943a07adad8f7c2
jackalsin <jackal.sin@hotmail.com>
2020-02-11 22:11:18 -0800

    Add 207

---
+++ src/main/java/interviews/hulu/_207_Course_Schedule/Solution.java
@@ -0,0 +1,9 @@
+package interviews.hulu._207_Course_Schedule;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/11/2020
+ */
+public interface Solution {
+  boolean canFinish(int numCourses, int[][] prerequisites);
+}
---
+++ src/test/java/interviews/hulu/_207_Course_Schedule/SolutionITest.java
@@ -0,0 +1,34 @@
+package interviews.hulu._207_Course_Schedule;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/11/2020
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testPossible(Solution solution) {
+    assertTrue(solution.canFinish(2, new int[][]{{1, 0}}));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testImpossible(Solution solution) {
+    assertFalse(solution.canFinish(2, new int[][]{{1, 0}, {0, 1}}));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
