e81e3281de3b9d19da89b1b31757dd46857f63a1
jackalsin <jackal.sin@hotmail.com>
2020-02-13 22:41:55 -0800

    Add 121

---
+++ src/main/java/interviews/tableau/_121_Best_Time_to_Buy_and_Sell_Stock/Solution.java
@@ -0,0 +1,9 @@
+package interviews.tableau._121_Best_Time_to_Buy_and_Sell_Stock;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/13/2020
+ */
+public interface Solution {
+  int maxProfit(int[] prices);
+}
