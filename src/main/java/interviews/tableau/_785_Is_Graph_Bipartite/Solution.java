682d74f9f4990b757fd16057655da18844e73a9a
jackalsin <jackal.sin@hotmail.com>
2020-02-17 15:05:29 -0800

    Add 785

---
+++ src/main/java/interviews/tableau/_785_Is_Graph_Bipartite/Solution.java
@@ -0,0 +1,9 @@
+package interviews.tableau._785_Is_Graph_Bipartite;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/17/2020
+ */
+public interface Solution {
+   boolean isBipartite(int[][] graph);
+}
