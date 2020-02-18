d4727df7588d15dfea5c7e4e676563414df7238a
jackalsin <jackal.sin@hotmail.com>
2020-02-17 21:44:47 -0800

    Add removeUnique

---
+++ src/test/java/interviews/tableau/removeNotDuplicated/SolutionITest.java
@@ -0,0 +1,41 @@
+package interviews.tableau.removeNotDuplicated;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/17/2020
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final int[] input = {1, 1, 2, 3, 4, 5};
+    final int[] expected = {1, 1, 2, 3, 4, 5};
+    assertEquals(2, solution.removeNotDuplicated(input));
+    assertArrayEquals(expected, input);
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
+    final int[] input = {2, 3, 4, 5, 1, 1};
+    final int[] expected = {1, 1, 2, 3, 4, 5};
+    assertEquals(2, solution.removeNotDuplicated(input));
+    assertEquals(1, input[0]);
+    assertEquals(1, input[1]);
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
