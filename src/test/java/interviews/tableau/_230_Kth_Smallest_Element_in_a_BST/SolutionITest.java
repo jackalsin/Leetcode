f8afc2de625128d0b5907bdaebb0bd23b8c97a87
jackalsin <jackal.sin@hotmail.com>
2020-02-19 22:38:39 -0800

    Add 230

---
+++ src/test/java/interviews/tableau/_230_Kth_Smallest_Element_in_a_BST/SolutionITest.java
@@ -0,0 +1,39 @@
+package interviews.tableau._230_Kth_Smallest_Element_in_a_BST;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import utils.TreeNode;
+import utils.TreeNodes;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/19/2020
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOne(Solution solution) {
+    TreeNode root = TreeNodes.getTreeLevelOrder(1);
+    assertEquals(1, solution.kthSmallest(root, 1));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void test4Level(Solution solution) {
+    TreeNode root = TreeNodes.getTreeLevelOrder(4, 2, 6, 1, 3, 5, 7);
+    for (int i = 1; i <= 7; i++) {
+      assertEquals("k = " + i + " pass", i, solution.kthSmallest(root, i));
+    }
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
