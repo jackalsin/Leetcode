26505f0d882478758851b8efbf223efb299d76dc
jackalsin <jackal.sin@hotmail.com>
2020-02-23 18:23:17 -0800

    Add 236

--- src/test/java/interviews/hulu/_236_Lowest_Common_Ancestor_of_a_Binary_Tree/SolutionITest.java
+++ src/test/java/interviews/hulu/_236_Lowest_Common_Ancestor_of_a_Binary_Tree/SolutionITest.java
@@ -58,7 +58,8 @@ class SolutionITest {
 
   static Stream<Solution> solutionProvider() {
     return Stream.of(
-        new SolutionI()
+        new SolutionI(),
+        new SolutionII()
     );
   }
 }
