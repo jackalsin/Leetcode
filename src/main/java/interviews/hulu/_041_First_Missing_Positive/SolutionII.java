1c6017fff94cffd8f909019d2bbcb3fbe23dfaab
jackalsin <jackal.sin@hotmail.com>
2020-02-28 10:20:56 -0800

    add 1063

---
+++ src/main/java/interviews/hulu/_041_First_Missing_Positive/SolutionII.java
@@ -0,0 +1,31 @@
+package interviews.hulu._041_First_Missing_Positive;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/28/2020
+ */
+public final class SolutionII implements Solution {
+  @Override
+  public int firstMissingPositive(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    for (int i = 0; i < nums.length; ++i) {
+      while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != i + 1) {
+        swap(nums, i, nums[i] - 1);
+      }
+    }
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != i + 1) {
+        return i + 1;
+      }
+    }
+    return nums.length + 1;
+  }
+
+  private static void swap(final int[] nums, final int i, final int j) {
+    final int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  }
+}
---
+++ src/main/java/interviews/hulu/_1063_Number_of_Valid_Subarrays/SolutionII.java
@@ -0,0 +1,28 @@
+package interviews.hulu._1063_Number_of_Valid_Subarrays;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author jacka
+ * @version 1.0 on 2/28/2020
+ */
+public final class SolutionII implements Solution {
+  @Override
+  public int validSubarrays(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    final Deque<Integer> stack = new ArrayDeque<>();
+    int count = 0;
+    for (int num : nums) {
+      while (!stack.isEmpty() && stack.peek() > num) {
+        stack.pop();
+      }
+      stack.push(num);
+      count += stack.size();
+//      System.out.println("stack = " + stack + ", count = " + count);
+    }
+    return count;
+  }
+}
