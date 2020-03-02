d3e2e6d0f906f988ba777bf3e78d023351251ea1
jackalsin <jackal.sin@hotmail.com>
2020-03-01 17:30:11 -0800

    Add 589

---
+++ src/main/java/_0551_0600/_589_N_ary_Tree_Preorder_Traversal/Solution.java
@@ -0,0 +1,14 @@
+package _0551_0600._589_N_ary_Tree_Preorder_Traversal;
+
+
+import utils.treeNode.Node;
+
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/1/2020
+ */
+public interface Solution {
+  List<Integer> preorder(Node root);
+}
