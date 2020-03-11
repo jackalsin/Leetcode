5a6be8dace733c3e67d83f39997127f86d39b66d
jackalsin <jackal.sin@hotmail.com>
2020-03-10 19:09:34 -0700

    add 663

---
+++ src/main/java/_0651_0700/_663_Equal_Tree_Partition/SolutionI.java
@@ -0,0 +1,36 @@
+package _0651_0700._663_Equal_Tree_Partition;
+
+import utils.TreeNode;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/9/2020
+ */
+public final class SolutionI implements Solution {
+  private final Set<Integer> cache = new HashSet<>();
+
+  public boolean checkEqualTree(TreeNode root) {
+    final int left = checkEqualTreeHelper(root.left),
+        right = checkEqualTreeHelper(root.right),
+        sum = left + right + root.val;
+    if (sum % 2 != 0) {
+      return false;
+    }
+    return cache.contains(sum / 2);
+  }
+
+  private int checkEqualTreeHelper(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    final int left = checkEqualTreeHelper(root.left),
+        right = checkEqualTreeHelper(root.right),
+        sum = left + right + root.val;
+    cache.add(sum);
+    return sum;
+  }
+}
