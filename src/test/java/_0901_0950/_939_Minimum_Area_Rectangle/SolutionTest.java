a210dd46d29b8f9fb97ae5fb3e1861df60ec346a
jackalsin <jackal.sin@hotmail.com>
2020-03-22 16:59:31 -0700

    Add 939

---
+++ src/test/java/_0901_0950/_939_Minimum_Area_Rectangle/SolutionTest.java
@@ -0,0 +1,35 @@
+package _0901_0950._939_Minimum_Area_Rectangle;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new DiagonalSolution(),
+        new OptimalSolution()
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final int[][] input = {
+        {1, 1}, {1, 3}, {3, 1}, {3, 3}, {2, 2}
+    };
+    assertEquals(4, solution.minAreaRect(input));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
+    final int[][] input = {
+        {1, 1}, {1, 3}, {3, 1}, {3, 3}, {4, 1}, {4, 3}
+    };
+    assertEquals(2, solution.minAreaRect(input));
+  }
+}
