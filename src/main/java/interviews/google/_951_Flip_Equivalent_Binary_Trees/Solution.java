32370c709e5e3f43f6e927c29cc9e461a5ddbe00
jackalsin <jackal.sin@hotmail.com>
2020-03-26 19:56:59 -0700

    Add 951

---
+++ src/main/java/interviews/google/_951_Flip_Equivalent_Binary_Trees/Solution.java
@@ -0,0 +1,11 @@
+package interviews.google._951_Flip_Equivalent_Binary_Trees;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/26/2020
+ */
+public interface Solution {
+  boolean flipEquiv(TreeNode root1, TreeNode root2);
+}
