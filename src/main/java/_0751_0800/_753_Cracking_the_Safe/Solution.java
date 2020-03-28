374f192bead0de521e8424d30d7b739b6047723c
jackalsin <jackal.sin@hotmail.com>
2020-03-27 21:06:18 -0700

    Add 753

--- src/main/java/_0751_0800/_753_Cracking_the_Safe/Solution.java
+++ src/main/java/_0751_0800/_753_Cracking_the_Safe/Solution.java
@@ -9,4 +9,11 @@ public interface Solution {
    * @param k k choice
    */
   String crackSafe(int n, int k);
+  /*
+  此题解法似乎容易想到，根据题意，最后n位，k个candidate
+  假设所有的combination 为一个vertex，那么对于每个vertex有
+  1）k indegree 头n - 1个相同，
+  2) k outdegree 最后n - 1个相同
+  那么也就是必定存在一个欧拉路径，那直接暴力找，找到就是
+  */
 }
---
+++ src/main/java/interviews/google/_753_Cracking_the_Safe/SolutionI.java
@@ -0,0 +1,59 @@
+package interviews.google._753_Cracking_the_Safe;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/27/2020
+ */
+public final class SolutionI implements Solution {
+  private final Set<Integer> visited = new HashSet<>();
+
+  /**
+   * @param n n digits
+   * @param k k candidates
+   * @return
+   */
+  public String crackSafe(int n, int k) {
+    final int times = getTimes(n);
+    final int goalSize = (int) Math.pow(k, n);
+    final int start = 0;
+    final StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < n; ++i) {
+      sb.append('0');
+    }
+    visited.add(start);
+    dfs(sb, goalSize, times, k, start, 1);
+    return sb.toString();
+  }
+
+  private boolean dfs(final StringBuilder sb, final int goalSize, final int times, final int K, final int cur,
+                      final int count) {
+    if (count == goalSize) {
+      return true;
+    }
+    final int len = sb.length();
+    for (int i = 0; i < K; ++i) {
+      final int next = cur * 10 % times + i;
+      if (visited.contains(next)) continue;
+      sb.append((char) ('0' + i));
+      visited.add(next);
+      if (dfs(sb, goalSize, times, K, next, count + 1)) {
+//        sb.setLength(len);
+        return true;
+      }
+      visited.remove(next);
+      sb.setLength(len);
+    }
+    return false;
+  }
+
+  private static int getTimes(final int n) {
+    int res = 1;
+    for (int i = 0; i < n; ++i) {
+      res *= 10;
+    }
+    return res;
+  }
+}
