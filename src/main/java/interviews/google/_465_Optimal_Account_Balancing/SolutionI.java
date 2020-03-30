0cdb14b55b4ded48127e518985a7933ce1380ed2
jackalsin <jackal.sin@hotmail.com>
2020-03-29 21:35:10 -0700

    Add 465

---
+++ src/main/java/interviews/google/_465_Optimal_Account_Balancing/SolutionI.java
@@ -0,0 +1,55 @@
+package interviews.google._465_Optimal_Account_Balancing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author jacka
+ * @version 1.0 on 1/15/2018.
+ */
+public final class SolutionI implements Solution {
+  private final Map<List<Long>, Integer> cache = new HashMap<>();
+
+  public int minTransfers(int[][] transactions) {
+    final Map<Integer, Integer> idToBal = new HashMap<>();
+    for (final int[] transaction : transactions) {
+      idToBal.put(transaction[0], idToBal.getOrDefault(transaction[0], 0) - transaction[2]);
+      idToBal.put(transaction[1], idToBal.getOrDefault(transaction[1], 0) + transaction[2]);
+    }
+    final List<Long> balances = new ArrayList<>();
+    for (Map.Entry<Integer, Integer> entry : idToBal.entrySet()) {
+      if (entry.getValue() != 0) {
+        balances.add((long) entry.getValue());
+      }
+    }
+    return dfs(balances, 0);
+  }
+
+  private int dfs(List<Long> balances, int start) {
+    while (start < balances.size() && balances.get(start) == 0) {
+      start++;
+    }
+    if (start == balances.size()) {
+      return 0;
+    }
+    if (cache.containsKey(balances)) {
+      return cache.get(balances);
+    }
+    long res = Integer.MAX_VALUE;
+    final long startVal = balances.get(start);
+    balances.set(start, 0L);
+    for (int i = start + 1; i < balances.size(); i++) {
+      if (startVal * balances.get(i) < 0) {
+        balances.set(i, balances.get(i) + startVal);
+        res = Math.min(res, dfs(balances, start + 1) + 1L);
+        balances.set(i, balances.get(i) - startVal);
+      }
+    }
+    balances.set(start, startVal);
+    cache.put(balances, (int) res);
+    return (int) res;
+  }
+
+}
--- src/test/java/interviews/google/_465_Optimal_Account_Balancing/SolutionTest.java
+++ src/test/java/interviews/google/_465_Optimal_Account_Balancing/SolutionTest.java
@@ -1,43 +1,48 @@
 package interviews.google._465_Optimal_Account_Balancing;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.Assert.assertEquals;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author jacka
  * @version 1.0 on 1/15/2018.
  */
 public class SolutionTest {
-  private Solution solution;
-
-  @Before
-  public void setUp() throws Exception {
-    solution = new Solution();
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII()
+    );
   }
 
-  @Test
-  public void testOnlineCase1() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase1(Solution solution) {
     final int[][] trans = {{0, 1, 10}, {2, 0, 5}};
     assertEquals(2, solution.minTransfers(trans));
   }
 
-  @Test
-  public void testOnlineCase2() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testFailedCase2(Solution solution) {
+    final int[][] trans = {{1, 5, 8}, {8, 9, 8}, {2, 3, 9}, {4, 3, 1}};
+    assertEquals(4, solution.minTransfers(trans));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase2(Solution solution) {
     final int[][] trans = {{0, 1, 10}, {1, 0, 1}, {1, 2, 5}, {2, 0, 5}};
     assertEquals(1, solution.minTransfers(trans));
   }
 
-//  @Test
-//  public void testDfs() throws Exception {
-//    final List<Integer> debt = new ArrayList<>(List.of(5, 4, 3, -10, -2));
-//    assertEquals(4, solution.dfs(debt, 0, 0));
-//
-//  }
-
-  @Test
-  public void testFailedCase1() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testFailedCase1(Solution solution) {
     final int[][] trans = {{2, 0, 5}, {3, 4, 4}};
     assertEquals(2, solution.minTransfers(trans));
   }
