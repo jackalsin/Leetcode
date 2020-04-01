c2899c233c4dd5f33d5e29e290e1f4817dc20330
jackalsin <jackal.sin@hotmail.com>
2020-03-31 18:07:51 -0700

    Add 833

---
+++ src/main/java/interviews/google/_833_Find_And_Replace_in_String/Solution.java
@@ -0,0 +1,9 @@
+package interviews.google._833_Find_And_Replace_in_String;
+
+/**
+ * @author jacka
+ * @version 1.0 on 3/31/2020
+ */
+public interface Solution {
+  String findReplaceString(String S, int[] indexes, String[] sources, String[] targets);
+}
