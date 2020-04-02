e79e231e5fcf44a73718b757d94ed73794e52eda
jackalsin <jackal.sin@hotmail.com>
2020-04-01 21:21:09 -0700

    Add 315

---
+++ src/main/java/interviews/google/_315_Count_of_Smaller_Numbers_After_Self/Solution.java
@@ -0,0 +1,11 @@
+package interviews.google._315_Count_of_Smaller_Numbers_After_Self;
+
+import java.util.List;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/1/2020
+ */
+public interface Solution {
+  List<Integer> countSmaller(int[] nums);
+}
