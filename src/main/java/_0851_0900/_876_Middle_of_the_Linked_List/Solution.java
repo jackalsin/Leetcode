180d0965bfe90098517c8751689a8ebab419f5a4
jackalsin <jackal.sin@hotmail.com>
2020-04-08 17:54:53 -0700

    Add 876

---
+++ src/main/java/_0851_0900/_876_Middle_of_the_Linked_List/Solution.java
@@ -0,0 +1,11 @@
+package _0851_0900._876_Middle_of_the_Linked_List;
+
+import utils.ListNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/8/2020
+ */
+public interface Solution {
+  ListNode middleNode(ListNode head);
+}
---
+++ src/main/java/_0851_0900/_876_Middle_of_the_Linked_List/SolutionI.java
@@ -0,0 +1,18 @@
+package _0851_0900._876_Middle_of_the_Linked_List;
+
+import utils.ListNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/8/2020
+ */
+public final class SolutionI implements Solution {
+  @Override
+  public ListNode middleNode(final ListNode head) {
+    ListNode slow = head, fast = head;
+    for (; fast != null && fast.next != null;
+         fast = fast.next.next, slow = slow.next) {
+    }
+    return slow;
+  }
+}
