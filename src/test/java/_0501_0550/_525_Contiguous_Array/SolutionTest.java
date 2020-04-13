62debcd741d9704606d3ef3b13d1a2a68e660b0b
jackalsin <jackal.sin@hotmail.com>
2020-04-13 10:38:31 -0700

    Add 525

--- src/test/java/_0501_0550/_525_Contiguous_Array/SolutionTest.java
+++ src/test/java/_0501_0550/_525_Contiguous_Array/SolutionTest.java
@@ -1,36 +1,42 @@
 package _0501_0550._525_Contiguous_Array;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
-  private Solution solution;
 
-  @BeforeEach
-  void setUp() {
-    solution = new Solution();
-  }
-
-  @Test
-  void testOnlineCase1() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
     assertEquals(2, solution.findMaxLength(new int[]{0, 1}));
   }
 
-  @Test
-  void testOnlineCase2() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
     assertEquals(2, solution.findMaxLength(new int[]{0, 1, 0}));
   }
 
-  @Test
-  void testOnlineCase101() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase101(Solution solution) {
     assertEquals(2, solution.findMaxLength(new int[]{1, 0, 1}));
   }
 
-  @Test
-  void testFailedCase1() {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testFailedCase1(Solution solution) {
     final int[] input = new int[]{0, 0, 1, 0, 0, 0, 1, 1};
     assertEquals(6, solution.findMaxLength(input));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
   }
 }
