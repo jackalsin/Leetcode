d79da7c4c303b2a3f55d931ab213936e54ae391b
jackalsin <jackal.sin@hotmail.com>
2020-04-13 19:25:08 -0700

    Add 482

---
+++ src/main/java/interviews/google/_482_License_Key_Formatting/Solution.java
@@ -0,0 +1,9 @@
+package interviews.google._482_License_Key_Formatting;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/13/2020
+ */
+public interface Solution {
+  String licenseKeyFormatting(String S, int K);
+}
---
+++ src/main/java/interviews/google/_482_License_Key_Formatting/SolutionI.java
@@ -0,0 +1,28 @@
+package interviews.google._482_License_Key_Formatting;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/13/2020
+ */
+public final class SolutionI implements Solution {
+  private static final char DASH = '-';
+
+  public String licenseKeyFormatting(String S, int K) {
+    final StringBuilder sb = new StringBuilder();
+    for (int i = S.length() - 1; i >= 0; --i) {
+      final char chr = Character.toUpperCase(S.charAt(i));
+      if (chr == DASH) {
+        continue;
+      }
+      sb.append(chr);
+      if (sb.length() % (K + 1) == K) {
+        sb.append(DASH);
+      }
+    }
+    final int len = sb.length() - 1;
+    if (sb.length() > 0 && sb.charAt(len) == DASH) {
+      sb.setLength(len);
+    }
+    return sb.reverse().toString();
+  }
+}
