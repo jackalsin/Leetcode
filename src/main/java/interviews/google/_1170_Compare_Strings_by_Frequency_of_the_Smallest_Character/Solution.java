ede6fabe6c1d4d30391d53a7db2444461a517829
jackalsin <jackal.sin@hotmail.com>
2020-04-13 20:06:24 -0700

    Add 1170

---
+++ src/main/java/interviews/google/_1170_Compare_Strings_by_Frequency_of_the_Smallest_Character/Solution.java
@@ -0,0 +1,9 @@
+package interviews.google._1170_Compare_Strings_by_Frequency_of_the_Smallest_Character;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/13/2020
+ */
+public interface Solution {
+  int[] numSmallerByFrequency(String[] queries, String[] words);
+}
---
+++ src/test/java/interviews/google/_1170_Compare_Strings_by_Frequency_of_the_Smallest_Character/SolutionITest.java
@@ -0,0 +1,61 @@
+package interviews.google._1170_Compare_Strings_by_Frequency_of_the_Smallest_Character;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/13/2020
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final String[] queries = {"cbd"}, words = {"zaaaz"};
+    final int[] expected = {1},
+        actual = solution.numSmallerByFrequency(queries, words);
+    assertArrayEquals(expected, actual);
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
+    final String[] queries = {"bbb", "cc"}, words = {"a", "aa", "aaa", "aaaa"};
+    final int[] expected = {1, 2},
+        actual = solution.numSmallerByFrequency(queries, words);
+    assertArrayEquals(expected, actual);
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase3(Solution solution) {
+    final String[] queries = {"bba", "abaaaaaa", "aaaaaa", "bbabbabaab", "aba", "aa", "baab", "bbbbbb", "aab",
+        "bbabbaabb"}, words = {"aaabbb", "aab", "babbab", "babbbb", "b", "bbbbbbbbab", "a", "bbbbbbbbbb", "baaabbaab"
+        , "aa"};
+    final int[] expected = {6, 1, 1, 2, 3, 3, 3, 1, 3, 2},
+        actual = solution.numSmallerByFrequency(queries, words);
+    assertArrayEquals(expected, actual);
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase4(Solution solution) {
+    final String[] queries = {"aabbabbb", "abbbabaa", "aabbbabaa", "aabba", "abb", "a", "ba", "aa", "ba", "baabbbaaaa"
+        , "babaa", "bbbbabaa"}, words = {"b", "aaaba", "aaaabba", "aa", "aabaabab", "aabbaaabbb", "ababb", "bbb",
+        "aabbbabb", "aab", "bbaaababba", "baaaaa"};
+    final int[] expected = {6, 5, 0, 6, 11, 11, 11, 8, 11, 0, 6, 6},
+        actual = solution.numSmallerByFrequency(queries, words);
+    assertArrayEquals(expected, actual);
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
