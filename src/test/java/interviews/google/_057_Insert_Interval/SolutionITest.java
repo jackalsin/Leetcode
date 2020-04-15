5b87cd0b4ba4bd32746f651d2b143345d8e9e4f8
jackalsin <jackal.sin@hotmail.com>
2020-04-14 22:49:20 -0700

    Add 047

---
+++ src/test/java/interviews/google/_057_Insert_Interval/SolutionITest.java
@@ -0,0 +1,48 @@
+package interviews.google._057_Insert_Interval;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import utils.TwoDimensionArray;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/14/2020
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testWithOnlineCase1(Solution solution) {
+    final int[][] input = (new int[][]{{1, 3}, {6, 9}});
+    final int[][] expected = (new int[][]{{1, 5}, {6, 9}});
+    assertArrayEquals(expected, solution.insert(input, new int[]{2, 5}));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testWithOnlineCase2(Solution solution) {
+    final int[][] input = (new int[][]{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}});
+    final int[][] expected = (new int[][]{{1, 2}, {3, 10}, {12, 16}});
+    assertArrayEquals(expected, solution.insert(input, new int[]{4, 9}));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testWithMissingLastIndex(Solution solution) {
+    final int[][] input = (new int[][]{{1, 5}});
+    final int[][] expected = (new int[][]{{1, 5}}),
+        actual = solution.insert(input, new int[]{2, 3});
+    assertArrayEquals(expected, actual);
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
