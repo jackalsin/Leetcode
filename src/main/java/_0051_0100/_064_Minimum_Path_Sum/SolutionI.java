ff02b9c26821f75b1c6d41863b2eeb7fb59c15d1
jackalsin <jackal.sin@hotmail.com>
2020-04-18 07:44:23 -0700

    Add 064

--- src/main/java/_0051_0100/_064_Minimum_Path_Sum/SolutionI.java
+++ src/main/java/_0051_0100/_064_Minimum_Path_Sum/SolutionI.java
@@ -11,7 +11,7 @@ public final class SolutionI implements Solution {
       return 0;
     }
     final int rows = grid.length, cols = grid[0].length;
-    for(int j = 1; j < cols; ++j) {
+    for (int j = 1; j < cols; ++j) {
       grid[0][j] += grid[0][j - 1];
     }
     for (int i = 1; i < rows; ++i) {
