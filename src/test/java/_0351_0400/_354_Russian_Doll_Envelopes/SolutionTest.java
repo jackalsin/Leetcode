26835a385505bc6b10d73454424f6fd21d7d3ddd
jackalsin <jackal.sin@hotmail.com>
2020-04-20 21:09:37 -0700

    Add 354

--- src/test/java/_0351_0400/_354_Russian_Doll_Envelopes/SolutionTest.java
+++ src/test/java/_0351_0400/_354_Russian_Doll_Envelopes/SolutionTest.java
@@ -1,36 +1,40 @@
 package _0351_0400._354_Russian_Doll_Envelopes;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.Assert.assertEquals;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Zhiwei.Xin
  * @version 1.0 on 10/9/2017.
  */
 public class SolutionTest {
-  private Solution solution;
-
-  @Before
-  public void setUp() throws Exception {
-    solution = new Solution();
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new Solution()
+    );
   }
 
-  @Test
-  public void testOnlineCase() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase(Solution solution) {
     final int[][] input = {{5, 4}, {6, 4}, {6, 7}, {2, 3}};
     assertEquals(3, solution.maxEnvelopes(input));
   }
 
-  @Test
-  public void testEmpty() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testEmpty(Solution solution) {
     final int[][] input = {};
     assertEquals(0, solution.maxEnvelopes(input));
   }
 
-  @Test
-  public void testOnlineCase2() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase2(Solution solution) {
     final int[][] input = {{5, 4}, {6, 5}, {6, 7}, {2, 3}, {7, 6}};
     assertEquals(4, solution.maxEnvelopes(input));
   }
---
+++ src/test/java/interviews/google/_354_Russian_Doll_Envelopes/SolutionITest.java
@@ -0,0 +1,72 @@
+package interviews.google._354_Russian_Doll_Envelopes;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/20/2020
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase(Solution solution) {
+    final int[][] input = {{5, 4}, {6, 4}, {6, 7}, {2, 3}};
+    assertEquals(3, solution.maxEnvelopes(input));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testEmpty(Solution solution) {
+    final int[][] input = {};
+    assertEquals(0, solution.maxEnvelopes(input));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase2(Solution solution) {
+    final int[][] input = {{5, 4}, {6, 5}, {6, 7}, {2, 3}, {7, 6}};
+    assertEquals(4, solution.maxEnvelopes(input));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase3(Solution solution) {
+    final int[][] input = {{1, 1}, {1, 1}, {1, 1}};
+    assertEquals(1, solution.maxEnvelopes(input));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase4(Solution solution) {
+    final int[][] input = {{4, 5}, {4, 6}, {6, 7}, {2, 3}, {1, 1}, {1, 1}};
+    assertEquals(4, solution.maxEnvelopes(input));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase5(Solution solution) {
+    final int[][] input = {{2, 100}, {3, 200}, {4, 300}, {5, 500}, {5, 400}, {5, 250}, {6, 370}, {6, 360}, {7, 380}};
+    // [2, 100] -> [3, 200] -> [4, 300] -> [5, 400]
+    // [2, 100] -> [3, 200] -> [5, 250] -> [6, 360] -> [7, 380]
+    assertEquals(5, solution.maxEnvelopes(input));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase6(Solution solution) {
+    final int[][] input = {{46, 89}, {50, 53}, {52, 68}, {72, 45}, {77, 81}};
+    assertEquals(3, solution.maxEnvelopes(input));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
