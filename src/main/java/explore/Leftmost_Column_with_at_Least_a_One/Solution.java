f9a0079f6af7f9b8135fd42929603de37b1e1bd7
jackalsin <jackal.sin@hotmail.com>
2020-04-21 08:39:50 -0700

    Add left most column with at least a one

---
+++ src/main/java/explore/Leftmost_Column_with_at_Least_a_One/Solution.java
@@ -0,0 +1,11 @@
+package explore.Leftmost_Column_with_at_Least_a_One;
+
+import utils.Leftmost_Column_with_at_Least_a_One.BinaryMatrix;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/21/2020
+ */
+public interface Solution {
+  int leftMostColumnWithOne(BinaryMatrix binaryMatrix);
+}
---
+++ src/test/java/explore/Leftmost_Column_with_at_Least_a_One/SolutionITest.java
@@ -0,0 +1,64 @@
+package explore.Leftmost_Column_with_at_Least_a_One;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import utils.Leftmost_Column_with_at_Least_a_One.BinaryMatrix;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/21/2020
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final int[][] matrix = {
+        {0, 0}, {1, 1}
+    };
+    final BinaryMatrix bm = new BinaryMatrix(matrix);
+    assertEquals(0, solution.leftMostColumnWithOne(bm));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase2(Solution solution) {
+    final int[][] matrix = {
+        {0, 0}, {0, 0}
+    };
+    final BinaryMatrix bm = new BinaryMatrix(matrix);
+    assertEquals(-1, solution.leftMostColumnWithOne(bm));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase3(Solution solution) {
+    final int[][] matrix = {
+        {0, 0}, {0, 1}
+    };
+    final BinaryMatrix bm = new BinaryMatrix(matrix);
+    assertEquals(1, solution.leftMostColumnWithOne(bm));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase4(Solution solution) {
+    final int[][] matrix = {
+        {0, 0, 0, 1},
+        {0, 0, 1, 1},
+        {0, 1, 1, 1}
+    };
+    final BinaryMatrix bm = new BinaryMatrix(matrix);
+    assertEquals(1, solution.leftMostColumnWithOne(bm));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
