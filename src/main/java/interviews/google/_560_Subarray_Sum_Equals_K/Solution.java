e4e0e260ba99584c63934021af287dee02fc619a
jackalsin <jackal.sin@hotmail.com>
2020-04-22 09:36:59 -0700

    Add 560

---
+++ src/main/java/interviews/google/_560_Subarray_Sum_Equals_K/Solution.java
@@ -0,0 +1,9 @@
+package interviews.google._560_Subarray_Sum_Equals_K;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/22/2020
+ */
+public interface Solution {
+  public int subarraySum(int[] nums, int k);
+}
