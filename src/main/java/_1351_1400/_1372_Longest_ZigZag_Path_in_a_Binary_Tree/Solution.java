18cbefdd0ae03845d9c7d351f0f82660ec6749bd
jackalsin <jackal.sin@hotmail.com>
2020-04-22 21:20:56 -0700

    Add 1372

---
+++ src/main/java/_1351_1400/_1372_Longest_ZigZag_Path_in_a_Binary_Tree/Solution.java
@@ -0,0 +1,11 @@
+package _1351_1400._1372_Longest_ZigZag_Path_in_a_Binary_Tree;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/22/2020
+ */
+public interface Solution {
+  int longestZigZag(TreeNode root);
+}
