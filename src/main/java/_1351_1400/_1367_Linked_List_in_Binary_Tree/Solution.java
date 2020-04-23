41d8e58d9a8b43a733459c5f8b4ba4704dbcc892
jackalsin <jackal.sin@hotmail.com>
2020-04-22 19:55:47 -0700

    Add 1367

---
+++ src/main/java/_1351_1400/_1367_Linked_List_in_Binary_Tree/Solution.java
@@ -0,0 +1,12 @@
+package _1351_1400._1367_Linked_List_in_Binary_Tree;
+
+import utils.ListNode;
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 4/22/2020
+ */
+public interface Solution {
+   boolean isSubPath(ListNode head, TreeNode root);
+}
