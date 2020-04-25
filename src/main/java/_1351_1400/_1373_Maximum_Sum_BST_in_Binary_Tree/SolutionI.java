2068eaefd7a83d079d1ee209f08803a5190142c2
jackalsin <jackal.sin@hotmail.com>
2020-04-25 09:31:34 -0700

    Add 1373

--- src/main/java/_1351_1400/_1373_Maximum_Sum_BST_in_Binary_Tree/SolutionI.java
+++ src/main/java/_1351_1400/_1373_Maximum_Sum_BST_in_Binary_Tree/SolutionI.java
@@ -14,50 +14,24 @@ public final class SolutionI implements Solution {
     return result;
   }
 
-  private Result maxSumBSTHelper(final TreeNode root) {
+  /**
+   * Return null when it's not a BST, otherwise, return {min, max, sum}
+   *
+   * @param root
+   * @return
+   */
+  private int[] maxSumBSTHelper(final TreeNode root) {
     if (root == null) {
-      return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE, true);
+      return new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 0};
     }
-    if (root.left == null && root.right == null) {
-      result = Math.max(root.val, result);
-      return new Result(root.val, root.val, root.val, true);
-    } else if (root.left == null) { // root.right != null
-      final Result right = maxSumBSTHelper(root.right);
-      if (!right.isBst || (root.val >= right.left)) {
-        return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE, false);
-      }
-      final int sum = root.val + right.sum;
-      result = Math.max(sum, result);
-      return new Result(sum, root.val, right.right, true);
-    } else if (root.right == null) {
-      final Result left = maxSumBSTHelper(root.left);
-      if (!left.isBst || (root.val <= left.right)) {
-        return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE, false);
-      }
-      final int sum = root.val + left.sum;
-      result = Math.max(sum, result);
-      return new Result(sum, left.left, root.val, true);
-    } else {
-      final Result left = maxSumBSTHelper(root.left),
-          right = maxSumBSTHelper(root.right);
-      if (!left.isBst || !right.isBst || left.right >= root.val || right.left <= root.val) {
-        return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE, false);
-      }
-      final int sum = root.val + left.sum + right.sum;
-      result = Math.max(result, sum);
-      return new Result(sum, left.left, right.right, true);
+    final int[] left = maxSumBSTHelper(root.left),
+        right = maxSumBSTHelper(root.right);
+    if (!(left != null && right != null && left[1] < root.val && root.val < right[0])) {
+      return null;
     }
+    final int sum = root.val + left[2] + right[2];
+    result = Math.max(result, sum);
+    return new int[]{Math.min(left[0], root.val), Math.max(right[1], root.val), sum};
   }
 
-  private static final class Result {
-    private final int sum, left, right;
-    private final boolean isBst;
-
-    private Result(int sum, int left, int right, boolean isBst) {
-      this.sum = sum;
-      this.left = left;
-      this.right = right;
-      this.isBst = isBst;
-    }
-  }
 }
