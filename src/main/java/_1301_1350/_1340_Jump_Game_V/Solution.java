f21c3306f2775c282b05e345feceb3931493ec4e
jackalsin <jackal.sin@hotmail.com>
2020-04-29 23:49:05 -0700

    Update 1340

--- src/main/java/_1301_1350/_1340_Jump_Game_V/Solution.java
+++ src/main/java/_1301_1350/_1340_Jump_Game_V/Solution.java
@@ -5,5 +5,8 @@ package _1301_1350._1340_Jump_Game_V;
  * @version 1.0 on 4/26/2020
  */
 public interface Solution {
-  public int maxJumps(int[] arr, int d);
+  /**
+   * TODO:
+   */
+  int maxJumps(int[] arr, int d);
 }
