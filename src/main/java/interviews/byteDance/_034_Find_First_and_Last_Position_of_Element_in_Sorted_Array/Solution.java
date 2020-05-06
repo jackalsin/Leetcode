34816efea23dfa4688c8d76a0c0cb04a2e4a7e78
jackalsin <jackal.sin@hotmail.com>
2020-05-05 22:27:40 -0700

    Add 034

---
+++ src/main/java/interviews/byteDance/_034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.java
@@ -0,0 +1,10 @@
+package interviews.byteDance._034_Find_First_and_Last_Position_of_Element_in_Sorted_Array;
+
+/**
+ * @author jacka
+ * @version 1.0 on 5/5/2020
+ */
+public interface Solution {
+  public int[] searchRange(int[] nums, int target);
+
+}
