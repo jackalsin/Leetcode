b574e7a638978529bb13a337ac1f38088fb50142
jackalsin <jackal.sin@hotmail.com>
2020-05-07 16:08:42 -0700

    Add 146

--- src/test/java/interviews/byteDance/_143_Reorder_List/SolutionITest.java
+++ src/test/java/interviews/byteDance/_143_Reorder_List/SolutionITest.java
@@ -1,4 +1,4 @@
-package _0101_0150._143_Reorder_List;
+package interviews.byteDance._143_Reorder_List;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -7,19 +7,13 @@ import utils.ListNodes;
 
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * @author Zhiwei.Xin
- * @version 1.0 on 7/8/2017.
+ * @author zhixi
+ * @version 1.0 on 5/7/2020
  */
-public class SolutionTest {
-  static Stream<Solution> solutionProvider() {
-    return Stream.of(
-        new Solution()
-    );
-  }
-
+class SolutionITest {
   @ParameterizedTest
   @MethodSource("solutionProvider")
   public void test1(Solution solution) {
@@ -63,5 +57,11 @@ public class SolutionTest {
     ListNode expected = ListNodes.getListOfNodes(1, 5, 2, 4, 3);
     solution.reorderList(head);
     assertEquals(expected, head);
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
   }
 }
