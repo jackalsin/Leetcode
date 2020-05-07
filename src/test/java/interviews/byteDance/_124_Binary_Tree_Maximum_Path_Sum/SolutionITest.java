0810e3a6de26bf32e4f7677108abddc909d52fb2
jackalsin <jackal.sin@hotmail.com>
2020-05-07 15:23:04 -0700

    Add 124

--- src/test/java/interviews/byteDance/_124_Binary_Tree_Maximum_Path_Sum/SolutionITest.java
+++ src/test/java/interviews/byteDance/_124_Binary_Tree_Maximum_Path_Sum/SolutionITest.java
@@ -1,4 +1,4 @@
-package _0101_0150._124_Binary_Tree_Maximum_Path_Sum;
+package interviews.byteDance._124_Binary_Tree_Maximum_Path_Sum;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,15 +10,10 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * @author Zhiwei.Xin
- * @version 1.0 on 6/26/2017.
+ * @author zhixi
+ * @version 1.0 on 5/7/2020
  */
-public class SolutionTest {
-  static Stream<Solution> solutionProvider() {
-    return Stream.of(
-        new Solution()
-    );
-  }
+class SolutionITest {
 
   @ParameterizedTest
   @MethodSource("solutionProvider")
@@ -27,11 +22,9 @@ public class SolutionTest {
     assertEquals(6, solution.maxPathSum(root));
   }
 
-  @ParameterizedTest
-  @MethodSource("solutionProvider")
-  public void test42(Solution solution) {
-    TreeNode root = TreeNodes.getTreeLevelOrder(-10, 9, 20, null, null, 15, 7);
-    assertEquals(42, solution.maxPathSum(root));
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
   }
-
 }
