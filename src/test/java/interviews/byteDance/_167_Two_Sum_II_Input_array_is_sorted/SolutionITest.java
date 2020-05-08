dca594e3ac76ee639a45163fb2e7fbe530b03b0d
jackalsin <jackal.sin@hotmail.com>
2020-05-07 19:58:39 -0700

    Add 167

---
+++ src/test/java/interviews/byteDance/_167_Two_Sum_II_Input_array_is_sorted/SolutionITest.java
@@ -0,0 +1,35 @@
+package interviews.byteDance._167_Two_Sum_II_Input_array_is_sorted;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 5/7/2020
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase(Solution solution) {
+    final int[] input = new int[]{2, 7, 11, 15};
+    assertArrayEquals(new int[]{1, 2}, solution.twoSum(input, 9));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  void testOnlineCase1(Solution solution) {
+    final int[] input = new int[]{5, 25, 75};
+    assertArrayEquals(new int[]{1, 2}, solution.twoSum(input, 100));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
