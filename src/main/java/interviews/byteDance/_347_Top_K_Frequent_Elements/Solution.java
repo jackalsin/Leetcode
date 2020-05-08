59aeaa846ea4e4256cc3feb43767d18954516b49
jackalsin <jackal.sin@hotmail.com>
2020-05-08 15:56:26 -0700

    Add 347

---
+++ src/main/java/interviews/byteDance/_347_Top_K_Frequent_Elements/Solution.java
@@ -0,0 +1,9 @@
+package interviews.byteDance._347_Top_K_Frequent_Elements;
+
+/**
+ * @author jacka
+ * @version 1.0 on 5/8/2020
+ */
+public interface Solution {
+  int[] topKFrequent(int[] nums, int k);
+}
