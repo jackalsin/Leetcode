36893ad9876b142b47680c76208c9c5c6993d8e5
jackalsin <jackal.sin@hotmail.com>
2020-05-09 12:02:17 -0700

    Add 351

--- src/test/java/interviews/byteDance/_351_Android_Unlock_Patterns/SolutionITest.java
+++ src/test/java/interviews/byteDance/_351_Android_Unlock_Patterns/SolutionITest.java
@@ -1,23 +1,18 @@
-package _0351_0400._351_Android_Unlock_Patterns;
+package interviews.byteDance._351_Android_Unlock_Patterns;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author jacka
- * @version 1.0 on 10/8/2017.
+ * @version 1.0 on 5/8/2020
  */
-public class SolutionTest {
-  static Stream<Solution> solutionProvider() {
-    return Stream.of(
-        new Solution()
-    );
-  }
+class SolutionITest {
+
 
   @ParameterizedTest
   @MethodSource("solutionProvider")
@@ -34,7 +29,12 @@ public class SolutionTest {
   @ParameterizedTest
   @MethodSource("solutionProvider")
   public void testOnlineCaseM1N3(Solution solution) {
-    Assertions.assertEquals(385, solution.numberOfPatterns(1, 3));
+    assertEquals(385, solution.numberOfPatterns(1, 3));
   }
 
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
 }
--- src/test/java/interviews/microsoft/noodleEssence/_351_Android_Unlock_Patterns/SolutionTest.java
+++ src/test/java/interviews/microsoft/noodleEssence/_351_Android_Unlock_Patterns/SolutionTest.java
@@ -20,7 +20,14 @@ class SolutionTest {
 
   @Test
   void testOnlineCaseM1N2() throws Exception {
+    // 1 -> 6, 2 -> 8, 5 -> 9
     assertEquals(65, solution.numberOfPatterns(1, 2));
+  }
+
+  @Test
+  void testOnlineCaseM1N3() throws Exception {
+    // 1 -> 37, 2 -> 45, 5 -> 57
+    assertEquals(385, solution.numberOfPatterns(1, 3));
   }
 
 }
