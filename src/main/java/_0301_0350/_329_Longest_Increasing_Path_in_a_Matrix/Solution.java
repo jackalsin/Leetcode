77679e9dd96c92f6ec195db4953e73ee3b2bdbab
jackalsin <jackal.sin@hotmail.com>
2020-05-08 20:11:32 -0700

    Add 329

--- src/main/java/_0301_0350/_329_Longest_Increasing_Path_in_a_Matrix/Solution.java
+++ src/main/java/_0301_0350/_329_Longest_Increasing_Path_in_a_Matrix/Solution.java
@@ -2,11 +2,13 @@ package _0301_0350._329_Longest_Increasing_Path_in_a_Matrix;
 
 /**
  * Runtime: 14 ms
+ *
  * @author Zhiwei.Xin
  * @version 1.0 on 9/29/2017.
  */
 public class Solution {
   private static final int[][] DIRS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
   public int longestIncreasingPath(int[][] matrix) {
     if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
     final int rows = matrix.length, cols = matrix[0].length;
@@ -28,8 +30,9 @@ public class Solution {
     for (int[] dir : DIRS) {
       int nextRow = dir[0] + row, nextCol = dir[1] + col;
       if (0 <= nextCol && nextCol < cols && 0 <= nextRow && nextRow < rows
-          && matrix[nextRow][nextCol] > matrix[row][col]) { // Use matrix[x][y] <= matrix[i][j] so we don't need a visited[m][n] array
-        max = Math.max(max, 1+ dfs(matrix, cache, rows, cols, nextRow, nextCol));
+          && matrix[nextRow][nextCol] > matrix[row][col]) { // Use matrix[x][y] <= matrix[i][j] so we don't need a
+        // visited[m][n] array
+        max = Math.max(max, 1 + dfs(matrix, cache, rows, cols, nextRow, nextCol));
       }
     }
     cache[row][col] = max;
---
+++ src/test/java/interviews/byteDance/_329_Longest_Increasing_Path_in_a_Matrix/SolutionITest.java
@@ -0,0 +1,43 @@
+package interviews.byteDance._329_Longest_Increasing_Path_in_a_Matrix;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jacka
+ * @version 1.0 on 5/8/2020
+ */
+class SolutionITest {
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase(Solution solution) {
+    final int[][] board = {
+        {9, 9, 4},
+        {6, 6, 8},
+        {2, 1, 1},
+    };
+    assertEquals(4, solution.longestIncreasingPath(board));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionProvider")
+  public void testOnlineCase2(Solution solution) {
+    final int[][] board = {
+        {3, 4, 5},
+        {3, 2, 6},
+        {2, 2, 1},
+    };
+    assertEquals(4, solution.longestIncreasingPath(board));
+  }
+
+  static Stream<Solution> solutionProvider() {
+    return Stream.of(
+        new SolutionI()
+    );
+  }
+}
