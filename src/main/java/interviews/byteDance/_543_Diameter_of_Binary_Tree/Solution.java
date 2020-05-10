1f930f614c6667a540095d637b9ab482cc923f30
jackalsin <jackal.sin@hotmail.com>
2020-05-10 10:28:15 -0700

    Add 543

---
+++ src/main/java/interviews/byteDance/_543_Diameter_of_Binary_Tree/Solution.java
@@ -0,0 +1,12 @@
+package interviews.byteDance._543_Diameter_of_Binary_Tree;
+
+import utils.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 5/10/2020
+ */
+public interface Solution {
+  public int diameterOfBinaryTree(TreeNode root);
+
+}
