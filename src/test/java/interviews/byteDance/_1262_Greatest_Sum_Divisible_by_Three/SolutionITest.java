44fad5d54fc4267d75305439220afd4213ac2613
jackalsin <jackal.sin@hotmail.com>
2020-05-10 18:08:16 -0700

    Add 1262

--- src/test/java/interviews/byteDance/_1262_Greatest_Sum_Divisible_by_Three/SolutionITest.java
+++ src/test/java/interviews/byteDance/_1262_Greatest_Sum_Divisible_by_Three/SolutionITest.java
@@ -1,4 +1,4 @@
-package _1251_1300._1262_Greatest_Sum_Divisible_by_Three;
+package interviews.byteDance._1262_Greatest_Sum_Divisible_by_Three;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author jacka
- * @version 1.0 on 5/1/2020
+ * @version 1.0 on 5/10/2020
  */
 class SolutionITest {
 
@@ -43,8 +43,7 @@ class SolutionITest {
 
   static Stream<Solution> solutionStream() {
     return Stream.of(
-        new ReverseDeductionSolution(),
-        new DpSolution()
+        new SolutionI()
     );
   }
 }
