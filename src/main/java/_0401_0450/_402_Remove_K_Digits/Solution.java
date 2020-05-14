0e5308c87d15184e37c2d5edb2df98685649115a
jackalsin <jackal.sin@hotmail.com>
2020-05-13 19:16:44 -0700

    Add 402

--- src/main/java/_0401_0450/_402_Remove_K_Digits/Solution.java
+++ src/main/java/_0401_0450/_402_Remove_K_Digits/Solution.java
@@ -1,41 +1,13 @@
 package _0401_0450._402_Remove_K_Digits;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 /**
  * @author jacka
  * @version 1.0 on 11/12/2017.
  */
-public class Solution {
+public interface Solution {
   /**
    * Given a stack, check if the last one is larger than current char, if yes,
-   *
-   * @param num
-   * @param k
-   * @return
+   * TODO:
    */
-  public String removeKdigits(String num, int k) {
-    final int length = num.length() - k;
-    final Deque<Character> stack = new ArrayDeque<>();
-    for (int i = 0; i < num.length(); i++) {
-      final char curChar = num.charAt(i);
-      while (stack.size() > 0 && stack.peekLast() > curChar && k-- > 0) {
-        stack.removeLast();
-      }
-      stack.addLast(curChar);
-    }
-    int leading0Counts = 0;
-    while (stack.size() > 1 && stack.peekFirst() == '0') {
-      stack.removeFirst();
-      leading0Counts++;
-    }
-    StringBuilder sb = new StringBuilder();
-    while (!stack.isEmpty()) {
-      sb.append(stack.removeFirst());
-    }
-    sb.delete(length - leading0Counts, sb.length());
-    return sb.length() == 0 ? "0" : sb.toString();
-  }
-
+  public String removeKdigits(String num, int k);
 }
--- src/test/java/_0401_0450/_402_Remove_K_Digits/SolutionTest.java
+++ src/test/java/_0401_0450/_402_Remove_K_Digits/SolutionTest.java
@@ -1,24 +1,26 @@
 package _0401_0450._402_Remove_K_Digits;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.Assert.assertEquals;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author jacka
  * @version 1.0 on 11/12/2017.
  */
 public class SolutionTest {
-  private Solution solution;
-
-  @Before
-  public void setUp() throws Exception {
-    solution = new Solution();
+  static Stream<Solution> solutionStream() {
+    return Stream.of(
+        new SolutionI()
+    );
   }
 
-  @Test
-  public void testOnline1432219() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  public void testOnline1432219(Solution solution) {
     assertEquals("1219", solution.removeKdigits("1432219", 3));
   }
 
@@ -27,24 +29,28 @@ public class SolutionTest {
    *
    * @throws Exception
    */
-  @Test
-  public void testOnline10200() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  public void testOnline10200(Solution solution) {
     assertEquals("200", solution.removeKdigits("10200", 1));
   }
 
-  @Test
-  public void testOnline10() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  public void testOnline10(Solution solution) {
     assertEquals("0", solution.removeKdigits("10", 2));
 //    assertEquals("0", solution.sampleSolution("9", 1));
   }
 
-  @Test
-  public void testFailedOn9() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  public void testFailedOn9(Solution solution) {
     assertEquals("0", solution.removeKdigits("9", 1));
   }
 
-  @Test
-  public void testFailedOn112() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  public void testFailedOn112(Solution solution) {
     assertEquals("11", solution.removeKdigits("112", 1));
   }
 }
