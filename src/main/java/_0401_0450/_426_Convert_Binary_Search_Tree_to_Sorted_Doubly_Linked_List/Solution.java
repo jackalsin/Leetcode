ce2bc7f5f709e290197d9c695c25f382a0349fa1
jackalsin <jackal.sin@hotmail.com>
2020-05-25 09:17:40 -0700

    Add 426

---
+++ src/main/java/_0401_0450/_426_Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List/Solution.java
@@ -0,0 +1,12 @@
+package _0401_0450._426_Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List;
+
+
+import utils.node.Node;
+
+/**
+ * @author jacka
+ * @version 1.0 on 5/24/2020
+ */
+public interface Solution {
+  Node treeToDoublyList(Node root);
+}
---
+++ src/main/java/utils/node/Node.java
@@ -0,0 +1,50 @@
+package utils.node;
+
+import java.util.Objects;
+
+/**
+ * @author jacka
+ * @version 1.0 on 5/24/2020
+ */
+public final class Node {
+  public int val;
+  public Node left;
+  public Node right;
+
+  public Node() {
+  }
+
+  public Node(int _val) {
+    val = _val;
+  }
+
+  public Node(int _val, Node _left, Node _right) {
+    val = _val;
+    left = _left;
+    right = _right;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Node)) return false;
+    Node node = (Node) o;
+    return val == node.val &&
+        Objects.equals(left, node.left) &&
+        Objects.equals(right, node.right);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(val, left, right);
+  }
+
+  @Override
+  public String toString() {
+    return "Node{" +
+        "val=" + val +
+        ", left=" + left +
+        ", right=" + right +
+        '}';
+  }
+}
