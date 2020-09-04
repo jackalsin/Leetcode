d1eb8c77ef179d68a6752736c499bdb2b242b0f1
jackalsin <jackal.sin@hotmail.com>
2020-09-03 20:55:34 -0700

    Add 837

--- src/main/java/_0801_0850/_837_New_21_Game/ONSolution.java
+++ src/main/java/_0801_0850/_837_New_21_Game/ONSolution.java
@@ -6,23 +6,27 @@ package _0801_0850._837_New_21_Game;
  */
 public final class ONSolution implements Solution {
   /**
+   * It stops when >= K, ask for the probability <= N;
    * <a href="https://leetcode.com/problems/new-21-game/discuss/132334/One-Pass-DP-O(N)"></a>
    */
   @Override
   public double new21Game(int N, int K, int W) {
-    if (K == 0 || N >= K + W) return 1.0;
+    if (K == 0 || N >= K + W) return 1d;
     final double[] dp = new double[N + 1];
     dp[0] = 1d;
-    double wSum = 1, res = 0;
+    double lastWSum = 1d, result = 0;
     for (int i = 1; i <= N; ++i) {
-      dp[i] = wSum / W;
+      dp[i] = lastWSum / W;
       if (i < K) {
-        wSum += dp[i];
+        lastWSum += dp[i];
       } else {
-        res += dp[i];
+        result += dp[i];
       }
-      if (i - W >= 0) wSum -= dp[i - W];
+      if (i >= W) {
+        lastWSum -= dp[i - W];
+      }
     }
-    return res;
+//    System.out.println(Arrays.toString(dp));
+    return result;
   }
 }
