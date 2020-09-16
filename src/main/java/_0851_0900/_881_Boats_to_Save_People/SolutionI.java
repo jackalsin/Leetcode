c4aa4e648ddd677ee26d615909b799d72fe0483d
jackalsin <jackal.sin@hotmail.com>
2020-09-15 21:11:49 -0700

    Add 881

--- src/main/java/_0851_0900/_881_Boats_to_Save_People/SolutionI.java
+++ src/main/java/_0851_0900/_881_Boats_to_Save_People/SolutionI.java
@@ -1,7 +1,6 @@
 package _0851_0900._881_Boats_to_Save_People;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Arrays;
 
 /**
  * @author jacka
@@ -10,32 +9,12 @@ import java.util.TreeMap;
 public final class SolutionI implements Solution {
   @Override
   public int numRescueBoats(int[] people, int limit) {
-    final TreeMap<Integer, Integer> treeMap = new TreeMap<>();
-    for (final int p : people) {
-      treeMap.put(p, treeMap.getOrDefault(p, 0) + 1);
-    }
+    Arrays.sort(people);
+    final int n = people.length;
     int result = 0;
-    nextBoat:
-    while (!treeMap.isEmpty()) {
-      result++;
-      int remainWeight = limit, remainSeat = 2;
-      while (remainWeight > 0 && remainSeat > 0) {
-        final Map.Entry<Integer, Integer> e = treeMap.floorEntry(remainWeight);
-        if (e == null) {
-          continue nextBoat;
-        }
-        final int weight = e.getKey(), count = e.getValue();
-        if (remainWeight >= weight) {
-          remainWeight -= weight;
-          remainSeat--;
-          if (count == 1) {
-            treeMap.remove(weight);
-          } else {
-            treeMap.put(weight, count - 1);
-          }
-        } else {
-          continue nextBoat;
-        }
+    for (int i = 0, j = n - 1; i <= j; --j, result++) {
+      if (people[i] + people[j] <= limit) {
+        ++i;
       }
     }
     return result;
