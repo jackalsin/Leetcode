3f4c1ed834d98809088e7558ab3a80e93576f860
jackalsin <jackal.sin@hotmail.com>
2020-09-29 23:41:57 -0700

    Add 499

---
+++ src/main/java/_0451_0500/_499_The_Maze_III/BfsSolution.java
@@ -0,0 +1,83 @@
+package _0451_0500._499_The_Maze_III;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @author jacka
+ * @version 1.0 on 9/29/2020
+ */
+public final class BfsSolution implements Solution {
+  private static final int[][] DIRS = {
+      {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+  };
+  private static final char[] DIRS_CHARS = {'d', 'u', 'r', 'l'};
+
+  private static final int WALL = 1, EMPTY = 0;
+  private static final String IMPOSSIBLE = "impossible";
+
+
+  private int[][] maze;
+  private State[][] visited;
+
+  private int minLen = Integer.MAX_VALUE;
+  private String minPath = null;
+
+  public String findShortestWay(int[][] maze, int[] ball, int[] hole) {
+    if (maze == null || maze.length == 0) {
+      return IMPOSSIBLE;
+    }
+    final int rows = maze.length, cols = maze[0].length;
+    this.maze = maze;
+    visited = new State[rows][cols];
+    final Queue<State> q = new ArrayDeque<>();
+    final State startState = new State(ball[0], ball[1], 0, "");
+    visited[ball[0]][ball[1]] = startState;
+    q.add(startState);
+    while (!q.isEmpty()) {
+      final State state = q.remove();
+      if (visited[state.x][state.y] != null
+          && (visited[state.x][state.y].distance != state.distance
+          || !visited[state.x][state.y].path.equals(state.path))) continue; // no need to visit when is not min
+      nextDir:
+      for (int i = 0; i < 4; ++i) {
+        final int[] d = DIRS[i];
+        final String nextPath = state.path + DIRS_CHARS[i];
+        int nextX = state.x, nextY = state.y, nextDistance = state.distance;
+        while (nextX + d[0] >= 0 && nextX + d[0] < rows && nextY + d[1] >= 0 && nextY + d[1] < cols
+            && maze[nextX + d[0]][nextY + d[1]] != WALL) {
+          nextX += d[0];
+          nextY += d[1];
+          nextDistance++;
+          if (nextX == hole[0] && nextY == hole[1]) {
+            if (nextDistance < minLen || nextDistance == minLen && nextPath.compareTo(minPath) < 0) {
+              minPath = nextPath;
+              minLen = nextDistance;
+            }
+            continue nextDir;
+          }
+        }
+        if (visited[nextX][nextY] == null
+            || visited[nextX][nextY].distance > nextDistance
+            || (visited[nextX][nextY].distance == nextDistance && visited[nextX][nextY].path.compareTo(nextPath) > 0)) {
+          final State nextState = new State(nextX, nextY, nextDistance, nextPath);
+          visited[nextX][nextY] = nextState;
+          q.add(nextState);
+        }
+      }
+    }
+    return minPath == null ? IMPOSSIBLE : minPath;
+  }
+
+  private static final class State {
+    private final int x, y, distance;
+    private final String path;
+
+    private State(int x, int y, int distance, String path) {
+      this.x = x;
+      this.y = y;
+      this.distance = distance;
+      this.path = path;
+    }
+  }
+}
--- src/test/java/interviews/newsbreak/_499_The_Maze_III/SolutionITest.java
+++ src/test/java/interviews/newsbreak/_499_The_Maze_III/SolutionITest.java
@@ -1,4 +1,4 @@
-package _0451_0500._499_The_Maze_III;
+package interviews.newsbreak._499_The_Maze_III;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -7,14 +7,11 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SolutionTest {
-
-  static Stream<Solution> solutionStream() {
-    return Stream.of(
-        new DfsSolution(),
-        new BfsSolution()
-    );
-  }
+/**
+ * @author jacka
+ * @version 1.0 on 9/29/2020
+ */
+class SolutionITest {
 
   @ParameterizedTest
   @MethodSource("solutionStream")
@@ -53,5 +50,23 @@ class SolutionTest {
         {0, 0, 0, 0, 0}
     };
     assertEquals("ldldrdr", solution.findShortestWay(board, new int[]{0, 4}, new int[]{4, 4}));
+  }
+
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  void testFailedCase2(Solution solution) {
+    final int[][] board = {
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0, 1, 0},
+        {0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1}
+    };
+    assertEquals("dldr", solution.findShortestWay(board, new int[]{0, 4}, new int[]{3, 5}));
+  }
+
+  static Stream<Solution> solutionStream() {
+    return Stream.of(
+        new SolutionI()
+    );
   }
 }
