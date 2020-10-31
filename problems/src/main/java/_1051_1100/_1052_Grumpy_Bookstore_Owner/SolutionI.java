a56c0f4a230c6c537d43781c7fea5a57ca13e2fc
jackalsin <jackal.sin@hotmail.com>
2020-10-31 16:21:07 -0700

    Add 1052

--- problems/src/main/java/_1051_1100/_1052_Grumpy_Bookstore_Owner/SolutionI.java
+++ problems/src/main/java/_1051_1100/_1052_Grumpy_Bookstore_Owner/SolutionI.java
@@ -9,25 +9,20 @@ public final class SolutionI implements Solution {
 
   public int maxSatisfied(int[] customers, int[] grumpy, int X) {
     final int n = customers.length;
-    final int[] unhappyCustomers = new int[n];
-    int alreadyHappyCustomers = 0;
-    for (int i = 0; i < n; ++i) {
+    int happyCustomers = 0, maxUnhappyInX = 0;
+    for (int i = 0, curToAdd = 0; i < n; ++i) {
       if (GRUMPY == grumpy[i]) {
-        unhappyCustomers[i] = customers[i];
+        curToAdd += customers[i];
       } else {
-        alreadyHappyCustomers += customers[i];
+        happyCustomers += customers[i];
+      }
+      if (i - X >= 0 && grumpy[i - X] == GRUMPY) {
+        curToAdd -= customers[i - X];
+      }
+      if (curToAdd > maxUnhappyInX) {
+        maxUnhappyInX = curToAdd;
       }
     }
-//    System.out.println("already = " + alreadyHappyCustomers);
-//    System.out.println("unhappyCustomers = " + Arrays.toString(unhappyCustomers));
-    int max = 0;
-    for (int i = 0, curAdd = 0; i < n; i++) {
-      curAdd += unhappyCustomers[i];
-      if (i - X >= 0) {
-        curAdd -= unhappyCustomers[i - X];
-      }
-      max = Math.max(max, curAdd);
-    }
-    return max + alreadyHappyCustomers;
+    return maxUnhappyInX + happyCustomers;
   }
 }
