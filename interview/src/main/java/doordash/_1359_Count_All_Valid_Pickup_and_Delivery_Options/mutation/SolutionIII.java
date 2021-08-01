1334fd9d262401c64f8951e3afa381f03a48ee22
jackalsin <jackal.sin@hotmail.com>
2021-08-01 12:52:50 -0700

    Add 1359

--- interview/src/main/java/doordash/_1359_Count_All_Valid_Pickup_and_Delivery_Options/mutation/SolutionIII.java
+++ interview/src/main/java/doordash/_1359_Count_All_Valid_Pickup_and_Delivery_Options/mutation/SolutionIII.java
@@ -1,6 +1,7 @@
 package doordash._1359_Count_All_Valid_Pickup_and_Delivery_Options.mutation;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,36 +10,46 @@ public final class SolutionIII implements Solution {
   private static final String P = "P", D = "D";
 
   public Set<List<String>> countOrders(int n) {
+    final BitSet leftCandidate = new BitSet();
+    for (int i = 1; i <= n; ++i) {
+      leftCandidate.set(i);
+    }
     final Set<List<String>> result = new HashSet<>();
-    countOrder(result, n, new ArrayList<>(), new HashSet<>(), new HashSet<>());
+    final List<String> curPath = new ArrayList<>();
+    countOrders(result, curPath, leftCandidate, new BitSet(), n);
     return result;
   }
 
-  private static void countOrder(final Set<List<String>> result, final int n, final List<String> curPath,
-                                 final Set<Integer> leftUsed, final Set<Integer> rightUsed) {
-    if (curPath.size() == 2 * n) {
+  private static void countOrders(final Set<List<String>> result,
+                                  final List<String> curPath, final BitSet leftCandidate,
+                                  final BitSet rightCandidate, final int n) {
+    if (2 * n == curPath.size()) {
       result.add(new ArrayList<>(curPath));
       return;
     }
-
+    final int size = curPath.size();
+    // try left;
     for (int i = 1; i <= n; ++i) {
-      if (leftUsed.add(i)) {
-        final String toAdd = P + i;
-        curPath.add(toAdd);
-        countOrder(result, n, curPath, leftUsed, rightUsed);
-        curPath.remove(curPath.size() - 1);
-        leftUsed.remove(i);
+      if (leftCandidate.get(i)) {
+        leftCandidate.clear(i);
+        rightCandidate.set(i);
+        curPath.add(P + i);
+        countOrders(result, curPath, leftCandidate, rightCandidate, n);
+        curPath.remove(size);
+        rightCandidate.clear(i);
+        leftCandidate.set(i);
       }
     }
 
     for (int i = 1; i <= n; ++i) {
-      if (leftUsed.contains(i) && rightUsed.add(i)) {
-        final String toAdd = D + i;
-        curPath.add(toAdd);
-        countOrder(result, n, curPath, leftUsed, rightUsed);
-        curPath.remove(curPath.size() - 1);
-        rightUsed.remove(i);
+      if (rightCandidate.get(i)) {
+        rightCandidate.clear(i);
+        curPath.add(D + i);
+        countOrders(result, curPath, leftCandidate, rightCandidate, n);
+        curPath.remove(size);
+        rightCandidate.set(i);
       }
     }
+
   }
 }
