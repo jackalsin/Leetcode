5fa08f18c09d7625e0c0bfcc39935cdc6b7fe1a8
jackalsin <jackal.sin@hotmail.com>
2021-07-31 19:23:49 -0700

    Add 795

--- interview/src/main/java/doordash/_795_Number_of_Subarrays_with_Bounded_Maximum/SolutionIII.java
+++ interview/src/main/java/doordash/_795_Number_of_Subarrays_with_Bounded_Maximum/SolutionIII.java
@@ -7,14 +7,14 @@ package doordash._795_Number_of_Subarrays_with_Bounded_Maximum;
 public final class SolutionIII implements Solution {
   @Override
   public int numSubarrayBoundedMax(int[] nums, int left, int right) {
-    return numSubarrayBoundedMax(nums, right) - numSubarrayBoundedMax(nums, left - 1);
+    return countLessThanOrEqualsTo(nums, right) - countLessThanOrEqualsTo(nums, left - 1);
   }
 
-  private int numSubarrayBoundedMax(int[] nums, int right) {
-    int res = 0, cur = 0;
-    for (int n : nums) {
-      cur = n <= right ? cur + 1 : 0;
-      res += cur;
+  private static int countLessThanOrEqualsTo(final int[] nums, final int max) {
+    int res = 0, count = 0;
+    for (int num : nums) {
+      count = num <= max ? count + 1 : 0;
+      res += count;
     }
     return res;
   }
