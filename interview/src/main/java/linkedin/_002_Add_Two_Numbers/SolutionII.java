02bb60e470161c355a3bbfa3d08af12c5290e9cd
jackalsin <jackal.sin@hotmail.com>
2021-08-03 21:52:14 -0700

    Add 002

--- interview/src/main/java/linkedin/_002_Add_Two_Numbers/SolutionII.java
+++ interview/src/main/java/linkedin/_002_Add_Two_Numbers/SolutionII.java
@@ -4,28 +4,22 @@ import definition.ListNode;
 
 public final class SolutionII implements Solution {
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    assert l1 != null && l2 != null;
     final ListNode dummy = new ListNode(0);
     ListNode cur = dummy;
-
     int carry = 0;
-    while (carry != 0 || l1 != null || l2 != null) {
-      final int num1 = l1 == null ? 0 : l1.val, num2 = l2 == null ? 0 : l2.val, sum = num1 + num2 + carry, curDigit =
-          sum % 10;
+    while (l1 != null || l2 != null || carry != 0) {
+      final int l1Val = l1 == null ? 0 : l1.val,
+          l2Val = l2 == null ? 0 : l2.val,
+          sum = l1Val + l2Val + carry,
+          val = sum % 10;
       carry = sum / 10;
+      cur.next = new ListNode(val);
 
-      cur.next = new ListNode(curDigit);
-
+      if (l1 != null) l1 = l1.next;
+      if (l2 != null) l2 = l2.next;
       cur = cur.next;
-
-      if (l1 != null) {
-        l1 = l1.next;
-      }
-
-      if (l2 != null) {
-        l2 = l2.next;
-      }
     }
-
     return dummy.next;
   }
 
