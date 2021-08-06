a8bf3f23160b5bc80570fc2cbdec87799f1f61cb
jackalsin <jackal.sin@hotmail.com>
2021-08-05 21:22:29 -0700

    Add 012

--- interview/src/main/java/linkedin/_012_Integer_to_Roman/SolutionIII.java
+++ interview/src/main/java/linkedin/_012_Integer_to_Roman/SolutionIII.java
@@ -5,29 +5,25 @@ package linkedin._012_Integer_to_Roman;
  * @version 1.0 on 8/3/2019
  */
 public final class SolutionIII implements Solution {
-  //  I             1
-//  V             5
-//  X             10
-//  L             50
-//  C             100
-//  D             500
-//  M             1000;
-  private static final String[] THOUSANDS = {
-      "", "M", "MM", "MMM"
-  }, HUNDREDS = {
-      "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"
+  private static final String[] ONES = {
+      "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"
   }, TENS = {
       "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"
-  }, ONES = {
-      "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"
+  }, HUNDREDS = {
+      "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"
+  }, THOUSANDS = {
+      "", "M", "MM", "MMM"
   };
 
   public String intToRoman(int num) {
     final StringBuilder sb = new StringBuilder();
     sb.append(THOUSANDS[num / 1000]);
-    sb.append(HUNDREDS[(num / 100) % 10]);
-    sb.append(TENS[(num / 10) % 10]);
-    sb.append(ONES[num % 10]);
+    num -= num / 1000 * 1000;
+    sb.append(HUNDREDS[num / 100]);
+    num -= num / 100 * 100;
+    sb.append(TENS[num / 10]);
+    num -= num / 10 * 10;
+    sb.append(ONES[num]);
     return sb.toString();
   }
 }
