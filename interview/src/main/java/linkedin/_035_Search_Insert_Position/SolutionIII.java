641eefb10ed79bd88a92f00db7f962265672cdb9
jackalsin <jackal.sin@hotmail.com>
2021-08-07 15:11:24 -0700

    Add 035

--- interview/src/main/java/linkedin/_035_Search_Insert_Position/SolutionIII.java
+++ interview/src/main/java/linkedin/_035_Search_Insert_Position/SolutionIII.java
@@ -3,22 +3,18 @@ package linkedin._035_Search_Insert_Position;
 public final class SolutionIII implements Solution {
   @Override
   public int searchInsert(int[] nums, int target) {
-    if (nums == null || nums.length == 0) {
-      return 0;
-    }
+    if (nums == null || nums.length == 0) return 0;
+    if (target < nums[0]) return 0;
+    if (target > nums[nums.length - 1]) return nums.length;
     int left = 0, right = nums.length - 1;
     while (left < right) {
-      final int mid = (right - left) / 2 + left;
+      final int mid = left + (right - left) / 2;
       if (nums[mid] >= target) {
         right = mid;
       } else {
         left = mid + 1;
       }
     }
-    if (nums[left] >= target) {
-      return left;
-    }
-    // all elements are lower than target
-    return nums.length;
+    return left;
   }
 }
--- interview/src/test/java/linkedin/_035_Search_Insert_Position/SolutionITest.java
+++ interview/src/test/java/linkedin/_035_Search_Insert_Position/SolutionITest.java
@@ -1,52 +1,61 @@
 package linkedin._035_Search_Insert_Position;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionITest {
-  private Solution solution;
-
-  @BeforeEach
-  void setUp() throws Exception {
-    solution = new SolutionI();
+  static Stream<Solution> solutionStream() {
+    return Stream.of(
+        new SolutionI(),
+        new SolutionII(),
+        new SolutionIII()
+    );
   }
 
   // non-exist value
-  @Test
-  void testWithLeadingValue() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  void testWithLeadingValue(Solution solution) {
     final int[] input = new int[]{1, 3, 5, 7, 9};
     assertEquals(0, solution.searchInsert(input, 0));
   }
 
-  @Test
-  void testWithTrailingValue() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  void testWithTrailingValue(Solution solution) {
     final int[] input = new int[]{1, 3, 5, 7, 9};
     assertEquals(5, solution.searchInsert(input, 10));
   }
 
-  @Test
-  void testWithMidMissingValue() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  void testWithMidMissingValue(Solution solution) {
     final int[] input = new int[]{1, 3, 5, 7, 9};
     assertEquals(3, solution.searchInsert(input, 6));
   }
   //test for existing value
 
-  @Test
-  void testWith0thValue() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  void testWith0thValue(Solution solution) {
     final int[] input = new int[]{1, 3, 5, 7, 9};
     assertEquals(0, solution.searchInsert(input, 1));
   }
 
-  @Test
-  void testWithLastValue() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  void testWithLastValue(Solution solution) {
     final int[] input = new int[]{1, 3, 5, 7, 9};
     assertEquals(4, solution.searchInsert(input, 9));
   }
 
-  @Test
-  void testWithMidValue() throws Exception {
+  @ParameterizedTest
+  @MethodSource("solutionStream")
+  void testWithMidValue(Solution solution) {
     final int[] input = new int[]{1, 3, 5, 7, 9};
     assertEquals(2, solution.searchInsert(input, 5));
   }
