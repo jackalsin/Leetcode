04e64060023221c5ca796969bca6f346ecb0661f
jackalsin <jackal.sin@hotmail.com>
2021-08-07 14:47:10 -0700

    Add 034

--- interview/src/main/java/linkedin/_034_Search_for_a_Range/SolutionVI.java
+++ interview/src/main/java/linkedin/_034_Search_for_a_Range/SolutionVI.java
@@ -7,9 +7,7 @@ package linkedin._034_Search_for_a_Range;
 public final class SolutionVI implements Solution {
   @Override
   public int[] searchRange(int[] nums, int target) {
-    if (nums == null || nums.length == 0) {
-      return new int[]{-1, -1};
-    }
+    if (nums == null || nums.length == 0) return new int[]{-1, -1};
     return new int[]{first(nums, target), last(nums, target)};
   }
 
@@ -23,25 +21,21 @@ public final class SolutionVI implements Solution {
         left = mid + 1;
       }
     }
-    if (nums[left] == target) {
-      return left;
-    }
+    if (nums[left] == target) return left;
     return -1;
   }
 
   private static int last(final int[] nums, final int target) {
     int left = 0, right = nums.length - 1;
     while (left < right) {
-      final int mid = left + (right - left + 1) / 2;
+      final int mid = left + (1 + right - left) / 2;
       if (nums[mid] <= target) {
         left = mid;
       } else {
         right = mid - 1;
       }
     }
-    if (nums[left] == target) {
-      return left;
-    }
+    if (nums[left] == target) return left;
     return -1;
   }
 }
