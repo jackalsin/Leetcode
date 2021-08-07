9756e8510130a6db3caeb85e9b2608e40c687f7f
jackalsin <jackal.sin@hotmail.com>
2021-08-06 23:37:50 -0700

    Add 021

--- interview/src/main/java/linkedin/_021_Merge_Two_Sorted_Lists/SolutionIII.java
+++ interview/src/main/java/linkedin/_021_Merge_Two_Sorted_Lists/SolutionIII.java
@@ -8,28 +8,27 @@ import definition.ListNode;
  */
 public final class SolutionIII implements Solution {
   @Override
-  public ListNode mergeTwoLists(final ListNode l1, final ListNode l2) {
-    if (l1 == null) {
-      return l2;
-    }
-    if (l2 == null) {
-      return l1;
-    }
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
     final ListNode dummy = new ListNode(1);
-    ListNode left = l1, right = l2, cur = dummy;
-    while (left != null || right != null) {
-      if (left == null) {
-        cur.next = right;
-        right = right.next;
-      } else if (right == null) {
-        cur.next = left;
-        left = left.next;
-      } else if (left.val < right.val) {
-        cur.next = left;
-        left = left.next;
+    ListNode cur = dummy;
+    while (l1 != null || l2 != null) {
+      if (l1 == null) {
+        cur.next = new ListNode(l2.val);
+        l2 = l2.next;
+      } else if (l2 == null) {
+        cur.next = new ListNode(l1.val);
+        l1 = l1.next;
       } else {
-        cur.next = right;
-        right = right.next;
+        final int l1Val = l1.val, l2Val = l2.val;
+        if (l1Val < l2Val) {
+          cur.next = new ListNode(l1Val);
+          l1 = l1.next;
+        } else {
+          cur.next = new ListNode(l2Val);
+          l2 = l2.next;
+        }
       }
       cur = cur.next;
     }
