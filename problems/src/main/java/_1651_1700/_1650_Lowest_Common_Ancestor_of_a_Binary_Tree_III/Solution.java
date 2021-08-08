2a23ff4a202543b96c2ec2cd9f0a1f985fef0e81
jackalsin <jackal.sin@hotmail.com>
2021-08-07 18:55:13 -0700

    Add 1650

--- problems/src/main/java/_1651_1700/_1650_Lowest_Common_Ancestor_of_a_Binary_Tree_III/Solution.java
+++ problems/src/main/java/_1651_1700/_1650_Lowest_Common_Ancestor_of_a_Binary_Tree_III/Solution.java
@@ -3,5 +3,8 @@ package _1651_1700._1650_Lowest_Common_Ancestor_of_a_Binary_Tree_III;
 import definition._1650_Lowest_Common_Ancestor_of_a_Binary_Tree_III.Node;
 
 public interface Solution {
+  /**
+   * TODO:
+   */
   Node lowestCommonAncestor(Node p, Node q);
 }
