5f7037fb169b0a2bf6e72adf4515d325836f3847
jackalsin <jackal.sin@hotmail.com>
2021-08-08 17:46:55 -0700

    Add 072

--- interview/src/main/java/linkedin/_072_Edit_Distance/SolutionIV.java
+++ interview/src/main/java/linkedin/_072_Edit_Distance/SolutionIV.java
@@ -7,24 +7,27 @@ package linkedin._072_Edit_Distance;
 public final class SolutionIV implements Solution {
   @Override
   public int minDistance(String word1, String word2) {
-    if (word1 == null || word2 == null) {
-      return 0;
-    }
+    if (word1 == null || word2 == null) return 0;
     final int m = word1.length(), n = word2.length();
     final int[][] dp = new int[m + 1][n + 1];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       dp[0][i + 1] = i + 1;
     }
     for (int i = 0; i < m; ++i) {
       dp[i + 1][0] = i + 1;
-      final char iChar = word1.charAt(i);
       for (int j = 0; j < n; ++j) {
-        final char jChar = word2.charAt(j);
-        dp[i + 1][j + 1] = dp[i][j] + (iChar == jChar ? 0 : 1);
-        dp[i + 1][j + 1] = Math.min(dp[i + 1][j + 1], dp[i][j + 1] + 1);
-        dp[i + 1][j + 1] = Math.min(dp[i + 1][j + 1], dp[i + 1][j] + 1);
+        dp[i + 1][j + 1] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i][j]);
+        if (word1.charAt(i) == word2.charAt(j)) {
+          dp[i + 1][j + 1] = Math.min(dp[i + 1][j + 1], dp[i][j]);
+        }
       }
     }
     return dp[m][n];
+  }
+
+  private static int min(final int a, final int... b) {
+    int res = a;
+    for (int c : b) res = Math.min(res, c);
+    return res;
   }
 }
