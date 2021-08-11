9c1ce8352e8755972a4267199a2e77df4fd63841
jackalsin <jackal.sin@hotmail.com>
2021-08-11 13:50:50 -0700

    Add 716

--- interview/src/main/java/linkedin/_716_Max_Stack/MaxStackII.java
+++ interview/src/main/java/linkedin/_716_Max_Stack/MaxStackII.java
@@ -6,43 +6,41 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public final class MaxStackII implements MaxStack {
-  private final TreeMap<Integer, List<Node>> treemap = new TreeMap<>((x, y) -> Integer.compare(y, x));
+  private final TreeMap<Integer, List<Node>> valToNodes = new TreeMap<>();
   private final Node head = new Node(-1), tail = new Node(-1);
 
   {
-    tail.prev = head;
     head.next = tail;
+    tail.prev = head;
   }
-
   public void push(int x) {
-    final Node cur = new Node(x);
-    treemap.computeIfAbsent(x, a -> new ArrayList<>()).add(cur);
-    insertAfter(tail.prev, cur);
+    final Node node = new Node(x);
+    append(tail.prev, node);
+    valToNodes.computeIfAbsent(x, k -> new ArrayList<>()).add(node);
   }
 
-  private void insertAfter(final Node prev, final Node cur) {
+  private static void append(final Node prev, final Node cur) {
     final Node next = prev.next;
     prev.next = cur;
     cur.prev = prev;
 
-    next.prev = cur;
     cur.next = next;
+    next.prev = cur;
   }
 
   public int pop() {
-    final Node cur = tail.prev;
-    removeFromDoubleLinkedList(cur);
-    final List<Node> curList = treemap.get(cur.val);
-    if (curList.size() == 1) {
-      treemap.remove(cur.val);
-    } else {
-      curList.remove(curList.size() - 1);
+    final Node toRemove = tail.prev;
+    removeFromDoubleLinkedList(toRemove);
+    final List<Node> curList = valToNodes.get(toRemove.val);
+    curList.remove(curList.size() - 1);
+    if (curList.isEmpty()) {
+      valToNodes.remove(toRemove.val);
     }
-    return cur.val;
+    return toRemove.val;
   }
 
-  private void removeFromDoubleLinkedList(final Node cur) {
-    final Node prev = cur.prev, next = cur.next;
+  private static void removeFromDoubleLinkedList(final Node toRemove) {
+    final Node prev = toRemove.prev, next = toRemove.next;
     prev.next = next;
     next.prev = prev;
   }
@@ -52,21 +50,19 @@ public final class MaxStackII implements MaxStack {
   }
 
   public int peekMax() {
-    return treemap.firstKey();
+    return valToNodes.lastKey();
   }
 
   public int popMax() {
-    final Map.Entry<Integer, List<Node>> entry = treemap.firstEntry();
-    final int maxKey = entry.getKey();
-    final List<Node> curList = entry.getValue();
-    final Node cur = curList.get(curList.size() - 1);
-    if (curList.size() == 1) {
-      treemap.remove(maxKey);
-    } else {
-      curList.remove(curList.size() - 1);
+    final Map.Entry<Integer, List<Node>> toRemoveEntry = valToNodes.lastEntry();
+    final List<Node> curList = toRemoveEntry.getValue();
+    final int key = toRemoveEntry.getKey();
+    final Node toRemove = curList.remove(curList.size() - 1);
+    removeFromDoubleLinkedList(toRemove);
+    if (curList.isEmpty()) {
+      valToNodes.remove(key);
     }
-    removeFromDoubleLinkedList(cur);
-    return maxKey;
+    return key;
   }
 
   private static final class Node {
@@ -77,5 +73,6 @@ public final class MaxStackII implements MaxStack {
       this.val = val;
     }
   }
+
 }
 
