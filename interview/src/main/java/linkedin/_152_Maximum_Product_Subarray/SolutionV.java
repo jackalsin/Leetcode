cd7495caaf17a7a01ba0349c4b102fa314b944b6
Zhiwei Xin <zhixi@microsoft.com>
2021-08-10 20:25:39 -0700

    Add 152

--- interview/src/main/java/linkedin/_152_Maximum_Product_Subarray/SolutionV.java
+++ interview/src/main/java/linkedin/_152_Maximum_Product_Subarray/SolutionV.java
@@ -5,28 +5,28 @@ package linkedin._152_Maximum_Product_Subarray;
  * @version 1.0 on 10/21/2017.
  */
 public final class SolutionV implements Solution {
+  @Override
   public int maxProduct(int[] nums) {
-    if (nums == null || nums.length == 0) {
-      return 0;
-    }
-    int minProdEndHere = nums[0],
-        maxProdEndHere = nums[0],
-        maxSoFar = nums[0];
-    for (int i = 1; i < nums.length; i++) {
-      final int prevMinProdEndHere = minProdEndHere,
-          prevMaxProdEndHere = maxProdEndHere;
-      minProdEndHere = min(prevMinProdEndHere * nums[i], prevMaxProdEndHere * nums[i], nums[i]);
-      maxProdEndHere = max(prevMinProdEndHere * nums[i], prevMaxProdEndHere * nums[i], nums[i]);
-      maxSoFar = Math.max(maxSoFar, maxProdEndHere);
+    if (nums == null) return 0;
+    int maxSoFar = nums[0], minSoFar = nums[0], maxEndHere = nums[0], minEndHere = nums[0];
+    for (int i = 1; i < nums.length; ++i) {
+      final int prevMaxEndHere = maxEndHere, prevMinEndHere = minEndHere;
+      maxEndHere = max(nums[i], prevMaxEndHere * nums[i], prevMinEndHere * nums[i]);
+      minEndHere = min(nums[i], prevMaxEndHere * nums[i], prevMinEndHere * nums[i]);
+      maxSoFar = Math.max(maxSoFar, maxEndHere);
+      minSoFar = Math.min(minSoFar, minEndHere);
     }
     return maxSoFar;
   }
 
-  private static int min(int a, int b, int c) {
-    return Math.min(Math.min(a, b), c);
+  private static int min(int a, final int... b) {
+    for (int c : b) a = Math.min(a, c);
+    return a;
   }
 
-  private static int max(int a, int b, int c) {
-    return Math.max(Math.max(a, b), c);
+  private static int max(int a, final int... b) {
+    for (int c : b) a = Math.max(a, c);
+    return a;
   }
+
 }
