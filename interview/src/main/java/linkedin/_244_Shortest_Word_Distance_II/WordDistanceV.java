a184f7d4d3222a40c3db7698fe46548181a9d439
jackalsin <jackal.sin@hotmail.com>
2021-08-11 13:21:44 -0700

    Add 244

--- interview/src/main/java/linkedin/_244_Shortest_Word_Distance_II/WordDistanceV.java
+++ interview/src/main/java/linkedin/_244_Shortest_Word_Distance_II/WordDistanceV.java
@@ -10,36 +10,27 @@ import java.util.Map;
  * @version 1.0 on 8/31/2019
  */
 public final class WordDistanceV implements WordDistance {
-  private final Map<String, List<Integer>> indexes = new HashMap<>();
-
+  private final Map<String, List<Integer>> valToIndex = new HashMap<>();
   public WordDistanceV(String[] words) {
-    if (words == null) {
-      return;
-    }
+    if (words == null) return;
     for (int i = 0; i < words.length; ++i) {
-      final String word = words[i];
-      if (!indexes.containsKey(word)) {
-        indexes.put(word, new ArrayList<>());
-      }
-      indexes.get(word).add(i);
+      valToIndex.computeIfAbsent(words[i], k -> new ArrayList<>()).add(i);
     }
   }
 
   public int shortest(String word1, String word2) {
-    final List<Integer> list1 = indexes.get(word1),
-        list2 = indexes.get(word2);
+    final List<Integer> index1 = valToIndex.get(word1),
+        index2 = valToIndex.get(word2);
     int min = Integer.MAX_VALUE;
-    for (int i = 0, j = 0; i < list1.size() || j < list2.size(); ) {
-      final int normalizedI = i == list1.size() ? list1.size() - 1 : i,
-          normalizedJ = j == list2.size() ? list2.size() - 1 : j,
-          index1 = list1.get(normalizedI),
-          index2 = list2.get(normalizedJ);
-      min = Math.min(min, Math.abs(index1 - index2));
-      if (j == list2.size() || (i < list1.size() && index1 < index2)) {
-        i++;
-      } else {
-        j++;
-      }
+    System.out.println(index1 + ", " + index2);
+    for (int i = 0, j = 0; i < index1.size() || j < index2.size(); ) {
+      final int left = i == index1.size() ? index1.get(i - 1) : index1.get(i),
+          right = j == index2.size() ? index2.get(j - 1) : index2.get(j);
+      min = Math.min(min, Math.abs(right - left));
+      if (i == index1.size()) ++j;
+      else if (j == index2.size()) ++i;
+      else if (left < right) ++i;
+      else ++j;
     }
     return min;
   }
