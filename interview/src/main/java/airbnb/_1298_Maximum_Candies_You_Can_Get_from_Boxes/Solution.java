66b3b2820b780988d437ad0ba5d3efa17dd50b23
jackalsin <jackal.sin@hotmail.com>
2021-08-15 14:53:35 -0700

    Add 1298

---
+++ interview/src/main/java/airbnb/_1298_Maximum_Candies_You_Can_Get_from_Boxes/Solution.java
@@ -0,0 +1,9 @@
+package airbnb._1298_Maximum_Candies_You_Can_Get_from_Boxes;
+
+/**
+ * @author zhixi
+ * @version 1.0 on 8/14/2021
+ */
+public interface Solution {
+  int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes);
+}
