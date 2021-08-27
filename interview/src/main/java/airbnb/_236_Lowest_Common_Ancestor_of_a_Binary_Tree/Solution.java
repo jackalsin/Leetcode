7a946ee4f3efcb96b49f9b49e538a4ff020e1ee9
jackalsin <jackal.sin@hotmail.com>
2021-08-26 20:11:15 -0700

    Add 236

---
+++ interview/src/main/java/airbnb/_236_Lowest_Common_Ancestor_of_a_Binary_Tree/Solution.java
@@ -0,0 +1,11 @@
+package airbnb._236_Lowest_Common_Ancestor_of_a_Binary_Tree;
+
+import definition.binaryTree.TreeNode;
+
+/**
+ * @author jacka
+ * @version 1.0 on 8/25/2021
+ */
+public interface Solution {
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q);
+}
